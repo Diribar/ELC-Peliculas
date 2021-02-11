384763afe86dbee3bbd2730a90c89d0858f8ad67
Diribar <76135160+Diribar@users.noreply.github.com>
2021-02-11 17:32:35 -0300

    Páginas renombradas

--- app.js
+++ app.js
@@ -2,7 +2,8 @@ const express = require('express')
 const app = express()
 let path = require('path')
 
-app.listen(3000, () => console.log('Servidor funcionando...'))
+app.listen(3001, () => console.log('Servidor funcionando en puerto 3001...'))
 app.use(express.static(path.resolve('public')));
 
-app.get('/', (req, res) => {res.sendFile(path.resolve(__dirname, 'views/home.html'))})
+app.get('/', (req, res) => {res.sendFile(path.resolve(__dirname, 'views/0-0-HOME.html'))})
+app.get('/peliculas', (req, res) => {res.sendFile(path.resolve(__dirname, 'views/0-1-PELICULAS.html'))})
