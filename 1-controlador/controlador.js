3151a01698f89c9a0a6c345fa8192d315358ab52
Diribar <76135160+Diribar@users.noreply.github.com>
2021-02-13 09:02:44 -0300

    Precio a EJS y vistas parciales

--- 1-controlador/controlador.js
+++
@@ -1,9 +0,0 @@
-// var masVendidos = require('../public/javascripts/masVendidos')
-// var novedades = require('../public/javascripts/novedades')
-
-const controlador = {
-	home: (req,res) => {res.render('home')},
-	main: (req,res) => {res.render(req.params.id)},
-};
-
-module.exports = controlador;
--- app.js
+++ app.js
@@ -1,15 +1,9 @@
 const express = require('express')
-let path = require('path')
-const ruta = require('./2-rutas/rutas');
-
 const app = express()
+let path = require('path')
 
-
+app.listen(3001, () => console.log('Servidor funcionando en puerto 3001...'))
 app.use(express.static(path.resolve('public')));
 
-app.set("view engine", "ejs")
-app.listen(3001, () => console.log('Servidor funcionando en puerto 3001...'))
-
-app.get('/', ruta)
-app.get('/:id', ruta)
-app.get('/peliculas/:id', ruta)
+app.get('/', (req, res) => {res.sendFile(path.resolve(__dirname, 'views/0-0-HOME.html'))})
+app.get('/peliculas', (req, res) => {res.sendFile(path.resolve(__dirname, 'views/0-1-PELICULAS.html'))})
