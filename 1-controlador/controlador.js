2bbe690ce1b5eb212b740bf724b5a2fa1a739aed
Diribar <76135160+Diribar@users.noreply.github.com>
2021-02-15 10:34:43 -0300

    Problemas con el centrado vertical del filtro

--- 1-controlador/controlador.js
+++ 1-controlador/controlador.js
@@ -2,7 +2,7 @@
 // var novedades = require('../public/javascripts/novedades')
 
 const controlador = {
-	home: (req,res) => {res.render('home')},
+	home: (req,res) => {res.render('ecc')},
 	main: (req,res) => {res.render(req.params.id)},
 };
 
