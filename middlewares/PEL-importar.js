628280c2b52e9b7f60505e5062d196634335cfce
Diribar <76135160+Diribar@users.noreply.github.com>
2021-04-09 15:35:11 -0300

    Miscelaneas

--- middlewares/PEL-importar.js
+++ middlewares/PEL-importar.js
@@ -1,15 +1,45 @@
+const fs = require('fs');
 const path = require('path');
-const funcionImportarPeliculas = require(path.join(__dirname, '../modelos/PEL-importar.js'));
+const ruta_nombre_importar = path.join(__dirname, '../bases_de_datos/tablas/importarPeliculas.json');
+function leer(n) {return JSON.parse(fs.readFileSync(n, 'utf-8'))};
 
 module.exports =  (req,res,next) => {
     //console.log(req.body)
-    let aux = req.body.comentario;
-    if (aux != "") {
-        aux = funcionImportarPeliculas(aux)
-        //console.log(aux)
+    let alfa = req.body.comentario;
+    if (alfa != "") {
+        //aux = funcionImportarPeliculas(alfa)
+        // Convertir texto en array
+		alfa = alfa.split("\r\n");
+		alfa.unshift("Título en castellano")
+		// console.log(alfa)
+		// Limpiar espacios innecesarios
+		for (let i=0; i<alfa.length; i++) {alfa[i]=alfa[i].trim()}
+		// Convertir "Títulos de vista" en "Títulos de tabla" y eliminar los que no coincidan
+		let BDtitulos = leer(ruta_nombre_importar);
+		for (let i=0; i<alfa.length; i=i+2) {
+			let indice = BDtitulos.indexOf(alfa[i]);
+			if (indice != -1) {
+				alfa[i] = BDtitulos[indice+1]
+			} else {
+				alfa[i]=""
+				alfa[i+1]=""
+			}
+		}
+		// Limpiar campos vacíos
+		alfa = alfa.filter(n => {return n != ""})
+		// Convertir array en JSON
+		let beta="{"
+		for (let i=0; i < alfa.length; i=i+2) {
+			if (i>0) {beta=beta + ', '}
+			beta = beta + '"'+ alfa[i] + '": "' + alfa[i+1] + '"';
+		}
+		beta = beta + "}"
+		// Convertir JSON en objeto
+		beta = JSON.parse(beta)
+		//console.log(beta)
         req.body = {
             ...req.body,
-            ...aux,
+            ...beta,
         };
         //console.log(req.body)
     };
