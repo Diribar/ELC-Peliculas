86fd5ed2c91b5b14d1ab7fc395d0b2da49dae866
Diribar <76135160+Diribar@users.noreply.github.com>
2021-05-04 14:24:01 -0300

    Aggiorne el menu institucional

--- controladores y rutas/controladores/institucional.js
+++ controladores y rutas/controladores/institucional.js
@@ -1,31 +1,8 @@
-// ************ Requires ************
-const fs = require('fs');
-const path = require('path');
-
-// ************ Variables ************
-const ruta_nombre = path.join(__dirname, '../../bases_de_datos/tablas/menuInstitucional.json');
-
-// ************ Funciones ************
-function leer(n) {return JSON.parse(fs.readFileSync(n, 'utf-8'))};
-
-// *********** Controlador ***********
 module.exports = {
-
-	main: (req,res) => { // HOME - QUIÉNES SOMOS - CONTÁCTANOS
-		//url = req.params.id;
-		//let BD = leer(ruta_nombre);
-		//let vista = BD.find(n => n.url == url)
-		//let titulo = vista.titulo
-		//let nombre = vista.nombre
-		return
-		return res.render("0-Institucional", {
-			nombre,
-			titulo,
-		})
-	},
-
 	home: (req,res) => {
 		return res.redirect("/peliculas")
 	},
-
+	nosotros: (req,res) => {
+		return res.render("1-Nosotros")
+	},
 };
--- controladores y rutas/rutas/institucional.js
+++ controladores y rutas/rutas/institucional.js
@@ -4,8 +4,8 @@ const router = express.Router();
 const controlador = require('../controladores/institucional');
 
 // Controladores *******************************************
-router.get('/', controlador.home) // Redirecciona a Películas
-router.get('/:id', controlador.main) // Acerca de nosotros, Contáctanos
+router.get('/', controlador.home) 				// Redirecciona a Películas
+router.get('/nosotros', controlador.nosotros) 	// Acerca de nosotros
 
 // Exportarlo **********************************************
 module.exports = router;
