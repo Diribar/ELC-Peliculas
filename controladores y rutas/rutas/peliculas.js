a47d6eeb196c26c9785ab77ad29a371c094957dd
Diribar <76135160+Diribar@users.noreply.github.com>
2021-05-06 07:01:24 -0300

    Reordenar carpetas

--- controladores y rutas/rutas/peliculas.js
+++ controladores y rutas/rutas/peliculas.js
@@ -7,13 +7,13 @@ const opciones = require('../controladores/peliculas_opciones')
 //************************ Middlewares ******************************
 const soloUsuarios = require('../../middlewares/usuarios/soloUsuarios');
 const soloAdmin = require('../../middlewares/usuarios/soloAdmin');
-const importarA = require('../../middlewares/1A_palabras_clave/1importarA');
-const importarB = require('../../middlewares/1B_importar/importarB');
-const validar1A = require('../../middlewares/0_validarForms/validar1A');
-const validar1B = require('../../middlewares/0_validarForms/validar1B');
-const validar1C = require('../../middlewares/0_validarForms/validar1C');
-const validar2 = require('../../middlewares/0_validarForms/validar2');
-const validar3 = require('../../middlewares/0_validarForms/validar3');
+const validar1A = require('../../middlewares/PEL-0-validarForms/validar1A');
+const validar1B = require('../../middlewares/PEL-0-validarForms/validar1B');
+const validar1C = require('../../middlewares/PEL-0-validarForms/validar1C');
+const validar2 = require('../../middlewares/PEL-0-validarForms/validar2');
+const validar3 = require('../../middlewares/PEL-0-validarForms/validar3');
+const importarA = require('../../middlewares/PEL-1A-importar_x_API/1importarA');
+const importarB = require('../../middlewares/PEL-1B-importar_x_copy/importarB');
 const uploadFile = require('../../middlewares/varios/multer');
 
 //******************* Controladores de CRUD *************************
