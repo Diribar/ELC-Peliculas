6c41d5ca5a61d736314b15d12cfd579b432913ba
Diribar <76135160+Diribar@users.noreply.github.com>
2021-05-07 15:46:44 -0300

    Miscelaneas

---
+++ bases_de_datos/modelos/PEL_categorias.js
@@ -0,0 +1,22 @@
+module.exports = (sequelize, dt) => {
+	const alias = "categoria";
+	const columns = {
+		id: {type: dt.INTEGER, primaryKey: true},
+		nombre: {type: dt.STRING(20)}
+	};
+	const config = {
+		tableName: "categorias",
+		timestamps: false
+	};
+
+	const entidad = sequelize.define(alias,columns,config);
+
+	entidad.associate = n => {
+		entidad.hasMany(n.usuario, {
+			as: "peliculas",
+			foreignKey: "categoria_id"
+		});
+	};
+
+	return entidad;
+};
