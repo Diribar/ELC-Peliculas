d34fccb58116e56656f92026e47d02a2f0afb271
Diribar <76135160+Diribar@users.noreply.github.com>
2021-06-17 11:09:06 -0300

    Solucionado el error de Login cuando nos logueábamos por primera vez

--- middlewares/varios/userLogs.js
+++ middlewares/varios/userLogs.js
@@ -1,11 +1,6 @@
-module.exports = (req,res,next) => {
-	let URL = req.originalUrl
-	let hasta = URL.slice(1).indexOf("/") > 0 ? URL.slice(1).indexOf("/")+1 : URL.length
-	let tema = URL.slice(1, hasta)
-	if (tema != "login" && tema != "usuarios") {
-		req.session.urlReferencia = URL
-	}
-	res.locals.urlReferencia = req.session.urlReferencia
-	//console.log(req.session.urlReferencia);
-	next()
-}
+const funciones = require("../../modelos/funciones");
+
+module.exports = (req, res, next) => {
+	funciones.userLogs(req, res)
+	next();
+};
