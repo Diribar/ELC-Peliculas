2b20d45a2ffd4183f66ee465d57170da3e2cd7f2
Diribar <76135160+Diribar@users.noreply.github.com>
2021-06-16 21:43:51 -0300

    Miscelaneas

--- controladores y rutas/3-Peliculas/PEL-Opc-Contr.js
+++ controladores y rutas/3-Peliculas/PEL-Opc-Contr.js
@@ -1,10 +1,6 @@
 // ************ Requires ************
 const tablasVarias = require("../../modelos/BD_varios");
 
-// ************ Variables ************
-//const ruta_nombre_opciones = path.join(__dirname, "../../bases_de_datos/tablas/menuOpciones.json");
-//const ruta_nombre_tipos = path.join(__dirname, '../../bases_de_datos/tablas/menuTipos.json');
-
 // *********** Controlador ***********
 module.exports = {
 	rubro: async (req, res) => {
@@ -36,13 +32,11 @@ module.exports = {
 	tipo: async (req, res) => {
 		// Obtener el código de Opción y Tipo
 		let url = req.url.slice(1);
-		// Averiguar la opción elegida
-		let opcion = url.slice(0, url.indexOf("/"));
 		// // Obtener las Opciones, la Opción elegida, los Tipos para la opción elegida y el título
+		let opcion = url.slice(0, url.indexOf("/"));
 		let [opciones_BD, opcionElegida, tipos_BD, titulo] = await datos(opcion);
 		// Obtener el Tipo elegido
 		let tipoElegido = tipos_BD.filter((n) => n.url == url)[0];
-		//return res.send(tipoElegido);
 		// Ir a la vista
 		res.render("0-Opciones", {
 			titulo,
