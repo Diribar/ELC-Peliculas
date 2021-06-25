787ea2716b94c60e05f704e769ad9426c0c391b1
Diribar <76135160+Diribar@users.noreply.github.com>
2021-06-25 17:36:26 -0300

    Miscelaneas

--- Backup/SQL-Create.sql
+++ Backup/SQL-Create.sql
@@ -105,6 +105,7 @@ CREATE TABLE colecciones_cabecera (
 	nombre_castellano VARCHAR(100) NOT NULL,
 	ano_estreno INT UNSIGNED NULL,
 	ano_fin INT UNSIGNED NULL,
+	pais_id VARCHAR(2) NULL,
 	productor VARCHAR(50) NULL,
 	sinopsis VARCHAR(800) NOT NULL,
 	avatar VARCHAR(100) NULL,
--- bases_de_datos/modelos/PEL_colecciones_1cabecera.js
+++ bases_de_datos/modelos/PEL_colecciones_1cabecera.js
@@ -8,6 +8,7 @@ module.exports = (sequelize, dt) => {
 		nombre_castellano: { type: dt.STRING(100) },
 		ano_estreno: { type: dt.INTEGER },
 		ano_fin: { type: dt.INTEGER },
+		pais_id: { type: dt.STRING(2) },
 		productor: { type: dt.STRING(50) },
 		en_produccion: { type: dt.BOOLEAN },
 		sinopsis: { type: dt.STRING(800) },
