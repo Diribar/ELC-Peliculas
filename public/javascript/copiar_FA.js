1b7ae3ce3c9244348ceeaaf8c1a1874cbb9bf874
Diribar <76135160+Diribar@users.noreply.github.com>
2021-07-26 19:20:18 -0300

    Dinámica de ayuda actualizada

--- public/javascript/copiar_FA.js
+++ public/javascript/copiar_FA.js
@@ -1,78 +1,27 @@
 window.addEventListener("load", () => {
-	// Declarar las variables
-	let contenido = document.querySelector("textarea");
-	let despues = contenido.value;
-	let form = document.querySelector("form");
-	let button = document.querySelector("button");
-	let iconoAyuda = document.querySelectorAll(".fa-question-circle");
-	let mensajeAyuda = document.querySelectorAll(".mensajeAyuda");
+	// Declarar las variables de Input
+	let rubroAPI = document.querySelector('select[name="rubroAPI"]');
+	let direccion = document.querySelector('input[name="direccion"]');
+	let contenido = document.querySelector('textarea[name="contenido"]');
 
-	// Verificar o Avanzar
-	form.addEventListener("submit", async (e) => {
-		e.preventDefault();
-		let valor = contenido.value;
-		if (valor) {
-			if (despues != contenido.value) {
-				despues = contenido.value;
-				lectura = await procesarContenido(contenido.value);
-				console.log(lectura)
-			}
-		} else {
-			if (valor) {
-				e.currentTarget.submit();
-			}
-		}
-	});
+	// Declarar las variables de Error
+	let iconoError = document.querySelectorAll(".fa-times-circle");
+	let mensajeError = document.querySelectorAll(".mensajeError");
 
-	// Validar ante cambios en los inputs
-	contenido.addEventListener("keyup", () => {
-		if (
-			contenido.value.length == 0 ||
-			contenido.value != despues
-		) {
-			borrarComentario();
-		}
-	});
-
-	// Detectar si se hace "click" en Ayuda
-	for (let i=0; i<iconoAyuda.length; i++) {
-		iconoAyuda[i].addEventListener("click", () => {
-			mensajeAyuda[i].classList.toggle("ocultar");
-		});
-	}
-
-	// Cerrar los dropdowns en desuso
+	// Comportamientos cuando se hace click
 	window.onclick = (e) => {
-		!e.target.matches("#direccion")
-			? mensajeAyuda[0].classList.add("ocultar")
-			: "";
-		!e.target.matches("#contenido")
-			? mensajeAyuda[1].classList.add("ocultar")
-			: "";
+		ayuda(e);
+		//errorRubroApi(e)
 	};
+
 });
 
-const procesarContenido = async (contenido) => {
-	// Procesando la información
-	let resultadoDeBusqueda = document.querySelector("#resultadoDeBusqueda");
-	resultadoDeBusqueda.innerHTML = "Procesando la información...";
-	// Procesar los datos de la película
-	let encodedValue = encodeURIComponent(contenido);
-	let url =
-		"/peliculas/agregar/api/procesarcopiado/?contenido=" + encodedValue;
-	let lectura = await fetch(url).then((n) => n.json());
-	// Información procesada
-	resultadoDeBusqueda.innerHTML = "";
-	resultadoDeBusqueda.classList.replace("mostrar", "ocultar");
-	return lectura;
+let ayuda = (e) => {
+	const mensajeAyuda = document.querySelectorAll(".mensajeAyuda");
+	e.target.matches("#ayuda_direccion")
+	 	? mensajeAyuda[0].classList.toggle("ocultar")
+	 	: mensajeAyuda[0].classList.add("ocultar");
+	e.target.matches("#ayuda_contenido")
+	 	? mensajeAyuda[1].classList.toggle("ocultar")
+	 	: mensajeAyuda[1].classList.add("ocultar");
 }
-
-const borrarComentario = () => {
-	document.querySelector("#resultadoDeBusqueda").innerHTML = "";
-	document
-		.querySelector("#resultadoDeBusqueda")
-		.classList.remove("resultadoInvalido");
-	document
-		.querySelector("#resultadoDeBusqueda")
-		.classList.remove("resultadoExitoso");
-};
