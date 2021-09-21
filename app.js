0e60821cbbef56f4eaddf943fab90470cee81ca8
Diribar <76135160+Diribar@users.noreply.github.com>
2021-09-21 13:58:13 -0300

    Miscelaneas

--- app.js
+++ app.js
@@ -33,8 +33,8 @@ app.set("views", [
 	path.resolve(__dirname, "./views/1-Institucional"),
 	path.resolve(__dirname, "./views/2-Usuarios"),
 	path.resolve(__dirname, "./views/3-AgregarProductos"),
-	path.resolve(__dirname, "./views/3-Productos-RUD"),
-	path.resolve(__dirname, "./views/4-ElegirOpciones"),
+	path.resolve(__dirname, "./views/4-Productos-RUD"),
+	path.resolve(__dirname, "./views/5-ElegirOpciones"),
 ]);
 
 // ************************* Rutas ********************************
