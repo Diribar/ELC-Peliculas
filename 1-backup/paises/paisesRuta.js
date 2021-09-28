1e179a078d51fa07aff1fd06a4e75ee5189779fb
Diribar <76135160+Diribar@users.noreply.github.com>
2021-09-28 00:55:02 -0300

    Borrado de archivos viejos y sin uso

--- 1-backup/paises/paisesRuta.js
+++
@@ -1,8 +0,0 @@
-//************************* Requires *******************************
-const express = require('express');
-const router = express.Router();
-const paises = require('../controladores/paisesController')
-
-router.get('/', paises.listado)
-
-module.exports = router;
