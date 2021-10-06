a45b1a4f923345bc78004a820b9fae4867b16bbc
Diribar <76135160+Diribar@users.noreply.github.com>
2021-10-06 12:35:41 -0300

    Miscelaneas

--- middlewares/varios/multer.js
+++ middlewares/varios/multer.js
@@ -3,11 +3,13 @@ const multer = require("multer");
 
 const storage = multer.diskStorage({
 	destination: (req, file, cb) => {
+		console.log(req.body.ruta);
 		cb(null, req.body.ruta);
 	},
 	filename: (req, file, cb) => {
 		ext = path.extname(file.originalname);
 		//nombre = path.basename(file.originalname, ext);
+		console.log(file.originalname, ext);
 		cb(null, Date.now() + ext);
 	},
 });
