d9aa35d137de18b29ae68397bf35b3d287844dba
Diribar <76135160+Diribar@users.noreply.github.com>
2021-10-12 14:07:50 -0300

    Miscelaneas

--- rutas_y_controladores/3-Productos/RutasAgregar1Producto.js
+++ rutas_y_controladores/3-Productos/RutasAgregar1Producto.js
@@ -2,7 +2,7 @@
 let express = require("express");
 let router = express.Router();
 let API = require("./1-Agregar-API");
-let vistas = require("./1-Agregar1Productos");
+let agregar = require("./1-Agregar1Productos");
 
 //************************ Middlewares ******************************
 let usuarios = require("../../middlewares/usuarios/soloUsuarios");
@@ -18,17 +18,17 @@ router.get("/api/averiguar-cant-prod/", API.cantProductos);
 //router.get("/api/averiguar-fa-ya-en-bd/", API.averiguarYaEnBD_FA);
 
 // Controladores de vistas de "Agregar Productos"
-router.get("/palabras-clave", usuarios, vistas.palabrasClaveForm);
-router.post("/palabras-clave", usuarios, vistas.palabrasClaveGuardar);
-router.get("/desambiguar", usuarios, vistas.desambiguarForm);
-router.post("/desambiguar", usuarios, vistas.desambiguarGuardar);
-router.get("/copiar-fa", usuarios, autorizadoFA, vistas.copiarFA_Form);
-router.post("/copiar-fa", usuarios, vistas.copiarFA_Guardar);
-router.post("/datosDuros", usuarios, vistas.datosDuros);
+router.get("/palabras-clave", usuarios, agregar.palabrasClaveForm);
+router.post("/palabras-clave", usuarios, agregar.palabrasClaveGuardar);
+router.get("/desambiguar", usuarios, agregar.desambiguarForm);
+router.post("/desambiguar", usuarios, agregar.desambiguarGuardar);
+router.get("/copiar-fa", usuarios, autorizadoFA, agregar.copiarFA_Form);
+router.post("/copiar-fa", usuarios, agregar.copiarFA_Guardar);
+router.post("/datosDuros", usuarios, agregar.datosDuros);
 
 // Controladores de vistas auxiliares
-router.get("/responsabilidad", usuarios, vistas.responsabilidad);
-router.get("/ya-en-bd", usuarios, vistas.yaEnBD_Form);//router.post("/ya-en-bd", usuarios, vistas.yaEnBD_Form);
+router.get("/responsabilidad", usuarios, agregar.responsabilidad);
+router.get("/ya-en-bd", usuarios, agregar.yaEnBD_Form);//router.post("/ya-en-bd", usuarios, agregar.yaEnBD_Form);
 
 // Fin
 module.exports = router;
