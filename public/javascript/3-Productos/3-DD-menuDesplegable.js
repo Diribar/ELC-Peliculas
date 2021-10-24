d2bb12586b37dcb05528cb6869153053f2cd2d37
Diribar <76135160+Diribar@users.noreply.github.com>
2021-10-24 02:33:43 -0300

    Miscelaneas

--- public/javascript/3-Productos/3-DD-menuDesplegable.js
+++ public/javascript/3-Productos/3-DD-menuDesplegable.js
@@ -1,13 +1,6 @@
 window.addEventListener("load", () => {
 	let inputs = document.querySelectorAll(".input[disabled]");
 	let avisos = document.querySelectorAll(".aviso");
-	mensaje1 = "Podrás editar este campo más adelante.";
-	mensaje2 = "Al terminar, se te explicará cómo.";
-
-	// Agregarle el mensaje a los avisos
-	for (let i = 0; i < inputs.length; i++) {
-		avisos[i].innerHTML="<p>"+mensaje1+"</p><p>"+mensaje2+"</p>"
-	}
 
 	// Acciones si se detecta un click en un input
 	window.addEventListener("click", (e) => {
