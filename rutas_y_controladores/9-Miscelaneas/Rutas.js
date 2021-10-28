6ff8204637d2c53b51b21c8a4794f3293de5992d
Diribar <76135160+Diribar@users.noreply.github.com>
2021-10-27 22:55:33 -0300

    Cambios de numeración en carpetas

--- rutas_y_controladores/9-Miscelaneas/Rutas.js
+++ rutas_y_controladores/9-Miscelaneas/Rutas.js
@@ -4,6 +4,8 @@ const router = express.Router();
 const controlador = require("./Controlador");
 
 // Controladores *******************************************
+router.get('/', controlador.home) 				// Redirecciona a Películas
+router.get('/nosotros', controlador.nosotros) 	// Acerca de nosotros
 router.get("/agregar/personaje-historico", controlador.personajeHistorico);
 router.get("/agregar/hecho-historico", controlador.hechoHistorico);
 
