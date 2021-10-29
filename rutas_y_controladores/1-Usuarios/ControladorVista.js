ea55432c0bf6fc9415f20543d2b66b14f4624849
Diribar <76135160+Diribar@users.noreply.github.com>
2021-10-28 22:46:24 -0300

    Mejoras en el circuito de Login

--- rutas_y_controladores/1-Usuarios/ControladorVista.js
+++ rutas_y_controladores/1-Usuarios/ControladorVista.js
@@ -10,6 +10,64 @@ let bcryptjs = require("bcryptjs");
 
 // *********** Controlador ***********
 module.exports = {
+	altaMailForm: (req, res) => {
+		tema = "usuario";
+		codigo = "mail";
+		let data_entry = req.session.data_entry
+			? req.session.data_entry
+			: false;
+		let errores = req.session.errores ? req.session.errores : false;
+		return res.render("Home", {
+			tema,
+			codigo,
+			link: req.originalUrl,
+			data_entry,
+			errores,
+		});
+	},
+
+	altaMailGuardar: async (req, res) => {
+		// Averiguar si hay errores de validación
+		let datos = req.body;
+		let errores = await validarUsuarios.registroMail(datos.email);
+		// Redireccionar si hubo algún error de validación
+		if (errores.hay) {
+			req.session.data_entry = req.body;
+			req.session.errores = errores;
+			return res.redirect("/usuarios/altaredireccionar");
+		}
+		// Si no hubieron errores de validación...
+		// Enviar la contraseña por mail
+		asunto = "Contraseña para ELC";
+		email = req.body.email;
+		codigo = Math.round(Math.random() * Math.pow(10, 10)) + "";
+		console.log(codigo);
+		comentario = "La contraseña del mail " + email + " es: " + codigo;
+		funciones.enviarMail(asunto, email, comentario).catch(console.error);
+		// Guardar el registro
+		await BD_usuarios.altaMailContrasena(email, codigo);
+		// Obtener los datos del usuario
+		req.session.email = email;
+		// Redireccionar
+		return res.redirect("/usuarios/altaredireccionar");
+	},
+
+	altaRedireccionar: async (req, res) => {
+		!req.session.usuario ? res.redirect("/usuarios/login") : "";
+		let status_registro = req.session.usuario.status_registro_id + "";
+		// Redireccionar
+		//console.log("status_registro: " + status_registro);
+		status_registro == 1
+			? res.redirect("/usuarios/login")
+			: status_registro == 2
+			? res.redirect("/usuarios/datos-perennes")
+			: status_registro == 3
+			? res.redirect("/usuarios/datos-editables")
+			: req.session.urlReferencia
+			? res.redirect(req.session.urlReferencia)
+			: res.redirect("/");
+	},
+
 	loginForm: (req, res) => {
 		// 1. Tema y Código
 		tema = "usuario";
@@ -32,7 +90,6 @@ module.exports = {
 	loginGuardar: async (req, res) => {
 		// 1. Obtener los datos del usuario
 		let usuario = await BD_usuarios.obtenerPorMail(req.body.email);
-		//return res.send(usuario)
 		// 2. Averiguar si hay errores de validación
 		let errores = await validarUsuarios.login(req.body);
 		contrasena = usuario ? usuario.contrasena : "";
@@ -51,11 +108,12 @@ module.exports = {
 		}
 		// 4. Si corresponde, actualizar el Status del Usuario
 		if (usuario.status_registro_id == 1) {
-			usuario = await BD_usuarios.actualizarStatusUsuario(usuario.id, 2);
+			await BD_usuarios.actualizarStatusUsuario(usuario.id, 2);
 		}
 		// 5. Iniciar la sesión
-		req.session.usuario = usuario;
+		req.session.usuario = await BD_usuarios.obtenerPorId(usuario.id);
 		res.cookie("email", req.body.email, { maxAge: 1000 * 60 * 60 * 24 });
+		delete req.session["email"];
 		// Redireccionar
 		return res.redirect("/usuarios/altaredireccionar");
 	},
@@ -67,78 +125,8 @@ module.exports = {
 		return res.redirect(url);
 	},
 
-	altaRedireccionar: async (req, res) => {
-		let usuario = req.session.usuario
-			? req.session.usuario
-			: req.session.email
-			? await BD_usuarios.obtenerPorMail(req.session.email)
-			: req.cookies.email
-			? await BD_usuarios.obtenerPorMail(req.cookies.email)
-			: null;
-		!usuario ? res.redirect("/") : "";
-		//return res.send(usuario)
-		let status_registro = usuario.status_registro_id + "";
-		// Redireccionar
-		//console.log("status_registro: " + status_registro);
-		status_registro == 1
-			? res.redirect("/usuarios/login")
-			: status_registro == 2
-			? res.redirect("/usuarios/datos-perennes")
-			: status_registro == 3
-			? res.redirect("/usuarios/datos-editables")
-			: req.session.urlReferencia
-			? res.redirect(req.session.urlReferencia)
-			: res.redirect("/");
-	},
-
-	altaMailForm: (req, res) => {
-		tema = "usuario";
-		codigo = "mail";
-		let data_entry = req.session.data_entry
-			? req.session.data_entry
-			: false;
-		let errores = req.session.errores ? req.session.errores : false;
-		return res.render("Home", {
-			tema,
-			codigo,
-			link: req.originalUrl,
-			data_entry,
-			errores,
-		});
-	},
-
-	altaMailGuardar: async (req, res) => {
-		// Averiguar si hay errores de validación
-		let datos = req.body;
-		let errores = await validarUsuarios.mail(datos.email);
-		// Redireccionar si hubo algún error de validación
-		if (errores.hay) {
-			req.session.data_entry = req.body;
-			req.session.errores = errores;
-			return res.redirect("/usuarios/altaredireccionar");
-		}
-		// Si no hubieron errores de validación...
-		// Enviar la contraseña por mail
-		asunto = "Contraseña para ELC";
-		email = req.body.email;
-		codigo = Math.round(Math.random() * Math.pow(10, 10)) + "";
-		comentario = "La contraseña del mail " + email + " es: " + codigo;
-		funciones.enviarMail(asunto, email, comentario).catch(console.error);
-		// Guardar el registro
-		await BD_usuarios.altaMailContrasena(email, codigo);
-		// Obtener los datos del usuario
-		req.session.email = email;
-		// Redireccionar
-		return res.redirect("/usuarios/altaredireccionar");
-	},
-
 	altaPerennesForm: async (req, res) => {
-		!req.session.usuario
-			? (req.session.usuario = await BD_usuarios.obtenerPorMail(
-					req.cookies.email
-			  ))
-			: "";
-		delete req.session["email"];
+		!req.session.usuario ? res.redirect("/usuarios/login") : "";
 		tema = "usuario";
 		codigo = "perennes";
 		let data_entry = req.session.data_entry
@@ -155,10 +143,11 @@ module.exports = {
 	},
 
 	altaPerennesGuardar: async (req, res) => {
+		!req.session.usuario ? res.redirect("/usuarios/login") : "";
+		let usuario = req.session.usuario;
 		// Averiguar si hay errores de validación
 		let datos = req.body;
 		let errores = await validarUsuarios.perennes(datos);
-		//return res.send(errores)
 		// Redireccionar si hubo algún error de validación
 		if (errores.hay) {
 			req.session.data_entry = req.body;
@@ -168,23 +157,16 @@ module.exports = {
 		// Si no hubieron errores de validación...
 		// Actualizar el registro
 		req.body.status_registro_id = 3;
-		req.session.usuario = await BD_usuarios.agregarDatosPerennes(
-			req.session.usuario.id,
-			req.body
-		);
-		return res.send(req.session.usuario);
+		await BD_usuarios.agregarDatosPerennes(usuario.id, req.body);
+		req.session.usuario = await BD_usuarios.obtenerPorId(usuario.id);
 		// Redireccionar
 		return res.redirect("/usuarios/altaredireccionar");
 	},
 
 	altaEditablesForm: async (req, res) => {
+		!req.session.usuario ? res.redirect("/usuarios/login") : "";
 		tema = "usuario";
 		codigo = "editables";
-		!req.session.usuario
-			? (req.session.usuario = await BD_usuarios.obtenerPorMail(
-					req.cookies.email
-			  ))
-			: "";
 		let paises = await BD_varios.ObtenerTodos("paises", "nombre");
 		let hablaHispana = paises.filter((n) => n.idioma == "Spanish");
 		let hablaNoHispana = paises.filter((n) => n.idioma != "Spanish");
@@ -209,6 +191,7 @@ module.exports = {
 	},
 
 	altaEditablesGuardar: async (req, res) => {
+		!req.session.usuario ? res.redirect("/usuarios/login") : "";
 		let usuario = req.session.usuario;
 		// Averiguar si hay errores de validación
 		let datos = req.body;
@@ -226,10 +209,8 @@ module.exports = {
 		// Grabar novedades en el usuario
 		req.body.status_registro_id = 4;
 		req.body.avatar = req.file ? req.file.filename : "-";
-		req.session.usuario = await BD_usuarios.agregarDatosEditables(
-			usuario.id,
-			req.body
-		);
+		await BD_usuarios.agregarDatosEditables(usuario.id, req.body);
+		req.session.usuario = await BD_usuarios.obtenerPorId(usuario.id);
 		// Pendiente mover el archivo a la carpeta definitiva
 		// Redireccionar
 		return res.redirect("/usuarios/altaredireccionar");
