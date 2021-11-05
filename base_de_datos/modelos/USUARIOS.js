cc02c95e615d7c98438609bcb765ad70a8e87855
Diribar <76135160+Diribar@users.noreply.github.com>
2021-11-04 22:36:23 -0300

    Miscelaneas

--- base_de_datos/modelos/USUARIOS.js
+++ base_de_datos/modelos/USUARIOS.js
@@ -1,6 +1,6 @@
 module.exports = (sequelize, dt) => {
-    const alias = "usuarios";
-    const columns = {
+	const alias = "usuarios";
+	const columns = {
 		email: { type: dt.STRING(100) },
 		contrasena: { type: dt.STRING(50) },
 		status_registro_id: { type: dt.INTEGER },
@@ -24,20 +24,20 @@ module.exports = (sequelize, dt) => {
 		borrado_por_id: { type: dt.INTEGER },
 	};
 	const config = {
-        tableName: "usuarios",
-        createdAt: 'creado_en',
-        updatedAt: 'editado_en'
-    };
-    const entidad = sequelize.define(alias,columns,config);
-    entidad.associate = n => {
-        entidad.belongsTo(n.sexos, {as: "sexo", foreignKey: "sexo_id"});
-        entidad.belongsTo(n.paises, {as: "pais", foreignKey: "pais_id"});
-        entidad.belongsTo(n.roles_usuarios, {as: "rol_usuario", foreignKey: "rol_usuario_id"});
-        entidad.belongsTo(n.status_registro, {as: "status_registro", foreignKey: "status_registro_id"});
-        entidad.belongsTo(n.estados_eclesiales, {as: "estado_eclesial", foreignKey: "estado_eclesial_id"});
-        entidad.belongsTo(n.usuarios, {as: "borrado_por", foreignKey: "borrado_por_id"});
+		tableName: "usuarios",
+		createdAt: 'creado_en',
+		updatedAt: 'editado_en'
+	};
+	const entidad = sequelize.define(alias,columns,config);
+	entidad.associate = n => {
+		entidad.belongsTo(n.sexos, {as: "sexo", foreignKey: "sexo_id"});
+		entidad.belongsTo(n.paises, {as: "pais", foreignKey: "pais_id"});
+		entidad.belongsTo(n.roles_usuarios, {as: "rol_usuario", foreignKey: "rol_usuario_id"});
+		entidad.belongsTo(n.status_registro, {as: "status_registro", foreignKey: "status_registro_id"});
+		entidad.belongsTo(n.estados_eclesiales, {as: "estado_eclesial", foreignKey: "estado_eclesial_id"});
+		entidad.belongsTo(n.usuarios, {as: "borrado_por", foreignKey: "borrado_por_id"});
 		entidad.hasMany(n.calificaciones_us, {as: "calificaciones",foreignKey: "usuario_id"});
 		entidad.hasMany(n.interes_en_prod_us, {as: "interes_en_prod",foreignKey: "usuario_id"});
-    };
-    return entidad;
+	};
+	return entidad;
 }; 
