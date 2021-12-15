0b7575aba06067adc86e661b92b5c76cb395e646
unknown <diegoiribarren2015@gmail.com>
2021-12-15 16:32:57 -0300

    Cambios en Desambiguar

--- public/progrFrontEnd/2-Prod-Agregar/2A-DESAMB-Desplazam.js
+++ public/progrFrontEnd/2-Prod-Agregar/2A-DESAMB-Desplazam.js
@@ -1,37 +1,42 @@
 window.addEventListener("load", async () => {
 	// Definir variables
-	let resultado = document.querySelector("#resultadoDesamb");
-	let form = document.querySelectorAll("#resultadoDesamb form");
-	let anchoTotal = resultado.scrollWidth;
-	let anchoVisible = resultado.offsetWidth;
-	let anchoForm = form[0].clientWidth;
-	let desplazamiento = parseInt(anchoVisible / anchoForm) * anchoForm;
+	let resultado = document.querySelector("#resultadoDesamb"); // Todo el 'ul'
+	let ventana = resultado.offsetWidth; // El ancho de la ventana de lo que se ve (700)
+	let anchoForm = document.querySelector("#resultadoDesamb form").clientWidth; // El ancho del formulario (330)
+	let cantFormsVisibles = parseInt(ventana / anchoForm); // Cant. de forms visibles en simultáneo
+	let boton = document.querySelectorAll("#resultadoDesamb button");
+	let indiceFocus = 0;
 
 	// Desplazamiento por teclado
 	window.addEventListener("keydown", (e) => {
-		//console.log(e.key);
-		if (e.key == "Home") resultado.scrollTo(0, 0);
-		if (e.key == "End") resultado.scrollTo(anchoTotal, 0);
-		if (e.key == "PageDown")
-			resultado.scrollTo(
-				parseInt(resultado.scrollLeft / desplazamiento) * desplazamiento + desplazamiento,
-				0
-			);
-		if (e.key == "PageUp")
-			resultado.scrollTo(
-				parseInt(resultado.scrollLeft / desplazamiento + 0.99) * desplazamiento -
-					desplazamiento,
-				0
-			);
-		if (e.key == "ArrowDown" || e.key == "ArrowRight")
-			resultado.scrollTo(
-				parseInt(resultado.scrollLeft / anchoForm) * anchoForm + anchoForm,
-				0
-			);
-		if (e.key == "ArrowUp" || e.key == "ArrowLeft")
-			resultado.scrollTo(
-				parseInt(resultado.scrollLeft / anchoForm + 0.99) * anchoForm - anchoForm,
-				0
-			);
+		// Comandos iniciales
+		posicion = resultado.scrollLeft;
+		if (e.key == "ArrowLeft" || e.key == "ArrowRight") e.preventDefault();
+
+		// Home y End
+		if (e.key == "Home") indiceFocus = 0;
+		if (e.key == "End") indiceFocus = boton.length - 1;
+
+		// Page Up / Down
+		if (e.key == "PageUp") indiceFocus = indiceFocus - cantFormsVisibles;
+		if (e.key == "PageDown") indiceFocus = indiceFocus + cantFormsVisibles;
+
+		// Arrows
+		if (e.key == "ArrowUp" || e.key == "ArrowLeft") indiceFocus = indiceFocus - 1;
+		if (e.key == "ArrowDown" || e.key == "ArrowRight") indiceFocus = indiceFocus + 1;
+
+		// Asignar el nuevo 'focus'
+		indiceFocus =
+			indiceFocus < 0 ? 0 : indiceFocus > boton.length - 1 ? boton.length - 1 : indiceFocus;
+
+		// si el indice x anchoForm < posición => posición=indice x anchoForm
+		if (indiceFocus * anchoForm < posicion) posicion = indiceFocus * anchoForm;
+
+		// si el (indice+1) x anchoForm > (posición+ventana) => posición=posicion - anchoForm
+		if ((indiceFocus + 1) * anchoForm > posicion + ventana) posicion = indiceFocus * anchoForm - anchoForm;
+
+		boton[indiceFocus].focus();
+		resultado.scrollTo(posicion, 0);
+		// console.log(indiceFocus * anchoForm);
 	});
 });
