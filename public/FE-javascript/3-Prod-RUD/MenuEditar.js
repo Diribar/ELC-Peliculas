aebd2bf3aaad0f4c01676c747bf9d2f92c3aff00
unknown <diegoiribarren2015@gmail.com>
2022-01-05 00:01:32 -0300

    Protocolo de captura terminado

--- public/FE-javascript/3-Prod-RUD/MenuEditar.js
+++ public/FE-javascript/3-Prod-RUD/MenuEditar.js
@@ -1,24 +1,24 @@
 window.addEventListener("load", () => {
 	// Definir variables
 	let linkEditarProd = document.querySelector("#editarProducto");
-	let API_EditarProd = "/producto/api/averiguar-si-esta-capturado/?entidad=";
+	let API_EditarProd = "/producto/api/averiguar-si-esta-disponible/?entidad=";
 
-	// Detectar si se le hace 'click'
+	// Acciones si se le hace 'click'
 	linkEditarProd.addEventListener("click", async (e) => {
 		// Obtener los parámetros
 		url = window.location.search;
 		indice = url.indexOf("&id=");
 		entidad = url.slice(9, indice);
 		id = url.slice(indice + 4);
-		// Obtener feedback del producto
-		feedback = await fetch(API_EditarProd + entidad + "&id=" + id).then((n) => n.json());
-		// Averiguar si el usuario está logueado, y en caso negativo redirigir a Login
-		console.log(feedback);
-		if (feedback=="hacerLogin") window.location.href ="/usuarios/login"
-		// Acciones si se le hace 'click'
-		// API con los datos de la entidad y el producto_id
-		// true --> ir a Editar
-		// false --> cartel de "Bloqueado"
-		// Fin
+		// Averiguar si el producto está disponible
+		// Campos: status, código, mensaje
+		disponible = await fetch(API_EditarProd + entidad + "&id=" + id).then((n) => n.json());
+		console.log(disponible);
+		// Acciones si el status es false
+		if (!disponible.status) {
+			// Si el usuario no está logueado, redirigir a Login
+			if (disponible.codigo == "hacerLogin") window.location.href = "/usuarios/login";
+			// false --> cartel de "Bloqueado" + motivo
+		} // else (true) --> ir a Editar
 	});
 });
