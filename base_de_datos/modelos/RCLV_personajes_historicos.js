ea715c09ab4fa261b3723632d2feb3b39228e3d9
unknown <diegoiribarren2015@gmail.com>
2022-01-08 09:16:48 -0300

    Miscelaneas

--- base_de_datos/modelos/RCLV_personajes_historicos.js
+++ base_de_datos/modelos/RCLV_personajes_historicos.js
@@ -32,28 +32,16 @@ module.exports = (sequelize, dt) => {
 	const entidad = sequelize.define(alias, columns, config);
 	entidad.associate = (n) => {
 		entidad.belongsTo(n.dias_del_ano, {as: "fecha", foreignKey: "dia_del_ano_id"});
-		entidad.belongsTo(n.procesos_canonizacion, {
-			as: "proceso_canonizacion",
-			foreignKey: "proceso_canonizacion_id",
-		});
+		entidad.belongsTo(n.procesos_canonizacion, {as: "proceso_canonizacion",	foreignKey: "proceso_canonizacion_id",});
 		entidad.belongsTo(n.roles_iglesia, {as: "rol_iglesia", foreignKey: "rol_iglesia_id"});
 
 		entidad.belongsTo(n.usuarios, {as: "creada_por", foreignKey: "creada_por_id"});
 		entidad.belongsTo(n.usuarios, {as: "analizada_por", foreignKey: "alta_analizada_por_id"});
-		entidad.belongsTo(n.status_registro, {
-			as: "status_registro",
-			foreignKey: "status_registro_id",
-		});
+		entidad.belongsTo(n.status_registro, {as: "status_registro", foreignKey: "status_registro_id",});
 		entidad.belongsTo(n.usuarios, {as: "editada_por", foreignKey: "editada_por_id"});
-		entidad.belongsTo(n.usuarios, {
-			as: "edic_analizada_por",
-			foreignKey: "edic_analizada_por_id",
-		});
+		entidad.belongsTo(n.usuarios, {as: "edic_analizada_por", foreignKey: "edic_analizada_por_id",});
 		entidad.belongsTo(n.usuarios, {as: "capturada_por", foreignKey: "capturada_por_id"});
-		entidad.belongsTo(n.motivos_para_borrar, {
-			as: "borrada_motivo",
-			foreignKey: "borrada_motivo_id",
-		});
+		entidad.belongsTo(n.motivos_para_borrar, {as: "borrada_motivo", foreignKey: "borrada_motivo_id",});
 
 		entidad.hasMany(n.peliculas, {as: "peliculas", foreignKey: "personaje_historico_id"});
 		entidad.hasMany(n.colecciones, {as: "colecciones", foreignKey: "personaje_historico_id"});
