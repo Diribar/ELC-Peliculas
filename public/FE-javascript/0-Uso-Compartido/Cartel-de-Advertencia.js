b7f516cf5e1fab6998c84bd0cf7b67fa7f938187
unknown <diegoiribarren2015@gmail.com>
2022-02-10 15:17:22 -0300

    Ayuda en Edición

---
+++ public/FE-javascript/0-Uso-Compartido/Cartel-de-Advertencia.js
@@ -0,0 +1,19 @@
+window.addEventListener("load", () => {
+	// Variables
+	let taparElFondo = document.querySelector("#tapar-el-fondo");
+	let cartelAdvertencia = document.querySelector("#cartelAdvertencia");
+	let cancelar = document.querySelector("#cartelAdvertencia .fa-arrow-alt-circle-left");
+	// let continuar = document.querySelector("#cartelAdvertencia .fa-check-circle");
+
+	cancelar.addEventListener("click", () => {
+		taparElFondo.classList.add("ocultar");
+		cartelAdvertencia.classList.add("ocultar");
+	});
+
+	window.addEventListener("keydown", (e) => {
+		if (e.key == "Escape") {
+			taparElFondo.classList.add("ocultar");
+			cartelAdvertencia.classList.add("ocultar");
+		}
+	});
+});
