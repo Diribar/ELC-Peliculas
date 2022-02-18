cf019b81c916b25164514a74a2dcf9c4f7abd7b5
unknown <diegoiribarren2015@gmail.com>
2022-02-18 19:11:41 -0300

    Miscelaneas

--- public/FE-javascript/3-Prod-RUD/2-Edicion-Errores.js
+++ public/FE-javascript/3-Prod-RUD/2-Edicion-Errores.js
@@ -21,7 +21,7 @@ window.addEventListener("load", async () => {
 	// Variables de íconos
 	let detalle = document.querySelector("#cuerpo #flechas .fa-circle-info");
 	let links = document.querySelector("#cuerpo #flechas .fa-globe");
-	let edicion = document.querySelector("#cuerpo #comandos .pencil-alt");
+	let edicion = document.querySelector("#cuerpo #comandos .fa-pencil-alt");
 	let original = document.querySelector("#cuerpo #comandos .fa-home");
 	let guardar = document.querySelector("#cuerpo #comandos .fa-save");
 	let descartar = document.querySelector("#cuerpo #comandos .fa-rotate-right");
