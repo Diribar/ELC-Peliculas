3d90f9038eb002696b6c5c84419794e6416403e3
unknown <diegoiribarren2015@gmail.com>
2022-02-20 10:22:47 -0300

    Miscelaneas

--- base_de_datos/modelos/EDIC_Links_prods.js
+++ base_de_datos/modelos/EDIC_Links_prods.js
@@ -7,19 +7,10 @@ module.exports = (sequelize, dt) => {
 		link_prov_id: {type: dt.INTEGER},
 		gratuito: {type: dt.BOOLEAN},
 
-		creado_por_id: {type: dt.INTEGER},
-		creado_en: {type: dt.DATE},
-		alta_analizada_por_id: {type: dt.INTEGER},
-		alta_analizada_en: {type: dt.DATE},
-		lead_time_creacion: {type: dt.INTEGER},
-		status_registro_id: {type: dt.INTEGER},
-
+		
 		editado_por_id: {type: dt.INTEGER},
 		editado_en: {type: dt.DATE},
-		edic_analizada_por_id: {type: dt.INTEGER},
-		edic_analizada_en: {type: dt.DATE},
-		lead_time_edicion: {type: dt.INTEGER},
-
+		status_registro_id: {type: dt.INTEGER},
 		capturado_por_id: {type: dt.INTEGER},
 		capturado_en: {type: dt.DATE},
 	};
@@ -33,10 +24,7 @@ module.exports = (sequelize, dt) => {
 		entidad.belongsTo(n.links_tipos, {as: "link_tipo", foreignKey: "link_tipo_id"});
 		entidad.belongsTo(n.links_provs, {as: "link_prov", foreignKey: "link_prov_id"});
 
-		entidad.belongsTo(n.usuarios, {as: "creado_por", foreignKey: "creado_por_id"});
-		entidad.belongsTo(n.usuarios, {as: "alta_analizada_por", foreignKey: "alta_analizada_por_id"});
 		entidad.belongsTo(n.usuarios, {as: "editado_por", foreignKey: "editado_por_id"});
-		entidad.belongsTo(n.usuarios, {as: "edic_analizada_por", foreignKey: "edic_analizada_por_id"});
 		entidad.belongsTo(n.usuarios, {as: "capturado_por", foreignKey: "capturado_por_id"});
 		entidad.belongsTo(n.status_registro_prod, {as: "status_registro", foreignKey: "status_registro_id"});
 	};
