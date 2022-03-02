e1302af280259895e0208e6845d339bd276b0f07
unknown <diegoiribarren2015@gmail.com>
2022-03-02 12:39:49 -0300

    Miscelaneas

--- base_de_datos/modelos/AUX_roles_iglesia.js
+++ base_de_datos/modelos/AUX_roles_iglesia.js
@@ -13,8 +13,6 @@ module.exports = (sequelize, dt) => {
 	};
 	const entidad = sequelize.define(alias, columns, config);
 	entidad.associate = (n) => {
-		entidad.belongsTo(n.sexos, {as: "sexo", foreignKey: "sexo_id"});
-		
 		entidad.hasMany(n.usuarios, {as: "usuarios", foreignKey: "rol_iglesia_id"});
 		entidad.hasMany(n.RCLV_personajes, {as: "personajes", foreignKey: "rol_iglesia_id"});
 	};
