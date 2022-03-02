290ca5f7fab7b0fa0b4963b339d53491502314b0
unknown <diegoiribarren2015@gmail.com>
2022-03-02 11:20:29 -0300

    Miscelaneas

--- middlewares/varios/productoYaEnBD.js
+++ middlewares/varios/productoYaEnBD.js
@@ -7,12 +7,12 @@ module.exports = async (req, res, next) => {
 		? req.cookies.datosTerminaste
 		: "";
 	if (datosTerminaste != "" && datosTerminaste.fuente != "IM") {
-		ELC_id = await BD_varias.obtenerELC_id(
+		let ELC_id = await BD_varias.obtenerELC_id(
 			datosTerminaste.entidad,
 			datosTerminaste.fuente + "_id",
 			datosTerminaste[datosTerminaste.fuente + "_id"]
 		);
-		ruta =
+		let ruta =
 			"/producto/agregar/ya-en-bd/?entidad=" + datosTerminaste.entidad + "&valor=" + ELC_id;
 		if (ELC_id) return res.redirect(ruta);
 	}
