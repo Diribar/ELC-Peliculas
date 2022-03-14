b654ab2ea3cdd79c76314051eb62bc283b3c2bf2
unknown <diegoiribarren2015@gmail.com>
2022-03-14 14:47:18 -0300

    Miscelaneas

--- rutas_y_controladores/9-Miscelaneas/ControladorAPI.js
+++ rutas_y_controladores/9-Miscelaneas/ControladorAPI.js
@@ -6,25 +6,6 @@ let {Op} = require("sequelize");
 
 // *********** Controlador ***********
 module.exports = {
-	// RCLV
-	buscarOtrosCasos: async (req, res) => {
-		let {mes_id, dia, entidad} = req.query;
-		dia_del_ano_id = await BD_varias.obtenerTodos("dias_del_ano", "id")
-			.then((n) => n.filter((m) => m.mes_id == mes_id))
-			.then((n) => n.find((m) => m.dia == dia))
-			.then((n) => n.id);
-		casos = await BD_varias.obtenerTodos(entidad, "nombre")
-			.then((n) => n.filter((m) => m.dia_del_ano_id == dia_del_ano_id))
-			.then((n) => n.map((m) => m.nombre));
-		return res.json(casos);
-	},
-
-	// RCLV
-	validarRCLV: async (req, res) => {
-		let errores = await validarRCLV["RCLV_" + req.query.RCLV](req.query);
-		return res.json(errores);
-	},
-
 	// Quick Search
 	quickSearch: async (req, res) => {
 		palabras = req.query.palabras.split(" ");
