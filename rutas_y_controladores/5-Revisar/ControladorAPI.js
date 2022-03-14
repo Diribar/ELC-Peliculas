0be7390d55e740e7227790f0e6d88981dfb2f48a
unknown <diegoiribarren2015@gmail.com>
2022-03-14 14:55:33 -0300

    Miscelaneas

--- rutas_y_controladores/5-Revisar/ControladorAPI.js
+++ rutas_y_controladores/5-Revisar/ControladorAPI.js
@@ -2,7 +2,6 @@
 let validarRCLV = require("../../funciones/Varias/ValidarRCLV");
 let BD_varias = require("../../funciones/BD/varias");
 let BD_especificas = require("../../funciones/BD/especificas");
-let {Op} = require("sequelize");
 
 // *********** Controlador ***********
 module.exports = {
