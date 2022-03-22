98246ee19597671d64c046ace27797df4f58e38f
unknown <diegoiribarren2015@gmail.com>
2022-03-22 10:49:30 -0300

    Miscelaneas

--- middlewares/producto/permisoRevUsuario.js
+++ middlewares/producto/permisoRevUsuario.js
@@ -4,74 +4,14 @@ const especificas = require("../../funciones/Varias/Especificas");
 const BD_genericas = require("../../funciones/BD/Genericas");
 
 module.exports = async (req, res, next) => {
-	// Definir variables
-	let entidad = req.query.entidad;
-	let prodID = req.query.id;
-	let userID = req.session.usuario.id;
-	let haceUnaHora = especificas.haceUnaHora();
-	let mensaje;
-	// CONTROLES PARA PRODUCTO *******************************************************
-	let prodOriginal = await BD_genericas.obtenerPorIdConInclude(entidad, prodID, [
-		"status_registro",
-		"capturado_por",
-	]).then((n) => (n ? n.toJSON() : ""));
-	// Problema1: PRODUCTO NO ENCONTRADO ----------------------------------------------
-	if (!prodOriginal) mensaje = "Producto no encontrado";
-	else {
-		// Problemas VARIOS
-		// 1-¿Producto en estado 'pend_aprobar'?
-		if (prodOriginal.status_registro.pend_aprobar) {
-			// 2-¿Creado por el usuario actual?
-			let creadoPorElUsuario1 = prodOriginal.creado_por_id == userID;
-			let creadoPorElUsuario2 =
-				entidad == "capitulos" && prodOriginal.coleccion.creado_por_id == userID;
-			// Problema2: EL REVISOR NO DEBE REVISAR UN PRODUCTO AGREGADO POR ÉL ------
-			if (creadoPorElUsuario1 || creadoPorElUsuario2)
-				mensaje = "El producto debe ser analizado por otro revisor, no por su creador";
-			else {
-				// Creado por otro usuario
-				// 3. ¿Creado > haceUnaHora?
-				let espera = parseInt((prodOriginal.creado_en - haceUnaHora) / 1000);
-				let unidad = espera <= 60 ? "segundos" : "minutos";
-				if (espera > 60) espera = parseInt(espera / 60);
-				// Problema3: EL PRODUCTO TODAVÍA ESTÁ EN MANOS DE SU CREADOR ---------
-				if (espera > 0)
-					mensaje = "El producto estará disponible para su revisión en " + espera + " " + unidad;
-				else {
-					// Creado hace > 1 hora
-					// 4. Capturado > haceUnaHora
-					if (prodOriginal.capturado_en && prodOriginal.capturado_en > haceUnaHora) {
-						// Capturado por otro usuario
-						// Problema4: EL PRODUCTO ESTÁ CAPTURADO POR OTRO USUARIO -----
-						if (prodOriginal.capturado_por_id != userID) {
-							let usuarioCaptura = prodOriginal.capturado_por.apodo;
-							let horarioCaptura =
-								prodOriginal.capturado_en.getDate() +
-								"/" +
-								prodOriginal.capturado_en.getMonth() +
-								" " +
-								prodOriginal.capturado_en.getHours() +
-								":" +
-								prodOriginal.capturado_en.getMinutes();
-							mensaje =
-								"El producto está en revisión por el usuario " +
-								usuarioCaptura +
-								", desde las " +
-								horarioCaptura +
-								"hs";
-						}
-					} else {
-						// CAPTURA EL PRODUCTO
-						let datos = {
-							capturado_en: new Date(),
-							capturado_por_id: userID,
-						};
-						BD_genericas.actualizarPorId(entidad, prodID, datos);
-					}
-				}
-			}
-		}
-	}
+	// - Revisa si tiene capturas en alguno de: 3 Tipos de Producto, 3 Tipos de RCLV
+	// 1. Sin capturas de otros productos
+	// 	--> NEXT
+	// 2. Con capturas de otros productos
+	// 	3. Con capturas < 1 hora
+	// 		--> REDIRECCIONA: 'Tenés que liberar estos casos...'
+	// 	3. Sin capturas < 1 hora
+	// 		--> Las LIBERA + NEXT
 	// Fin
 	if (mensaje) return res.render("Errores", {mensaje});
 	next();
