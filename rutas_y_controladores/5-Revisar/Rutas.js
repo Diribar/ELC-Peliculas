3fa1e7d250e8092ecfd0531f478e373803ea2ecb
unknown <diegoiribarren2015@gmail.com>
2022-03-22 10:45:18 -0300

    Ordenar Middlewares

--- rutas_y_controladores/5-Revisar/Rutas.js
+++ rutas_y_controladores/5-Revisar/Rutas.js
@@ -7,12 +7,13 @@ const vista = require("./ControladorVista");
 
 //************************ Middlewares ******************************
 const soloGestionProd = require("../../middlewares/usuarios/solo3-gestion-prod");
-const entidadId = require("../../middlewares/producto/entidadId");
-const permisoProducto = require("../../middlewares/producto/permisoRV_producto");
+const entidadId = require("../../middlewares/producto/validarEntidadId");
+const permisoProducto = require("../../middlewares/producto/permisoRevProducto");
+const permisoUsuario = require("../../middlewares/producto/permisoRevUsuario");
 
 // Rutas *******************************************
 router.get("/vision-general", soloGestionProd, vista.visionGeneral);
-router.get("/producto", soloGestionProd, entidadId, permisoProducto, vista.producto);
+router.get("/producto", soloGestionProd, entidadId, permisoProducto, permisoUsuario, vista.producto);
 // router.get("/rclv", soloGestionProd, vista.visionGeneral);
 // router.get("/links", soloGestionProd, vista.visionGeneral);
 
