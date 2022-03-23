358968ad0f8d7d933619b5fcf832decbc4909697
unknown <diegoiribarren2015@gmail.com>
2022-03-23 10:47:14 -0300

    Captura 'activa' en Producto

--- middlewares/producto/permisoRevProducto.js
+++ middlewares/producto/permisoRevProducto.js
@@ -36,15 +36,13 @@ module.exports = async (req, res, next) => {
 				// Problema3: EL PRODUCTO TODAVÍA ESTÁ EN MANOS DE SU CREADOR
 				// ¿Creado > haceUnaHora?
 				let espera = parseInt((prodOriginal.creado_en - haceUnaHora) / 1000);
-				let unidad = espera <= 60 ? "segundos" : "minutos";
+				let unidad = espera > 60 ? "minutos" : "segundos";
 				if (espera > 60) espera = parseInt(espera / 60);
 				if (espera > 0)
 					mensaje = "El producto estará disponible para su revisión en " + espera + " " + unidad;
 				// --------------------------------------------------------------------
 				else {
-					// Creado < haceUnaHora
-					// ----------------------------------------------------------------
-					let horarioCaptura;
+					// Definir nuevas variables
 					let meses = await BD_genericas.obtenerTodos("meses", "id").then((n) =>
 						n.map((m) => m.abrev)
 					);
@@ -57,36 +55,43 @@ module.exports = async (req, res, next) => {
 							prodOriginal.capturado_en.getHours() +
 							":" +
 							String(prodOriginal.capturado_en.getMinutes() + 1).padStart(2, "0");
-					// Capturado > haceUnaHora
-					if (prodOriginal.capturado_en > haceUnaHora) {
-						// Problema4: EL PRODUCTO ESTÁ CAPTURADO POR OTRO USUARIO
-						if (prodOriginal.capturado_por_id != userID)
-							mensaje =
-								"El producto está en revisión por el usuario " +
-								prodOriginal.capturado_por.apodo +
-								", desde las " +
-								horarioCaptura +
-								"hs";
-					} else {
-						// No capturado o capturado < haceUnaHora
-						// Problema5: EL USUARIO DEJÓ CAPTURADO ESTE PRODUCTO LUEGO DE LA HORA Y NO TRANSCURRIERON AÚN LAS 2 HORAS
-						// ¿Capturado por este usuario > haceDosHoras?
+					// ----------------------------------------------------------------
+					// Problema4: EL PRODUCTO ESTÁ CAPTURADO POR OTRO USUARIO EN FORMA 'ACTIVA'
+					if (
+						prodOriginal.capturado_en > haceUnaHora &&
+						prodOriginal.capturado_por_id != userID &&
+						prodOriginal.captura_activa
+					)
+						mensaje =
+							"El producto está en revisión por el usuario " +
+							prodOriginal.capturado_por.apodo +
+							", desde las " +
+							horarioCaptura +
+							"hs";
+					// Problema5: EL USUARIO DEJÓ INCONCLUSA LA REVISIÓN LUEGO DE LA HORA Y NO TRANSCURRIERON AÚN LAS 2 HORAS
+					else if (
+						prodOriginal.capturado_en < haceUnaHora &&
+						prodOriginal.capturado_en > haceDosHoras &&
+						prodOriginal.capturado_por_id == userID
+					)
+						mensaje =
+							"Tu revisión de este producto quedó inconclusa desde un poco antes de las " +
+							horarioCaptura +
+							"hs.. Podrás volver a revisarlo luego de transcurridas 2 horas desde ese horario.";
+					// Comienzo de las soluciones
+					// 1. Activar si no lo está, de lo contrario no hace nada
+					else if (!prodOriginal.captura_activa) {
+						let datos = {captura_activa: 1};
+						// 2. Cambiar de usuario si estaba capturado por otro
+						if (prodOriginal.capturado_por_id != userID) datos += {capturado_por_id: userID};
+						// 3. Fijarle la nueva hora de captura si corresponde
 						if (
-							prodOriginal.capturado_en > haceDosHoras &&
-							prodOriginal.capturado_por_id == userID
+							prodOriginal.capturado_por_id != userID ||
+							prodOriginal.capturado_en < haceDosHoras
 						)
-							mensaje =
-								"El producto quedó capturado por vos desde las " +
-								horarioCaptura +
-								"hs.. Podrás volver a capturarlo luego de transcurridas 2 horas desde ese horario.";
-						else {
-							// Sin problemas: CAPTURA DEL PRODUCTO
-							let datos = {
-								capturado_en: especificas.ahora(),
-								capturado_por_id: userID,
-							};
-							BD_genericas.actualizarPorId(entidad, prodID, datos);
-						}
+							datos += {capturado_en: especificas.ahora()};
+						// CAPTURA DEL PRODUCTO
+						BD_genericas.actualizarPorId(entidad, prodID, datos);
 					}
 				}
 			}
