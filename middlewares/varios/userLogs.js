05daf4e22cea9085712795ebca8ab3cb6e5ce655
unknown <diegoiribarren2015@gmail.com>
2022-03-30 20:27:40 -0300

    Miscelaneas

--- middlewares/varios/userLogs.js
+++ middlewares/varios/userLogs.js
@@ -6,7 +6,12 @@ module.exports = (req, res, next) => {
 	if (!req.session.urlAnterior) req.session.urlAnterior = "/";
 	let urlAnt = req.session.urlReferencia;
 	let url = req.originalUrl;
-	if (!url.includes("/usuarios/") && !url.includes("/api/") && url != req.session.urlReferencia) {
+	if (
+		!url.includes("/usuarios/") &&
+		!url.includes("/api/") &&
+		!url.includes("/redireccionar/") &&
+		url != req.session.urlReferencia
+	) {
 		req.session.urlReferencia = url;
 		if (urlAnt != req.session.urlReferencia) req.session.urlAnterior = urlAnt;
 	}
