14d2096fdd2db950672875273a63cd6ae24b2e55
unknown <diegoiribarren2015@gmail.com>
2022-04-08 21:25:43 -0300

    Creación del archivo JS

---
+++ public/FE-javascript/5-Revisar/2-ProdEditar.js
@@ -0,0 +1,60 @@
+"use strict";
+window.addEventListener("load", () => {
+	// Variables
+	let prodEntidad = new URL(window.location.href).searchParams.get("entidad");
+	let prodID = new URL(window.location.href).searchParams.get("id");
+	let edicion_id = new URL(window.location.href).searchParams.get("edicion_id");
+
+	// Flechas
+	let aprobar = document.querySelector("#imagenes #editada img");
+	let mostrarMenuMotivos = document.querySelector("#imagenes #original img");
+
+	// Motivos para borrar
+	let taparElFondo = document.querySelector("#tapar-el-fondo");
+	let menuMotivosBorrar = document.querySelector("#motivosRechazo");
+	let motivosRechazo = document.querySelector("#motivosRechazo select");
+	let cancelar = document.querySelector("#comandosRechazar .fa-circle-left");
+	let rechazar = document.querySelector("#comandosRechazar .fa-circle-right");
+
+	// Aprobar el nuevo avatar
+	aprobar.addEventListener("click", async () => {
+		aprobar.style.transform = "scale(1)";
+		aprobar.style.cursor = "wait";
+		let ruta = "/revision/producto/edicion/api/aprobarAvatar/?entidad=";
+		await fetch(ruta + prodEntidad + "&id=" + prodID + "&edicion_id=" + edicion_id);
+		window.location.href =
+			"/revision/redireccionar/?entidad=" + prodEntidad + "&id=" + prodID + "&edicion_id=" + edicion_id;
+	});
+
+	// Menú inactivar
+	mostrarMenuMotivos.addEventListener("click", () => {
+		menuMotivosBorrar.classList.remove("ocultar");
+		taparElFondo.classList.remove("ocultar");
+	});
+
+	// Cancelar menú motivos para borrar
+	cancelar.addEventListener("click", () => {
+		menuMotivosBorrar.classList.add("ocultar");
+		taparElFondo.classList.add("ocultar");
+	});
+
+	// Rechazar el nuevo avatar
+	rechazar.addEventListener("click", async () => {
+		let motivo = motivosRechazo.value;
+		if (motivo) {
+			rechazar.style.transform = "scale(1)";
+			menuMotivosBorrar.style.cursor = "wait";
+			let ruta = "/revision/producto/edicion/api/rechazarAvatar/?entidad=";
+			await fetch(
+				ruta + prodEntidad + "&id=" + prodID + "&edicion_id=" + edicion_id + "&motivo_id=" + motivo
+			);
+			window.location.href =
+				"/revision/redireccionar/?entidad=" +
+				prodEntidad +
+				"&id=" +
+				prodID +
+				"&edicion_id=" +
+				edicion_id;
+		}
+	});
+});
