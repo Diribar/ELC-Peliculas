56e066eb4e4f5c75c18f027b4969bb390fff1902
unknown <diegoiribarren2015@gmail.com>
2022-04-12 15:12:58 -0300

    Miscelaneas

--- public/FE-javascript/3-Prod-RUD/2-Edicion-Links_RCLV.js
+++ public/FE-javascript/3-Prod-RUD/2-Edicion-Links_RCLV.js
@@ -2,15 +2,15 @@
 window.addEventListener("load", async () => {
 	// Variables generales
 	let links = document.querySelectorAll(".input-error i.linkRCLV");
-	let entidad = new URL(window.location.href).searchParams.get("entidad");
-	let producto_id = new URL(window.location.href).searchParams.get("id");
+	let prodEntidad = new URL(window.location.href).searchParams.get("entidad");
+	let prodID = new URL(window.location.href).searchParams.get("id");
 
 	// Links a Relación con la vida
 	for (let i = 0; i < links.length; i++) {
 		links[i].addEventListener("click", () => {
 			if (!links[i].classList.contains("inactivo")) {
 				// Obtener la entidad_RCLV
-				let entidad_RCLV = links[i].className.includes("personaje")
+				let RCLV_entidad = links[i].className.includes("personaje")
 					? "RCLV_personajes"
 					: links[i].className.includes("hecho")
 					? "RCLV_hechos"
@@ -18,11 +18,11 @@ window.addEventListener("load", async () => {
 				// Para ir a la vista RCLV
 				window.location.href =
 					"/producto/rclv/redireccionar/?origen=edicion&entidad=" +
-					entidad + // la entidad del producto
+					prodEntidad +
 					"&id=" +
-					producto_id +
+					prodID +
 					"&entidad_RCLV=" +
-					entidad_RCLV;
+					RCLV_entidad;
 			}
 		});
 	}
