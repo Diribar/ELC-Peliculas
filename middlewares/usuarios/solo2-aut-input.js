914938dbeefa23d95ff1b510a21f4eb726dc9b44
unknown <diegoiribarren2015@gmail.com>
2022-05-10 17:48:16 -0300

    Miscelaneas

--- middlewares/usuarios/solo2-aut-input.js
+++ middlewares/usuarios/solo2-aut-input.js
@@ -1,14 +1,16 @@
 "use strict";
 // Requires
-const funciones = require("../../funciones/3-Procesos/Compartidas");
-const variables = require("../../funciones/3-Procesos/Variables");
+const usuarios = require("../../funciones/3-Procesos/8-Usuarios");
 
 module.exports = (req, res, next) => {
 	let usuario = req.session.usuario;
+	let informacion;
+	// Redireccionar si el usuario no está logueado
 	if (!usuario) return res.redirect("/usuarios/login");
+	// Redireccionar si el usuario no tiene el permiso necesario
 	if (!usuario.rol_usuario.aut_input) {
 		let linkUsuarioAutInput = "/usuarios/autorizado-input/solicitud";
-		let informacion = {
+		informacion = {
 			mensajes: [
 				"Se requiere aumentar el nivel de confianza, para ingresar información a nuestro sistema. Podés gestionarlo vos mismo haciendo click abajo, en la flecha hacia la derecha.",
 			],
@@ -17,24 +19,11 @@ module.exports = (req, res, next) => {
 				{nombre: "fa-circle-right", link: linkUsuarioAutInput, titulo: "Solicitar el permiso"},
 			],
 		};
-		return res.render("Errores", {informacion});
 	}
-	if (usuario.penalizado_hasta && new Date(usuario.penalizado_hasta) > new Date(funciones.ahora())) {
-		let hasta = new Date(usuario.penalizado_hasta);
-		let fecha =
-			hasta.getDate() + "/" + meses[hasta.getMonth()] + "/" + String(hasta.getFullYear()).slice(-2);
-		let hora = hasta.getHours() + ":" + String(hasta.getMinutes() + 1).padStart(2, "0");
-		let informacion = {
-			mensajes: [
-				"Necesitamos que la información que nos brindes esté más alineada con nuestro perfil y sea precisa",
-				"Podrás volver a ingresar información el día " + fecha + ", a las " + hora + "hs.",
-			],
-			iconos: [
-				{nombre: "fa-circle-left", link: req.session.urlAnterior, titulo: "Ir a la vista anterior"},
-				{nombre: "fa-house", link: "/", titulo: "Ir a la vista de inicio"},
-			],
-		};
-		return res.render("Errores", {informacion});
-	}
+	// Detectar si el usuario está penalizado
+	if (!informacion) informacion = usuarios.detectarUsuarioPenalizado(usuario);
+	// Si corresponde, mostrar el mensaje de error
+	if (informacion) return res.render("Errores", {informacion});
+	// Fin
 	next();
 };
