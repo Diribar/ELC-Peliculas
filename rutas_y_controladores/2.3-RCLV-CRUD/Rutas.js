5f5d4d53955c193c2158f6bedb9edd0a66be8130
unknown <diegoiribarren2015@gmail.com>
2022-06-23 18:36:39 -0300

    Miscelaneas

--- rutas_y_controladores/2.3-RCLV-CRUD/Rutas.js
+++ rutas_y_controladores/2.3-RCLV-CRUD/Rutas.js
@@ -6,7 +6,12 @@ const API = require("./ControladorAPI");
 const vista = require("./ControladorVista");
 
 //************************ Middlewares ******************************
+// Login y Roles de Usuario
 const soloAutInput = require("../../middlewares/usuarios/solo2-aut-input");
+// Existen la entidad y el producto
+const entidad = require("../../middlewares/producto/entidadNombre");
+const id = require("../../middlewares/producto/entidadID");
+// Temas de captura
 const aptoDE = require("../../middlewares/captura/aptoDE");
 const permReg = require("../../middlewares/captura/permReg");
 const permUserReg = require("../../middlewares/captura/permUserReg");
@@ -20,13 +25,22 @@ router.get("/api/validar-campo", API.validarCampo);
 router.get("/api/validar-consolidado", API.validarConsolidado);
 
 // Rutas de vistas - Relación con la vida
-router.get("/redireccionar", soloAutInput, vista.redireccionar);
-router.get("/agregar", soloAutInput, entidad, id, aptoDE, permReg, permUserReg, capturaActivar, vista.RCLV_AltaForm);
+router.get("/redireccionar", soloAutInput, aptoDE, vista.redireccionar);
+// router.get("/agregar", soloAutInput, entidad, id, aptoDE, vista.RCLV_AltaForm);
 router.post("/agregar", soloAutInput, entidad, id, aptoDE, vista.RCLV_Grabar);
-
-router.get("/detalle", soloAutInput, entidad, id, capturaInactivar, vista.RCLV_Form);
-router.get("/editar", soloAutInput, entidad, id, aptoDE, permReg, permUserReg, capturaActivar, vista.RCLV_Form);
-router.get("/eliminar", soloAutInput, aptoDE, vista.RCLV_Eliminar);
+router.get("/detalle", entidad, id, capturaInactivar, vista.RCLV_Form);
+router.get(
+	"/editar",
+	soloAutInput,
+	entidad,
+	id,
+	aptoDE,
+	permReg,
+	permUserReg,
+	capturaActivar,
+	vista.RCLV_Form
+);
+// router.get("/eliminar", soloAutInput, entidad, id, aptoDE, permReg, permUserReg, vista.RCLV_Eliminar);
 
 // Exportarlo **********************************************
 module.exports = router;
