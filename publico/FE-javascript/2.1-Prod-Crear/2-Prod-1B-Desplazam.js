a3a102dff42f2f28dfd7e1f9803f98fa28aac8e4
unknown <diegoiribarren2015@gmail.com>
2022-08-05 09:56:41 -0300

    Miscelaneas

--- publico/FE-javascript/2.1-Prod-Crear/2-Prod-1B-Desplazam.js
+++ publico/FE-javascript/2.1-Prod-Crear/2-Prod-1B-Desplazam.js
@@ -13,14 +13,27 @@ window.addEventListener("load", async () => {
 		// Comandos iniciales
 		let posicion = resultado.scrollLeft;
 		let mover = true;
-		e.preventDefault();
+		let teclasDesplazamiento = [
+			"Home",
+			"End",
+			"PageUp",
+			"PageDown",
+			"ArrowUp",
+			"ArrowDown",
+			"ArrowLeft",
+			"ArrowRight",
+			"Tab",
+		];
+		if (teclasDesplazamiento.includes(e.key)) e.preventDefault();
 
 		// Home y End
 		if (e.key == "Home") indiceFocus = 0;
 		else if (e.key == "End") indiceFocus = boton.length - 1;
 		// Page Up / Down
-		else if (e.key == "PageUp") indiceFocus = indiceFocus - cantFormsVisibles;
-		else if (e.key == "PageDown") indiceFocus = indiceFocus + cantFormsVisibles;
+		else if (e.key == "PageUp" || (e.key == "Tab" && e.shiftKey))
+			indiceFocus = indiceFocus - cantFormsVisibles;
+		else if (e.key == "PageDown" || (e.key == "Tab" && !e.shiftKey))
+			indiceFocus = indiceFocus + cantFormsVisibles;
 		// Arrows
 		else if (e.key == "ArrowUp" || e.key == "ArrowLeft") indiceFocus = indiceFocus - 1;
 		else if (e.key == "ArrowDown" || e.key == "ArrowRight") indiceFocus = indiceFocus + 1;
