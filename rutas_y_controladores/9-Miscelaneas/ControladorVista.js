f44fc6e3dddef3821ad4646c6d7d25e8677ea524
unknown <diegoiribarren2015@gmail.com>
2022-08-11 00:20:37 -0300

    Miscelaneas

--- rutas_y_controladores/9-Miscelaneas/ControladorVista.js
+++ rutas_y_controladores/9-Miscelaneas/ControladorVista.js
@@ -35,11 +35,10 @@ module.exports = {
 	// Miscelaneas
 	redireccionar: async (req, res) => {
 		// Variables
-		let {destino, prodEntidad, prodID} = req.query;
-		let datos = req.query;
+		let {destino, entidad, id} = req.query;
 		// Si es 'tablero', ir a tablero
 		if (destino == "tablero") destino = "/revision/tablero-de-control";
-		if (destino == "prodDetalle") destino = "/producto/detalle/?entidad=" + prodEntidad + "&id=" + prodID;
+		if (destino == "prodDetalle") destino = "/producto/detalle/?entidad=" + entidad + "&id=" + id;
 		if (!destino) destino = "/";
 		// Redireccionar a la vista que corresponda
 		return res.redirect(destino);
