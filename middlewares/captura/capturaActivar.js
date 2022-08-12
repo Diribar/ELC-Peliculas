de30355247b435f43aca858d9af06ab9c206da4f
unknown <diegoiribarren2015@gmail.com>
2022-08-12 13:10:47 -0300

    Miscelaneas

--- middlewares/captura/capturaActivar.js
+++ middlewares/captura/capturaActivar.js
@@ -18,8 +18,10 @@ module.exports = async (req, res, next) => {
 	let horarioFinal = funciones.nuevoHorario(1, capturado_en);
 	horarioFinal = funciones.horarioTexto(horarioFinal);
 
-	// No se debe capturar si el registro está en status "creado"
-	if (!registro.status_registro.creado) {
+	// No se debe capturar si se cumplen estas 2 condiciones:
+	// 	1. El registro está en status "creado"
+	//	2. Se quiere acceder desde una vista que no es de revisión (ej: Detalle)
+	if (!registro.status_registro.creado|| urlBase == "/revision") {
 		// Activa la entidad y el usuario
 		let datos = {captura_activa: true, capturado_por_id: userID};
 		// Fija la nueva hora de captura si corresponde
