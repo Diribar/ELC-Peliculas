a2da9816f64898f2127a5082c4a2301e60778598
unknown <diegoiribarren2015@gmail.com>
2022-08-29 18:13:06 -0300

    Nuevo orden en Login

--- publico/FE-javascript/1-Usuarios/9-Login.js
+++ publico/FE-javascript/1-Usuarios/9-Login.js
@@ -3,42 +3,45 @@ window.addEventListener("load", async () => {
 	// Variables generales
 	let form = document.querySelector("#dataEntry");
 	let inputs = document.querySelectorAll("#dataEntry input");
+	// Variables de errores
 	let iconoError = document.querySelectorAll(".fa-circle-xmark");
 	let iconoOK = document.querySelectorAll(".fa-circle-check");
-	let mensajesError = document.querySelectorAll(".mensajeError");
-	let button = document.querySelector("form button[type='submit']");
-	let statusInicial = true;
 	let resultadoInvalido = document.querySelector("#resultadoInvalido");
+	let mensajesError = document.querySelectorAll(".mensajeError");
+	// Varias
+	let button = document.querySelector("form button[type='submit']");
+	let errores;
 
 	// Fórmulas ----------------------------------------------------
 	// Devuelve todos los errores
-	let averiguarErrores =async () => {
+	let averiguaLosErrores = async () => {
 		let url;
 		inputs.forEach((input, i) => {
 			if (i) url += "&";
 			url += inputs[i].name + "=" + encodeURIComponent(inputs[i].value);
 		});
 		// Obtener los errores
-		let errores=await fetch("/usuarios/api/validar-login/?" + url).then((n) => n.json());
+		errores = await fetch("/usuarios/api/validar-login/?" + url).then((n) => n.json());
 		// Fin
-		return 
+		return;
 	};
-
 	// Anula/activa el botón 'Submit', muestra el ícono de error/acierto
-	let accionesSiHayErrores = (i, errores) => {
+	let consecuenciasDeErrores = (i, errores) => {
 		// Averiguar si hay un error
 		let campo = inputs[i].name;
 		let mensaje = errores[campo];
 		mensajesError[i].innerHTML = mensaje;
+		// IconosError
+		mensaje ? iconoError[i].classList.remove("ocultar") : iconoError[i].classList.add("ocultar");
+		// IconosOK
+		mensaje ? iconoOK[i].classList.add("ocultar") : iconoOK[i].classList.remove("ocultar");
+	};
+	let botonSubmit = () => {
 		// En caso de error
 		if (mensaje) {
-			iconoError[i].classList.remove("ocultar");
-			iconoOK[i].classList.add("ocultar");
 			button.classList.add("inactivo");
 		} else {
 			// En caso de que no haya error
-			iconoError[i].classList.add("ocultar");
-			iconoOK[i].classList.remove("ocultar");
 			let sinErrores = true;
 			for (let j = 0; j < inputs.length; j++) {
 				iconoOK[j].classList.contains("ocultar") || !resultadoInvalido.classList.contains("ocultar")
@@ -53,9 +56,9 @@ window.addEventListener("load", async () => {
 	for (let i = 0; i < inputs.length; i++) {
 		inputs[i].addEventListener("change", async () => {
 			resultadoInvalido.classList.add("ocultar");
-			let errores = await averiguarErrores();
+			let errores = await averiguaLosErrores();
 			// Realiza acciones sobre el input cambiado
-			accionesSiHayErrores(i, errores);
+			consecuenciasDeErrores(i, errores);
 		});
 	}
 
@@ -65,12 +68,9 @@ window.addEventListener("load", async () => {
 	});
 
 	// Status inicial
-	if (statusInicial) {
-		let errores = await averiguarErrores();
-		// Revisa los errores en los inputs
-		for (let i = 0; i < inputs.length; i++) {
-			inputs[i].value != "" ? accionesSiHayErrores(i, errores) : "";
-		}
-		statusInicial = false;
+	await averiguaLosErrores();
+	// Revisa los errores en los inputs
+	for (let i = 0; i < inputs.length; i++) {
+		inputs[i].value != "" ? consecuenciasDeErrores(i, errores) : "";
 	}
 });
