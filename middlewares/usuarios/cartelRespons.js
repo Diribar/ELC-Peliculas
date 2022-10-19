5b48f2719061a38c931a7e56993395a9954dee3f
unknown <diegoiribarren2015@gmail.com>
2022-10-18 23:10:39 -0300

    Miscelaneas

--- middlewares/usuarios/cartelRespons.js
+++ middlewares/usuarios/cartelRespons.js
@@ -5,7 +5,6 @@ const BD_genericas = require("../../funciones/2-BD/Genericas");
 module.exports = (req, res, next) => {
 	// Variables
 	const usuario = req.session.usuario;
-	console.log(req.session.usuario.mostrar_cartel_respons);
 	if (req.session.usuario.mostrar_cartel_respons) {
 		BD_genericas.actualizarPorId("usuarios", usuario.id, {mostrar_cartel_respons: false});
 		return res.redirect("/producto/agregar/responsabilidad");
