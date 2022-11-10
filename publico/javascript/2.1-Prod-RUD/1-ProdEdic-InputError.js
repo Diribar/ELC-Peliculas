bc413ebc20b55016a7450ff247936f0b59275259
unknown <diegoiribarren2015@gmail.com>
2022-11-10 17:25:55 -0300

    Miscelaneas

--- publico/javascript/2.1-Prod-RUD/1-ProdEdic-InputError.js
+++ publico/javascript/2.1-Prod-RUD/1-ProdEdic-InputError.js
@@ -111,7 +111,7 @@ window.addEventListener("load", async () => {
 			// Acciones sobre la edición guardada
 			if (version.edicG_existe) {
 				v.botonesActivarVersion[1].classList.remove("inactivoVersion");
-				if (!v.origPendAprobar) v.botonesDescartar[1].classList.remove("inactivoVersion");
+				if (!version.origPendAprobar) v.botonesDescartar[1].classList.remove("inactivoVersion");
 				else v.botonesDescartar[1].classList.add("inactivoVersion");
 			} else {
 				v.botonesActivarVersion[1].classList.add("inactivoVersion");
