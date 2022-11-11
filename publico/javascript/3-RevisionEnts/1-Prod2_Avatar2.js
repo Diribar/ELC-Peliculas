b081364bd27089e72ba47cae2dd8bf8219ac941c
unknown <diegoiribarren2015@gmail.com>
2022-11-11 19:19:41 -0300

    Leyendas en Revisión avatar productos

--- publico/javascript/3-RevisionEnts/1-Prod2_Avatar2.js
+++ publico/javascript/3-RevisionEnts/1-Prod2_Avatar2.js
@@ -1,7 +1,13 @@
 "use strict";
 window.addEventListener("load", () => {
-	let beneficios = document.querySelectorAll("#medio #titulo")[0];
-	let perjuicios = document.querySelectorAll("#medio #titulo")[1];
+	let beneficios = {
+		titulo: document.querySelectorAll("#medio #titulo")[0],
+		ul: document.querySelectorAll("#medio #beneficios"),
+	};
+	let perjuicios = {
+		titulo: document.querySelectorAll("#medio #titulo")[1],
+		ul: document.querySelectorAll("#medio #perjuicios"),
+	};
 
 	// Variables de Imagen Nueva
 	let imagenNuevaDOM = document.querySelector("#izquierda img");
@@ -24,46 +30,89 @@ window.addEventListener("load", () => {
 	imagenActual.ratioAdecuado = imagenActual.ratio >= 1.42 && imagenActual.ratio <= 2;
 
 	// Comparaciones de ratio
-	let mejorRatio =
-		!imagenActual.ratioAdecuado &&
-		(imagenNueva.ratioAdecuado || (imagenNueva.ratio > imagenActual.ratio && imagenNueva.ratio <= 2));
-	let peorRatio =
-		!imagenNueva.ratioAdecuado &&
-		(imagenActual.ratioAdecuado || (imagenActual.ratio > imagenNueva.ratio && imagenActual.ratio <= 2));
+	beneficios = {
+		...beneficios,
+		ratioResultado:
+			!imagenActual.ratioAdecuado &&
+			(imagenNueva.ratioAdecuado || (imagenNueva.ratio > imagenActual.ratio && imagenNueva.ratio <= 2)),
+		ratioLeyenda: "Mejor relación de alto y ancho",
+	};
+	perjuicios = {
+		...perjuicios,
+		ratioResultado:
+			!imagenNueva.ratioAdecuado &&
+			(imagenActual.ratioAdecuado ||
+				(imagenActual.ratio > imagenNueva.ratio && imagenActual.ratio <= 2)),
+		ratioLeyenda: "Peor relación de alto y ancho",
+	};
 
 	// Comparaciones de calidad
-	let mejorCalidad = (imagenActual.alto < 700) & (imagenNueva.alto > imagenActual.alto);
-	let peorCalidad = (imagenNueva.alto < 700) & (imagenActual.alto > imagenNueva.alto);
+	beneficios = {
+		...beneficios,
+		calidadResultado: (imagenActual.alto < 700) & (imagenNueva.alto > imagenActual.alto),
+		calidadLeyenda: "Mejor resolución",
+	};
+	perjuicios = {
+		...perjuicios,
+		calidadResultado: (imagenNueva.alto < 700) & (imagenActual.alto > imagenNueva.alto),
+		calidadLeyenda: "Peor resolución",
+	};
 
-	// Averiguar si el original proviene de un link externo
-	let imagenActualEsLinkExterno = !imagenActualDOM.src.includes("imagenes");
+	// Funciones
+	let feedback = (bloque, infos) => {
+		if (typeof infos == "string") {
+			// Agregar titulo
+			let h3 = document.createElement("h3");
+			h3.innerText = bloque.slice(0, -1) + ":";
+			bloque == "beneficios" ? beneficios.titulo.appendChild(h3) : perjuicios.titulo.appendChild(h3);
+			// Agregar párrafo
+			let p = document.createElement("p");
+			p.innerText = (
+				!beneficios[infos + "Leyenda"] && !perjuicios[infos + "Leyenda"]
+					? infos
+					: bloque == "beneficios"
+					? beneficios[infos + "Leyenda"]
+					: perjuicios[infos + "Leyenda"]
+			).toLowerCase();
+			bloque == "beneficios" ? beneficios.titulo.appendChild(p) : perjuicios.titulo.appendChild(p);
+		} else {
+			// Agregar titulo
+			let h3 = document.createElement("h3");
+			h3.innerText = bloque + ":";
+			bloque == "beneficios" ? beneficios.titulo.appendChild(h3) : perjuicios.titulo.appendChild(h3);
+			for (let info of infos) {
+				let li = document.createElement("li");
+				li.innerText = [bloque][info + "Leyenda"];
+				bloque == "beneficios" ? beneficios.ul.appendChild(li) : perjuicios.ul.appendChild(li);
+			}
+		}
+		// Fin
+		return;
+	};
 
 	// Beneficios
-	if (!mejorRatio) document.querySelector("#beneficios #ratio").classList.add("ocultar");
-	if (!mejorCalidad) document.querySelector("#beneficios #resolucion").classList.add("ocultar");
-	if (!mejorRatio && !mejorCalidad) {
-		const para = document.createElement("p");
-		para.innerText = "ninguno";
-		beneficios.appendChild(para);
-	}
-	
+	!beneficios.ratioResultado && !beneficios.calidadResultado
+		? feedback("beneficios", "ninguno")
+		: !beneficios.ratioResultado
+		? feedback("beneficios", "calidad")
+		: !beneficios.calidadResultado
+		? feedback("beneficios", "ratio")
+		: feedback("beneficios", ["ratio", "calidad"]);
 
 	// Perjuicios
-	if (!peorRatio) document.querySelector("#perjuicios #ratio").classList.add("ocultar");
-	if (!peorCalidad) document.querySelector("#perjuicios #resolucion").classList.add("ocultar");
-	if (!peorRatio && !peorCalidad) {
-		const para = document.createElement("p");
-		para.innerText = "ninguno";
-		perjuicios.appendChild(para);
-	}
+	!perjuicios.ratioResultado && !perjuicios.calidadResultado
+		? feedback("perjuicios", "ninguno")
+		: !perjuicios.ratioResultado
+		? feedback("perjuicios", "calidad")
+		: !perjuicios.calidadResultado
+		? feedback("perjuicios", "ratio")
+		: feedback("perjuicios", ["ratio", "calidad"]);
 
-	console.log(
-		"mejorRatio:" + mejorRatio,
-		"peorRatio:" + peorRatio,
-		"mejorCalidad:" + mejorCalidad,
-		"peorCalidad:" + peorCalidad,
-		"imagenActualEsLinkExterno:" + imagenActualEsLinkExterno,
-		"imagenNueva-ratio:" + imagenNueva.ratio,
-		"imagenActual-ratio:" + imagenActual.ratio
-	);
+	// if (!peorRatio.resultado) document.querySelector("#perjuicios #ratio").classList.add("ocultar");
+	// if (!peorCalidad.resultado) document.querySelector("#perjuicios #resolucion").classList.add("ocultar");
+	// if (!peorRatio.resultado && !peorCalidad.resultado) {
+	// 	const para = document.createElement("p");
+	// 	para.innerText = "ninguno";
+	// 	perjuicios.appendChild(para);
+	// }
 });
