c85212c2f0f9e77d2281dba44d9d0ebc7942af1e
unknown <diegoiribarren2015@gmail.com>
2022-11-15 18:37:54 -0300

    Miscelaneas

--- publico/javascript/3-RevisionEnts/RE1-Prod1_Alta.js
+++ publico/javascript/3-RevisionEnts/RE1-Prod1_Alta.js
@@ -6,31 +6,30 @@ window.addEventListener("load", () => {
 	// Motivos para borrar
 	let taparElFondo = document.querySelector("#tapar-el-fondo");
 	let menuMotivosBorrar = document.querySelector("#motivosRechazo");
-	let motivosRechazo = document.querySelector("#motivosRechazo select");
+	let motivoRechazo = document.querySelector("#motivosRechazo select");
 	let cancelar = document.querySelector("#comandosRechazar .fa-circle-left");
 	let inactivar = document.querySelector("#comandosRechazar button");
 
-	// Menú motivos para borrar
+	// Muestra el menú "motivos para borrar"
 	mostrarMenuMotivos.addEventListener("click", () => {
 		menuMotivosBorrar.classList.remove("ocultar");
 		taparElFondo.classList.remove("ocultar");
 	});
 
-	// Cancelar menú motivos para borrar
+	// Oculta el menú "motivos para borrar"
 	cancelar.addEventListener("click", () => {
 		menuMotivosBorrar.classList.add("ocultar");
 		taparElFondo.classList.add("ocultar");
 	});
 
-	// Elegir motivo rechazo
-	motivosRechazo.addEventListener("click", () => {
-		if (motivosRechazo.value) inactivar.classList.remove("inactivo")
+	// Elegi motivo rechazo
+	motivoRechazo.addEventListener("change", () => {
+		if (motivoRechazo.value) inactivar.classList.remove("inactivo")
 		else inactivar.classList.add("inactivo")
 	});
 
 	// Inactivar
 	inactivar.addEventListener("click", (e) => {
-		let motivo = motivosRechazo.value;
-		if (!motivo) e.preventDefault();
+		if (!motivoRechazo.value) e.preventDefault();
 	});
 });
