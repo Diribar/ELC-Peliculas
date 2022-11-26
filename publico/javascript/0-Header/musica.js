da7090a7101d83ec0f8b6c4258bcdc7897158778
unknown <diegoiribarren2015@gmail.com>
2022-11-26 10:28:33 -0300

    Miscelaneas

--- publico/javascript/0-Header/musica.js
+++ publico/javascript/0-Header/musica.js
@@ -1,6 +1,6 @@
 "use strict";
 window.addEventListener("load", () => {
-	let iconoPlay = document.querySelector("header #icono-musica");
+	let iconoPlay = document.querySelector("header #icono_musica");
 	let musica = document.querySelector("header audio");
 	iconoPlay.addEventListener("click", () => {
 		musica.paused ? musica.play() : musica.pause()
