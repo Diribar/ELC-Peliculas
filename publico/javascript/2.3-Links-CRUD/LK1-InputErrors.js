d4f13f012e78d63241863a5adbee6cdab872264b
unknown <diegoiribarren2015@gmail.com>
2022-12-21 09:58:17 -0300

    Solución de errores - Links validar

--- publico/javascript/2.3-Links-CRUD/LK1-InputErrors.js
+++ publico/javascript/2.3-Links-CRUD/LK1-InputErrors.js
@@ -10,9 +10,6 @@ window.addEventListener("load", async () => {
 		// Variables de Edición y Alta
 		campos: document.querySelectorAll("tbody .campo"),
 
-		// Variables de Alta
-		filaAlta: filas - 1,
-
 		// Inputs
 		inputs: document.querySelectorAll("tbody .inputError .input"),
 		camposInput: Array.from(document.querySelectorAll("tbody .alta .input")).map((n) => n.name),
@@ -37,6 +34,7 @@ window.addEventListener("load", async () => {
 	};
 	let columnas = v.camposInput.length;
 	let filas = v.inputs.length / columnas;
+	let filaAlta = filas - 1;
 	let provs = await fetch(v.rutaObtieneProvs).then((n) => n.json());
 
 	// FUNCIONES ---------------------------------------------------------------
@@ -52,7 +50,7 @@ window.addEventListener("load", async () => {
 		},
 		depuraUrl: () => {
 			// Obtiene el valor actual
-			let valor = v.urlInputs[v.filaAlta].value;
+			let valor = v.urlInputs[filaAlta].value;
 			// Obtiene ambos índices
 			let indice1 = valor.indexOf("www.");
 			let indice2 = valor.indexOf("//");
@@ -71,19 +69,19 @@ window.addEventListener("load", async () => {
 				url = nuevaUrl.join("") + producto;
 			}
 			// Conclusiones
-			v.urlInputs[v.filaAlta].value = url;
+			v.urlInputs[filaAlta].value = url;
 			return url;
 		},
 		controlesEnUrl: async (fila) => {
 			// Detectar errores y aplicar consecuencias
-			let error = fila == v.filaAlta ? await mensajeDeError(fila, "url") : "";
+			let error = fila == filaAlta ? await mensajeDeError(fila, "url") : "";
 			return !error || !error.url;
 		},
 		controlesEnCalidad: async (fila, prov) => {
 			// Si el resultado es conocido --> ponerlo
 			let condicion = !!prov.calidad;
 			if (condicion) v.calidadInputs[fila].value = prov.calidad;
-			if (fila == v.filaAlta) v.calidadInputs[fila].disabled = condicion;
+			if (fila == filaAlta) v.calidadInputs[fila].disabled = condicion;
 			// Detectar errores y aplicar consecuencias
 			let error = await mensajeDeError(fila, "calidad");
 			// Fin
@@ -93,7 +91,7 @@ window.addEventListener("load", async () => {
 			// Si el resultado es conocido --> ponerlo
 			let condicion = prov.siempre_castellano == 1;
 			if (condicion) v.castellanoInputs[fila].value = "1";
-			if (fila == v.filaAlta) v.castellanoInputs[fila].disabled = condicion;
+			if (fila == filaAlta) v.castellanoInputs[fila].disabled = condicion;
 			// Detectar errores y aplicar consecuencias
 			let error = await mensajeDeError(fila, "castellano");
 			// Fin
@@ -102,6 +100,7 @@ window.addEventListener("load", async () => {
 		controlesEnSubtitulosCastellano: async (fila) => {
 			// Si el resultado es conocido --> ponerlo
 			let condicion = v.castellanoInputs[fila].value == "1";
+			console.log(condicion,v.castellanoInputs[fila].value);
 			if (condicion) v.subtit_castellano[fila].value = "-";
 			v.subtit_castellano[fila].disabled = condicion;
 			// Detectar errores y aplicar consecuencias
@@ -115,7 +114,7 @@ window.addEventListener("load", async () => {
 			let condicion2 = prov.siempre_pago;
 			if (condicion1) v.gratuitoInputs[fila].value = "1";
 			else if (condicion2) v.gratuitoInputs[fila].value = "0";
-			if (fila == v.filaAlta) v.gratuitoInputs[fila].disabled = condicion1 || condicion2;
+			if (fila == filaAlta) v.gratuitoInputs[fila].disabled = condicion1 || condicion2;
 			// Detectar errores y aplicar consecuencias
 			let error = await mensajeDeError(fila, "gratuito");
 			// Fin
@@ -125,7 +124,7 @@ window.addEventListener("load", async () => {
 			// Si el resultado es conocido --> ponerlo
 			let condicion = !prov.trailer || !prov.pelicula || v.colecciones;
 			if (condicion) v.tipoInputs[fila].value = !prov.pelicula ? 1 : 2;
-			if (fila == v.filaAlta) v.tipoInputs[fila].disabled = condicion;
+			if (fila == filaAlta) v.tipoInputs[fila].disabled = condicion;
 			// Detectar errores y aplicar consecuencias
 			let error = await mensajeDeError(fila, "tipo_id");
 			// Fin
@@ -205,18 +204,18 @@ window.addEventListener("load", async () => {
 			// Si no lo reconoce, se asume el 'desconocido'
 			prov = prov ? prov : provs.find((n) => n.generico);
 			// Acciones si es la fila de altas
-			if (fila == v.filaAlta) {
+			if (fila == filaAlta) {
 				// Muestra el ícono de genérico o de OK
 				prov.generico
-					? v.provsDesconocido[v.filaAlta].classList.remove("prov_id")
-					: v.provsDesconocido[v.filaAlta].classList.add("prov_id");
+					? v.provsDesconocido[filaAlta].classList.remove("prov_id")
+					: v.provsDesconocido[filaAlta].classList.add("prov_id");
 				!prov.generico
-					? v.provsConocido[v.filaAlta].classList.remove("prov_id")
-					: v.provsConocido[v.filaAlta].classList.add("prov_id");
+					? v.provsConocido[filaAlta].classList.remove("prov_id")
+					: v.provsConocido[filaAlta].classList.add("prov_id");
 			}
 			return prov;
 		};
-		// Barrer el contenido de izquierda a derecha
+		// Barre el contenido de izquierda a derecha
 		let OK = true;
 		let autofocus = false;
 		let focoEnColumna;
@@ -249,7 +248,7 @@ window.addEventListener("load", async () => {
 		let columna = v.camposInput.indexOf(campo);
 		// Obtiene el índice
 		let indice = fila * columnas + columna;
-		// completo o n° parte
+		// Casos en los que se necesita considerar el campo anterior
 		let [campoAnt, valorAnt] =
 			campo == "subtit_castellano" || campo == "completo" || campo == "parte"
 				? [v.inputs[indice - 1].name + "=", v.inputs[indice - 1].value + "&"]
@@ -281,10 +280,11 @@ window.addEventListener("load", async () => {
 		// Si hubo un error (fila=filas), interrumpir
 		if (fila == filas) return;
 		// Si se ingresó un url en el alta, depurarlo
-		if (fila == v.filaAlta && !columna) fn.depuraUrl();
+		if (fila == filaAlta && !columna) fn.depuraUrl();
 		controlesDataEntry(fila, columna);
 	});
 
 	// Startup
+	// Revisa las ediciones y el alta
 	for (let fila = 0; fila < filas - 1; fila++) await controlesDataEntry(fila, 0);
 });
