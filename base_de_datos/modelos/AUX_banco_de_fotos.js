39c5a7f323a03950b1f5986534457caea03d2704
unknown <diegoiribarren2015@gmail.com>
2022-12-22 15:12:57 -0300

    Miscelaneas

--- base_de_datos/modelos/AUX_banco_de_fotos.js
+++ base_de_datos/modelos/AUX_banco_de_fotos.js
@@ -1,10 +1,13 @@
 module.exports = (sequelize, dt) => {
 	const alias = "banco_fotos";
 	const columns = {
-		nombre: {type: dt.STRING(30)},
 		dia_del_ano_id: {type: dt.INTEGER},
-		nombre_archivo: {type: dt.STRING(30)},
-		fecha_movil: {type: dt.BOOLEAN},
+		nombre: {type: dt.STRING(45)},
+		nombre_archivo: {type: dt.STRING(45)},
+
+		personaje_id: {type: dt.INTEGER},
+		hecho_id: {type: dt.INTEGER},
+		valor_id: {type: dt.INTEGER},
 	};
 	const config = {
 		tableName: "aux_banco_fotos",
@@ -13,6 +16,10 @@ module.exports = (sequelize, dt) => {
 	const entidad = sequelize.define(alias, columns, config);
 	entidad.associate = (n) => {
 		entidad.belongsTo(n.dias_del_ano, {as: "dia_del_ano", foreignKey: "dia_del_ano_id"});
+
+		entidad.belongsTo(n.personajes, {as: "personaje", foreignKey: "personaje_id"});
+		entidad.belongsTo(n.hechos, {as: "hecho", foreignKey: "hecho_id"});
+		entidad.belongsTo(n.valores, {as: "valor", foreignKey: "valor_id"});
 	}
 	return entidad;
 };
