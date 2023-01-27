48d8ec0754b1279e68e44dfe693dec5a7ede3219
unknown <diegoiribarren2015@gmail.com>
2023-01-27 01:19:59 -0300

    Miscelaneas

--- middlewares/usuarios/filtro-soloUsuarios.js
+++ middlewares/usuarios/filtro-soloUsuarios.js
@@ -2,7 +2,6 @@
 module.exports = (req, res, next) => {
 	// Variables
 	let usuario = req.session.usuario;
-	let url = req.originalUrl;
 	// Redirecciona si el usuario está sin login
 	if (!usuario) return res.redirect("/usuarios/redireccionar");
 
