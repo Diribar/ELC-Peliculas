a81f826dd8f491273562603f922395e7c6890705
unknown <diegoiribarren2015@gmail.com>
2023-02-01 19:16:15 -0300

    Miscelaneas

--- rutas_y_controladores/3-RevisionEnts/RE-ControlAPI.js
+++ rutas_y_controladores/3-RevisionEnts/RE-ControlAPI.js
@@ -87,7 +87,7 @@ module.exports = {
 		if (!decisAprob && (creado || recuperar)) {
 			motivo_id = req.query.motivo_id;
 			motivo = await BD_genericas.obtienePorId("altas_motivos_rech", motivo_id);
-			comp.usuarioAumentaPenaliz(sugerido_por_id, motivo,"links");
+			comp.usuarioAumentaPenaliz(sugerido_por_id, motivo, "links");
 		}
 
 		// LINK - Pasa a status aprobado/rechazado
