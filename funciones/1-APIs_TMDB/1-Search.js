af3787a2bd1aa7468aed2a01104d224a90e0f365
diribar <diegoiribarren2015@gmail.com>
2023-02-15 10:33:05 -0300

    Miscelaneas

--- funciones/1-APIs_TMDB/1-Search.js
+++ funciones/1-APIs_TMDB/1-Search.js
@@ -1,14 +1,15 @@
-"use strict"
+"use strict";
 // ************ Requires ************
 const API_key = process.env.API_key;
 const fetch = require("node-fetch");
 
 module.exports = async (palabrasClave, TMDB_entidad, page) => {
 	// PARTES DEL URL
-	// "https://api.themoviedb.org/4/search/movie      ?api_key=&language=es&query=karol%20uomo%20papa&page=1&include_adult=false"
+	// "https://api.themoviedb.org/3/search/movie      ?api_key=&language=es&query=karol%20uomo%20papa&page=1&include_adult=false"
 	// "https://api.themoviedb.org/3/search/collection ?api_key=&language=es&query=karol%20uomo%20papa&page=1
-	// "https://api.themoviedb.org/4/search/tv         ?api_key=&language=es&query=karol%20uomo%20papa&page=1&include_adult=false"
-	let version = TMDB_entidad != "collection" ? 4 : 3;
+	// "https://api.themoviedb.org/3/search/tv         ?api_key=&language=es&query=karol%20uomo%20papa&page=1&include_adult=false"
+	// let version = TMDB_entidad != "collection" ? 4 : 3;
+	let version = 3;
 	let url =
 		"https://api.themoviedb.org/" +
 		version +
@@ -23,7 +24,7 @@ module.exports = async (palabrasClave, TMDB_entidad, page) => {
 		"&include_adult=false";
 	// BUSCAR LA INFO
 	let resultado = await fetch(url).then((n) => n.json());
-	if (resultado.hasOwnProperty('success') && !resultado.success) {
+	if (resultado.hasOwnProperty("success") && !resultado.success) {
 		console.log("searchTMDB_fetch", url, resultado);
 		resultado = {
 			page: 1,
