7e160318d0ede7a6a1c26147b2876ad2eec91e4c
diribar <diegoiribarren2015@gmail.com>
2023-02-15 18:54:02 -0300

    ID > 10 para RCLV repetidos

--- rutas_y_controladores/2.2-RCLV-CRUD/RCLV-ControlAPI.js
+++ rutas_y_controladores/2.2-RCLV-CRUD/RCLV-ControlAPI.js
@@ -8,10 +8,10 @@ module.exports = {
 	registrosConEsaFecha: async (req, res) => {
 		let {entidad, mes_id, dia, id} = req.query;
 		let objeto = {mes_id, dia};
-		let dia_del_ano_id =
-			dia != "0" ? dias_del_ano.find((n) => n.dia == objeto.dia && n.mes_id == objeto.mes_id).id : 400;
+		let dia_del_ano_id = dia != "0" ? dias_del_ano.find((n) => n.dia == objeto.dia && n.mes_id == objeto.mes_id).id : 400;
 		let casos = await BD_genericas.obtieneTodosPorCampos(entidad, {dia_del_ano_id})
 			.then((n) => n.filter((m) => m.id != id))
+			.then((n) => n.filter((m) => m.id > 10))
 			.then((n) => n.map((m) => m.nombre));
 		return res.json(casos);
 	},
