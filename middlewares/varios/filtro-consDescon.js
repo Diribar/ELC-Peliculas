387009ea23c4290f27249c3fe4a6cdcd2d80faca
diribar <diegoiribarren2015@gmail.com>
2023-02-18 13:53:55 -0300

    Miscelaneas

--- middlewares/varios/filtro-consDescon.js
+++ middlewares/varios/filtro-consDescon.js
@@ -6,10 +6,10 @@ module.exports = async (req, res, next) => {
 	// Variables
 	let layoutElegido = req.path.replace("/", "");
 
-	// Acciones si la opción elegida no existe
+	// Acciones si no se reconoce la consulta elegida
 	if (!variables.layouts.find((n) => n.url == layoutElegido)) {
 		let informacion = {
-			mensajes: variables.urlDesconocida,
+			mensajes: [variables.urlDesconocida],
 			iconos: [variables.vistaAnterior(req.session.urlAnterior), variables.vistaInicio],
 		};
 		return res.render("CMP-0Estructura", {informacion});
