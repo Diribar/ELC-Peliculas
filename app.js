dd418849153aec878401e7f4e65598792b7b55d1
diribar <diegoiribarren2015@gmail.com>
2023-02-18 14:03:24 -0300

    Miscelaneas

--- app.js
+++ app.js
@@ -4,12 +4,12 @@ global.unDia = 60 * 60 * 1000 * 24; // Para usar la variable en todo el proyecto
 global.unMes = 60 * 60 * 1000 * 24 * 30; // Para usar la variable en todo el proyecto
 
 // REQUIRES Y MIDDLEWARES DE APLICACIÓN ------------------------------------------
-require("dotenv").config();
-const comp = require("./funciones/3-Procesos/Compartidas");
 // Para usar el archivo '.env' --> se debe colocar al principio
+require("dotenv").config();
 global.localhost = process.env.localhost;
 // Averigua la fecha de la 'Línea de Cambio de Fecha'
 global.horarioLCF = null;
+const comp = require("./funciones/3-Procesos/Compartidas");
 comp.horarioLCF();
 // Para usar propiedades de express
 const express = require("express");
