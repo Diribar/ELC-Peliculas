82c52a67be75a85faed21c12bd5b0b08b6f4f2d2
diribar <diegoiribarren2015@gmail.com>
2023-02-27 18:14:00 -0300

    Miscelaneas

--- rutas_y_controladores/2.1-Prod-RUD/PR-ControlAPI.js
+++ rutas_y_controladores/2.1-Prod-RUD/PR-ControlAPI.js
@@ -2,7 +2,6 @@
 // ************ Requires *************
 const BD_genericas = require("../../funciones/2-BD/Genericas");
 const comp = require("../../funciones/3-Procesos/Compartidas");
-const variables = require("../../funciones/3-Procesos/Variables");
 const procsCRUD = require("../2.0-Familias-CRUD/FM-Procesos");
 const valida = require("./PR-FN-Validar");
 
