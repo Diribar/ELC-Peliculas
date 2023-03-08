4037a10db6ec6aface1f30310dbfd4e45967f189
diribar <diegoiribarren2015@gmail.com>
2023-03-08 10:17:37 -0300

    Mejora continua

--- publico/javascript/2.3-Links-CRUD/LK0-PeliTrailer.js
+++ publico/javascript/2.3-Links-CRUD/LK0-PeliTrailer.js
@@ -1,16 +1,16 @@
 "use strict";
 window.addEventListener("load", () => {
 	// Variables
-	let selectTipo = document.querySelector("select#tipoLink");
+	let peliculaTrailer = document.querySelector("select#peliculaTrailer");
 	let filasExistentes = document.querySelectorAll("tr.yaExistentes");
 	let filasEdicionesCRUD = document.querySelectorAll("tr.edicion.crud");
 	let filasEdicionesRevisar = document.querySelectorAll("tr.edicion.revisar");
 	let tipoAltas = document.querySelector("tr.alta .tipo select[name='tipo_id']");
 
 	// Add Event Listener
-	selectTipo.addEventListener("change", () => {
+	peliculaTrailer.addEventListener("change", () => {
 		// Obtiene el valor
-		let valor = selectTipo.value;
+		let valor = peliculaTrailer.value;
 		// Ocultar y mostrar las filas que correspondan
 		if (filasExistentes.length)
 			filasExistentes.forEach((filaExistente, fila) => {
