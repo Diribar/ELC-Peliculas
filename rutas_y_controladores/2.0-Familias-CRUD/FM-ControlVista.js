13a5884c3e31598697e0f6476d1545641359ab8e
diribar <diegoiribarren2015@gmail.com>
2023-03-09 17:47:33 -0300

    Solucion de errores

---
+++ rutas_y_controladores/2.0-Familias-CRUD/FM-ControlVista.js
@@ -0,0 +1,12 @@
+"use strict";
+// ************ Requires *************
+const BD_genericas = require("../../funciones/2-BD/Genericas");
+const BD_especificas = require("../../funciones/2-BD/Especificas");
+
+// *********** Controlador ***********
+module.exports = {
+	inactivar: (req, res) => {
+		
+	},
+	recuperar: (req, res) => {},
+};
--- rutas_y_controladores/2.0-Familias-CRUD/Rutas.js
+++ rutas_y_controladores/2.0-Familias-CRUD/Rutas.js
@@ -3,14 +3,19 @@
 const express = require("express");
 const router = express.Router();
 const API = require("./FM-ControlAPI");
+const vista = require("./FM-ControlVista");
 
 //************************ Rutas ****************************
 // Rutas de APIs
 // Tridente: Detalle, Edición, Links
-router.get("/obtiene-col-cap", API.obtieneColCap);
-router.get("/obtiene-cap-ant-y-post", API.obtieneCapAntPostID);
-router.get("/obtiene-cap-id", API.obtieneCapID);
-router.get("/averigua-capitulos", API.obtieneCapitulos);
+router.get("/api/obtiene-col-cap", API.obtieneColCap);
+router.get("/api/obtiene-cap-ant-y-post", API.obtieneCapAntPostID);
+router.get("/api/obtiene-cap-id", API.obtieneCapID);
+router.get("/api/averigua-capitulos", API.obtieneCapitulos);
+
+// Rutas de vistas
+router.get("/inactivar", vista.inactivar);
+router.get("/recuperar", vista.recuperar);
 
 // Fin
 module.exports = router;
