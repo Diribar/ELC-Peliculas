8cc193b90559515933623aa6f0f5d4a384622712
diribar <diegoiribarren2015@gmail.com>
2023-03-09 21:24:58 -0300

    Miscelaneas

--- base_de_datos/modelos/RCLV_2hechos.js
+++ base_de_datos/modelos/RCLV_2hechos.js
@@ -3,8 +3,7 @@ module.exports = (sequelize, dt) => {
 	const columns = {
 		nombre: {type: dt.STRING(30)},
 		prods_aprob: {type: dt.INTEGER},
-		avatar_fijo_id: {type: dt.INTEGER},
-		avatar_movil_id: {type: dt.INTEGER},
+		avatar: {type: dt.STRING(15)},
 
 		dia_del_ano_id: {type: dt.INTEGER},
 		ano: {type: dt.INTEGER},
@@ -55,9 +54,6 @@ module.exports = (sequelize, dt) => {
 		entidad.belongsTo(n.altas_motivos_rech, {as: "motivo", foreignKey: "motivo_id"});
 		entidad.belongsTo(n.usuarios, {as: "sugerido_por", foreignKey: "sugerido_por_id"});
 		entidad.belongsTo(n.usuarios, {as: "capturado_por", foreignKey: "capturado_por_id"});
-
-		entidad.belongsTo(n.imagenes_fijo, {as: "avatar_fijo", foreignKey: "avatar_fijo_id"});
-		entidad.belongsTo(n.imagenes_movil, {as: "avatar_movil", foreignKey: "avatar_movil_id"});
 
 		entidad.hasMany(n.peliculas, {as: "peliculas", foreignKey: "hecho_id"});
 		entidad.hasMany(n.colecciones, {as: "colecciones", foreignKey: "hecho_id"});
