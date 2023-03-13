01d804fc5e531ecb9442a24f2c136d4e8ad213ee
diribar <diegoiribarren2015@gmail.com>
2023-03-13 15:30:15 -0300

    Estandarizacion

--- middlewares/varios/locals.js
+++ middlewares/varios/locals.js
@@ -2,7 +2,7 @@
 module.exports = async (req, res, next) => {
 	// Guarda localhost
 	// if (!res.locals.localhost) res.locals.localhost = global.localhost;
-	if (!res.locals.titulosImgDer) res.locals.titulosImgDer = global.titulosImgDer;
+	// if (!res.locals.titulosImgDer) res.locals.titulosImgDer = global.titulosImgDer;
 
 	// Fin
 	next();
