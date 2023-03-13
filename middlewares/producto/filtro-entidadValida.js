b897d74d3da93503b0394199d32c76dad3b4d8d7
diribar <diegoiribarren2015@gmail.com>
2023-03-13 17:26:34 -0300

    Miscelaneas

--- middlewares/producto/filtro-entidadValida.js
+++ middlewares/producto/filtro-entidadValida.js
@@ -20,7 +20,6 @@ module.exports = (req, res, next) => {
 			producto: ["/links/", "/crud/", familia1],
 			rclv: ["/crud/", familia1],
 		};
-		// console.log(23,familia1,familia2);
 		if (!familia1 || !rutasPorFamilia[familia1].some((n) => familia2.includes(n) || familia2.startsWith(n)))
 			informacion = {
 				mensajes: ["La entidad ingresada es inválida."],
