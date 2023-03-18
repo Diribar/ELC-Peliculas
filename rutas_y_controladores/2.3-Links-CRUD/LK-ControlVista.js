63865fd06a7dd3182541f37d0d4cf20dc9b652e7
diribar <diegoiribarren2015@gmail.com>
2023-03-18 00:09:26 -0300

    Miscelaneas

--- rutas_y_controladores/2.3-Links-CRUD/LK-ControlVista.js
+++ rutas_y_controladores/2.3-Links-CRUD/LK-ControlVista.js
@@ -6,7 +6,6 @@ const comp = require("../../funciones/3-Procesos/Compartidas");
 const procsCRUD = require("../2.0-Familias-CRUD/FM-Procesos");
 const procesos = require("./LK-FN-Procesos");
 const variables = require("../../funciones/3-Procesos/Variables");
-const {constant} = require("async");
 
 // *********** Controlador ***********
 module.exports = {
