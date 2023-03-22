7e26f3cffd00b9727526a4e893d3e60ebd39bec2
diribar <diegoiribarren2015@gmail.com>
2023-03-21 23:57:27 -0300

    Filtros start-up

--- publico/javascript/6-Consultas/CN-FiltrosDinamica.js
+++ publico/javascript/6-Consultas/CN-FiltrosDinamica.js
@@ -8,13 +8,23 @@ window.addEventListener("load", () => {
 	// Novedad en algún lado
 
 		// 1. Si la novedad está en el filtro personalizado
+			// Si el filtro personalizado es el Estándar, inactiva los íconos 'modificar nombre' y 'eliminar'
+			// De lo contrario, 'des-inactiva' los iconos 'modificar nombre' y 'eliminar'
+			// Inactiva las opciones 'reiniciar' y 'actualizar'
+
 			// Obtiene las opciones de la BD y actualiza los filtros
 
 		// 2. Si la novedad está en cualquier otro campo...
-			// Inactiva las opciones de 'Modificar nombre' y 'Eliminar' en la vista
+			// Si el filtro personalizado es el Estándar
+			// Desaparece el nombre de la vista e inactiva todos los íconos menos 'nuevo'
 
-			// Si el filtro personalizado es el Estándar, desaparece el nombre de la vista
-
+			// De lo contrario, 
+				// Si el filtro personalizado es uno definido,
+					// Inactiva la opcion de 'Modificar nombre' en la vista
+					// Activa la opcion de recargar y guardar
+				// De lo contrario, no hace nada
+				
+		
 		// 3. Comunes
 			// Obtiene los valores de todos los filtros elegidos
 
