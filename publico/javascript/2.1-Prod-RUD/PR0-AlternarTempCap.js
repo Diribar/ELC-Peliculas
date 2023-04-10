71f3a3ce2af8c87fd7126291f7ba61417549f8a8
diribar <diegoiribarren2015@gmail.com>
2023-04-10 15:45:12 -0300

    Capitulo ant/sig en /Revision/Links

--- publico/javascript/2.1-Prod-RUD/PR0-AlternarTempCap.js
+++ publico/javascript/2.1-Prod-RUD/PR0-AlternarTempCap.js
@@ -55,37 +55,4 @@ window.addEventListener("load", async () => {
 			"&origen=" +
 			origen;
 	});
-
-	// CAPÍTULOS ANTERIOR O POSTERIOR
-	// Obtiene el ID del capítulo anterior y del posterior
-	ruta = "/crud/api/obtiene-cap-ant-y-post/?id=";
-	let [capAntID, capPostID] = await fetch(ruta + prodID).then((n) => n.json());
-	// Acción si se elije "capítulo anterior"
-	let botonCapAnt = document.querySelector("#cuerpo #encabezado .fa-circle-left");
-	if (capAntID) {
-		botonCapAnt.classList.remove("inactivo");
-		botonCapAnt.addEventListener("click", () => {
-			location.href =
-				"/inactivar-captura/?entidad=capitulos&id=" +
-				prodID +
-				"&prodEntidad=capitulos&prodID=" +
-				capAntID +
-				"&origen=" +
-				origen;
-		});
-	} else botonCapAnt.classList.add("inactivo");
-	// Acción si se elije "capítulo posterior"
-	let botonCapPost = document.querySelector("#cuerpo #encabezado .fa-circle-right");
-	if (capPostID) {
-		botonCapPost.classList.remove("inactivo");
-		botonCapPost.addEventListener("click", () => {
-			location.href =
-				"/inactivar-captura/?entidad=capitulos&id=" +
-				prodID +
-				"&prodEntidad=capitulos&prodID=" +
-				capPostID +
-				"&origen=" +
-				origen;
-		});
-	} else botonCapPost.classList.add("inactivo");
 });
--- rutas_y_controladores/9-Miscelaneas/MS-ControlVista.js
+++ rutas_y_controladores/9-Miscelaneas/MS-ControlVista.js
@@ -18,26 +18,33 @@ module.exports = {
 		let {origen, prodEntidad, prodID, entidad, id, urlDestino} = req.query;
 		// return res.send(req.query)
 		// Si es 'tablero', ir a tablero
-		let destino =
+		let destino = false
+			? null
+			: // Producto
 			origen == "DA"
-				? "/producto/agregar/datos-adicionales"
-				: origen == "DTP"
-				? "/producto/detalle/?entidad=" + (prodEntidad ? prodEntidad : entidad) + "&id=" + (prodID ? prodID : id)
-				: origen == "EDP"
-				? "/producto/edicion/?entidad=" + (prodEntidad ? prodEntidad : entidad) + "&id=" + (prodID ? prodID : id)
-				: origen == "LK"
-				? "/links/abm/?entidad=" + (prodEntidad ? prodEntidad : entidad) + "&id=" + (prodID ? prodID : id)
-				: origen == "DTR"
-				? "/rclv/detalle/?entidad=" + entidad + "&id=" + id
-				: origen == "TE"
-				? "/revision/tablero-de-control"
-				: origen == "TU"
-				? "/revision/usuarios/tablero-de-control"
-				: origen == "MT"
-				? "/mantenimiento"
-				: urlDestino
-				? urlDestino
-				: "/";
+			? "/producto/agregar/datos-adicionales"
+			: origen == "DTP"
+			? "/producto/detalle/?entidad=" + (prodEntidad ? prodEntidad : entidad) + "&id=" + (prodID ? prodID : id)
+			: origen == "EDP"
+			? "/producto/edicion/?entidad=" + (prodEntidad ? prodEntidad : entidad) + "&id=" + (prodID ? prodID : id)
+			: origen == "LK"
+			? "/links/abm/?entidad=" + (prodEntidad ? prodEntidad : entidad) + "&id=" + (prodID ? prodID : id)
+			: // RCLV
+			origen == "DTR"
+			? "/rclv/detalle/?entidad=" + entidad + "&id=" + id
+			: // Revisión
+			origen == "TE"
+			? "/revision/tablero-de-control"
+			: origen == "RLK"
+			? "/revision/links/?entidad=" + (prodEntidad ? prodEntidad : entidad) + "&id=" + (prodID ? prodID : id)
+			: // Otros
+			origen == "TU"
+			? "/revision/usuarios/tablero-de-control"
+			: origen == "MT"
+			? "/mantenimiento"
+			: urlDestino
+			? urlDestino
+			: "/";
 		// Redireccionar a la vista que corresponda
 		return res.redirect(destino);
 	},
