f7dd8884684e97aa86bc5498650f917caab494e8
diribar <diegoiribarren2015@gmail.com>
2023-04-28 17:53:04 -0300

    Mejora continua

--- publico/javascript/2.2-RCLVs-CRUD/RCLV-AltaEdicion.js
+++ publico/javascript/2.2-RCLVs-CRUD/RCLV-AltaEdicion.js
@@ -764,7 +764,7 @@ window.addEventListener("load", async () => {
 			await validacs.fecha();
 
 			// Impactos en repetidos
-			if (varios.OK.fecha && ["mes_id", "dia"].includes(campo)) {
+			if (varios.OK.fecha && ["mes_id", "dia", "tipoFecha"].includes(campo)) {
 				await impactos.fecha.muestraPosiblesRepetidos();
 				validacs.repetido();
 			}
