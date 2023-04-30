1c1924c584bbfcca4d194766ac6b3c630d55c224
diribar <diegoiribarren2015@gmail.com>
2023-04-30 02:00:23 -0300

    Miscelaneas

--- rutas_y_controladores/3-RevisionEnts/RE-ControlVista.js
+++ rutas_y_controladores/3-RevisionEnts/RE-ControlVista.js
@@ -409,7 +409,6 @@ module.exports = {
 		// 1. PROCESOS PARTICULARES PARA AVATAR
 		await procesos.edicion.procsParticsAvatar({entidad, original, edicion, aprob});
 		delete edicion.avatar_url;
-		console.log(412);
 
 		// 2. PROCESOS COMUNES A TODOS LOS CAMPOS
 		[edicion] = await procesos.edicion.edicAprobRech({entidad, original, edicion, revID, campo, aprob, motivo_id});
