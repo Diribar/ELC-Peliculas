6c1ffdfdf431d97d48c5031cfbaf9a6ee9130e95
diribar <diegoiribarren2015@gmail.com>
2023-05-09 15:25:08 -0300

    Miscelaneas

--- middlewares/usuarios/filtro-usSt2-MailVal.js
+++ middlewares/usuarios/filtro-usSt2-MailVal.js
@@ -5,8 +5,8 @@ module.exports = (req, res, next) => {
 	// Redirecciona si el usuario está en alguna de esta situaciones:
 	// - Sin login
 	// - En un status distinto a 'mail_validado'
-	
+
 	// Fin
-	if (!usuario || !usuario.status_registro.mail_validado)  res.redirect("/usuarios/garantiza-login-y-completo");
+	if (!usuario || !usuario.status_registro.mail_validado) res.redirect("/usuarios/garantiza-login-y-completo");
 	else next();
 };
