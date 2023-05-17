1896f8ac12c22d44bc7fa5ddb21ca1979a73bb23
diribar <diegoiribarren2015@gmail.com>
2023-05-17 13:09:00 -0300

    Mejora continua

--- base_de_datos/modelos/AUX_hist_ediciones.js
+++ base_de_datos/modelos/AUX_hist_ediciones.js
@@ -1,11 +1,15 @@
 module.exports = (sequelize, dt) => {
-	const alias = "edics_aprob";
+	const alias = "ediciones";
 	const columns = {
 		entidad: {type: dt.STRING(20)},
 		entidad_id: {type: dt.INTEGER},
 		campo: {type: dt.STRING(25)},
 		titulo: {type: dt.STRING(35)},
+		valorRech: {type: dt.STRING(100)},
 		valorAprob: {type: dt.STRING(100)},
+
+		motivo_id: {type: dt.INTEGER},
+		duracion: {type: dt.DECIMAL},
 
 		sugerido_por_id: {type: dt.INTEGER},
 		sugerido_en: {type: dt.DATE},
@@ -14,11 +18,12 @@ module.exports = (sequelize, dt) => {
 		comunicado_en: {type: dt.DATE},
 	};
 	const config = {
-		tableName: "edics_aprob",
+		tableName: "aux_hist_ediciones",
 		timestamps: false,
 	};
 	const entidad = sequelize.define(alias, columns, config);
 	entidad.associate = (n) => {
+		entidad.belongsTo(n.motivos_rech_edic, {as: "motivo", foreignKey: "motivo_id"});
 		entidad.belongsTo(n.usuarios, {as: "editado_por", foreignKey: "sugerido_por_id"});
 		entidad.belongsTo(n.usuarios, {as: "revisada_por", foreignKey: "revisado_por_id"});	
 	};
