8549d2da072e83bd2e760224875c553aeb9dc8bb
diribar <diegoiribarren2015@gmail.com>
2023-05-22 16:18:41 -0300

    Mejora continua

---
+++ publico/javascript/0-Compartido/TableroControl.js
@@ -0,0 +1,57 @@
+"use strict";
+window.addEventListener("load", async () => {
+	console.log("SI");
+	// Variables
+	let DOM = {
+		desplegar: document.querySelectorAll(".bloques .temas .desplegar"),
+		replegar: document.querySelectorAll(".bloques .temas .replegar"),
+		bloqueRegistros: document.querySelectorAll(".bloques .temas .bloqueRegistros"),
+	};
+	let varios = {
+		alturas: [],
+	};
+
+	// Fórmulas - Obtiene las alturas
+	for (let i = 0; i < DOM.bloqueRegistros.length; i++) {
+		DOM.bloqueRegistros[i].classList.remove("ocultar");
+		varios.alturas[i] = DOM.bloqueRegistros[i].offsetHeight;
+		DOM.bloqueRegistros[i].style.height = 0;
+	}
+
+	// Eventos - Desplegar
+	DOM.desplegar.forEach((desplegar, i) => {
+		desplegar.addEventListener("click", () => {
+			// Oculta el ícono y muestra el de replegar
+			desplegar.classList.add("ocultar");
+			DOM.replegar[i].classList.remove("ocultar");
+
+			// Despliega
+			let altura = 0;
+			DOM.bloqueRegistros[i].style.height = altura;
+			DOM.bloqueRegistros[i].classList.remove("ocultar");
+			console.log(varios.alturas[i]);
+			while (altura < varios.alturas[i]) {
+				// Cambia la altura
+				DOM.bloqueRegistros[i].style.height = altura + "px";
+				altura++
+			}
+		});
+	});
+	// Eventos - Replegar
+	DOM.replegar.forEach((replegar, i) => {
+		replegar.addEventListener("click", () => {
+			// Oculta el ícono y muestra el de replegar
+			replegar.classList.add("ocultar");
+			DOM.desplegar[i].classList.remove("ocultar");
+
+			// Despliega
+			let altura = varios.alturas[i];
+			DOM.bloqueRegistros[i].style.height = altura;
+			DOM.bloqueRegistros[i].classList.remove("ocultar");
+			while (altura >= 0) {
+				DOM.bloqueRegistros[i].style.height = altura + "px";
+				altura--;
+			}
+		});
+	});
+});
