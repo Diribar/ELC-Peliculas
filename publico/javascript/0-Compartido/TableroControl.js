f0a524e118c18c1f0c5d5969396e65a0a2861b1a
diribar <diegoiribarren2015@gmail.com>
2023-05-24 14:36:54 -0300

    Avance del proyecto

--- publico/javascript/0-Compartido/TableroControl.js
+++ publico/javascript/0-Compartido/TableroControl.js
@@ -5,12 +5,21 @@ window.addEventListener("load", async () => {
 		mostrarOcultar: document.querySelectorAll(".bloques .temas h3"),
 		bloqueRegistros: document.querySelectorAll(".bloques .temas .bloqueRegistros"),
 	};
+	let ruta = {
+		actualizarVisibles: "/crud/api/actualizar-visibles/?circuito=",
+	};
+	let circuito = location.pathname.slice(1);
+	const indice = circuito.indexOf("/");
+	if (indice != -1) circuito = circuito.slice(0, indice);
 
 	// Muestra u oculta
 	DOM.mostrarOcultar.forEach((mostrarOcultar, i) => {
 		// Revisa para cada bloque
 		mostrarOcultar.addEventListener("click", () => {
 			DOM.bloqueRegistros[i].classList.toggle("ocultar");
+			const desplegar = !DOM.bloqueRegistros[i].className.includes("ocultar");
+			console.log(ruta.actualizarVisibles + circuito + "&desplegar=" + desplegar);
+			fetch(ruta.actualizarVisibles + circuito + "&desplegar=" + desplegar);
 		});
 	});
 });
