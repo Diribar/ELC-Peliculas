8ee4fde71607cfd321e4da4d5ee835ce55d452df
diribar <diegoiribarren2015@gmail.com>
2023-05-24 15:06:15 -0300

    Avance del proyecto

--- publico/javascript/0-Compartido/TableroControl.js
+++ publico/javascript/0-Compartido/TableroControl.js
@@ -5,8 +5,9 @@ window.addEventListener("load", async () => {
 		mostrarOcultar: document.querySelectorAll(".bloques .temas h3"),
 		bloqueRegistros: document.querySelectorAll(".bloques .temas .bloqueRegistros"),
 	};
+
 	let ruta = {
-		actualizarVisibles: "/crud/api/actualiza-visibles/?circuito=",
+		actualizarVisibles: "/crud/api/actualiza-visibles/?datos=",
 	};
 	let circuito = location.pathname.slice(1);
 	const indice = circuito.indexOf("/");
@@ -16,10 +17,18 @@ window.addEventListener("load", async () => {
 	DOM.mostrarOcultar.forEach((mostrarOcultar, i) => {
 		// Revisa para cada bloque
 		mostrarOcultar.addEventListener("click", () => {
+			// Muestra u oculta el bloque de registros
 			DOM.bloqueRegistros[i].classList.toggle("ocultar");
+
+			// Genera los datos a exportar
+			const familia = DOM.mostrarOcultar[i].parentNode.parentNode.children[0].children[0].innerText.toLowerCase();
+			let titulo = DOM.mostrarOcultar[i].innerHTML.toLowerCase();
+			titulo = titulo.slice(0, titulo.indexOf("(") - 1);
 			const desplegar = !DOM.bloqueRegistros[i].className.includes("ocultar");
-			console.log(ruta.actualizarVisibles + circuito + "&desplegar=" + desplegar);
-			fetch(ruta.actualizarVisibles + circuito + "&desplegar=" + desplegar);
+			const datos = {circuito, familia, titulo, desplegar};
+
+			// Exporta los datos
+			fetch(ruta.actualizarVisibles + JSON.stringify(datos));
 		});
 	});
 });
