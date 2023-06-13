8b80cec3744ca838e3da853a65512df4747a3989
diribar <diegoiribarren2015@gmail.com>
2023-06-13 14:46:46 -0300

    Miscelaneas

--- rutas_y_controladores/3-RevisionEnts/RE-Procesos.js
+++ rutas_y_controladores/3-RevisionEnts/RE-Procesos.js
@@ -628,7 +628,7 @@ module.exports = {
 			delete edicion[campo];
 			if (relacInclude) delete edicion[relacInclude]; // Es necesario eliminarla para que no la compare
 
-			// 7. Averigua si no quedan campos, y en ese caso elimina el registro de edición
+			// 7. Disminuye la edición a su mínima expresión, y si corresponde la elimina
 			edicion = await procsCRUD.puleEdicion(entidad, originalGuardado, edicion);
 
 			// Fin
