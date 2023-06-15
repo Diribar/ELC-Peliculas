a8fd2f2a4fcabd1aef83feaf6919aa3ddcaabca9
diribar <diegoiribarren2015@gmail.com>
2023-06-15 14:37:53 -0300

    Miscelaneas

--- funciones/3-Rutinas/RT-Control.js
+++ funciones/3-Rutinas/RT-Control.js
@@ -42,6 +42,7 @@ module.exports = {
 		await this.conjuntoDeRutinasDiarias();
 		await this.conjuntoDeRutinasSemanales();
 		// this.epoca();
+		// this.BorraImagenesSinRegistro()
 	},
 	epoca: async () => {
 		// Variables
