2161cff332ddc4ad2fe4b299e5d11a4537f78ec3
diribar <diegoiribarren2015@gmail.com>
2023-06-19 18:12:42 -0300

    Miscelaneas

--- rutas_y_controladores/3-RevisionEnts/RE-ControlAPI.js
+++ rutas_y_controladores/3-RevisionEnts/RE-ControlAPI.js
@@ -35,14 +35,14 @@ module.exports = {
 		// Obtiene la versión original con include
 		const entID = entidad == "links" ? edicion.link_id : req.query.id;
 		const original = await BD_genericas.obtienePorIdConInclude(entidad, entID, [...include, "statusRegistro"]);
-		const originalGuardado = aprob ? {...original, [campo]: edicion[campo]} : {...original};
-
+		
 		// Realiza muchísimas tareas y obtiene la edición en su mínima expresión
 		const objeto = {entidad, original, edicion, revID, campo, aprob, motivo_id};
 		edicion = await procesos.edicion.edicAprobRech(objeto);
-
+		
 		// Acciones si se terminó de revisar la edición
 		if (!edicion) {
+			const originalGuardado = aprob ? {...original, [campo]: edicion[campo]} : {...original};
 			let edicsEliminadas = procsCRUD.revisiones.eliminaDemasEdiciones({entidad, original: originalGuardado, id: entID});
 			statusAprob = procsCRUD.revisiones.statusAprob({entidad, registro: originalGuardado});
 			[statusAprob, edicsEliminadas] = await Promise.all([statusAprob, edicsEliminadas]);
