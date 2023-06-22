66ad5ddbe67bd6f6c046927602810be68f68b639
diribar <diegoiribarren2015@gmail.com>
2023-06-22 14:39:03 -0300

    Mejora de procesos

--- rutas_y_controladores/2.1-Prod-Agregar/PA-FN-Desambiguar.js
+++ rutas_y_controladores/2.1-Prod-Agregar/PA-FN-Desambiguar.js
@@ -7,8 +7,66 @@ const variables = require("../../funciones/1-Procesos/Variables");
 
 module.exports = {
 	movie: {
-		obtieneInfo: async (datos) => {
-			return await obtieneInfo_Movie(datos);
+		obtieneInfo: async function (datos) {
+			// Variables
+			datos = {...datos, fuente: "TMDB", TMDB_entidad: "movie"}; // La entidad puede ser 'peliculas' o 'capitulos', y se agrega más adelante
+
+			// Obtiene las API
+			let detalles = APIsTMDB.details("movie", datos.TMDB_id);
+			let creditos = APIsTMDB.credits("movie", datos.TMDB_id);
+			let datosAPI = await Promise.all([detalles, creditos]).then(([a, b]) => ({...a, ...b}));
+
+			// Procesa la información
+			if (Object.keys(datosAPI).length) datos = {...datos, ...this.datosPelis(datosAPI)};
+
+			// Limpia el resultado de caracteres especiales
+			const avatar = datos.avatar;
+			datos = comp.convierteLetras.alCastellano(datos);
+			if (avatar) datos.avatar = avatar;
+
+			// Fin
+			return datos;
+		},
+		datosPelis: (datosAPI) => {
+			// Variables
+			let datos = {};
+
+			// Procesa la información
+			if (!datosAPI.belongs_to_collection) {
+				datos.entidadNombre = "Película";
+				datos.entidad = "peliculas";
+			}
+			// IMDB_id, nombreOriginal, nombreCastellano
+			if (datosAPI.imdb_id) datos.IMDB_id = datosAPI.imdb_id;
+			if (datosAPI.original_title) datos.nombreOriginal = datosAPI.original_title;
+			if (datosAPI.title) datos.nombreCastellano = datosAPI.title;
+			// Idioma
+			if (datosAPI.original_language) datos.idiomaOriginal_id = datosAPI.original_language;
+
+			// año de estreno, duración, país de origen
+			if (datosAPI.release_date) datos.anoEstreno = parseInt(datosAPI.release_date.slice(0, 4));
+			if (datosAPI.runtime) datos.duracion = datosAPI.runtime;
+			if (datosAPI.production_countries.length > 0)
+				datos.paises_id = datosAPI.production_countries.map((n) => n.iso_3166_1).join(" ");
+			// sinopsis, avatar
+			if (datosAPI.overview) datos.sinopsis = fuenteSinopsisTMDB(datosAPI.overview);
+			if (datosAPI.poster_path) datos.avatar = "https://image.tmdb.org/t/p/original" + datosAPI.poster_path;
+			// Producción
+			if (datosAPI.production_companies.length > 0) datos.produccion = limpiaValores(datosAPI.production_companies);
+			// Crew
+			if (datosAPI.crew.length > 0) {
+				const direccion = limpiaValores(datosAPI.crew.filter((n) => n.department == "Directing"));
+				if (direccion) datos.direccion = direccion;
+				const guion = limpiaValores(datosAPI.crew.filter((n) => n.department == "Writing"));
+				if (guion) datos.guion = guion;
+				const musica = limpiaValores(datosAPI.crew.filter((n) => n.department == "Sound"));
+				if (musica) datos.musica = musica;
+			}
+			// Cast
+			if (datosAPI.cast.length > 0) datos.actores = FN_actores(datosAPI.cast);
+
+			// Fin
+			return datos;
 		},
 	},
 	collection: {
@@ -182,71 +240,8 @@ module.exports = {
 			return datos;
 		},
 	},
-	prodsIMFA:(palabrasClave)=>{},
-	ordenaProdsYaEnBD:(prodsYaEnBD)=>{},
-};
-
-// Funciones de movies
-let obtieneInfo_Movie = async (datos) => {
-	// Variables
-	datos = {...datos, fuente: "TMDB", TMDB_entidad: "movie"}; // La entidad puede ser 'peliculas' o 'capitulos', y se agrega más adelante
-
-	// Obtiene las API
-	let detalles = APIsTMDB.details("movie", datos.TMDB_id);
-	let creditos = APIsTMDB.credits("movie", datos.TMDB_id);
-	let datosAPI = await Promise.all([detalles, creditos]).then(([a, b]) => ({...a, ...b}));
-
-	// Procesa la información
-	if (Object.keys(datosAPI).length) datos = {...datos, ...datosPelis(datosAPI)};
-
-	// Limpia el resultado de caracteres especiales
-	const avatar = datos.avatar;
-	datos = comp.convierteLetras.alCastellano(datos);
-	if (avatar) datos.avatar = avatar;
-
-	// Fin
-	return datos;
-};
-let datosPelis = (datosAPI) => {
-	// Variables
-	let datos = {};
-
-	// Procesa la información
-	if (!datosAPI.belongs_to_collection) {
-		datos.entidadNombre = "Película";
-		datos.entidad = "peliculas";
-	}
-	// IMDB_id, nombreOriginal, nombreCastellano
-	if (datosAPI.imdb_id) datos.IMDB_id = datosAPI.imdb_id;
-	if (datosAPI.original_title) datos.nombreOriginal = datosAPI.original_title;
-	if (datosAPI.title) datos.nombreCastellano = datosAPI.title;
-	// Idioma
-	if (datosAPI.original_language) datos.idiomaOriginal_id = datosAPI.original_language;
-
-	// año de estreno, duración, país de origen
-	if (datosAPI.release_date) datos.anoEstreno = parseInt(datosAPI.release_date.slice(0, 4));
-	if (datosAPI.runtime) datos.duracion = datosAPI.runtime;
-	if (datosAPI.production_countries.length > 0)
-		datos.paises_id = datosAPI.production_countries.map((n) => n.iso_3166_1).join(" ");
-	// sinopsis, avatar
-	if (datosAPI.overview) datos.sinopsis = fuenteSinopsisTMDB(datosAPI.overview);
-	if (datosAPI.poster_path) datos.avatar = "https://image.tmdb.org/t/p/original" + datosAPI.poster_path;
-	// Producción
-	if (datosAPI.production_companies.length > 0) datos.produccion = limpiaValores(datosAPI.production_companies);
-	// Crew
-	if (datosAPI.crew.length > 0) {
-		const direccion = limpiaValores(datosAPI.crew.filter((n) => n.department == "Directing"));
-		if (direccion) datos.direccion = direccion;
-		const guion = limpiaValores(datosAPI.crew.filter((n) => n.department == "Writing"));
-		if (guion) datos.guion = guion;
-		const musica = limpiaValores(datosAPI.crew.filter((n) => n.department == "Sound"));
-		if (musica) datos.musica = musica;
-	}
-	// Cast
-	if (datosAPI.cast.length > 0) datos.actores = FN_actores(datosAPI.cast);
-
-	// Fin
-	return datos;
+	prodsIMFA: (palabrasClave) => {},
+	ordenaProdsYaEnBD: (prodsYaEnBD) => {},
 };
 
 // Funciones auxiliares
