c3b96c5dbe8564484ed14930f36973d2bc47c640
diribar <diegoiribarren2015@gmail.com>
2023-06-29 20:52:52 -0300

    Avance del proyecto

--- base_de_datos/modelos/CAL_criterio.js
+++ base_de_datos/modelos/CAL_criterio.js
@@ -1,12 +1,12 @@
 module.exports = (sequelize, dt) => {
-	const alias = "cal_registros";
+	const alias = "cal_criterio";
 	const columns = {
 		entidad: {type: dt.STRING(20)},
 		entidad_id: {type: dt.STRING(20)},
 		valor: {type: dt.INTEGER},
 	};
 	const config = {
-		tableName: "cal_registros",
+		tableName: "cal_criterio",
 		timestamps: false,
 	};
 	const entidad = sequelize.define(alias, columns, config);
