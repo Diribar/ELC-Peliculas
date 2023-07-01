e3e933aa94d3bbfc10524e177e2dac915acce3bc
diribar <diegoiribarren2015@gmail.com>
2023-07-01 06:46:50 -0300

    Mejora de procesos

--- rutas_y_controladores/2.1-Prod-RUD/PR-ControlVista.js
+++ rutas_y_controladores/2.1-Prod-RUD/PR-ControlVista.js
@@ -298,7 +298,7 @@ module.exports = {
 			const tema = "prod_rud";
 			const codigo = "calificar";
 			const {entidad, id} = req.query;
-			const origen = req.query.origen ? req.query.origen : "CAL";
+			const origen = req.query.origen// ? req.query.origen : "CAL";
 			const userID = req.session.usuario ? req.session.usuario.id : "";
 			const entidadNombre = comp.obtieneDesdeEntidad.entidadNombre(entidad);
 
