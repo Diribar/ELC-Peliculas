72bf13fe60e4876d273e6e6c2f620f60a351d160
diribar <diegoiribarren2015@gmail.com>
2023-07-02 09:11:25 -0300

    Avance del proyecto

--- base_de_datos/modelos/INT_opciones.js
+++ base_de_datos/modelos/INT_opciones.js
@@ -1,10 +1,10 @@
 module.exports = (sequelize, dt) => {
-	const alias = "regs_opciones";
+	const alias = "int_opciones";
 	const columns = {
 		nombre: {type: dt.STRING(20)},
 	};
 	const config = {
-		tableName: "regs_opciones",
+		tableName: "int_opciones",
 		timestamps: false,
 	};
 	const entidad = sequelize.define(alias, columns, config);
--- base_de_datos/modelos/US_usuarios.js
+++ base_de_datos/modelos/US_usuarios.js
@@ -72,8 +72,7 @@ module.exports = (sequelize, dt) => {
 		entidad.hasMany(n.capitulos, {as: "capitulos", foreignKey: "creadoPor_id"});
 
 		entidad.hasMany(n.cal_registros, {as: "calificaciones", foreignKey: "usuario_id"});
-		entidad.hasMany(n.regs_interes, {as: "interes", foreignKey: "usuario_id"});
-		entidad.hasMany(n.regs_ya_vistas, {as: "yaVistas", foreignKey: "usuario_id"});
+		entidad.hasMany(n.int_registros, {as: "interes", foreignKey: "usuario_id"});
 
 		entidad.belongsTo(n.usuarios, {as: "capturado_por", foreignKey: "capturadoPor_id"});
 	};
