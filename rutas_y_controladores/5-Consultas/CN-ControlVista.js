05e4a22cf339802973a90f47e8d103154409bf73
diribar <diegoiribarren2015@gmail.com>
2023-07-04 08:48:55 -0300

    Mejora de procesos

---
+++ rutas_y_controladores/5-Consultas/CN-ControlVista.js
@@ -0,0 +1,38 @@
+"use strict";
+// Variables
+const BD_genericas = require("../../funciones/2-BD/Genericas");
+const variables = require("../../funciones/1-Procesos/Variables");
+const procesos = require("./CN-Procesos");
+
+module.exports = {
+	consultas: async (req, res) => {
+		// Variables
+		const tema = "consultas";
+		const titulo = "Consulta de Películas";
+		const usuario = req.session.usuario ? req.session.usuario : {};
+		const userID = req.session.usuario ? usuario.id : "";
+		const filtrosPers = await procesos.filtrosPers(userID);
+		const filtros = procesos.filtros();
+		let opcionesElegidas = {};
+
+		// Filtro elegido
+		const filtro_id =
+			userID && usuario.filtro_id ? usuario.filtro_id : req.cookies && req.cookies.filtro_id ? req.cookies.filtro_id : 1;
+
+		// Opciones elegidas
+		// Obtiene la información de la BD
+		const aux =
+			filtro_id == 1
+				? filtroEstandarCampos
+				: await BD_genericas.obtieneTodosPorCondicion("filtros_campos", {cabecera_id: filtro_id});
+		// Convierte el array en objeto literal
+		aux.map((m) => (opcionesElegidas[m.campo] = m.valor));
+
+		return res.send(filtros)
+		// Va a la vista
+		return res.render("CMP-0Estructura", {
+			...{tema, titulo},
+			...{filtro_id, opcionesElegidas, filtrosPers, filtros},
+		});
+	},
+};
