e7139920bdb74e100e09cc9fc2e2ab7b9db136b9
diribar <diegoiribarren2015@gmail.com>
2023-07-05 14:05:45 -0300

    Avance de consultas

--- rutas_y_controladores/5-Consultas/CN-Rutas.js
+++ rutas_y_controladores/5-Consultas/CN-Rutas.js
@@ -11,6 +11,7 @@ router.get("/api/obtiene-las-preferencias-del-filtroPers", API.obtiene.prefsFilt
 router.get("/api/obtiene-los-dias-del-ano", API.obtiene.diasDelAno);
 
 // API - Filtros personalizados
+router.get("/api/guarda-nuevo-filtroPers", API.guarda.nuevoFiltroPers);
 router.get("/api/actualiza-filtroPers_id", API.actualiza.filtroPers_id);
 router.get("/api/actualiza-prefs-filtroPers", API.actualiza.prefsFiltroPers);
 
