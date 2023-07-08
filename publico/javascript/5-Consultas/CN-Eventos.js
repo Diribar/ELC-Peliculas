7736862e6e20f77c2f0e0c7dffbcc12306aa4d96
diribar <diegoiribarren2015@gmail.com>
2023-07-08 01:48:46 -0300

    Mejora de procesos

--- publico/javascript/5-Consultas/CN-Eventos.js
+++ publico/javascript/5-Consultas/CN-Eventos.js
@@ -141,7 +141,7 @@ window.addEventListener("load", async () => {
 					if (v.edicion) configCons.edicion = true;
 
 					// Guarda la información en la base de datos
-					await cambiosEnBD.guardaUnaConfiguracion(configCons);
+					if (v.nuevo || v.edicion || v.propio) await cambiosEnBD.guardaUnaConfiguracion(configCons);
 				} else true;
 			} else if (nombre == "eliminar") {
 				// Si hay un error, interrumpe la función
