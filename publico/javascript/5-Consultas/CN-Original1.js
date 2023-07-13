059ff62c93305552b2d3b93f62caf366d07b9c41
diribar <diegoiribarren2015@gmail.com>
2023-07-13 16:46:20 -0300

    Miscelaneas

---
+++ publico/javascript/5-Consultas/CN-Original1.js
@@ -0,0 +1,20 @@
+"use strict";
+window.addEventListener("load", async () => {
+	// Variables
+
+	// Funciones
+	let zonaDeProds = {
+		obtieneLosProductos: async () => {
+			// Actualiza los resultados
+			if (!resultados.length) {
+			} else {
+			}
+
+			// Actualiza la información mostrada
+			vistaProds.classList.remove("ocultar");
+
+			// Fin
+			return;
+		},
+	};
+});
