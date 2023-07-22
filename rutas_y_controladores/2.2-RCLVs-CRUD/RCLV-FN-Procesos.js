2b1d5086cdc275d178eaaf4a3054447f7ff59864
diribar <diegoiribarren2015@gmail.com>
2023-07-22 13:41:49 -0300

    Mejora continua

--- rutas_y_controladores/2.2-RCLVs-CRUD/RCLV-FN-Procesos.js
+++ rutas_y_controladores/2.2-RCLVs-CRUD/RCLV-FN-Procesos.js
@@ -7,12 +7,36 @@ const variables = require("../../funciones/1-Procesos/Variables");
 
 module.exports = {
 	detalle: {
+		actualizaProdsRCLV_conEdicionPropia: async (RCLV, usuario) => {
+			// Si el usuario no está logueado, devuelve el RCLV intacto
+			if (!usuario) return RCLV;
+
+			// Actualiza los registros de productos
+			const userID = usuario.id;
+			for (let entProd of variables.entidades.prods) {
+				// Si el RCLV no tiene productos de esa familia, saltea la rutina
+				const prodsEnRCLV = RCLV[entProd];
+				if (!prodsEnRCLV) continue;
+
+				// Rutina por producto
+				for (let i = 0; i < prodsEnRCLV.length; i++) {
+					let [original, edicion] = await procsCRUD.obtieneOriginalEdicion(entProd, prodsEnRCLV[i].id, userID);
+					if (edicion) {
+						const avatar = procsCRUD.obtieneAvatar(original, edicion).edic;
+						RCLV[entProd][i] = {...original, ...edicion, avatar, id: original.id};
+					}
+				}
+			}
+
+			// Fin
+			return RCLV;
+		},
 		prodsDelRCLV: async function (RCLV, usuario) {
 			// Variables
-			let userID = usuario ? usuario.id : "";
+			const userID = usuario ? usuario.id : "";
 			for (let entidad of variables.entidades.prods) if (!RCLV[entidad]) RCLV[entidad] = [];
 
-			// Convierte las ediciones propias de productos en productos
+			// Convierte en productos, a las ediciones propias de productos, con 'campo_id' vinculado al RCLV,
 			if (usuario) {
 				// Obtiene las ediciones
 				let ediciones = RCLV.prods_ediciones ? RCLV.prods_ediciones : [];
@@ -21,7 +45,7 @@ module.exports = {
 				let edicionesPropias = ediciones ? ediciones.filter((n) => n.editadoPor_id == userID) : [];
 
 				// Acciones si hay ediciones propias
-				if (edicionesPropias.length) {
+				if (edicionesPropias.length)
 					// Obtiene los productos de esas ediciones
 					for (let edicion of edicionesPropias) {
 						// Obtiene la entidad con la que está asociada la edición del RCLV, y su campo 'producto_id'
@@ -35,17 +59,19 @@ module.exports = {
 						// Fin
 						RCLV[entProd].push(producto);
 					}
-				}
 			}
+
 			// Completa la información de cada tipo de producto y une los productos en una sola array
 			let prodsDelRCLV = [];
 			for (let entidad of variables.entidades.prods) {
 				// Completa la información de cada producto dentro del tipo de producto
-				let aux = RCLV[entidad].map((registro) => {
-					// Averigua la ruta y el nombre del avatar
-					let avatar = procsCRUD.obtieneAvatar(registro).edic;
+				const aux = RCLV[entidad].map((registro) => {
+					// Variables
+					const avatar = procsCRUD.obtieneAvatar(registro).edic;
+					const entidadNombre = comp.obtieneDesdeEntidad.entidadNombre(entidad);
+
 					// Agrega la entidad, el avatar, y el nombre de la entidad
-					return {...registro, entidad, avatar, entidadNombre: comp.obtieneDesdeEntidad.entidadNombre(entidad)};
+					return {...registro, entidad, avatar, entidadNombre};
 				});
 				prodsDelRCLV.push(...aux);
 			}
