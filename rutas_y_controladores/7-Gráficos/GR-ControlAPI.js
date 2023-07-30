3c6758bbea6bf747f90ea7cc8b4a4e67783039af
diribar <diegoiribarren2015@gmail.com>
2023-07-30 19:46:41 -0300

    Miscelaneas

--- rutas_y_controladores/7-Gráficos/GR-ControlAPI.js
+++ rutas_y_controladores/7-Gráficos/GR-ControlAPI.js
@@ -4,10 +4,10 @@ const BD_genericas = require("../../funciones/2-BD/Genericas");
 const procesos = require("./CN-Procesos");
 
 module.exports = {
-	resultados: async (req, res) => {
+	vencimLinks: async (req, res) => {
 		// Variables
 
 		// Fin
-		
+
 	},
 };
