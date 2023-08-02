dd3422793580191d579000ac9b2939784d62f20b
diribar <diegoiribarren2015@gmail.com>
2023-08-02 14:56:43 -0300

    Mejora de procesos

--- publico/javascript/5-Consultas/CN-Variables.js
+++ publico/javascript/5-Consultas/CN-Variables.js
@@ -39,16 +39,13 @@ window.addEventListener("load", async () => {
 		palClave: DOM.configCampos.querySelector("#palabrasClave input"),
 		palClaveAprob: DOM.configCampos.querySelector("#palabrasClave i"),
 
-		// Zona de productos
+		// Zona de resultados
 		asegurate: DOM.zonaProds.querySelector("#comencemos button#rojo"),
 		quieroVer: DOM.zonaProds.querySelector("#comencemos button#verde"),
 		noTenemos: DOM.zonaProds.querySelector("#comencemos button#azul"),
 		resultados: DOM.zonaProds.querySelectorAll("#vistaProds .resultados"),
-		// Productos
 		vistaProds: DOM.zonaProds.querySelector("#vistaProds"),
-		productos: DOM.zonaProds.querySelector("#vistaProds #productos"),
-		producto: DOM.zonaProds.querySelector("#vistaProds .producto"),
-		// Pelis por
+		botones: DOM.zonaProds.querySelector("#vistaProds #botones"),
 		listados: DOM.zonaProds.querySelector("#vistaProds #listados"),
 	};
 	for (let icono of DOM.iconosBotonera) DOM[icono.id] = icono;
