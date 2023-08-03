4dbdfb415fc7843a65a6deba603282d95b80abff
diribar <diegoiribarren2015@gmail.com>
2023-08-03 16:01:51 -0300

    Mejora de procesos

--- rutas_y_controladores/6-Institucional/IN-ControlVista.js
+++ rutas_y_controladores/6-Institucional/IN-ControlVista.js
@@ -1,17 +1,23 @@
 "use strict";
-const variables = require("../../funciones/1-Procesos/Variables");
 
 // *********** Controlador ***********
 module.exports = {
 	institucional: (req, res) => {
 		// Variables
-		const url = req.path.slice(1)
-		const vars = variables.opcsInstitucional[url];
+		const url = req.path.slice(1);
+		const vistas = Object.keys(vistasInstitucs);
+		const vistaActual = vistasInstitucs[url];
+		const indice = vistas.indexOf(url);
+
+		// Vistas anterior y posterior
+		const urlAnt = indice ? vistas[indice - 1] : null;
+		const urlPost = indice < vistas.length - 1 ? vistas[indice + 1] : "inicio";
 
 		// Fin
 		return res.render("CMP-0Estructura", {
 			tema: "institucional",
-			...vars,
+			...vistaActual,
+			...{urlAnt, urlPost},
 		});
 	},
 };
