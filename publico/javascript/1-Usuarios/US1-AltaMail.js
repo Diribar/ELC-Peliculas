70de04be48310d6eaae8e1fbfe25d155ea32813c
diribar <diegoiribarren2015@gmail.com>
2023-08-04 01:09:16 -0300

    Mejora de procesos

--- publico/javascript/1-Usuarios/US1-AltaMail.js
+++ publico/javascript/1-Usuarios/US1-AltaMail.js
@@ -1,66 +1,72 @@
 "use strict";
 window.addEventListener("load", () => {
-	// Variables generales
-	let form = document.querySelector("form");
-	let button = document.querySelector("form button[type='submit']");
-	let email = document.querySelector(".inputError .input[name='email']");
-	let iconosError = document.querySelectorAll(".inputError .fa-circle-xmark");
-	let iconosOK = document.querySelectorAll(".inputError .fa-circle-check");
-	let mensajesError = document.querySelectorAll(".inputError .mensajeError");
+	// Variables
+	let DOM = {
+		form: document.querySelector("form"),
+		button: document.querySelector("form button[type='submit']"),
+		email: document.querySelector(".inputError .input[name='email']"),
+		iconosError: document.querySelectorAll(".inputError .fa-circle-xmark"),
+		iconosOK: document.querySelectorAll(".inputError .fa-circle-check"),
+		mensajesError: document.querySelectorAll(".inputError .mensajeError"),
+		documNumero: document.querySelector(".inputError .input#documNumero"),
+		documPais_id: document.querySelector(".inputError .input#documPais_id"),
+	};
 
 	// Funciones -----------------------------
 	let mostrarIconos = (mensaje, i) => {
-		mensajesError[i].innerHTML = mensaje;
+		DOM.mensajesError[i].innerHTML = mensaje;
 		if (mensaje) {
-			iconosError[i].classList.remove("ocultar");
-			iconosOK[i].classList.add("ocultar");
+			DOM.iconosError[i].classList.remove("ocultar");
+			DOM.iconosOK[i].classList.add("ocultar");
 		} else {
-			iconosError[i].classList.add("ocultar");
-			iconosOK[i].classList.remove("ocultar");
+			DOM.iconosError[i].classList.add("ocultar");
+			DOM.iconosOK[i].classList.remove("ocultar");
 		}
+
 		// Botón Submit
 		botonSubmit();
+
 		// Fin
 		return;
 	};
 	let botonSubmit = () => {
-		let OK = Array.from(iconosOK)
+		let OK = Array.from(DOM.iconosOK)
 			.map((n) => n.className)
 			.every((n) => !n.includes("ocultar"));
-		// < iconosOK.length;
-		let error = Array.from(iconosError)
+
+		let error = Array.from(DOM.iconosError)
 			.map((n) => n.className)
 			.every((n) => n.includes("ocultar"));
-		OK && error ? button.classList.remove("inactivo") : button.classList.add("inactivo");
+
+		OK && error ? DOM.button.classList.remove("inactivo") : DOM.button.classList.add("inactivo");
 	};
 
 	// Acciones si se realizan cambios
-	email.addEventListener("input", async () => {
-		let campo = email.name;
-		let valor = email.value;
+	DOM.email.addEventListener("input", async () => {
+		let campo = DOM.email.name;
+		let valor = DOM.email.value;
 		let errores = await fetch("/usuarios/api/valida-mail/?" + campo + "=" + valor).then((n) => n.json());
 		let mensaje = errores[campo];
 		mostrarIconos(mensaje, 0);
 	});
-	let documNumero = document.querySelector(".inputError .input#documNumero");
-	let documPais_id = document.querySelector(".inputError .input#documPais_id");
-	if (documNumero && documPais_id) {
-		documNumero.addEventListener("input", () => {
+
+	if (DOM.documNumero && DOM.documPais_id) {
+		DOM.documNumero.addEventListener("input", () => {
 			// Impide los caracteres que no son válidos
-			documNumero.value = documNumero.value.toUpperCase().replace(/[^A-Z\d]/g, "");
-			let mensaje = !documNumero.value ? "Necesitamos que completes este campo" : "";
+			DOM.documNumero.value = DOM.documNumero.value.toUpperCase().replace(/[^A-Z\d]/g, "");
+			let mensaje = !DOM.documNumero.value ? "Necesitamos que completes este campo" : "";
 			mostrarIconos(mensaje, 1);
 		});
-		documPais_id.addEventListener("input", () => {
-			let mensaje = !documPais_id.value ? "Necesitamos que elijas un país" : "";
+		DOM.documPais_id.addEventListener("input", () => {
+			let mensaje = !DOM.documPais_id.value ? "Necesitamos que elijas un país" : "";
 			mostrarIconos(mensaje, 2);
 		});
 	}
 
 	// Submit
-	form.addEventListener("submit", (e) => {
-		if (button.classList.contains("inactivo")) e.preventDefault();
-		button.classList.add("inactivo");
+	DOM.form.addEventListener("submit", (e) => {
+		if (DOM.button.classList.contains("inactivo")) e.preventDefault();
+		DOM.button.classList.add("inactivo");
 	});
 
 	// Start-up: anular 'submit' si hay algún error
