931524d0d3d252fbadbe7ee4b3f73b650a244dd5
diribar <diegoiribarren2015@gmail.com>
2023-08-07 09:09:45 -0300

    Miscelaneas

--- publico/javascript/5-Consultas/CN-Variables.js
+++ publico/javascript/5-Consultas/CN-Variables.js
@@ -75,7 +75,7 @@ window.addEventListener("load", async () => {
 	};
 
 	// Start-up
-	DOM.encabMasPelis.classList.replace("ocultar", "aparece");
+	DOM.encabMasPelis.classList.replace("ocultar", "aparece"); // Tiene que estar en primer lugar, para no demorar su ejecución
 	await cambioDeConfig_id();
 	actualiza.cartelQuieroVerVisible();
 	await cambioDeCampos();
