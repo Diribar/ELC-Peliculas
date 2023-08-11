1696cdb55c5ffe6c1569956d4bcd332a96ad4efc
diribar <diegoiribarren2015@gmail.com>
2023-08-11 15:58:23 -0300

    Mejora continua

--- rutas_y_controladores/5-Consultas/CN-ControlVista.js
+++ rutas_y_controladores/5-Consultas/CN-ControlVista.js
@@ -11,20 +11,27 @@ module.exports = {
 		const titulo = "Consulta de Películas";
 		const usuario = req.session.usuario ? req.session.usuario : {};
 		const userID = req.session.usuario ? usuario.id : null;
-		const configsDeCabecera = await procesos.configs.cabecera(userID);
-		const configs = {
-			propios: configsDeCabecera.filter((n) => userID && n.usuario_id == userID),
-			predeterms: configsDeCabecera.filter((n) => n.usuario_id == 1),
+		const configs = await procesos.configs.cabecera(userID); // Se necesita esa función también para la API
+		const configsDeCabecera = {
+			propios: configs.filter((n) => userID && n.usuario_id == userID),
+			predeterms: configs.filter((n) => n.usuario_id == 1),
 		};
 		const configsConsCampos = procesos.configs.campos();
 
-		// Obtiene el ID del filtro personal elegido
-		const configCons_id = userID && usuario.configCons_id ? usuario.configCons_id : 1;
+		// Obtiene el ID de la configCons del usuario
+		const configConsUrl_id = Number(req.query.configCons_id);
+		const configConsUrl = configConsUrl_id ? await BD_genericas.obtienePorId("configsCons", configConsUrl_id) : "";
+
+		const configCons_id = configConsUrl
+			? configConsUrl.id
+			: userID && usuario.configCons_id
+			? usuario.configCons_id // El guardado en el usuario
+			: 2; // El 'default' es "Por fecha del año"
 
 		// Va a la vista
 		return res.render("CMP-0Estructura", {
 			...{tema, titulo},
-			...{configCons_id, configs, configsConsCampos, userID},
+			...{configCons_id, configsDeCabecera, configsConsCampos, userID},
 			omitirFooter: true,
 		});
 	},
