4bd82dbb5c4c415bbd73a987392b99ccda1d4c47
diribar <diegoiribarren2015@gmail.com>
2023-08-16 10:43:30 -0300

    Mejora de procesos

--- publico/javascript/0-Compartido/Funciones.js
+++ publico/javascript/0-Compartido/Funciones.js
@@ -61,7 +61,7 @@ let amplio = {
 let basico = {
 	restringeCaracteres: (e, respetarMinusc) => {
 		// Primeras tareas
-		amplio.restringeCaracteres(e, respetarMinusc)
+		amplio.restringeCaracteres(e, respetarMinusc);
 
 		// Reemplaza en la variable
 		let valor = e.target.value;
@@ -176,4 +176,215 @@ let desplazamHoriz = () => {
 
 let espera = (ms) => {
 	return new Promise((resolve) => setTimeout(resolve, ms));
+};
+
+let revisaAvatar = ({DOM, v, indice}) => {
+	// 1. Si se omitió ingresar un archivo, vuelve a la imagen original
+	if (!DOM.inputAvatar.value) {
+		// Actualiza el avatar
+		DOM.imgAvatar.src = v.imgInicial;
+
+		// Oculta el iconoOK
+		if (DOM.ocultaOK_imagen) DOM.ocultaOK_imagen.classList.add("ocultaOK_imagen");
+
+		// Actualiza los errores
+		v.esImagen = true;
+		FN.actualizaVarios(indice);
+
+		// Fin
+		return;
+	}
+
+	// 2. De lo contrario, actualiza los errores y el avatar
+	let reader = new FileReader();
+	reader.readAsDataURL(DOM.inputAvatar.files[0]);
+	reader.onload = () => {
+		let image = new Image();
+		image.src = reader.result;
+
+		// Acciones si es realmente una imagen
+		image.onload = () => {
+			// Actualiza la imagen del avatar en la vista
+			DOM.imgAvatar.src = reader.result;
+
+			// Muestra el iconoOK
+			if (DOM.ocultaOK_imagen) DOM.ocultaOK_imagen.classList.remove("ocultaOK_imagen");
+
+			// Actualiza los errores
+			v.esImagen = true;
+			FN.actualizaVarios(indice);
+
+			// Fin
+			return;
+		};
+
+		// Acciones si no es una imagen
+		image.onerror = () => {
+			// Limpia el avatar
+			DOM.imgAvatar.src = "/imagenes/0-Base/Avatar/Sin-Avatar.jpg";
+
+			// Limpia el input
+			DOM.inputAvatar.value = "";
+
+			// Actualiza los errores
+			v.esImagen = false;
+			FN.actualizaVarios(indice);
+
+			// Fin
+			return;
+		};
+	};
+};
+
+let revisaAvatarNuevo = function (indice) {
+	// 1. Si se omitió ingresar un archivo, vuelve a la imagen original
+	if (!inputAvatar.value) {
+		// Actualiza el avatar
+		imgAvatar.src = imgInicial;
+		// Oculta el iconoOK
+		if (ocultaOK_imagen) ocultaOK_imagen.classList.add("ocultaOK_imagen");
+		// Actualiza los errores
+		esImagen = true;
+		this.actualizaVarios(indice);
+		// Fin
+		return;
+	}
+	// 2. De lo contrario, actualiza los errores y el avatar
+	let reader = new FileReader();
+	reader.readAsDataURL(inputAvatar.files[0]);
+	reader.onload = () => {
+		let image = new Image();
+		image.src = reader.result;
+		// Acciones si es realmente una imagen
+		image.onload = () => {
+			// Actualiza la imagen del avatar en la vista
+			imgAvatar.src = reader.result;
+			// Muestra el iconoOK
+			if (ocultaOK_imagen) ocultaOK_imagen.classList.remove("ocultaOK_imagen");
+			// Actualiza los errores
+			esImagen = true;
+			FN.actualizaVarios(indice);
+			// Fin
+			return;
+		};
+		// Acciones si no es una imagen
+		image.onerror = () => {
+			// Limpia el avatar
+			imgAvatar.src = "/imagenes/0-Base/Avatar/Sin-Avatar.jpg";
+			// Limpia el input
+			inputAvatar.value = "";
+			// Actualiza los errores
+			esImagen = false;
+			FN.actualizaVarios(indice);
+			// Fin
+			return;
+		};
+	};
+};
+let revisaAvatarNuevo = function () {
+	// 1. Si se omitió ingresar un archivo, vuelve a la imagen original
+	if (!DOM.inputAvatarEdicN.value) {
+		// Actualiza el avatar
+		DOM.imgsAvatar[0].src = varios.avatarInicial;
+		// Actualiza los errores
+		varias.esImagen = true;
+		this.actualizaVarios();
+		// Fin
+		return;
+	}
+	// 2. De lo contrario, actualiza los errores y el avatar
+	let reader = new FileReader();
+	reader.readAsDataURL(DOM.inputAvatarEdicN.files[0]);
+	reader.onload = () => {
+		let image = new Image();
+		image.src = reader.result;
+		// Acciones si es realmente una imagen
+		image.onload = async () => {
+			// Actualiza la imagen del avatar en la vista
+			DOM.imgsAvatar[0].src = reader.result;
+			// Actualiza la variable 'avatar' en la versión 'edicN'
+			if (DOM.inputAvatarEdicN.value) version.edicN.avatar = DOM.inputAvatarEdicN.files[0].name;
+			// Actualiza los errores
+			varias.esImagen = true;
+			FN.actualizaVarios();
+			// Fin
+			return;
+		};
+		// Acciones si no es una imagen
+		image.onerror = () => {
+			// Limpia el avatar
+			DOM.imgsAvatar[0].src = "/imagenes/0-Base/Avatar/Sin-Avatar.jpg";
+			// Limpia el input
+			DOM.inputAvatarEdicN.value = "";
+			// Actualiza la variable 'avatar' en la versión 'edicN'
+			if (DOM.inputAvatarEdicN.value) version.edicN.avatar = "";
+			// Actualiza los errores
+			varias.esImagen = false;
+			FN.actualizaVarios();
+			// Fin
+			return;
+		};
+	};
+};
+let avatar = async () => {
+	// Si hubo alguna novedad en el avatar, muestra los resultados
+	DOM.iconosOK[0].classList.remove("ocultaAvatar");
+
+	// 1. Acciones si se omitió ingresar un archivo
+	if (!DOM.avatarInput.value) {
+		// Vuelve a la imagen original
+		DOM.avatarImg.src = varios.avatarInicial;
+
+		// Actualiza los errores
+		varios.esImagen = "";
+		varios.tamano = 0;
+		await validacs.avatar();
+
+		// Fin
+		validacs.muestraErroresOK();
+		validacs.botonSubmit();
+		return;
+	}
+	// 2. Acciones si se ingresó un archivo
+	let reader = new FileReader();
+	reader.readAsDataURL(DOM.avatarInput.files[0]);
+	reader.onload = () => {
+		let image = new Image();
+		image.src = reader.result;
+
+		// Acciones si es realmente una imagen
+		image.onload = async () => {
+			// Actualiza la imagen del avatar en la vista
+			DOM.avatarImg.src = reader.result;
+
+			// Actualiza los errores
+			varios.esImagen = "SI";
+			varios.tamano = DOM.avatarInput.files[0].size;
+			await validacs.avatar();
+
+			// Fin
+			validacs.muestraErroresOK();
+			validacs.botonSubmit();
+			return;
+		};
+
+		// Acciones si no es una imagen
+		image.onerror = async () => {
+			// Limpia el avatar
+			DOM.avatarImg.src = "/imagenes/0-Base/Avatar/Sin-Avatar.jpg";
+
+			// Actualiza los errores
+			varios.esImagen = "NO";
+			varios.tamano = 0;
+			await validacs.avatar();
+
+			// Limpia el input - debe estar después de la validación de errores debido al valor del input
+			DOM.avatarInput.value = "";
+
+			// Fin
+			validacs.muestraErroresOK();
+			validacs.botonSubmit();
+			return;
+		};
+	};
 };
