8639573a242a4b6d002d7cf246d6c5cc22ffa077
diribar <diegoiribarren2015@gmail.com>
2023-08-16 12:03:50 -0300

    Estandarización de procesos

--- publico/javascript/1-Usuarios/US-InputError.js
+++ publico/javascript/1-Usuarios/US-InputError.js
@@ -6,72 +6,74 @@ window.addEventListener("load", () => {
 	let tareas = {login: "login", editables: "editables", documento: "documento"};
 	if (!tareas[tarea]) return;
 
-	// VARIABLES GENERALES -----------------------------------------------------------------------
-	// Datos del formulario
-	let form = document.querySelector("form");
-	let button = document.querySelector("form button[type='submit']");
-	let inputs = document.querySelectorAll("form .inputError .input");
-	// OK/Errores
-	let iconosOK = document.querySelectorAll("form .inputError .fa-circle-check");
-	let ocultaOK_imagen = document.querySelector("form .inputError .fa-circle-check.ocultaOK_imagen");
-	let iconosError = document.querySelectorAll("form .inputError .fa-circle-xmark");
-	let mensajesError = document.querySelectorAll("form .inputError .mensajeError");
-	let credencialesInvalidas = document.querySelector("#credencialesInvalidas");
-	// Temas de avatar
-	let imgAvatar = document.querySelector("form .inputError label img");
-	let inputAvatar = document.querySelector("form .inputError input[name='avatar']");
-	const imgInicial = imgAvatar ? imgAvatar.src : "";
-	let esImagen = true;
-	// Varias
-	let rutaApi = "/usuarios/api/valida-" + tareas[tarea] + "/?";
+	// Variables
+	const form = document.querySelector("form");
+	let DOM = {
+		// OK/Errores
+		iconosOK: form.querySelectorAll(".inputError .fa-circle-check"),
+		ocultaOK_imagen: form.querySelector(".inputError .fa-circle-check.ocultaOK_imagen"),
+		iconosError: form.querySelectorAll(".inputError .fa-circle-xmark"),
+		mensajesError: form.querySelectorAll(".inputError .mensajeError"),
+		credencialesInvalidas: form.querySelector("#credencialesInvalidas"),
 
-	// FUNCIONES --------------------------------------------------------------
+		// Temas de avatar
+		imgAvatar: form.querySelector(".inputError label img"),
+		inputAvatar: form.querySelector(".inputError input[name='avatar']"),
+
+		// Varios
+		button: form.querySelector("button[type='submit']"),
+		inputs: form.querySelectorAll(".inputError .input"),
+	};
+	let v = {
+		imgInicial: DOM.imgAvatar ? DOM.imgAvatar.src : "",
+		esImagen: true,
+		rutaApi: "/usuarios/api/valida-" + tareas[tarea] + "/?",
+	};
+
+	// Funciones
 	let FN = {
 		averiguaLosErrores: async (indice) => {
 			// Variables
-			let campo = inputs[indice].name;
-			let valor = encodeURIComponent(inputs[indice].value);
+			let campo = DOM.inputs[indice].name;
+			let valor = encodeURIComponent(DOM.inputs[indice].value);
 			// Particularidad para 'avatar'
 			if (campo.includes("avatar")) {
-				valor += "&esImagen=" + (esImagen ? "SI" : "NO");
-				if (inputAvatar.value) valor += "&tamano=" + inputAvatar.files[0].size;
+				valor += "&esImagen=" + (v.esImagen ? "SI" : "NO");
+				if (DOM.inputAvatar.value) valor += "&tamano=" + DOM.inputAvatar.files[0].size;
 			}
 			// Averigua los errores
 			let errores =
 				// Corre la rutina, siempre que no sea una avatar opcional vacío
 				campo != "avatar" ||
-				inputs[indice].value ||
-				!esImagen ||
-				(tarea == "documento" && imgAvatar.src.includes("imagenes/0-Base"))
-					? await fetch(rutaApi + campo + "=" + valor).then((n) => n.json())
+				DOM.inputs[indice].value ||
+				!v.esImagen ||
+				(tarea == "documento" && DOM.imgAvatar.src.includes("imagenes/0-Base"))
+					? await fetch(v.rutaApi + campo + "=" + valor).then((n) => n.json())
 					: "";
 			// Fin
 			return errores;
 		},
 		muestraLosErrores: (error, indice) => {
 			// Variables
-			let campo = inputs[indice].name;
+			let campo = DOM.inputs[indice].name;
 			let mensaje = error[campo];
 			// Reemplaza el mensaje, con particularidad para 'avatar'
-			mensajesError[indice].innerHTML = mensaje;
+			DOM.mensajesError[indice].innerHTML = mensaje;
 			// Acciones en función de si hay o no mensajes de error
-			mensaje
-				? iconosError[indice].classList.remove("ocultar")
-				: iconosError[indice].classList.add("ocultar");
-			mensaje
-				? iconosOK[indice].classList.add("ocultar")
-				: iconosOK[indice].classList.remove("ocultar");
+			mensaje ? DOM.iconosError[indice].classList.remove("ocultar") : DOM.iconosError[indice].classList.add("ocultar");
+			mensaje ? DOM.iconosOK[indice].classList.add("ocultar") : DOM.iconosOK[indice].classList.remove("ocultar");
 		},
 		actualizaBotonGuardar: () => {
-			let OK = Array.from(iconosOK)
+			// Variables
+			const OK = Array.from(DOM.iconosOK)
 				.map((n) => n.className)
 				.every((n) => !n.includes("ocultar"));
-			// < iconosOK.length;
-			let error = Array.from(iconosError)
+			const error = Array.from(DOM.iconosError)
 				.map((n) => n.className)
 				.every((n) => n.includes("ocultar"));
+
 			// Fin
-			OK && error ? button.classList.remove("inactivo") : button.classList.add("inactivo");
+			OK && error ? DOM.button.classList.remove("inactivo") : DOM.button.classList.add("inactivo");
 		},
 		actualizaVarios: async function (indice) {
 			// Detecta si hay errores
@@ -83,31 +85,31 @@ window.addEventListener("load", () => {
 		},
 		revisaAvatarNuevo: function (indice) {
 			// 1. Si se omitió ingresar un archivo, vuelve a la imagen original
-			if (!inputAvatar.value) {
+			if (!DOM.inputAvatar.value) {
 				// Actualiza el avatar
-				imgAvatar.src = imgInicial;
+				DOM.imgAvatar.src = v.imgInicial;
 				// Oculta el iconoOK
-				if (ocultaOK_imagen) ocultaOK_imagen.classList.add("ocultaOK_imagen");
+				if (DOM.ocultaOK_imagen) DOM.ocultaOK_imagen.classList.add("ocultaOK_imagen");
 				// Actualiza los errores
-				esImagen = true;
+				v.esImagen = true;
 				this.actualizaVarios(indice);
 				// Fin
 				return;
 			}
 			// 2. De lo contrario, actualiza los errores y el avatar
 			let reader = new FileReader();
-			reader.readAsDataURL(inputAvatar.files[0]);
+			reader.readAsDataURL(DOM.inputAvatar.files[0]);
 			reader.onload = () => {
 				let image = new Image();
 				image.src = reader.result;
 				// Acciones si es realmente una imagen
 				image.onload = () => {
 					// Actualiza la imagen del avatar en la vista
-					imgAvatar.src = reader.result;
+					DOM.imgAvatar.src = reader.result;
 					// Muestra el iconoOK
-					if (ocultaOK_imagen) ocultaOK_imagen.classList.remove("ocultaOK_imagen");
+					if (DOM.ocultaOK_imagen) DOM.ocultaOK_imagen.classList.remove("ocultaOK_imagen");
 					// Actualiza los errores
-					esImagen = true;
+					v.esImagen = true;
 					FN.actualizaVarios(indice);
 					// Fin
 					return;
@@ -115,11 +117,11 @@ window.addEventListener("load", () => {
 				// Acciones si no es una imagen
 				image.onerror = () => {
 					// Limpia el avatar
-					imgAvatar.src = "/imagenes/0-Base/Avatar/Sin-Avatar.jpg";
+					DOM.imgAvatar.src = "/imagenes/0-Base/Avatar/Sin-Avatar.jpg";
 					// Limpia el input
-					inputAvatar.value = "";
+					DOM.inputAvatar.value = "";
 					// Actualiza los errores
-					esImagen = false;
+					v.esImagen = false;
 					FN.actualizaVarios(indice);
 					// Fin
 					return;
@@ -128,7 +130,7 @@ window.addEventListener("load", () => {
 		},
 		startUp: async function () {
 			// Averigua y muestra los errores
-			for (let indice = 0; indice < inputs.length; indice++) {
+			for (let indice = 0; indice < DOM.inputs.length; indice++) {
 				let errores = await this.averiguaLosErrores(indice);
 				this.muestraLosErrores(errores, indice);
 			}
@@ -137,19 +139,17 @@ window.addEventListener("load", () => {
 		},
 	};
 
-	// EVENT LISTENERS ---------------------------------------
-	inputs.forEach((input, indice) => {
+	// Eventos
+	DOM.inputs.forEach((input, indice) => {
 		input.addEventListener("input", async () => {
 			// Variables
 			let campo = input.name;
 			// Desactiva el cartel de 'credenciales inválidas'
 			if (
 				tarea == "login" ||
-				(tarea == "documento" &&
-					(campo == "documNumero" || campo == "documPais_id") &&
-					credencialesInvalidas)
+				(tarea == "documento" && (campo == "documNumero" || campo == "documPais_id") && DOM.credencialesInvalidas)
 			)
-				credencialesInvalidas.classList.add("ocultar");
+				DOM.credencialesInvalidas.classList.add("ocultar");
 
 			// Primera letra en mayúscula
 			if (tarea != "login" && input.localName == "input" && input.type == "text") {
@@ -167,7 +167,7 @@ window.addEventListener("load", () => {
 	});
 
 	form.addEventListener("submit", async (e) => {
-		if (button.classList.contains("inactivo")) {
+		if (DOM.button.classList.contains("inactivo")) {
 			e.preventDefault();
 			FN.startUp();
 		}
