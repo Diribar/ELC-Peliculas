50751a5d7d26ba0368f9cc96224a17b441ef48f2
diribar <diegoiribarren2015@gmail.com>
2023-08-22 13:48:56 -0300

    Mejora continua

---
+++ base_de_datos/modelos/CN_ordenesPorEntidad.js
@@ -0,0 +1,23 @@
+module.exports = (sequelize, dt) => {
+	const alias = "cn_ordenesPorEntidad";
+	const columns = {
+		numOrden: {type: dt.INTEGER},
+		entidad_id: {type: dt.INTEGER},
+		orden_id: {type: dt.INTEGER},
+		nombre: {type: dt.STRING(40)},
+
+		boton: {type: dt.INTEGER},
+		ordenDefault: {type: dt.BOOLEAN},
+		bhrSeguro: {type: dt.BOOLEAN},
+	};
+	const config = {
+		tableName: "cn_ordenes_por_ent",
+		timestamps: false,
+	};
+	const entidad = sequelize.define(alias, columns, config);
+	entidad.associate = (n) => {
+		entidad.belongsTo(n.cn_entidades, {as: "entidad", foreignKey: "entidad_id"});
+		entidad.belongsTo(n.cn_ordenes, {as: "orden", foreignKey: "orden_id"});
+	};
+	return entidad;
+};
