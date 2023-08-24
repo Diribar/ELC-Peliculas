b5065540a6356485829a4d3d10a7d9c46b900c64
diribar <diegoiribarren2015@gmail.com>
2023-08-24 14:30:18 -0300

    Mejora de procesos

--- publico/javascript/3-RevisionEnts/RE9-EdicionAvatarRech.js
+++ publico/javascript/3-RevisionEnts/RE9-EdicionAvatarRech.js
@@ -1,35 +1,37 @@
 "use strict";
 window.addEventListener("load", () => {
-	// Flechas
-	let accesoAlCartelRechazo = document.querySelector("#mostrarMotivos");
+	let DOM = {
+		// Flechas
+		accesoAlCartelRechazo: document.querySelector("#mostrarMotivos"),
 
-	// Motivos para borrar
-	let tapaElFondo = document.querySelector("#tapar-el-fondo");
-	let cartelRechazo = document.querySelector("#cartelRechazo");
-	let motivoRechazo = document.querySelector("#cartelRechazo select");
-	let cancelar = document.querySelector("#cartelRechazo .flechas .fa-circle-left");
-	let rechazar = document.querySelector("#cartelRechazo .flechas button");
+		// Motivos para borrar
+		tapaElFondo: document.querySelector("#tapaElFondo"),
+		cartelRechazo: document.querySelector("#cartelRechazo"),
+		motivoRechazo: document.querySelector("#cartelRechazo select"),
+		cancelar: document.querySelector("#cartelRechazo .flechas .fa-circle-left"),
+		rechazar: document.querySelector("#cartelRechazo .flechas button"),
+	};
 
 	// Muestra el menú "motivos para borrar"
-	accesoAlCartelRechazo.addEventListener("click", () => {
-		tapaElFondo.classList.remove("ocultar");
-		cartelRechazo.classList.remove("ocultar");
+	DOM.accesoAlCartelRechazo.addEventListener("click", () => {
+		DOM.tapaElFondo.classList.remove("ocultar");
+		DOM.cartelRechazo.classList.remove("ocultar");
 	});
 
 	// Oculta el menú "motivos para borrar"
-	cancelar.addEventListener("click", () => {
-		cartelRechazo.classList.add("ocultar");
-		tapaElFondo.classList.add("ocultar");
+	DOM.cancelar.addEventListener("click", () => {
+		DOM.tapaElFondo.classList.add("ocultar");
+		DOM.cartelRechazo.classList.add("ocultar");
 	});
 
 	// Elegi motivo rechazo
-	motivoRechazo.addEventListener("change", () => {
-		if (motivoRechazo.value) rechazar.classList.remove("inactivo")
-		else rechazar.classList.add("inactivo")
+	DOM.motivoRechazo.addEventListener("change", () => {
+		if (DOM.motivoRechazo.value) DOM.rechazar.classList.remove("inactivo");
+		else DOM.rechazar.classList.add("inactivo");
 	});
 
 	// Rechazar
-	rechazar.addEventListener("click", (e) => {
-		if (!motivoRechazo.value) e.preventDefault();
+	DOM.rechazar.addEventListener("click", (e) => {
+		if (!DOM.motivoRechazo.value) e.preventDefault();
 	});
 });
