0c57b2e43952453b7bd9121da6b333514f27fdbf
diribar <diegoiribarren2015@gmail.com>
2023-08-28 19:31:55 -0300

    Mejora continua

--- rutas_y_controladores/7-Gráficos/GR-Rutas.js
+++ rutas_y_controladores/7-Gráficos/GR-Rutas.js
@@ -7,10 +7,13 @@ const vista = require("./GR-ControlVista");
 // Middlewares ***********************************************
 
 // API
+router.get("/api/cantidad-de-peliculas-por-cfc-y-vpc", API.cantPelisPorCfcVpc);
 router.get("/api/vencimiento-de-links-por-semana", API.vencimLinks);
 router.get("/api/cantidad-de-links-por-proveedor", API.cantLinksPorProv);
 
 // Vistas *******************************************
+router.get("/cantidad-de-peliculas-por-cfc-y-vpc", vista.cantPelisPorCfcVpc);
+
 router.get("/vencimiento-de-links-por-semana", vista.vencimLinks);
 router.get("/cantidad-de-links-por-proveedor", vista.cantLinksPorProv);
 
