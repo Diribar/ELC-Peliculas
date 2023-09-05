bf038beb546a1c43db9c2d58f8f5d1b9d03cf925
diribar <diegoiribarren2015@gmail.com>
2023-09-05 12:45:03 -0300

    Mejora de procesos

--- publico/javascript/5-Consultas/CN-FN-Resultados.js
+++ publico/javascript/5-Consultas/CN-FN-Resultados.js
@@ -13,22 +13,20 @@ let resultados = {
 
 		// Tapa y limpia los resultados anteriores
 		if (!v.mostrarCartelQuieroVer) DOM.esperandoResultados.classList.replace("desaparece", "aparece");
-		DOM.pppOpcionesCartelCierra.classList.remove("ocultar");
 		DOM.botones.innerHTML = "";
 		DOM.listados.innerHTML = "";
 		v.infoResultados = null;
 
 		// Acciones si el orden es 'pppFecha'
 		if (v.ordenBD.codigo == "pppFecha") {
-			// Muestra el cartel 'loginPend' y termina
+			// Si el usuario no está logueado, muestra el cartel 'loginPend' y termina
 			if (!v.userID) {
 				DOM.loginPend.classList.remove("ocultar");
 				return;
 			}
-			// Muestra el cartel 'pppOpcionesCartel' y termina
+			// Si el usuario no tiene 'PPPs', muestra el cartel 'cartelOrdenPPP' y termina
 			else if (!v.usuarioTienePPP) {
-				DOM.pppOpcionesCartelCierra.classList.add("ocultar"); // oculta el ícono 'pppOpcionesCartelCierra'
-				DOM.pppOpcionesCartel.classList.remove("ocultar");
+				DOM.cartelOrdenPPP.classList.remove("ocultar");
 				return;
 			}
 		}
