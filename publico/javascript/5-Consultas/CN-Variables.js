ba73327b68a7f7d2386b6b39c0a9e3f98a8dc524
diribar <diegoiribarren2015@gmail.com>
2023-09-05 13:42:12 -0300

    Mejora continua

--- publico/javascript/5-Consultas/CN-Variables.js
+++ publico/javascript/5-Consultas/CN-Variables.js
@@ -55,14 +55,14 @@ window.addEventListener("load", async () => {
 
 		// Zona Disponible - Carteles e Imagen de fondo
 		esperandoResultados: DOM.zonaDisponible.querySelector("#vistaDeResults #esperandoResultados"),
-		carteles: DOM.zonaDisponible.querySelectorAll("#comencemos .cartel"),
-		asegurate: DOM.zonaDisponible.querySelector("#comencemos button#asegurate"),
-		quieroVer: DOM.zonaDisponible.querySelector("#comencemos button#quieroVer"),
-		noTenemos: DOM.zonaDisponible.querySelector("#comencemos button#noTenemos"),
-		loginPend: DOM.zonaDisponible.querySelector("#comencemos button#loginPend"),
-		telonFondo: DOM.zonaDisponible.querySelector("#comencemos img#telonFondo"),
-		cartelOrdenPPP: DOM.zonaDisponible.querySelector("#comencemos #cartelOrdenPPP"),
-		cartelUsSinPPP: DOM.zonaDisponible.querySelector("#comencemos #cartelUsSinPPP"),
+		telonFondo: DOM.zonaDisponible.querySelector("#vistaDeResults img#telonFondo"),
+		carteles: DOM.zonaDisponible.querySelectorAll("#carteles .cartel"),
+		asegurate: DOM.zonaDisponible.querySelector("#carteles button#asegurate"),
+		quieroVer: DOM.zonaDisponible.querySelector("#carteles button#quieroVer"),
+		noTenemos: DOM.zonaDisponible.querySelector("#carteles button#noTenemos"),
+		loginPend: DOM.zonaDisponible.querySelector("#carteles button#loginPend"),
+		cartelOrdenPPP: DOM.zonaDisponible.querySelector("#carteles #cartelOrdenPPP"),
+		cartelUsSinPPP: DOM.zonaDisponible.querySelector("#carteles #cartelUsSinPPP"),
 
 		// Zona Disponible - Resultados
 		resultados: DOM.zonaDisponible.querySelectorAll("#vistaDeResults .resultados"),
@@ -91,6 +91,7 @@ window.addEventListener("load", async () => {
 		localhost: await fetch("/api/localhost").then((n) => n.json()),
 		mostrarFiltros: false,
 		topeParaMasRecientes: 20,
+		mostrarSinPPP: 0,
 	};
 
 	// Start-up
