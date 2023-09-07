5c54c3efe7454042e5e6c105324bce42e25ae30c
diribar <diegoiribarren2015@gmail.com>
2023-09-07 11:19:20 -0300

    Mejora de procesos

--- base_de_datos/modelos/CAM_motivosEdics.js
+++ base_de_datos/modelos/CAM_motivosEdics.js
@@ -3,13 +3,13 @@ module.exports = (sequelize, dt) => {
 	const columns = {
 		orden: {type: dt.INTEGER},
 		descripcion: {type: dt.STRING(40)},
+		codigo: {type: dt.STRING(15)},
 		avatar_prods: {type: dt.BOOLEAN},
 		avatar_rclvs: {type: dt.BOOLEAN},
 		avatar_us: {type: dt.BOOLEAN},
 		prods: {type: dt.BOOLEAN},
 		rclvs: {type: dt.BOOLEAN},
 		links: {type: dt.BOOLEAN},
-		version_actual: {type: dt.BOOLEAN},
 		info_erronea: {type: dt.BOOLEAN},
 		rev_edicion: {type: dt.BOOLEAN},
 		penalizac: {type: dt.DECIMAL},
@@ -20,8 +20,6 @@ module.exports = (sequelize, dt) => {
 		timestamps: false,
 	};
 	const entidad = sequelize.define(alias, columns, config);
-	// entidad.associate = (n) => {
-	// 	entidad.hasMany(n.links, {as: "edicsRech", foreignKey: "motivo_id"});
-	// };
+
 	return entidad;
 };
