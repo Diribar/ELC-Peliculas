1598319a8fb9e34ab9dd95859c7925d586558813
diribar <diegoiribarren2015@gmail.com>
2023-09-29 23:27:11 -0300

    Mejora continua

--- app.js
+++ app.js
@@ -22,6 +22,7 @@ global.fechaDelAnoHoy_id = null;
 global.anoHoy = null;
 global.tamMaxImagen = 1000000; // 1Mb
 global.configConsDefault_id = 2; // El 'default' es "Sorprendeme"
+global.versionELC = "1.02"
 
 // Con 'require'
 require("dotenv").config(); // Para usar el archivo '.env' --> se debe colocar al principio
