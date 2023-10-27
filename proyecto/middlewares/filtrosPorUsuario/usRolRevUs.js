b0e333f7cfe798f13140d57d442717500bc2fbfb
diribar <diegoiribarren2015@gmail.com>
2023-10-26 22:25:53 -0300

    Mejora continua

--- proyecto/middlewares/filtrosPorUsuario/usRolRevUs.js
+++ proyecto/middlewares/filtrosPorUsuario/usRolRevUs.js
@@ -5,16 +5,16 @@ const procesos = require("../../rutas_y_controladores/1-Usuarios/US-FN-Procesos"
 module.exports = (req, res, next) => {
 	// Variables
 	const usuario = req.session.usuario;
+	let informacion;
+
+	// Revisa si el usuario tiene el status perenne
+	if (usuario.statusRegistro_id != stPerennes_id) informacion = procesos.validaPerenne(req);
+
+	// Revisa si el usuario tiene el rol necesario
 	const usuarioSinRolDeRevisor = {
 		mensajes: ["Se requiere un permiso especial para ingresar a esta vista."],
 		iconos: [{nombre: "fa-circle-left", link: req.session.urlAnterior, titulo: "Ir a la vista anterior"}],
 	};
-	let informacion;
-
-	// Revisa si el usuario tiene validada su identidad
-	informacion = procesos.feedbackSobreIdentidadValidada(req);
-
-	// Revisa si el usuario tiene el rol necesario
 	if (!informacion && !usuario.rolUsuario.revisorUs) informacion = usuarioSinRolDeRevisor;
 
 	// Si corresponde, muestra el mensaje de error
