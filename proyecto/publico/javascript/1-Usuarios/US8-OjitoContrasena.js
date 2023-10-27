67723ea7347a00704be2f12093e2c824e74cce50
diribar <diegoiribarren2015@gmail.com>
2023-10-27 20:24:12 -0300

    Mejora continua

--- proyecto/publico/javascript/1-Usuarios/US8-OjitoContrasena.js
+++ proyecto/publico/javascript/1-Usuarios/US8-OjitoContrasena.js
@@ -2,12 +2,26 @@
 window.addEventListener("load", () => {
 	let contrasena = document.querySelector("input[name='contrasena']");
 	let ojito = document.getElementById("eye");
+
+	// Visible
 	ojito.addEventListener("mousedown", () => {
 		contrasena.type = "text";
 		ojito.classList.remove("fa-eye-slash");
 		ojito.classList.add("fa-eye");
 	});
+	ojito.addEventListener("touchstart", () => {
+		contrasena.type = "text";
+		ojito.classList.remove("fa-eye-slash");
+		ojito.classList.add("fa-eye");
+	});
+
+	// Oculta
 	ojito.addEventListener("mouseup", () => {
+		contrasena.type = "password";
+		ojito.classList.remove("fa-eye");
+		ojito.classList.add("fa-eye-slash");
+	});
+	ojito.addEventListener("touchend", () => {
 		contrasena.type = "password";
 		ojito.classList.remove("fa-eye");
 		ojito.classList.add("fa-eye-slash");
--- proyecto/rutas_y_controladores/1-Usuarios/US-ControlVista.js
+++ proyecto/rutas_y_controladores/1-Usuarios/US-ControlVista.js
@@ -122,8 +122,7 @@ module.exports = {
 				mensajes: [
 					"Estimad" + usuario.sexo.letra_final + " " + usuario.apodo + ", completaste el alta satisfactoriamente.",
 					"Bienvenid" + usuario.sexo.letra_final + " a nuestro sitio como usuario.",
-					"Con tu alta de usuario, ya podés guardar tus consultas.",
-					"Si querés influir en nuestra base de datos, podés iniciar el trámite con el ícono 'Agregar una película' que está en el 'Header'.",
+					"Con tu alta de usuario, ya podés guardar tus consultas personalizadas.",
 				],
 				iconos: [variables.vistaEntendido(req.session.urlFueraDeUsuarios)],
 				titulo: "Bienvenido/a a la familia ELC",
