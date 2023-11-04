ccac3524c4d963ce3dfd5807bfb9a88ad2d0e43f
diribar <diegoiribarren2015@gmail.com>
2023-11-04 07:41:09 -0300

    Mejora de procesos

--- proyecto/base_de_datos/modelos/CN_ordenesPorEnt.js
+++ proyecto/base_de_datos/modelos/CN_ordenesPorEnt.js
@@ -2,7 +2,7 @@ module.exports = (sequelize, dt) => {
 	const alias = "cn_opcionesPorEnt";
 	const columns = {
 		entidad_id: {type: dt.INTEGER},
-		orden_id: {type: dt.INTEGER},
+		opcion_id: {type: dt.INTEGER},
 		nombre: {type: dt.STRING(40)},
 		boton: {type: dt.INTEGER},
 		ordenDefault: {type: dt.BOOLEAN},
@@ -15,7 +15,7 @@ module.exports = (sequelize, dt) => {
 	const entidad = sequelize.define(alias, columns, config);
 	entidad.associate = (n) => {
 		entidad.belongsTo(n.cn_entidades, {as: "entidad", foreignKey: "entidad_id"});
-		entidad.belongsTo(n.cn_opciones, {as: "opcion", foreignKey: "orden_id"});
+		entidad.belongsTo(n.cn_opciones, {as: "opcion", foreignKey: "opcion_id"});
 	};
 	return entidad;
 };
