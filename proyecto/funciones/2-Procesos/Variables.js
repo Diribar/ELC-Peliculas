368de583df6421738bfd7f724ff649263387a5aa
diribar <diegoiribarren2015@gmail.com>
2023-11-10 08:35:42 -0300

    Mejora continua

--- proyecto/funciones/2-Procesos/Variables.js
+++ proyecto/funciones/2-Procesos/Variables.js
@@ -146,6 +146,13 @@ module.exports = {
 				{id: "NN", nombre: "Sin proceso de canonizac.", condic: {[Op.startsWith]: "NN"}},
 			],
 		},
+		entidad: {
+			titulo: "Películas / Colecciones",
+			opciones: [
+				{id: "peliculas", nombre: "Sólo películas"},
+				{id: "colecciones", nombre: "Sólo colecciones"},
+			],
+		},
 	},
 
 	// Agregar Productos
