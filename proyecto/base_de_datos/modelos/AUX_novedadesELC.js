ee763c148d557f73aedb34dc91c4c550c2c0e029
diribar <diegoiribarren2015@gmail.com>
2023-11-15 17:15:37 -0300

    Mejora continua

---
+++ proyecto/base_de_datos/modelos/AUX_novedadesELC.js
@@ -0,0 +1,23 @@
+module.exports = (sequelize, dt) => {
+	const alias = "novedadesELC";
+	const columns = {
+		comentario: {type: dt.STRING(60)},
+		fechaNovedad: {type: dt.DATE},
+		versionElc: {type: dt.STRING(4)},
+
+		permInputs: {type: dt.BOOLEAN},
+		autTablEnts:{type: dt.BOOLEAN},
+		revisorPERL: {type: dt.BOOLEAN},
+		revisorLinks: {type: dt.BOOLEAN},
+		revisorEnts: {type: dt.BOOLEAN},
+		revisorUs: {type: dt.BOOLEAN},
+	};
+	const config = {
+		tableName: "aux_novedades_elc",
+		timestamps: false,
+	};
+
+	const entidad = sequelize.define(alias, columns, config);
+
+	return entidad;
+};
