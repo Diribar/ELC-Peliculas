c2b911e004c61713d70dde42aae3ff2e3125dd42
diribar <diegoiribarren2015@gmail.com>
2023-11-15 15:34:05 -0300

    Mejora de procesos

--- proyecto/middlewares/transversales/loginConCookie.js
+++ proyecto/middlewares/transversales/loginConCookie.js
@@ -4,21 +4,22 @@
 const procesos = require("../../rutas_y_controladores/1-Usuarios/US-FN-Procesos");
 
 module.exports = async (req, res, next) => {
-	// Uso de cookies para identificar al usuario
-	if (!req.session.usuario && req.cookies && req.cookies.email) {
-		// Obtiene los datos del usuario
-		let usuario = await BD_especificas.obtieneUsuarioPorMail(req.cookies.email);
+	// Condiciones a superar
+	if (req.session.usuario) return next(); // si ya tiene 'session', saltea la rutina
+	if (!req.cookies || !req.cookies.email) return next(); // si no hay cookies, saltea la rutina
 
-		// Acciones si existe el usuario y ya confirmó el mail
-		if (usuario && usuario.statusRegistro_id != mailPendValidar_id) {
-			req.session.usuario = usuario; // Pasa los datos del usuario a session
-			res.cookie("email", usuario.email, {maxAge: unDia}); // Actualiza la vigencia de la cookie
-			if (usuario.pais_id) procesos.actualizaElContadorDeLogins(usuario); // Notifica al contador de logins
-		}
+	// Obtiene los datos del usuario
+	let usuario = await BD_especificas.obtieneUsuarioPorMail(req.cookies.email);
+
+	// Acciones si existe el usuario y ya confirmó el mail
+	if (usuario && usuario.statusRegistro_id != mailPendValidar_id) {
+		req.session.usuario = usuario; // Pasa los datos del usuario a session
+		res.cookie("email", usuario.email, {maxAge: unDia}); // Actualiza la vigencia de la cookie
+		if (usuario.pais_id) procesos.actualizaElContadorDeLogins(usuario); // Notifica al contador de logins
 	}
 
 	// Graba los datos del usuario a 'locals' para la vista
-	if (req.session.usuario && !res.locals.usuario) res.locals.usuario = req.session.usuario;
+	if (usuario && !res.locals.usuario) res.locals.usuario = usuario;
 
 	// Fin
 	next();
