92d82170d5f9f6c344ebea876140a6d5e23c26f0
diribar <diegoiribarren2015@gmail.com>
2023-12-01 12:27:08 -0300

    Mejora continua

--- proyecto/rutas_y_controladores/7-Graficos/GR-Rutas.js
+++ proyecto/rutas_y_controladores/7-Graficos/GR-Rutas.js
@@ -15,9 +15,9 @@ router.get("/api/links-por-proveedor", API.linksPorProv);
 // Vistas *******************************************
 router.get("/peliculas-cfc-vpc", vista.pelisCfcVpc);
 router.get("/peliculas-publico", vista.pelisPublico);
-
 router.get("/vencimiento-de-links", vista.vencimLinks);
 router.get("/links-por-proveedor", vista.linksPorProv);
+router.get("/rangos-sin-efemerides", vista.rangosSinEfemerides);
 
 // Exportarlo **********************************************
 module.exports = router;
