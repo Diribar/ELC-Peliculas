0aade86bfd15267d08bc2076437d36d009d36971
diribar <diegoiribarren2015@gmail.com>
2023-12-05 13:20:21 -0300

    Mejora continua

--- proyecto/middlewares/filtrosPorRegistro/linksEnSemana.js
+++ proyecto/middlewares/filtrosPorRegistro/linksEnSemana.js
@@ -12,7 +12,17 @@ module.exports = async (req, res, next) => {
 		cantLinksEstaSem < promSemanal || // si se aprobaron menos que el promedio semanal
 		((linksVencidos.length > promSemanal || cantVencsAnts) && // si hay más vencidos que el promedio o quedan vencidos de la semana anterior
 			cantLinksEstaSem < 1.05 * promSemanal); // si se aprobaron menos que el promedio más la tolerancia
+	if (aprobsPerms) return next();
+
+	// Obtiene el producto y si no está aprobado redirige al tablero
+	const {entidad, id} = req.query;
+	const producto = await BD_genericas.obtienePorIdConInclude(entidad, id, "links");
+	if (!aprobados_ids.includes(producto.statusRegistro_id)) return res.redirect("/revision/tablero-de-control")
+
+	// Obtiene los links y si no hay alguno en algún status esperado, redirige al tablero
+	const links = producto.links.filter((n) => [creado_id, inactivar_id, recuperar_id].includes(n.statusRegistro_id));
+	if (!links.length) return res.redirect("/revision/tablero-de-control")
 
 	// Fin
-	return !aprobsPerms ? res.redirect("/revision/tablero-de-control") : next();
+	return next();
 };
