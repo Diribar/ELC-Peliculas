6075b2e80fa6a69d449b8d9cba6366ed8fc7c778
diribar <diegoiribarren2015@gmail.com>
2023-12-11 20:55:11 -0300

    Mejora de procesos

--- proyecto/rutas_y_controladores/2.1-Prod-RUD/PR-FN-Procesos.js
+++ proyecto/rutas_y_controladores/2.1-Prod-RUD/PR-FN-Procesos.js
@@ -83,8 +83,7 @@ module.exports = {
 			// Reemplaza los embeded
 			for (let link of links) {
 				const provEmbeded = provsEmbeded.find((n) => n.id == link.prov_id);
-				if (provEmbeded) link.url = urlSitio + "/links/visualizacion/?link_id=" + link.id;
-				else link.url = "//" + link.url;
+				link.url = provEmbeded ? urlSitio + "/links/visualizacion/?link_id=" + link.id : "//" + link.url;
 			}
 
 			// Los separa entre Películas y Trailers
