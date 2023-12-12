04c24cd83dbb26fd33d6eead1dc44422a1586f27
diribar <diegoiribarren2015@gmail.com>
2023-12-11 23:08:35 -0300

    Mejora continua

--- proyecto/publico/javascript/2.3-Links-CRUD/LK2-Visualizar.js
+++ proyecto/publico/javascript/2.3-Links-CRUD/LK2-Visualizar.js
@@ -1,21 +1,19 @@
 "use strict";
 window.addEventListener("load", async () => {
 	// Variables
-	const body = document.querySelector("body");
+	const cuerpoFooter = document.querySelector("#cuerpoFooter");
 
 	// Obtiene el link y si no existe, termina la función
 	const link_id = new URL(location.href).searchParams.get("link_id");
 	if (!link_id) return;
 	const link = await fetch("/links/api/obtiene-embeded-link/?link_id=" + link_id).then((n) => n.json());
 	if (!link) return;
-	console.log(link);
-	//return
 
 	// Agrega el entorno del iframe
 	const div = document.createElement("div");
 	div.id = "videoDetPeli";
 	div.className = "absoluteCentro";
-	body.appendChild(div);
+	cuerpoFooter.appendChild(div);
 
 	// Agrega el iframe
 	const iframe = document.createElement("iframe");
