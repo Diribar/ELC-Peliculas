76d690c83606840893babcd070fd2736f4006a4e
diribar <diegoiribarren2015@gmail.com>
2023-12-14 12:13:08 -0300

    Mejora continua

--- proyecto/publico/javascript/3-RevisionEnts/RE3-Link1-AltasBajas.js
+++ proyecto/publico/javascript/3-RevisionEnts/RE3-Link1-AltasBajas.js
@@ -1,46 +1,66 @@
 "use strict";
 window.addEventListener("load", () => {
 	// Variables
+	const prodEntidad = new URL(location.href).searchParams.get("entidad");
+	const prodID = new URL(location.href).searchParams.get("id");
 	let DOM = {
-		yaExistentes: document.querySelectorAll(".yaExistentes"),
+		// Íconos addEventListeners
 		iconosIN: document.querySelectorAll(".yaExistentes .in"),
 		iconosFuera: document.querySelectorAll(".yaExistentes .fuera"),
+
+		// Objetos a ocultar
+		iconosOut: document.querySelectorAll(".yaExistentes .out"),
+		motivos: document.querySelectorAll(".yaExistentes .motivo"),
+		taparMotivo: document.querySelectorAll(".yaExistentes .taparMotivo"),
+
+		// Otros
+		yaExistentes: document.querySelectorAll(".yaExistentes"),
 		linksUrl: document.querySelectorAll(".yaExistentes input[name='url'"),
 		ancho_status: document.querySelectorAll(".yaExistentes .ancho_status"),
 	};
 	let v = {
-		prodEntidad: new URL(location.href).searchParams.get("entidad"),
-		prodID: new URL(location.href).searchParams.get("id"),
+		condiciones: "?prodEntidad=" + prodEntidad + "&prodID=" + prodID,
+		columnas: DOM.taparMotivo.length / DOM.yaExistentes.length,
 		ruta: "/revision/api/link/alta-baja/",
 	};
-	const condiciones = "?prodEntidad=" + v.prodEntidad + "&prodID=" + v.prodID;
 
 	// Decisión tomada
-	DOM.iconosIN.forEach((icono, indice) => {
+	DOM.iconosIN.forEach((icono, fila) => {
 		icono.addEventListener("click", async () => {
 			// Variables
-			let url = condiciones;
+			let url = v.condiciones;
 			// Completar el url
-			url += "&url=" + encodeURIComponent(DOM.linksUrl[indice].value);
+			url += "&url=" + encodeURIComponent(DOM.linksUrl[fila].value);
 			url += "&IN=SI";
 			url += "&aprob=" + (icono.className.includes("aprob") ? "SI" : "NO");
 			let respuesta = await fetch(v.ruta + url).then((n) => n.json());
 			if (respuesta.reload) {
-				DOM.yaExistentes[indice].classList.replace("oscuro_false", "oscuro_true");
-				DOM.ancho_status[indice].innerHTML = "Aprobado"
+				// Oculta objetos
+				icono.classList.add("ocultar");
+				DOM.iconosOut[fila].classList.add("ocultar");
+				DOM.iconosFuera[fila].classList.add("ocultar");
+				DOM.motivos[fila].classList.add("ocultar");
+
+				// Muestra los 6 campos
+				for (let columna = 0; columna < v.columnas; columna++)
+					DOM.taparMotivo[fila * v.columnas + columna].classList.remove("ocultar");
+
+				// Otros cambios
+				DOM.yaExistentes[fila].classList.replace("oscuro_false", "oscuro_true");
+				DOM.ancho_status[fila].innerHTML = "Aprobado";
 			}
 		});
 	});
-	DOM.iconosFuera.forEach((icono, indice) => {
+	DOM.iconosFuera.forEach((icono, fila) => {
 		icono.addEventListener("click", async () => {
 			// Variables
-			let url = condiciones;
+			let url = v.condiciones;
 			// Completar el url
-			url += "&url=" + encodeURIComponent(DOM.linksUrl[indice].value);
+			url += "&url=" + encodeURIComponent(DOM.linksUrl[fila].value);
 			url += "&IN=NO";
 			url += "&aprob=" + (icono.className.includes("aprob") ? "SI" : "NO");
 			let respuesta = await fetch(v.ruta + url).then((n) => n.json());
-			if (respuesta.reload) DOM.yaExistentes[indice].classList.add("ocultar");
+			if (respuesta.reload) DOM.yaExistentes[fila].classList.add("ocultar");
 		});
 	});
 });
