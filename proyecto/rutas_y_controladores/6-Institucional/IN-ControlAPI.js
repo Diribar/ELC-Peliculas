06f64d7dda2b3f58ecc07914d03ec3c42a08e6c6
diribar <diegoiribarren2015@gmail.com>
2023-12-15 07:05:56 -0300

    Mejora de procesos

--- proyecto/rutas_y_controladores/6-Institucional/IN-ControlAPI.js
+++ proyecto/rutas_y_controladores/6-Institucional/IN-ControlAPI.js
@@ -6,7 +6,7 @@ const valida = require("./IN-FN-Validar");
 module.exports = {
 	validaContactanos: async (req, res) => {
 		// Averigua los errores solamente para esos campos
-		let errores = await valida.contactanos(req.query);
+		const errores = await valida.contactanos(req.query);
 
 		// Devuelve el resultado
 		return res.json(errores);
