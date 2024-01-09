6d0c7a953f46364c10cb0f5e1221acba611d6b7d
diribar <diegoiribarren2015@gmail.com>
2024-01-09 08:25:28 -0300

    Miscelaneas

--- proyecto/rutas_y_controladores/2.1-Prod-Agregar/PA-ControlAPI.js
+++ proyecto/rutas_y_controladores/2.1-Prod-Agregar/PA-ControlAPI.js
@@ -16,10 +16,13 @@ module.exports = {
 		// Variables
 		let palabrasClave = req.query.palabrasClave;
 		let resultado;
+
 		// Obtiene los productos
 		resultado = await buscar_x_PC.search(palabrasClave);
+
 		// Revisa si debe reemplazar una película por su colección
-		resultado = await buscar_x_PC.reemplazoDePeliPorColeccion(resultado);
+		resultado = await buscar_x_PC.reemplazoDePeliPorColeccion.consolidado(resultado);
+
 		// Prepara la respuesta
 		let cantProds = resultado.productos.length;
 		let cantProdsNuevos = resultado.productos.filter((n) => !n.yaEnBD_id).length;
@@ -53,7 +56,7 @@ module.exports = {
 			let productos = req.session.desambiguar.prodProv;
 
 			// Revisa si debe reemplazar una película por su colección
-			productos = await buscar_x_PC.reemplazoDePeliPorColeccion(productos);
+			productos = await buscar_x_PC.reemplazoDePeliPorColeccion.consolidado(productos);
 
 			// Conserva la información en session
 			req.session.desambiguar.prodProv = productos;
