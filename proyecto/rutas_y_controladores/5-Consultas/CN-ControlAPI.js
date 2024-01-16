3bd4163d31cdc67e57d915a5f7459a8a20c80b3a
diribar <diegoiribarren2015@gmail.com>
2024-01-16 12:13:05 -0300

    Mejora continua

--- proyecto/rutas_y_controladores/5-Consultas/CN-ControlAPI.js
+++ proyecto/rutas_y_controladores/5-Consultas/CN-ControlAPI.js
@@ -145,10 +145,7 @@ module.exports = {
 
 		// Obtiene los productos, rclvs y registros ppp del usuario
 		let prods = procesos.resultados.obtieneProds.comun({...configCons, opcion});
-		let rclvs =
-			opcion.codigo.startsWith("fechaDelAno")
-				? procesos.resultados.obtieneRclvs.porFechaDelAno(configCons)
-				: procesos.resultados.obtieneRclvs.comun({...configCons, opcion});
+		let rclvs = procesos.resultados.obtieneRclvs.consolidado({...configCons, opcion});
 		let pppRegistros = usuario_id
 			? BD_genericas.obtieneTodosPorCondicionConInclude("pppRegistros", {usuario_id}, "detalle")
 			: [];
