3c3f811ccb38e35ca5230be4b67ef81f25ffcf66
diribar <diegoiribarren2015@gmail.com>
2024-01-18 09:04:04 -0300

    Solución de errores

--- proyecto/rutas_y_controladores/9-Miscelaneas/MS-ControlAPI.js
+++ proyecto/rutas_y_controladores/9-Miscelaneas/MS-ControlAPI.js
@@ -50,8 +50,8 @@ module.exports = {
 		});
 
 		// Armado de la variable 'datos' para ediciones
-		datos.push({familia: "producto", entidad: "prodsEdicion", campos: camposProds}); // productos
-		datos.push({familia: "rclv", entidad: "rclvsEdicion", campos: camposPers}); // rclvs
+		datos.push({familia: "producto", entidad: "prodsEdicion", campos: camposProds, include: asocsProds}); // productos
+		datos.push({familia: "rclv", entidad: "rclvsEdicion", campos: camposPers, include: asocsRCLVs}); // rclvs
 
 		// Rutina
 		for (let dato of datos) {
@@ -62,14 +62,25 @@ module.exports = {
 			aux.push(
 				dato.original
 					? BD_especificas.quickSearchRegistros(condiciones, dato)
-					: BD_especificas.quickSearchEdiciones(condiciones, dato)
+					: BD_especificas.quickSearchEdiciones(condiciones, dato, dato.include)
 			);
 		}
 		await Promise.all(aux).then((n) => n.map((m) => resultados.push(...m)));
 
-		// Ordena los resultados
-		resultados.sort((a, b) => (a.nombre < b.nombre ? -1 : 1)); // segunda prioridad: nombre
-		resultados.sort((a, b) => (a.familia < b.familia ? -1 : 1)); // primera prioridad: familia
+		// Acciones si hay más de un resultado
+		if (resultados.length > 1) {
+			// Ordena los resultados
+			resultados.sort((a, b) => (a.nombre < b.nombre ? -1 : 1)); // segunda prioridad: nombre
+			resultados.sort((a, b) => (a.familia < b.familia ? -1 : 1)); // primera prioridad: familia
+
+			// Elimina duplicados
+			for (let i = resultados.length - 2; i >= 0; i--) {
+				const {entidad: entidad1, id: id1, nombre: nombre1, anoEstreno: anoEstreno1} = resultados[i];
+				const {entidad: entidad2, id: id2, nombre: nombre2, anoEstreno: anoEstreno2} = resultados[i + 1];
+				if ((entidad1 == entidad2 && id1 == id2 && nombre1 == nombre2, anoEstreno1 == anoEstreno2))
+					resultados.splice(i + 1, 1);
+			}
+		}
 
 		// Envia la info al FE
 		return res.json(resultados);
