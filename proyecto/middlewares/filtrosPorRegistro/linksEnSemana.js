37b336f36e4ca053033b36c987b0d1a2e71e81a6
diribar <diegoiribarren2015@gmail.com>
2024-01-21 13:01:53 -0300

    Solución de errores

--- proyecto/middlewares/filtrosPorRegistro/linksEnSemana.js
+++ proyecto/middlewares/filtrosPorRegistro/linksEnSemana.js
@@ -3,7 +3,8 @@ const procesos = require("../../rutas_y_controladores/3-RevisionEnts/RE-Procesos
 
 module.exports = async (req, res, next) => {
 	// Variables
-	const prodSig = await procesos.TC.obtieneLinkSig();
+	const revID = req.session.usuario.id;
+	const prodSig = await procesos.TC.obtieneSigProd(revID);
 
 	// Obtiene los links y si no hay alguno en algún status esperado, redirige al tablero
 	if (!prodSig) return res.redirect("/revision/tablero-de-control")
