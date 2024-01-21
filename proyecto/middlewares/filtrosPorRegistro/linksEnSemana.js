18f92c1d528240d7b78772554b33331b537a8898
diribar <diegoiribarren2015@gmail.com>
2024-01-21 12:34:45 -0300

    Mejora de procesos

--- proyecto/middlewares/filtrosPorRegistro/linksEnSemana.js
+++ proyecto/middlewares/filtrosPorRegistro/linksEnSemana.js
@@ -3,10 +3,10 @@ const procesos = require("../../rutas_y_controladores/3-RevisionEnts/RE-Procesos
 
 module.exports = async (req, res, next) => {
 	// Variables
-	const linkSig = await procesos.TC.obtieneLinkSig();
+	const prodSig = await procesos.TC.obtieneLinkSig();
 
 	// Obtiene los links y si no hay alguno en algún status esperado, redirige al tablero
-	if (!linkSig) return res.redirect("/revision/tablero-de-control")
+	if (!prodSig) return res.redirect("/revision/tablero-de-control")
 
 	// Fin
 	return next();
