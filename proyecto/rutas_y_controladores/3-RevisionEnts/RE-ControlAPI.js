f6221da0dda235a3e9f1626bac648b1414f5f0d8
diribar <diegoiribarren2015@gmail.com>
2024-01-25 14:54:37 -0300

    Solución de errores

--- proyecto/rutas_y_controladores/3-RevisionEnts/RE-ControlAPI.js
+++ proyecto/rutas_y_controladores/3-RevisionEnts/RE-ControlAPI.js
@@ -97,7 +97,12 @@ module.exports = {
 			}
 
 			// Más variables
-			const {id, statusRegistro_id, decisAprob, datos, campoDecision, motivo_id} = procesos.links.variables({link, ahora, req});
+			const {id, statusRegistro_id, decisAprob, datos, campoDecision, motivo_id} = procesos.links.variables({
+				link,
+				ahora,
+				req,
+				semana,
+			});
 
 			// CONSECUENCIAS - Actualiza el registro del link
 			await BD_genericas.actualizaPorId("links", id, datos);
