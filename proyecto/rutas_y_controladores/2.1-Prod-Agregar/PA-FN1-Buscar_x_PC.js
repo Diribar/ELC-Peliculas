f5274b71cbe85728ced27e5a297052a6ddf2327f
diribar <diegoiribarren2015@gmail.com>
2024-01-27 00:23:16 -0300

    Solución de errores

--- proyecto/rutas_y_controladores/2.1-Prod-Agregar/PA-FN1-Buscar_x_PC.js
+++ proyecto/rutas_y_controladores/2.1-Prod-Agregar/PA-FN1-Buscar_x_PC.js
@@ -274,8 +274,8 @@ module.exports = {
 			// Chequea que sea una colección, y que la cantidad de capítulos sea diferente entre TMDB y ELC - no hace falta el 'await'
 			for (let coleccion of resultados.prodsYaEnBD)
 				if (coleccion.capitulos && coleccion.capitulos != coleccion.capitulosELC) {
-					if (coleccion.TMDB_entidad == "collection") agregaCapitulosCollection(coleccion);
-					if (coleccion.TMDB_entidad == "tv") agregaCapitulosTV(coleccion);
+					if (coleccion.TMDB_entidad == "collection") agregaCapitulosCollection(coleccion); // sin 'await'
+					if (coleccion.TMDB_entidad == "tv") agregaCapitulosTV(coleccion); // sin 'await'
 				}
 
 			// Fin
@@ -283,12 +283,14 @@ module.exports = {
 		};
 		let agregaCapitulosCollection = async (coleccion) => {
 			// Recorre los capítulos
-			await coleccion.capsTMDB_id.forEach(async (capituloID_TMDB, indice) => {
-				// Averigua si algún capítulo es nuevo
-				if (!coleccion.capitulosID_ELC.includes(String(capituloID_TMDB))) {
-					// Si es nuevo, lo agrega
-					await procesos.confirma.agregaUnCap_Colec(coleccion, capituloID_TMDB, indice);
-				}
+			await coleccion.capsTMDB_id.forEach(async (capTMDB_id, indice) => {
+				// Si algún capítulo es nuevo, lo agrega
+				if (
+					!coleccion.capitulosID_ELC.includes(String(capTMDB_id)) && // el TMDB_id no existe entre los capítulos de la colección
+					!(await BD_genericas.obtienePorCondicion("capitulos", {TMDB_id: capTMDB_id}))&& // el TMDB_id no existe en la tabla de capítulos
+					!(await BD_genericas.obtienePorCondicion("peliculas", {TMDB_id: capTMDB_id})) // el TMDB_id no existe en la tabla de películas
+				)
+					await procesos.confirma.agregaUnCap_Colec(coleccion, capTMDB_id, indice);
 			});
 			// Fin
 			return;
