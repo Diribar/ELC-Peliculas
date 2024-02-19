7dceadc31466c63e1393f9b4ee93debcc4994b96
diribar <diegoiribarren2015@gmail.com>
2024-02-19 00:24:05 -0300

    Solución de errores

--- proyecto/middlewares/filtrosPorRegistro/linksEnSemana.js
+++ proyecto/middlewares/filtrosPorRegistro/linksEnSemana.js
@@ -7,13 +7,13 @@ module.exports = async (req, res, next) => {
 
 	// Averigua si hay links para procesar
 	const entidad = req.query.entidad == "capitulos" ? "capitulos" : "pelisColes";
-	const hayRegistros = cantLinksVencPorSem.paraProc[entidad];
+	let hayRegistros = cantLinksVencPorSem.paraProc[entidad];
 
 	// Averigua si hay linksEdicion para procesar
 	if (!hayRegistros) {
 		const campo_id = comp.obtieneDesdeEntidad.campo_id(req.query.entidad);
 		const id = req.query.id;
-		// hayRegistros = await BD_genericas.obtienePorCondicion("linksEdicion", {[campo_id]: id});
+		hayRegistros = await BD_genericas.obtienePorCondicion("linksEdicion", {[campo_id]: id});
 	}
 
 	// Si no hay registros a procesar, redirige al tablero
