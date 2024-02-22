93e8893c4912bc22556ef6df2238cfb52d366a4d
diribar <diegoiribarren2015@gmail.com>
2024-02-21 23:02:20 -0300

    Mejora de procesos

--- proyecto/rutas_y_controladores/4-RevisionUs/RU-Rutas.js
+++ proyecto/rutas_y_controladores/4-RevisionUs/RU-Rutas.js
@@ -8,19 +8,8 @@ const usAltaTerm = require("../../middlewares/filtrosPorUsuario/usAltaTerm");
 const usPenalizaciones = require("../../middlewares/filtrosPorUsuario/usPenalizaciones");
 const revisorUs = require("../../middlewares/filtrosPorUsuario/usRolRevUs");
 
-// Middlewares - Específicos de entidades
-const IDvalido = require("../../middlewares/filtrosPorRegistro/IDvalido");
-const statusCorrecto = require("../../middlewares/filtrosPorRegistro/statusCorrecto");
-const usAptoValidaIdent = require("../../middlewares/filtrosPorUsuario/usAptoValidaIdent");
-
-// Middlewares - Temas de captura
-const permUserReg = require("../../middlewares/filtrosPorRegistro/permUserReg");
-const capturaActivar = require("../../middlewares/varios/capturaActivar");
-const capturaInactivar = require("../../middlewares/varios/capturaInactivar");
-
 // Middlewares - Consolidados
 const aptoRevisor = [usAltaTerm, usPenalizaciones, revisorUs];
-const aptoRevMasRegistro = [...aptoRevisor, IDvalido, statusCorrecto, usAptoValidaIdent, permUserReg];
 
 // Vistas
 router.get("/tablero-de-control", aptoRevisor, vista.tableroControl);
