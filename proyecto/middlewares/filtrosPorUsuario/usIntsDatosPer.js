9bc4747019302c21199aabf33121fedda060d20d
diribar <diegoiribarren2015@gmail.com>
2024-02-24 09:18:17 -0300

    Mejora de procesos

--- proyecto/middlewares/filtrosPorUsuario/usIntsDatosPer.js
+++ proyecto/middlewares/filtrosPorUsuario/usIntsDatosPer.js
@@ -1,20 +1,16 @@
 "use strict";
-module.exports = (req, res, next) => {
+module.exports = async (req, res, next) => {
 	// Variables
-	let login, usuario;
+	const usuario = await BD_especificas.obtieneUsuarioPorMail(email);
 
 	// Reenvíos por cookies
 	if (req.cookies && req.cookies.intsLogin > intsLogins_PC) return res.redirect("/usuario/login/suspendido");
 	if (req.cookies && req.cookies.intsDatosPer > intsDatosPer_PC) return res.redirect("/usuario/olvido-contrasena/suspendido");
 
-	// Reenvíos x BD - sólo si existe la cookie de 'olvidoContr'
-	if (req.cookies && req.cookies.olvidoContr) {
-		if (req.cookies && req.cookies.login) login = req.cookies.login;
-		if (!login) return res.redirect("/usuario/login"); // debe existir la cookie de login
-		if (login && login.usuario) usuario = login.usuario;
-		if (usuario && usuario.intsDatosPer && usuario.intsDatosPer > intsDatosPer_BD)
-			return res.redirect("/usuario/olvido-contrasena/suspendido");
-	}
+	// Reenvíos x BD
+	if (!usuario) return res.redirect("/usuario/login");
+	if (usuario && usuario.intsLogin > instLogins_BD) return res.redirect("/usuario/login/suspendido");
+	if (usuario.intsDatosPer > intsDatosPer_BD) return res.redirect("/usuario/olvido-contrasena/suspendido");
 
 	// Fin
 	next();
