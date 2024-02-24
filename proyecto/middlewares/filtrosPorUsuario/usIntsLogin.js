25d1472d74589cc3793c02c2fcc7d69e32b097d7
diribar <diegoiribarren2015@gmail.com>
2024-02-24 08:26:29 -0300

    Mejora de procesos

--- proyecto/middlewares/filtrosPorUsuario/usIntsLogin.js
+++ proyecto/middlewares/filtrosPorUsuario/usIntsLogin.js
@@ -4,12 +4,12 @@ module.exports = (req, res, next) => {
 	let login, usuario;
 
 	// Reenvíos por cookies
-	if (req.cookies && req.cookies.intsLogin > 2) return res.redirect("/usuario/login/suspendido");
+	if (req.cookies && req.cookies.intsLogin > intsLogins_PC) return res.redirect("/usuario/login/suspendido");
 
 	// Reenvíos x BD
 	if (req.cookies && req.cookies.login) login = req.cookies.login;
 	if (login && login.usuario) usuario = login.usuario;
-	if (usuario && usuario.intsLogin && usuario.intsLogin > 3) return res.redirect("/usuario/login/suspendido");
+	if (usuario && usuario.intsLogin && usuario.intsLogin > instLogins_BD) return res.redirect("/usuario/login/suspendido");
 
 	// Fin
 	next();
