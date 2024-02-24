faa20628c2dd00dac5ee09cf8b15d794cbf760ab
diribar <diegoiribarren2015@gmail.com>
2024-02-24 09:10:52 -0300

    Mejora continua

--- proyecto/middlewares/filtrosPorUsuario/usIntsDatosPer.js
+++ proyecto/middlewares/filtrosPorUsuario/usIntsDatosPer.js
@@ -4,14 +4,17 @@ module.exports = (req, res, next) => {
 	let login, usuario;
 
 	// Reenvíos por cookies
+	if (req.cookies && req.cookies.intsLogin > intsLogins_PC) return res.redirect("/usuario/login/suspendido");
 	if (req.cookies && req.cookies.intsDatosPer > intsDatosPer_PC) return res.redirect("/usuario/olvido-contrasena/suspendido");
 
-	// Reenvíos x BD
-	if (req.session && req.session.login) login = req.session.login;
-	if (!login) return res.redirect("/usuario/login");
-	if (login && login.usuario) usuario = login.usuario;
-	if (usuario && usuario.intsDatosPer && usuario.intsDatosPer > intsDatosPer_BD)
-		return res.redirect("/usuario/olvido-contrasena/suspendido");
+	// Reenvíos x BD - sólo si existe la cookie de 'olvidoContr'
+	if (req.cookies && req.cookies.olvidoContr) {
+		if (req.cookies && req.cookies.login) login = req.cookies.login;
+		if (!login) return res.redirect("/usuario/login"); // debe existir la cookie de login
+		if (login && login.usuario) usuario = login.usuario;
+		if (usuario && usuario.intsDatosPer && usuario.intsDatosPer > intsDatosPer_BD)
+			return res.redirect("/usuario/olvido-contrasena/suspendido");
+	}
 
 	// Fin
 	next();
