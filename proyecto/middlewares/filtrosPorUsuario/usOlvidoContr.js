45a00523ccda8cec4748a05b6c3595715f90587f
diribar <diegoiribarren2015@gmail.com>
2024-02-24 20:14:05 -0300

    Solución de errores

--- proyecto/middlewares/filtrosPorUsuario/usOlvidoContr.js
+++ proyecto/middlewares/filtrosPorUsuario/usOlvidoContr.js
@@ -3,12 +3,12 @@
 const valida = require("../../rutas_y_controladores/1-Usuarios/US-FN-Validar");
 
 module.exports = async (req, res, next) => {
-	// Redirecciona por cookies
+		// Redirecciona por cookies
 	if (req.cookies && req.cookies.intentos_Login > intentos_Cookies) return res.redirect("/usuario/login/suspendido");
 	if (req.cookies && req.cookies.intentos_DP > intentos_Cookies) return res.redirect("/usuario/olvido-contrasena/suspendido");
 
 	// Redirecciona por errores con el mail
-	const {email} = req.query;
+		const {email} = req.query;
 	const {errores, usuario} = await valida.olvidoContr.email(email);
 	if (errores.hay) {
 		// Actualiza la session
@@ -20,18 +20,15 @@ module.exports = async (req, res, next) => {
 	}
 
 	// Redirecciona por BD
-	if (!usuario) return res.redirect("/usuario/login");
+		if (!usuario) return res.redirect("/usuario/login");
 	if (usuario && usuario.intentos_Login > intentos_BD) return res.redirect("/usuario/login/suspendido");
 	if (usuario.intentos_DP > intentos_BD) return res.redirect("/usuario/olvido-contrasena/suspendido");
 
-	// Crea la cookie
+	// Crea la session
 	const mostrarCampos = usuario.statusRegistro_id == perennes_id; // si el usuario tiene status 'perenne_id', se muestran todos los campos
-	const datos =
-		req.session && req.session.olvidoContr && req.session.olvidoContr.datos
-			? {...req.session.olvidoContr.datos, email}
-			: {email};
-	res.cookie("olvidoContr", {datos, errores, usuario, mostrarCampos}, {maxAge: unDia});
+	const datos = req.session && req.session.olvidoContr ? {...req.session.olvidoContr.datos, email} : {email};
+	req.session.olvidoContr = {datos, errores, usuario, mostrarCampos};
 
 	// Fin
-	next();
+		next();
 };
