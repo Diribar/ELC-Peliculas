95e639dbe3d6342e4ddb585fbbaa3f8cc0064940
diribar <diegoiribarren2015@gmail.com>
2024-02-23 23:11:48 -0300

    Mejora de procesos

--- proyecto/middlewares/filtrosPorUsuario/usIntsDatosPer.js
+++ proyecto/middlewares/filtrosPorUsuario/usIntsDatosPer.js
@@ -4,12 +4,13 @@ module.exports = (req, res, next) => {
 	let login, usuario;
 
 	// Reenvíos por cookies
-	if (req.cookies && req.cookies.intsDatosPer > 2) return res.redirect("/olvido-contrasena/suspendido");
+	if (req.cookies && req.cookies.intsDatosPer > 2) return res.redirect("/usuario/olvido-contrasena/suspendido");
 
 	// Reenvíos x BD
 	if (req.session && req.session.login) login = req.session.login;
+	if (!login) return res.redirect("/usuario/login");
 	if (login && login.usuario) usuario = login.usuario;
-	if (usuario && usuario.intsDatosPer && usuario.intsDatosPer > 3) return res.redirect("/olvido-contrasena/suspendido");
+	if (usuario && usuario.intsDatosPer && usuario.intsDatosPer > 3) return res.redirect("/usuario/olvido-contrasena/suspendido");
 
 	// Fin
 	next();
