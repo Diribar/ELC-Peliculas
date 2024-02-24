4df8a31c8083eba973f753d367e741af34fc0a93
diribar <diegoiribarren2015@gmail.com>
2024-02-24 20:46:55 -0300

    Mejora de procesos

--- proyecto/middlewares/filtrosPorUsuario/usOlvidoContr.js
+++ proyecto/middlewares/filtrosPorUsuario/usOlvidoContr.js
@@ -3,12 +3,12 @@
 const valida = require("../../rutas_y_controladores/1-Usuarios/US-FN-Validar");
 
 module.exports = async (req, res, next) => {
-		// Redirecciona por cookies
+	// Redirecciona por cookies
 	if (req.cookies && req.cookies.intentos_Login > intentos_Cookies) return res.redirect("/usuario/login/suspendido");
 	if (req.cookies && req.cookies.intentos_DP > intentos_Cookies) return res.redirect("/usuario/olvido-contrasena/suspendido");
 
 	// Redirecciona por errores con el mail
-		const {email} = req.query;
+	const {email} = req.query;
 	const {errores, usuario} = await valida.olvidoContr.email(email);
 	if (errores.hay) {
 		// Actualiza la session
@@ -20,7 +20,6 @@ module.exports = async (req, res, next) => {
 	}
 
 	// Redirecciona por BD
-		if (!usuario) return res.redirect("/usuario/login");
 	if (usuario && usuario.intentos_Login > intentos_BD) return res.redirect("/usuario/login/suspendido");
 	if (usuario.intentos_DP > intentos_BD) return res.redirect("/usuario/olvido-contrasena/suspendido");
 
@@ -30,5 +29,5 @@ module.exports = async (req, res, next) => {
 	req.session.olvidoContr = {datos, errores, usuario, mostrarCampos};
 
 	// Fin
-		next();
+	next();
 };
