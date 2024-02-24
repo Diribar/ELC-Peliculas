dc6ee947d0bf061fb60232563b8ff2f744d88185
diribar <diegoiribarren2015@gmail.com>
2024-02-24 09:18:58 -0300

    Mejora de procesos

--- proyecto/rutas_y_controladores/1-Usuarios/US-Rutas.js
+++ proyecto/rutas_y_controladores/1-Usuarios/US-Rutas.js
@@ -30,7 +30,7 @@ router.get("/api/video-de-consultas-visto", API.videoConsVisto);
 // Vistas - Sólo visitas
 router.get("/garantiza-login-y-completo", vista.loginCompleto);
 router.get("/alta-mail", usIntsLogin, visitas, vista.altaMail_olvidoContr);
-router.get("/olvido-contrasena", usIntsLogin, usIntsDatosPer, visitas, vista.altaMail_olvidoContr);
+router.get("/olvido-contrasena", usIntsDatosPer, visitas, vista.altaMail_olvidoContr);
 
 // Vistas - Editables
 router.get("/editables", statusCorrecto, vista.editables.form);
