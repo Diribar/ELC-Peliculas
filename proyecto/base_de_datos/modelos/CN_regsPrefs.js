0351a02774eeae3a804767877544394cf8829d2c
diribar <diegoiribarren2015@gmail.com>
2024-03-03 13:33:30 -0300

    Cambios de nombre

--- proyecto/base_de_datos/modelos/CN_regsPrefs.js
+++ proyecto/base_de_datos/modelos/CN_regsPrefs.js
@@ -1,12 +1,12 @@
 module.exports = (sequelize, dt) => {
-	const alias = "consRegsCampos";
+	const alias = "consRegsPrefs";
 	const columns = {
 		cabecera_id: {type: dt.INTEGER},
 		campo: {type: dt.STRING(20)},
 		valor: {type: dt.STRING(15)},
 	};
 	const config = {
-		tableName: "cn_regs_campos",
+		tableName: "cn_regs_prefs",
 		timestamps: false,
 	};
 	const entidad = sequelize.define(alias, columns, config);
