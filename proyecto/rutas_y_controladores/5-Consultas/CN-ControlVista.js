ec19004d53233cde153d124f4f2321dfded36bf5
diribar <diegoiribarren2015@gmail.com>
2024-03-03 12:28:36 -0300

    Mejora de procesos

--- proyecto/rutas_y_controladores/5-Consultas/CN-ControlVista.js
+++ proyecto/rutas_y_controladores/5-Consultas/CN-ControlVista.js
@@ -12,11 +12,21 @@ module.exports = {
 
 		// Configuraciones de consulta
 		const configsConsCabs = await procesos.configs.cabecera(userID); // Se necesita esa función también para la API
-		const configCons_cabeceras = {
-			propios: configsConsCabs.filter((n) => userID && n.usuario_id == userID),
-			predeterms: configsConsCabs.filter((n) => n.usuario_id == 1),
+		const configsCons = {
+			cabeceras: {
+				propios: configsConsCabs.filter((n) => userID && n.usuario_id == userID),
+				predeterms: configsConsCabs.filter((n) => n.usuario_id == 1),
+			},
+			prefs: procesos.configs.prefs(),
 		};
-		const configCons_campos = procesos.configs.campos();
+
+		// Si la configuración está en la url, toma el valor y redirige para eliminarlo de la url
+		const prefsCons = req.query;
+		if (Object.keys(prefsCons).length) {
+			procesos.configCons_url(req);
+			const ruta = req.protocol + "://" + req.headers.host + req.baseUrl;
+			return res.redirect(ruta);
+		}
 
 		// Variables para la vista
 		const cabecera_id = await procesos.configs.obtieneConfigCons_BD({usuario}).then((n) => n.cabecera_id);
@@ -25,26 +35,9 @@ module.exports = {
 		// Va a la vista
 		return res.render("CMP-0Estructura", {
 			...{tema, titulo, userID, cabecera_id},
-			...{configCons_cabeceras, configCons_campos, ayudas},
+			...{configsCons, ayudas},
 			omitirFooter: true,
 		});
 	},
 };
 
-let configCons_url = (req) => {
-	// Si la configuración está en la url, toma el valor y redirige para eliminarlo de la url
-	const configCons = Number(req.query.prefsCons);
-	const existe = configConsUrl_id ? configs.find((n) => n.id == configCons.id) : null;
-	if (existe) {
-		// Actualiza el usuario
-		if (usuario) {
-			const configCons_id = configCons.id;
-			BD_genericas.actualizaPorId("usuarios", userID, {configCons_id});
-			req.session.usuario = {...usuario, configCons_id};
-		}
-
-		// Redirecciona quitando los parámetros del 'url'
-		const ruta = req.baseUrl + req.path;
-		return res.redirect(ruta);
-	}
-};
