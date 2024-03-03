c82af461f0b85050460afb13cb612d966f045d17
diribar <diegoiribarren2015@gmail.com>
2024-03-03 07:40:02 -0300

    Cambios de nombre

--- proyecto/rutas_y_controladores/5-Consultas/CN-ControlAPI.js
+++ proyecto/rutas_y_controladores/5-Consultas/CN-ControlAPI.js
@@ -9,7 +9,7 @@ module.exports = {
 			const {configCons_id} = req.query;
 
 			// Obtiene la cabecera
-			const configCabecera = await BD_genericas.obtienePorId("configsConsCabeceras", configCons_id);
+			const configCabecera = await BD_genericas.obtienePorId("consRegsCabecera", configCons_id);
 
 			// Fin
 			return res.json(configCabecera);
@@ -97,7 +97,7 @@ module.exports = {
 
 			// Guarda el registro de cabecera
 			const objeto = {usuario_id, nombre: configCons.nombre};
-			const {id: configCons_id} = await BD_genericas.agregaRegistro("configsConsCabeceras", objeto);
+			const {id: configCons_id} = await BD_genericas.agregaRegistro("consRegsCabecera", objeto);
 
 			// Fin
 			return res.json(configCons_id);
@@ -120,7 +120,7 @@ module.exports = {
 			for (let campo in configCons) if (configCons[campo] == prefsConDefault[campo]) delete configCons[campo];
 
 			// Acciones para edición
-			if (configCons.edicion) BD_genericas.actualizaPorId("configsConsCabeceras", id, {nombre: configCons.nombre});
+			if (configCons.edicion) BD_genericas.actualizaPorId("consRegsCabecera", id, {nombre: configCons.nombre});
 			// Acciones para 'nuevo' y 'actualizar campos'
 			else {
 				// Si se guardan cambios, se eliminan session y cookie
@@ -147,7 +147,7 @@ module.exports = {
 			await BD_genericas.eliminaTodosPorCondicion("configsConsCampos", {configCons_id});
 
 			// Se elimina el registro de cabecera de la configuración
-			await BD_genericas.eliminaPorId("configsConsCabeceras", configCons_id);
+			await BD_genericas.eliminaPorId("consRegsCabecera", configCons_id);
 
 			// Fin
 			return res.json();
