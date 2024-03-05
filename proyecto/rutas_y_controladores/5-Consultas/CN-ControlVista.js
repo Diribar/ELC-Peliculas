2442e1b1a65eef1fdd0de2eaffa6cddd1c68f4e7
diribar <diegoiribarren2015@gmail.com>
2024-03-05 10:21:36 -0300

    Mejora de procesos

--- proyecto/rutas_y_controladores/5-Consultas/CN-ControlVista.js
+++ proyecto/rutas_y_controladores/5-Consultas/CN-ControlVista.js
@@ -4,6 +4,14 @@ const procesos = require("./CN-Procesos");
 
 module.exports = {
 	consultas: async (req, res) => {
+		// Si la configuración está en la url, toma el valor y redirige para eliminarla
+		const configCons = req.query;
+		if (Object.keys(configCons).length) {
+			procesos.configs.configCons_url(req);
+			const ruta = req.protocol + "://" + req.headers.host + req.baseUrl;
+			return res.redirect(ruta);
+		}
+
 		// Variables
 		const tema = "consultas";
 		const titulo = "Consulta de Películas";
@@ -11,7 +19,7 @@ module.exports = {
 		const userID = usuario ? usuario.id : null;
 
 		// Configuraciones de consulta
-		const configsConsCabs = await procesos.configs.cabecera(userID); // Se necesita esa función también para la API
+		const configsConsCabs = await procesos.configs.cabeceras(userID); // Se necesita esa función también para la API
 		const configsCons = {
 			cabeceras: {
 				propios: configsConsCabs.filter((n) => userID && n.usuario_id == userID),
@@ -20,22 +28,13 @@ module.exports = {
 			filtros: procesos.configs.filtros(),
 		};
 
-		// Si la configuración está en la url, toma el valor y redirige para eliminarlo de la url
-		const prefsCons = req.query;
-		if (Object.keys(prefsCons).length) {
-			procesos.configCons_url(req);
-			const ruta = req.protocol + "://" + req.headers.host + req.baseUrl;
-			return res.redirect(ruta);
-		}
-
 		// Variables para la vista
-		const cabecera_id = await procesos.configs.obtieneConfigCons_BD({usuario}).then((n) => n.cabecera_id);
 		const ayudas = procesos.configs.ayudas();
 
 		// Va a la vista
 		return res.render("CMP-0Estructura", {
 			...{tema, titulo, userID},
-			...{cabecera_id, configsCons, ayudas},
+			...{configsCons, ayudas}, // no lleva datos de la configuración actual
 			omitirFooter: true,
 		});
 	},
