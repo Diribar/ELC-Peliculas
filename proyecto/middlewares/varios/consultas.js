ac8237047acedf6d19dd5c92fb3fd9d91cca0d74
diribar <diegoiribarren2015@gmail.com>
2024-03-06 11:33:52 -0300

    Mejora de procesos

---
+++ proyecto/middlewares/varios/consultas.js
@@ -0,0 +1,19 @@
+"use strict";
+// Variables
+const procesos = require("../../rutas_y_controladores/5-Consultas/CN-Procesos");
+
+module.exports = async (req, res, next) => {
+	// Si la configuración está en la url, toma el valor y redirige para eliminarla
+	const configCons = req.query;
+	if (Object.keys(configCons).length) {
+		procesos.configs.configCons_url(req, res);
+		const ruta = req.protocol + "://" + req.headers.host + req.baseUrl;
+		return res.redirect(ruta);
+	}
+
+	// Activa session con cookie
+	if (!req.session.configCons && req.cookies.configCons) req.session.configCons = req.cookies.configCons;
+
+	// Continuar
+	next();
+};
