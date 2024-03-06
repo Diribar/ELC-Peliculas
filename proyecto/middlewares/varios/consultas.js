fdc01f36e7c700476c266872a51dc349981dbd6d
diribar <diegoiribarren2015@gmail.com>
2024-03-06 14:49:47 -0300

    Cambios de nombre

--- proyecto/middlewares/varios/consultas.js
+++ proyecto/middlewares/varios/consultas.js
@@ -6,7 +6,7 @@ module.exports = async (req, res, next) => {
 	// Si la configuración está en la url, toma el valor y redirige para eliminarla
 	const configCons = req.query;
 	if (Object.keys(configCons).length) {
-		procesos.configs.configCons_url(req, res);
+		procesos.varios.configCons_url(req, res);
 		const ruta = req.protocol + "://" + req.headers.host + req.baseUrl;
 		return res.redirect(ruta);
 	}
