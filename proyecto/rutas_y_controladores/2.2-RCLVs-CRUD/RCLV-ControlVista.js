85b3f09a788e73c67968f6b1fab8d79fc8fa96d8
diribar <diegoiribarren2015@gmail.com>
2024-03-19 17:45:39 -0300

    Miscelaneas

--- proyecto/rutas_y_controladores/2.2-RCLVs-CRUD/RCLV-ControlVista.js
+++ proyecto/rutas_y_controladores/2.2-RCLVs-CRUD/RCLV-ControlVista.js
@@ -58,7 +58,7 @@ module.exports = {
 			// Tema y Código
 			const {baseUrl, ruta} = comp.reqBasePathUrl(req);
 			const tema = baseUrl == "/rclv" ? "rclvCrud" : baseUrl == "/revision" ? "revisionEnts" : "";
-			const codigo = ruta.slice(1, -1); // Resultados posibles: 'agregar', 'edicion', 'alta', 'rclv/solapamiento'
+			const codigo = ruta.slice(1, -1); // resultados posibles: 'agregar', 'edicion', 'alta', 'rclv/alta', 'rclv/solapamiento'
 
 			// Más variables
 			const {entidad, id, prodEntidad, prodID} = req.query;
