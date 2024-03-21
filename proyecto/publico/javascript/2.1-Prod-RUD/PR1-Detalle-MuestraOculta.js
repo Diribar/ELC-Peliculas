bb585890bb5102be91f1f744443893a8b3a23f72
diribar <diegoiribarren2015@gmail.com>
2024-03-21 04:28:42 -0300

    Mejora continua

--- proyecto/publico/javascript/2.1-Prod-RUD/PR1-Detalle-MuestraOculta.js
+++ proyecto/publico/javascript/2.1-Prod-RUD/PR1-Detalle-MuestraOculta.js
@@ -2,15 +2,43 @@
 window.addEventListener("load", () => {
 	// Variables
 	let DOM = {
-		imagen: document.querySelector("#imgDerecha #centro img"),
-		links: document.querySelector("#imgDerecha #centro #links"),
-		conjuntoIconos: document.querySelector("#imgDerecha #conjuntoIconos"),
+		cuerpo: document.querySelector("#cuerpo #datos"),
+		imgDerecha: document.querySelector("#imgDerecha"),
+	};
+	DOM = {
+		...DOM,
+
+		// Sector Cuerpo
+		datosLargos: DOM.cuerpo.querySelector("#datosLargos"),
+		datosBreves: DOM.cuerpo.querySelector("#datosBreves"),
+
+		// Sector Imagen Derecha
+		centro: DOM.imgDerecha.querySelector("#centro img"),
+		conjuntoIconos: DOM.imgDerecha.querySelector("#conjuntoIconos"),
+
+	};
+	DOM = {
+		...DOM,
+
+		// Centro
+		imagen: centro.querySelector("img"),
+		links: centro.querySelector("#links"),
+		infoMobileParado: centro.querySelector("#marcoInfo"),
+
+		// Íconos
+		mostrarInfo: DOM.conjuntoIconos.querySelector("#mostrarInfo"),
+		mostrarCalif: DOM.conjuntoIconos.querySelector("#mostrarCalif"),
 	};
 
-	// Event listeners
+	// Event listeners - Toggles 'conjuntoIconos', 'centro links'
 	DOM.imagen.addEventListener("click", () => {
-		// Toggles 'conjuntoIconos', 'centro links'
 		DOM.links.classList.toggle("ocultar");
 		DOM.conjuntoIconos.classList.toggle("invisible");
 	});
+
+	// Event listeners - Muestra la info
+	DOM.mostrarInfo.addEventListener("click",()=>{
+		DOM.datosLargos.classList.toggle("ocultar")
+		DOM.infoMobileParado.classList.toggle("toggle")
+	})
 });
