21b9fc365882e5c00c4afa4530e48b24556bd716
diribar <diegoiribarren2015@gmail.com>
2024-03-22 00:35:34 -0300

    Miscelaneas

--- proyecto/baseDeDatos/modelos/CAM_histEdics.js
+++ proyecto/baseDeDatos/modelos/CAM_histEdics.js
@@ -23,7 +23,7 @@ module.exports = (sequelize, dt) => {
 		timestamps: false,
 	};
 	const entidad = sequelize.define(alias, columns, config);
-	console.log(26,entidad);
+
 	entidad.associate = (n) => {
 		entidad.belongsTo(n.motivosEdics, {as: "motivo", foreignKey: "motivo_id"});
 		entidad.belongsTo(n.usuarios, {as: "statusSugeridoPor", foreignKey: "sugeridoPor_id"});
