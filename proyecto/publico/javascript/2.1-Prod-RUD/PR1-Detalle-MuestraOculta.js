faf8f7dc07e05fc8ab0951afdd79642d15507f86
diribar <diegoiribarren2015@gmail.com>
2024-03-28 07:47:01 -0300

    Mejora continua

--- proyecto/publico/javascript/2.1-Prod-RUD/PR1-Detalle-MuestraOculta.js
+++ proyecto/publico/javascript/2.1-Prod-RUD/PR1-Detalle-MuestraOculta.js
@@ -9,14 +9,12 @@ window.addEventListener("load", () => {
 		...DOM,
 
 		// Sector Cuerpo
-		acostado: {
-			info: DOM.cuerpo.querySelector("#datosLargos"),
-			calif: DOM.cuerpo.querySelector("#datosBreves"),
-		},
+		datosLargos: DOM.cuerpo.querySelector("#datosLargos"),
+		datosBreves: DOM.cuerpo.querySelector("#datosBreves"),
 
 		// Sector Imagen Derecha
-		centro: DOM.imgDerecha.querySelector("#centro img"),
-		conjuntoIconos: DOM.imgDerecha.querySelector("#conjuntoIconos"),
+		centro: DOM.imgDerecha.querySelector("#centro"),
+		sectorIconos: DOM.imgDerecha.querySelector("#sectorIconos"),
 	};
 	DOM = {
 		...DOM,
@@ -24,47 +22,29 @@ window.addEventListener("load", () => {
 		// Centro
 		imagen: centro.querySelector("img"),
 		links: centro.querySelector("#links"),
-		parado: {
-			info: centro.querySelector("#marcoInfo"),
-			calif: centro.querySelector("#recuadroDB"),
-		},
 
 		// Íconos
-		muestraInfo: DOM.conjuntoIconos.querySelector("#muestraInfo"),
-		muestraCalif: DOM.conjuntoIconos.querySelector("#muestraCalif"),
+		muestraDL: DOM.sectorIconos.querySelector("#muestraDL"),
+		muestraDB: DOM.sectorIconos.querySelector("#muestraDB"),
 	};
 
-	// Event listeners - Toggles 'conjuntoIconos', 'centro links'
+	// Event listeners - Toggles 'sectorIconos', 'centro links'
 	DOM.imagen.addEventListener("click", () => {
-		DOM.parado.info.classList.add("toggle"); // información
-		DOM.parado.calif.classList.add("toggle");// calificación
-		DOM.links.classList.toggle("ocultar"); // links de la película
-		DOM.conjuntoIconos.classList.toggle("invisible"); // íconos
+		DOM.links.classList.toggle("ocultar"); // inicialmente visibles
+		DOM.sectorIconos.classList.toggle("ocultar"); // inicialmente visibles
+		DOM.datosLargos.classList.add("toggle"); // inicialmente ocultos
+		DOM.datosBreves.classList.add("toggle"); // inicialmente ocultos
 	});
 
-	// Event listeners - Muestra la info
-	DOM.muestraInfo.addEventListener("click", () => {
-		// Toggle 'info' para parado
-		DOM.parado.info.classList.toggle("toggle");
-
-		// Muestra 'info' para acostado
-		DOM.acostado.info.classList.remove("ocultar");
-
-		// Oculta 'calif'
-		DOM.acostado.calif.classList.add("toggle");
-		DOM.parado.calif.classList.add("toggle");
+	// Event listeners - Muestra la datosLargos
+	DOM.muestraDL.addEventListener("click", () => {
+		DOM.datosLargos.classList.toggle("toggle"); // Toggle 'datosLargos'
+		DOM.datosBreves.classList.add("toggle"); // Oculta 'datosBreves'
 	});
 
 	// Event listeners - Muestra la calificación
-	DOM.muestraCalif.addEventListener("click", () => {
-		// Toggle 'calif' para parado
-		DOM.parado.calif.classList.toggle("toggle");
-
-		// Muestra 'calif' para acostado
-		DOM.acostado.calif.classList.remove("toggle");
-
-		// Oculta 'info'
-		DOM.acostado.info.classList.add("ocultar");
-		DOM.parado.info.classList.add("toggle");
+	DOM.muestraDB.addEventListener("click", () => {
+		DOM.datosBreves.classList.toggle("toggle"); // Toggle 'datosBreves'
+		DOM.datosLargos.classList.add("toggle"); // Oculta 'datosLargos'
 	});
 });
