52998c98fdaefaa32324c7343da5abb6fb169fc3
diribar <diegoiribarren2015@gmail.com>
2024-03-28 07:50:47 -0300

    Miscelaneas

--- proyecto/publico/javascript/2.1-Prod-RUD/PR1-Detalle-MuestraOculta.js
+++ proyecto/publico/javascript/2.1-Prod-RUD/PR1-Detalle-MuestraOculta.js
@@ -30,21 +30,21 @@ window.addEventListener("load", () => {
 
 	// Event listeners - Toggles 'sectorIconos', 'centro links'
 	DOM.imagen.addEventListener("click", () => {
-		DOM.links.classList.toggle("ocultar"); // inicialmente visibles
-		DOM.sectorIconos.classList.toggle("ocultar"); // inicialmente visibles
-		DOM.datosLargos.classList.add("toggle"); // inicialmente ocultos
-		DOM.datosBreves.classList.add("toggle"); // inicialmente ocultos
+		DOM.links.classList.toggle("ocultar"); // inicialmente visibles siempre
+		DOM.sectorIconos.classList.toggle("ocultar"); // inicialmente visibles siempre
+		DOM.datosLargos.classList.add("toggle"); // inicialmente visibles en acostados
+		DOM.datosBreves.classList.add("toggle"); // inicialmente visibles en acostados
 	});
 
 	// Event listeners - Muestra la datosLargos
 	DOM.muestraDL.addEventListener("click", () => {
-		DOM.datosLargos.classList.toggle("toggle"); // Toggle 'datosLargos'
-		DOM.datosBreves.classList.add("toggle"); // Oculta 'datosBreves'
+		DOM.datosLargos.classList.toggle("toggle"); // inicialmente visibles en acostados
+		DOM.datosBreves.classList.add("toggle"); // inicialmente visibles en acostados
 	});
 
 	// Event listeners - Muestra la calificación
 	DOM.muestraDB.addEventListener("click", () => {
-		DOM.datosBreves.classList.toggle("toggle"); // Toggle 'datosBreves'
-		DOM.datosLargos.classList.add("toggle"); // Oculta 'datosLargos'
+		DOM.datosBreves.classList.toggle("toggle"); // inicialmente visibles en acostados
+		DOM.datosLargos.classList.add("toggle"); // inicialmente visibles en acostados
 	});
 });
