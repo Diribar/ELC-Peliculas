623ff0b301e0c325b0b35c3987a62e9b91a9cbe1
diribar <diegoiribarren2015@gmail.com>
2024-04-08 20:46:48 -0300

    Mejora continua

--- proyecto/publico/javascript/2.2-RCLVs-CRUD/RCLV-CambioPPP.js
+++ proyecto/publico/javascript/2.2-RCLVs-CRUD/RCLV-CambioPPP.js
@@ -1,7 +1,8 @@
 "use strict";
 window.addEventListener("load", async () => {
 	// Variables DOM
-	const iconosPPP = document.querySelectorAll("#prodsNuevos #prod i");
+	const iconosPPP = document.querySelectorAll("#prodsNuevos #prod #ppp i");
+	const leyendaPPP = document.querySelectorAll("#prodsNuevos #prod #ppp #texto");
 	const anchors = document.querySelectorAll("#prodsNuevos a");
 
 	// Variables varias
@@ -10,7 +11,6 @@ window.addEventListener("load", async () => {
 		pppRutaGuardar: "/producto/api/guarda-la-preferencia-del-usuario",
 	};
 	const v = await fetch(rutas.obtieneVariables).then((n) => n.json());
-	console.log(v);
 	let entidades = [];
 	let ids = [];
 
@@ -26,21 +26,26 @@ window.addEventListener("load", async () => {
 		const pppActual = v.pppOpcsArray.find((n) => iconosPPP[indice].className.endsWith(n.icono));
 		const pppActual_id = pppActual.id;
 
-		// Opción propuesta
-		const pppProp_id = pppActual_id > 1 ? pppActual_id - 1 : v.pppOpcsSimples.length;
-		const pppProp = v.pppOpcsArray.find((n) => n.id == pppProp_id);
+		// Opción nueva
+		const pppNueva_id = pppActual_id > 1 ? pppActual_id - 1 : v.pppOpcsSimples.length;
+		const pppNueva = v.pppOpcsArray.find((n) => n.id == pppNueva_id);
 
 		// Actualiza el ícono y el título
 		iconosPPP[indice].classList.remove(...pppActual.icono.split(" "));
-		iconosPPP[indice].classList.add(...pppProp.icono.split(" "));
-		iconosPPP[indice].title = pppProp.nombre;
+		iconosPPP[indice].classList.add(...pppNueva.icono.split(" "));
+		iconosPPP[indice].title = pppNueva.nombre;
+		leyendaPPP[indice].innerHTML = pppNueva.nombre;
 
 		// Actualiza la preferencia
 		iconosPPP[indice].classList.add("inactivo");
 		await fetch(
-			rutas.pppRutaGuardar + "/?entidad=" + entidades[indice] + "&entidad_id=" + ids[indice] + "&ppp_id=" + pppProp_id
+			rutas.pppRutaGuardar + "/?entidad=" + entidades[indice] + "&entidad_id=" + ids[indice] + "&ppp_id=" + pppNueva_id
 		);
 		iconosPPP[indice].classList.remove("inactivo");
+
+		// Muestra la leyenda 'Consulta copiada'
+		leyendaPPP[indice].classList.remove("ocultar");
+		setTimeout(() => leyendaPPP[indice].classList.add("ocultar"), v.setTimeOutStd);
 
 		// Fin
 		return;
