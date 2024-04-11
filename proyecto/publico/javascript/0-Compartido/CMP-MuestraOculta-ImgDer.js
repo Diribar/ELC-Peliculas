83805389c24f772bb2d64cde4c9c999aa8481116
diribar <diegoiribarren2015@gmail.com>
2024-04-11 09:01:19 -0300

    Mejora continua

--- proyecto/publico/javascript/0-Compartido/CMP-MuestraOculta-ImgDer.js
+++ proyecto/publico/javascript/0-Compartido/CMP-MuestraOculta-ImgDer.js
@@ -5,45 +5,48 @@ window.addEventListener("load", () => {
 		cuerpo: document.querySelector("#cuerpo #datos"),
 		imgDerecha: document.querySelector("#imgDerecha"),
 	};
+
+	// Sector Cuerpo
+	if (DOM.cuerpo) {
+		DOM.datosLargos = DOM.cuerpo.querySelector("#datosLargos");
+		DOM.datosBreves = DOM.cuerpo.querySelector("#datosBreves");
+	}
+
+	// Sector Imagen Derecha
 	DOM = {
 		...DOM,
-
-		// Sector Cuerpo
-		datosLargos: DOM.cuerpo.querySelector("#datosLargos"),
-		datosBreves: DOM.cuerpo.querySelector("#datosBreves"),
-
-		// Sector Imagen Derecha
 		imagen: DOM.imgDerecha.querySelector("img"),
 		links: DOM.imgDerecha.querySelector("#links"),
 		sectorIconos: DOM.imgDerecha.querySelector("#sectorIconos"),
 	};
-	DOM = {
-		...DOM,
 
-		// Íconos
-		muestraDL: DOM.sectorIconos.querySelector("#muestraDL"),
-		muestraDB: DOM.sectorIconos.querySelector("#muestraDB"),
-	};
+	// Íconos
+	if (DOM.sectorIconos) {
+		DOM.muestraDL = DOM.sectorIconos.querySelector("#muestraDL");
+		DOM.muestraDB = DOM.sectorIconos.querySelector("#muestraDB");
+	}
 	if (window.matchMedia("(orientation: landscape)").matches) DOM.datosLargos.classList.remove("toggle"); // inicialmente visibles en acostados
 
 	// Event listeners - Start-up / Sólo la imagen
 	DOM.imagen.addEventListener("click", () => {
 		if (DOM.links) DOM.links.classList.toggle("ocultar"); // inicialmente visibles siempre
-		DOM.sectorIconos.classList.remove("ocultar"); // inicialmente visibles siempre
-		DOM.datosLargos.classList.add("toggle"); // oculto
-		DOM.datosBreves.classList.add("toggle"); // oculto
+		if (DOM.sectorIconos) DOM.sectorIconos.classList.remove("ocultar"); // inicialmente visibles siempre
+		if (DOM.datosLargos) DOM.datosLargos.classList.add("toggle"); // oculto
+		if (DOM.datosBreves) DOM.datosBreves.classList.add("toggle"); // oculto
 	});
 
 	// Event listeners - Muestra datosLargos
-	DOM.muestraDL.addEventListener("click", () => {
-		DOM.datosLargos.classList.toggle("toggle");
-		DOM.datosBreves.classList.add("toggle"); // oculto
-	});
+	if (DOM.muestraDL)
+		DOM.muestraDL.addEventListener("click", () => {
+			DOM.datosLargos.classList.toggle("toggle");
+			DOM.datosBreves.classList.add("toggle"); // oculto
+		});
 
 	// Event listeners - Muestra datosBreves
-	DOM.muestraDB.addEventListener("click", () => {
-		if (DOM.muestraDB.className.includes("inactivo")) return;
-		DOM.datosBreves.classList.toggle("toggle");
-		DOM.datosLargos.classList.add("toggle"); // oculto
-	});
+	if (DOM.muestraDB)
+		DOM.muestraDB.addEventListener("click", () => {
+			if (DOM.muestraDB && DOM.muestraDB.className.includes("inactivo")) return;
+			DOM.datosBreves.classList.toggle("toggle");
+			DOM.datosLargos.classList.add("toggle"); // oculto
+		});
 });
