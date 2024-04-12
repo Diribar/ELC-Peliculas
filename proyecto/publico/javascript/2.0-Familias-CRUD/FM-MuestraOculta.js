e22888dc5fc8ef14da55e33532695e82136a9027
diribar <diegoiribarren2015@gmail.com>
2024-04-12 11:04:05 -0300

    Cambios de nombre

--- proyecto/publico/javascript/2.0-Familias-CRUD/FM-MuestraOculta.js
+++ proyecto/publico/javascript/2.0-Familias-CRUD/FM-MuestraOculta.js
@@ -4,7 +4,6 @@ window.addEventListener("load", () => {
 	let DOM = {
 		datos: document.querySelector("#cuerpo #datos"),
 		imgDerecha: document.querySelector("#imgDerecha"),
-		vistaDeResults: document.querySelector("#zonaDisponible #vistaDeResults"),
 	};
 
 	// Sector Cuerpo
@@ -19,7 +18,6 @@ window.addEventListener("load", () => {
 		imagen: DOM.imgDerecha.querySelector("img"),
 		links: DOM.imgDerecha.querySelector("#links"),
 		sectorIconos: DOM.imgDerecha.querySelector("#sectorIconos"),
-		cartelRCLV: DOM.imgDerecha.querySelector("#imgDerecha a:has(#cartelRCLV)"),
 	};
 
 	// Íconos
@@ -32,27 +30,23 @@ window.addEventListener("load", () => {
 	// Event listeners - Start-up / Sólo la imagen
 	DOM.imagen.addEventListener("click", () => {
 		if (DOM.links) DOM.links.classList.toggle("ocultar"); // inicialmente visibles siempre
-		if (DOM.sectorIconos) DOM.sectorIconos.classList.remove("ocultar"); // inicialmente visibles siempre
-		if (DOM.datosLargos) DOM.datosLargos.classList.add("toggle"); // oculto
-		if (DOM.datosBreves) DOM.datosBreves.classList.add("toggle"); // oculto
-
-		// Consultas
-		if (DOM.vistaDeResults) DOM.vistaDeResults.classList.toggle("toggle");
-		if (DOM.cartelRCLV) DOM.cartelRCLV.classList.toggle("toggle");
+		DOM.sectorIconos.classList.remove("ocultar"); // inicialmente visibles siempre
+		DOM.datosLargos.classList.add("toggle"); // oculto
+		DOM.datosBreves.classList.add("toggle"); // oculto
 	});
 
 	// Event listeners - Muestra datosLargos
-	if (DOM.muestraDL)
-		DOM.muestraDL.addEventListener("click", () => {
-			DOM.datosLargos.classList.toggle("toggle");
-			DOM.datosBreves.classList.add("toggle"); // oculto
-		});
+
+	DOM.muestraDL.addEventListener("click", () => {
+		DOM.datosLargos.classList.toggle("toggle");
+		DOM.datosBreves.classList.add("toggle"); // oculto
+	});
 
 	// Event listeners - Muestra datosBreves
-	if (DOM.muestraDB)
-		DOM.muestraDB.addEventListener("click", () => {
-			if (DOM.muestraDB && DOM.muestraDB.className.includes("inactivo")) return;
-			DOM.datosBreves.classList.toggle("toggle");
-			DOM.datosLargos.classList.add("toggle"); // oculto
-		});
+
+	DOM.muestraDB.addEventListener("click", () => {
+		if (DOM.muestraDB && DOM.muestraDB.className.includes("inactivo")) return;
+		DOM.datosBreves.classList.toggle("toggle");
+		DOM.datosLargos.classList.add("toggle"); // oculto
+	});
 });
