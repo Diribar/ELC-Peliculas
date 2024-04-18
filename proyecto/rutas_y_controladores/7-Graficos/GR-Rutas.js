85424bdcc2d6522f46f930f60ad17cc9307a2f5d
diribar <diegoiribarren2015@gmail.com>
2024-04-18 13:46:02 -0300

    Mejora continua

--- proyecto/rutas_y_controladores/7-Graficos/GR-Rutas.js
+++ proyecto/rutas_y_controladores/7-Graficos/GR-Rutas.js
@@ -4,6 +4,11 @@ const router = express.Router();
 const API = require("./GR-ControlAPI");
 const vista = require("./GR-ControlVista");
 
+// Middlewares - Específicos de usuarios
+const usAltaTerm = require("../../middlewares/filtrosPorUsuario/usAltaTerm");
+const usRolAutTablEnts = require("../../middlewares/filtrosPorUsuario/usRolAutTablEnts");
+const combinados = [usAltaTerm, usRolAutTablEnts];
+
 // API
 router.get("/api/peliculas-cfc-vpc", API.pelisCfcVpc);
 router.get("/api/peliculas-publico", API.pelisPublico);
@@ -13,12 +18,12 @@ router.get("/api/links-por-proveedor", API.linksPorProv);
 router.get("/api/rangos-sin-efemerides", API.rangosSinEfs);
 
 // Vistas
-router.get("/peliculas-cfc-vpc", vista.pelisCfcVpc);
-router.get("/peliculas-publico", vista.pelisPublico);
-router.get("/peliculas-epoca-estreno", vista.pelisEpocaEstreno);
-router.get("/links-vencimiento", vista.linksVencim);
-router.get("/links-por-proveedor", vista.linksPorProv);
-router.get("/rangos-sin-efemerides", vista.rangosSinEfs);
+router.get("/peliculas-cfc-vpc", combinados, vista.pelisCfcVpc);
+router.get("/peliculas-publico", combinados, vista.pelisPublico);
+router.get("/peliculas-epoca-estreno", combinados, vista.pelisEpocaEstreno);
+router.get("/links-vencimiento", combinados, vista.linksVencim);
+router.get("/links-por-proveedor", combinados, vista.linksPorProv);
+router.get("/rangos-sin-efemerides", combinados, vista.rangosSinEfs);
 
 // Fin
 module.exports = router;
