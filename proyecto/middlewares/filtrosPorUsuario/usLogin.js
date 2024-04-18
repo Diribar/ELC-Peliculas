ffaa37dbe8027a7cd23453326fdf24f1901c1a59
diribar <diegoiribarren2015@gmail.com>
2024-04-18 18:46:52 -0300

    Solución de errores

--- proyecto/middlewares/filtrosPorUsuario/usLogin.js
+++ proyecto/middlewares/filtrosPorUsuario/usLogin.js
@@ -1,13 +1,13 @@
 "use strict";
 module.exports = (req, res, next) => {
 	// Reenvíos por cookies
-	if (req.cookies && req.cookies.intentosLogin > intentosCookies) return res.redirect("/usuarios/login/suspendido");
+	if (req.cookies && req.cookies.intentosLogin >= intentosCookies) return res.redirect("/usuarios/login/suspendido");
 
 	// Reenvíos x BD
 	let login, usuario;
 	if (req.session && req.session.login) login = req.session.login;
 	if (login && login.usuario) usuario = login.usuario;
-	if (usuario && usuario.intentosLogin > intentosBD) return res.redirect("/usuarios/login/suspendido");
+	if (usuario && usuario.intentosLogin >= intentosBD) return res.redirect("/usuarios/login/suspendido");
 
 	// Fin
 	next();
