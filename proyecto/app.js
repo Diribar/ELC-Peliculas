8d363f2d4c40abc74c93e89e6e39c7a5c0d20fb1
diribar <diegoiribarren2015@gmail.com>
2024-04-19 02:41:36 -0300

    Miscelaneas

--- proyecto/app.js
+++ proyecto/app.js
@@ -16,7 +16,7 @@ global.linkAnoReciente = 3; // cantidad de años
 global.cantLinksVencPorSem = null;
 
 // Variable 'global' - Otras
-global.intentosCookies = 2; // cantidad que se tolera
+global.intentosCookies = 3; // cantidad que se tolera
 global.intentosBD = 3; // cantidad que se tolera
 global.usAutom_id = 2; // usuario 'automático'
 global.primerLunesDelAno = null;
