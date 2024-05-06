29264bb409003e328e427f69a8d17cc3576e6e56
diribar <diegoiribarren2015@gmail.com>
2024-05-06 11:06:25 -0300

    Mejora continua

--- proyecto/baseDeDatos/modelos/RCLV_canons.js
+++ proyecto/baseDeDatos/modelos/RCLV_canons.js
@@ -4,6 +4,9 @@ module.exports = (sequelize, dt) => {
 		id: {type: dt.STRING(2), primaryKey: true},
 		orden: {type: dt.INTEGER},
 		nombre: {type: dt.STRING(20)},
+		M: {type: dt.STRING(20)},
+		F: {type: dt.STRING(20)},
+		X: {type: dt.STRING(20)},
 	};
 	const config = {
 		tableName: "rclv_canons",
