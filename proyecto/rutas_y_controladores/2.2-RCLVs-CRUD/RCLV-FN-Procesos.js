dddf07747e59f163697d50c9bdfa555db82ce6ac
diribar <diegoiribarren2015@gmail.com>
2024-05-15 09:55:43 -0300

    Solución de errores

--- proyecto/rutas_y_controladores/2.2-RCLVs-CRUD/RCLV-FN-Procesos.js
+++ proyecto/rutas_y_controladores/2.2-RCLVs-CRUD/RCLV-FN-Procesos.js
@@ -212,7 +212,7 @@ module.exports = {
 
 			// Obtiene los datos que se guardan en la tabla
 			const campos = variables.camposRevisar.rclvs.filter((n) => n.rclvs || n[datos.entidad]).map((n) => n.nombre);
-			for (campo of campos) DE[campo] = datos[campo] ? datos[campo] : null;
+			for (let campo of campos) DE[campo] = datos[campo] ? datos[campo] : null;
 
 			// Variables con procesos
 			DE.fechaDelAno_id = tipoFecha == "SF" ? 400 : fechasDelAno.find((n) => n.mes_id == mes_id && n.dia == dia).id;
