40b49fd867f27ff33e7b56778be77eb0ccedb0b2
diribar <diegoiribarren2015@gmail.com>
2024-05-24 01:29:43 -0300

    Mejora continua

--- proyecto/middlewares/filtrosPorRegistro/comentarioNeces.js
+++ proyecto/middlewares/filtrosPorRegistro/comentarioNeces.js
@@ -2,17 +2,15 @@
 
 module.exports = async (req, res, next) => {
 	// Variables
-	const {comentario, motivo_id} = req.body;
-	const motivo = await BD_genericas.obtienePorId("motivosStatus", motivo_id);
+	const {comentario} = req.body;
+	let informacion;
 
 	// Problemas
-	const informacion =
-		!comentario && motivo.agregarComent
-			? {
-					mensajes: ["Necesitamos que nos des un comentario"],
-					iconos: [variables.vistaEntendido(req.session.urlAnterior)],
-			  }
-			: "";
+	if (!comentario)
+		informacion = {
+			mensajes: ["Necesitamos que nos des un comentario"],
+			iconos: [variables.vistaEntendido(req.session.urlAnterior)],
+		};
 
 	// Conclusiones
 	if (informacion) return res.render("CMP-0Estructura", {informacion});
--- proyecto/middlewares/filtrosPorRegistro/motivoNecesario.js
+++ proyecto/middlewares/filtrosPorRegistro/motivoNecesario.js
@@ -3,15 +3,24 @@
 module.exports = async (req, res, next) => {
 	// Variables
 	const {entidad, id, motivo_id} = {...req.query, ...req.body};
+	const link = req.baseUrl + req.path + "?entidad=" + entidad + "&id=" + id;
 	let informacion;
 
-	if (!motivo_id) {
-		let link = req.baseUrl + req.path + "?entidad=" + entidad + "&id=" + id;
+	// Si no hay motivo, redirige
+	if (!motivo_id)
 		informacion = {
 			mensajes: ["Necesitamos que nos digas el motivo"],
 			iconos: [variables.vistaEntendido(link)],
 		};
-	}
+
+	// Si correspondía un comentario, redirige
+	const motivo = motivosStatus.find((n) => n.id == motivo_id);
+	const comentario = req.body && req.body.comentario ? req.body.comentario : "";
+	if (motivo.agregarComent && !comentario)
+		informacion = {
+			mensajes: ["Necesitamos que nos des un comentario"],
+			iconos: [variables.vistaEntendido(link)],
+		};
 
 	// Conclusiones
 	if (informacion) return res.render("CMP-0Estructura", {informacion});
