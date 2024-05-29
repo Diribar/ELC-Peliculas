825722a5be1ae24f9369a002453019c156cec1a1
diribar <diegoiribarren2015@gmail.com>
2024-05-29 17:33:57 -0300

    Miscelaneas

--- proyecto/funciones/1-Rutinas/RT-Control.js
+++ proyecto/funciones/1-Rutinas/RT-Control.js
@@ -30,6 +30,7 @@ module.exports = {
 
 		// Comunica el fin de las rutinas
 		console.log();
+		// creaCapSinLink()
 		//await this.rutinasDiarias.LinksEnColes()
 		console.log("Rutinas de inicio terminadas en " + new Date().toLocaleString());
 
