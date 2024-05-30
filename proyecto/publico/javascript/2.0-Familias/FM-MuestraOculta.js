edd60eef5d42d66005c1d8532ad0ae399bc0c22e
diribar <diegoiribarren2015@gmail.com>
2024-05-30 14:32:13 -0300

    Mejora continua

--- proyecto/publico/javascript/2.0-Familias/FM-MuestraOculta.js
+++ proyecto/publico/javascript/2.0-Familias/FM-MuestraOculta.js
@@ -26,23 +26,21 @@ window.addEventListener("load", () => {
 	let muestraOculta = {
 		startUp: () => {
 			if (parado) {
-				// Datos Largos
-				if (calificar || rclvDetalle) {
-					DOM.datosLargos.classList.remove("toggle"); // muestra datosLargos
-					DOM.iconoDL.classList.add("toggle"); // oculta iconoDL
-				} else {
+				DOM.datosBreves.classList.add("toggle"); // oculta datosBreves
+
+				// Si existe links, los muestra y muestra iconoDL
+				if (DOM.links) {
+					DOM.links.classList.remove("toggle");
 					DOM.datosLargos.classList.add("toggle"); // oculta datosLargos
 					DOM.iconoDL.classList.remove("toggle"); // muestra iconoDL
+					DOM.iconoDB.classList.add("toggle"); // oculta iconoDB
 				}
-
-				// Datos Breves
-				DOM.datosBreves.classList.add("toggle"); // oculta datosBreves
-				calificar || rclvDetalle
-					? DOM.iconoDB.classList.remove("toggle") // muestra iconoDB
-					: DOM.iconoDB.classList.add("toggle"); // oculta iconoDB
-
-				// Links
-				if (DOM.links) DOM.links.classList.toggle("toggle");
+				// Si no existe links, muestra DL e ícono DB
+				else {
+					DOM.datosLargos.classList.remove("toggle"); // muestra datosLargos
+					DOM.iconoDL.classList.add("toggle"); // oculta iconoDL
+					DOM.iconoDB.classList.remove("toggle"); // muestra iconoDB
+				}
 			} else {
 				// Datos Largos
 				DOM.datosLargos.classList.remove("toggle"); // muestra datosLargos
@@ -61,31 +59,39 @@ window.addEventListener("load", () => {
 		},
 		imagen: () => {
 			if (parado) {
-				console.log(!DOM.datosLargos.className.includes("toggle"));
-				// Datos Largos - si alguno está visible, oculta datos largos
-				!DOM.datosLargos.className.includes("toggle") || !DOM.datosBreves.className.includes("toggle")
-					? DOM.datosLargos.classList.add("toggle") // oculta datosLargos
-					: DOM.datosLargos.classList.remove("toggle");
+				// si se ve algo, oculta todo
+				if (
+					!DOM.datosLargos.className.includes("toggle") || // se ven los datos largos
+					!DOM.datosBreves.className.includes("toggle") || // se ven los datos breves
+					(DOM.links && !DOM.links.className.includes("toggle")) // los links existen y se ven
+				) {
+					if (DOM.links) DOM.links.classList.add("toggle");
+					DOM.datosLargos.classList.add("toggle"); // oculta datosLargos
+					DOM.sectorIconos.classList.add("toggle");
+				}
+				// Acciones si está todo oculto
+				else {
+					// Muestra el sector íconos
+					DOM.sectorIconos.classList.remove("toggle");
 
-				// Botón Datos Largos
-				DOM.iconoDL.classList.add("toggle"); // oculta los íconos
-
-				// Datos Breves - lo oculta siempre
-				DOM.datosBreves.classList.add("toggle");
-
-				// Botón Datos Breves - si DL está oculto, lo oculta
-				DOM.datosLargos.className.includes("toggle")
-					? DOM.iconoDB.classList.add("toggle") // oculta iconoDB
-					: DOM.iconoDB.classList.remove("toggle"); // muestra iconoDB
-
-				// Sector Íconos - si DL está oculto, lo oculta
-				DOM.datosLargos.className.includes("toggle")
-					? DOM.sectorIconos.classList.add("ocultar") // oculta iconoDB
-					: DOM.sectorIconos.classList.remove("ocultar"); // muestra iconoDB
-
-				// Links
-				if (DOM.links) DOM.links.classList.toggle("toggle");
+					// Si existe links, los muestra y muestra iconoDL
+					if (DOM.links) {
+						DOM.links.classList.remove("toggle");
+						DOM.datosLargos.classList.add("toggle"); // oculta datosLargos
+						DOM.iconoDL.classList.remove("toggle");
+						DOM.iconoDB.classList.add("toggle"); // oculta iconoDB
+					}
+					// Si no existe links, muestra DL e ícono DB, y oculta ícono DL
+					else {
+						DOM.datosLargos.classList.remove("toggle");
+						DOM.iconoDL.classList.add("toggle");
+						DOM.iconoDB.classList.remove("toggle");
+					}
+				}
 			} else muestraOculta.startUp();
+
+			// Oculta los datos breves
+			DOM.datosBreves.classList.add("toggle");
 		},
 	};
 
@@ -120,12 +126,10 @@ window.addEventListener("load", () => {
 	// Event listeners - Start-up / 'click' en la imagen
 	DOM.imagen.addEventListener("click", () => {
 		muestraOculta.imagen();
-		// Fin
 		return;
 	});
 	DOM.sectorIconos.addEventListener("click", (e) => {
-		if (e.target.id != "sectorIconos") return;
-		muestraOculta.imagen();
+		if (e.target.id == "sectorIconos") muestraOculta.imagen();
 		return;
 	});
 
@@ -136,7 +140,6 @@ window.addEventListener("load", () => {
 	});
 
 	// Start-up
-	// return;
 	muestraOculta.startUp();
 	DOM.mobile.classList.remove("invisible");
 });
