54e42c71f6e77ba06f4ce5406b54db16660b5ebe
diribar <diegoiribarren2015@gmail.com>
2024-06-13 11:42:36 -0300

    Mejora continua

--- proyecto/baseDeDatos/modelos/CAM_comentsInactivos.js
+++ proyecto/baseDeDatos/modelos/CAM_comentsInactivos.js
@@ -1,23 +1,14 @@
 module.exports = (sequelize, dt) => {
 	const alias = "comentsInactivos";
 	const columns = {
-		pelicula_id: {type: dt.INTEGER},
-		coleccion_id: {type: dt.INTEGER},
-		grupoCol_id: {type: dt.INTEGER},
-		capitulo_id: {type: dt.INTEGER},
-
+		entidad: {type: dt.STRING(20)},
+		entidad_id: {type: dt.INTEGER},
 		comentario: {type: dt.STRING(150)},
 	};
 	const config = {
-		tableName: "cam_coment_inactivos",
+		tableName: "cam_coments_inactivos",
 		timestamps: false,
 	};
 	const entidad = sequelize.define(alias, columns, config);
-	entidad.associate = (n) => {
-		entidad.belongsTo(n.peliculas, {as: "pelicula", foreignKey: "pelicula_id"});
-		entidad.belongsTo(n.colecciones, {as: "coleccion", foreignKey: "coleccion_id"});
-		entidad.belongsTo(n.colecciones, {as: "grupoCol", foreignKey: "grupoCol_id"});
-		entidad.belongsTo(n.capitulos, {as: "capitulo", foreignKey: "capitulo_id"});
-	};
 	return entidad;
 };
