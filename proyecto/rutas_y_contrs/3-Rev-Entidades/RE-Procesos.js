02037d3ef331cc8f1a1d1cc4165bd4a9b6b8f4ae
diribar <diegoiribarren2015@gmail.com>
2024-06-27 19:09:32 -0300

    Mejora de procesos

--- proyecto/rutas_y_contrs/3-Rev-Entidades/RE-Procesos.js
+++ proyecto/rutas_y_contrs/3-Rev-Entidades/RE-Procesos.js
@@ -961,7 +961,7 @@ let FN_links = {
 		let respuesta, registros;
 
 		// Obtiene los links a revisar
-		const {originales, ediciones} = await BD_especificas.tablRevision.obtieneLinks(); // obtiene los links 'a revisar'
+		const {originales, ediciones} = await this.obtieneLinks(); // obtiene los links 'a revisar'
 		const creadoAprobs = originales.filter((n) => n.statusRegistro_id == creadoAprob_id);
 		const inacRecups = originales.filter((n) => inacRecup_ids.includes(n.statusRegistro_id));
 
@@ -1009,6 +1009,31 @@ let FN_links = {
 
 		// Fin
 		return null;
+	},
+	obtieneLinks: async () => {
+		// Variables
+		const include = variables.entidades.asocProds;
+
+		// Obtiene los links en status 'a revisar'
+		const condiciones = {
+			prodAprob: true,
+			statusRegistro_id: {[Op.and]: [{[Op.ne]: aprobado_id}, {[Op.ne]: inactivo_id}]},
+		};
+		const originales = db.links
+			.findAll({where: condiciones, include})
+			.then((n) => n.map((m) => m.toJSON()))
+			.then((n) => n.sort((a, b) => (a.capitulo_id && !b.capitulo_id ? -1 : !a.capitulo_id && b.capitulo_id ? 1 : 0))) // lotes por capítulos y no capítulos
+			.then((n) => n.sort((a, b) => (a.capitulo_id && b.capitulo_id ? a.grupoCol_id - b.grupoCol_id : 0))) // capítulos por colección
+			.then((n) => n.sort((a, b) => (a.statusSugeridoEn < b.statusSugeridoEn ? -1 : 1))); // lotes por 'statusSugeridoEn'
+
+		// Obtiene todas las ediciones
+		const ediciones = db.linksEdicion.findAll({include}).then((n) => n.map((m) => m.toJSON()));
+
+		// Los consolida
+		const links = await Promise.all([originales, ediciones]).then(([originales, ediciones]) => ({originales, ediciones}));
+
+		// Fin
+		return links;
 	},
 	obtieneProdLink: function ({links, datos}) {
 		if (!links.length) return;
