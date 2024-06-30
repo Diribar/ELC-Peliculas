d4acf35f1d62f2ecf4e05f00a079dad2b395167f
diribar <diegoiribarren2015@gmail.com>
2024-06-30 11:19:09 -0300

    Mejora continua

--- proyecto/middlewares/filtrosPorRegistro/motivoNecesario.js
+++ proyecto/middlewares/filtrosPorRegistro/motivoNecesario.js
@@ -2,26 +2,35 @@
 
 module.exports = async (req, res, next) => {
 	// Variables
-	const {entidad, id, motivo_id, comentario} = {...req.query, ...req.body};
+	const {entidad, id, motivo_id, comentario, entDupl, idDupl} = {...req.query, ...req.body};
 	const link = req.baseUrl + req.path + "?entidad=" + entidad + "&id=" + id;
-	let informacion;
+	let mensajes, motivo;
 
 	// Si no hay motivo, redirige
-	if (!motivo_id)
-		informacion = {
-			mensajes: ["Necesitamos que nos digas el motivo"],
-			iconos: [variables.vistaEntendido(link)],
-		};
+	if (!mensajes && !motivo_id) mensajes = ["Necesitamos que nos digas el motivo"];
 
-	// Si correspondía un comentario, redirige
-	const motivo = motivosStatus.find((n) => n.id == motivo_id);
-	if (motivo.agregarComent && !comentario)
-		informacion = {
-			mensajes: ["Necesitamos que nos des un comentario"],
-			iconos: [variables.vistaEntendido(link)],
-		};
+	// Si no se reconoce el motivo, redirige
+	if (!mensajes) {
+		motivo = motivosStatus.find((n) => n.id == motivo_id);
+		if (!motivo) mensajes = ["No reconocemos ese motivo"];
+	}
+
+	// Si correspondía un comentario y no se completó, redirige
+	if (!mensajes && motivo.agregarComent && !comentario) mensajes = ["Necesitamos que nos des un comentario"];
+
+	// Si el motivo es 'duplicado', se fija si se informó la 'entidad' y el 'id'
+	if (!mensajes && motivo.codigo == "duplicado") {
+		if (!entDupl && !idDupl)
+			mensajes = ["Necesitamos que nos informes la entidad y el id del producto con el que está duplicado"];
+		else if (!entDupl) mensajes = ["Necesitamos que nos informes la entidad del producto con el que está duplicado"];
+		else if (!idDupl) mensajes = ["Necesitamos que nos informes el id del producto con el que está duplicado"];
+		else {
+			const producto = await baseDeDatos.obtienePorId(entDupl, idDupl);
+			if (!producto) mensajes = ["No tenemos ningún producto con esa combinación de entidad e id"];
+		}
+	}
 
 	// Conclusiones
-	if (informacion) return res.render("CMP-0Estructura", {informacion});
+	if (mensajes) return res.render("CMP-0Estructura", {informacion: {mensajes, iconos: [variables.vistaEntendido(link)]}});
 	else next();
 };
