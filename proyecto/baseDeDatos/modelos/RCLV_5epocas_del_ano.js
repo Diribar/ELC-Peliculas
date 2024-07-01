9ff553202969d5fc4a054e56eddb6f027e12ce8f
diribar <diegoiribarren2015@gmail.com>
2024-07-01 01:00:52 -0300

    Miscelaneas

--- proyecto/baseDeDatos/modelos/RCLV_5epocas_del_ano.js
+++ proyecto/baseDeDatos/modelos/RCLV_5epocas_del_ano.js
@@ -38,6 +38,7 @@ module.exports = (sequelize, dt) => {
 		capturadoEn: {type: dt.DATE},
 		capturaActiva: {type: dt.BOOLEAN},
 
+		motivo_id: {type: dt.INTEGER},
 		statusRegistro_id: {type: dt.INTEGER},
 	};
 	const config = {
@@ -56,6 +57,7 @@ module.exports = (sequelize, dt) => {
 		entidad.belongsTo(n.usuarios, {as: "edicRevisadaPor", foreignKey: "edicRevisadaPor_id"});
 
 		entidad.belongsTo(n.statusRegistros, {as: "statusRegistro", foreignKey: "statusRegistro_id"});
+		entidad.belongsTo(n.motivosStatus, {as: "motivo", foreignKey: "motivo_id"});
 		entidad.belongsTo(n.usuarios, {as: "capturadoPor", foreignKey: "capturadoPor_id"});
 
 		entidad.hasMany(n.peliculas, {as: "peliculas", foreignKey: "epocaDelAno_id"});
