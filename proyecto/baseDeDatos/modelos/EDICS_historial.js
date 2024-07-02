9094a34812ed22f4839c824d24cb6ab0efc6a84e
diribar <diegoiribarren2015@gmail.com>
2024-07-02 01:19:39 -0300

    Miscelaneas

--- proyecto/baseDeDatos/modelos/EDICS_historial.js
+++ proyecto/baseDeDatos/modelos/EDICS_historial.js
@@ -19,7 +19,7 @@ module.exports = (sequelize, dt) => {
 		comunicadoEn: {type: dt.DATE},
 	};
 	const config = {
-		tableName: "edics_hist",
+		tableName: "edics_historial",
 		timestamps: false,
 	};
 	const entidad = sequelize.define(alias, columns, config);
