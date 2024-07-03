5f68aefefa5728b566bfc425c4c59e8780b2271c
diribar <diegoiribarren2015@gmail.com>
2024-07-03 17:38:46 -0300

    Miscelaneas

--- proyecto/middlewares/transversales/urlDesconocida.js
+++ proyecto/middlewares/transversales/urlDesconocida.js
@@ -4,9 +4,8 @@ module.exports = async (req, res, next) => {
 	// Acciones si no se reconoce la url
 	let informacion = {
 		mensajes: [variables.urlDesconocida],
-		iconos: [variables.vistaAnterior(req.session.urlActual), variables.vistaInicio], // se usa actual porque no llegó a cambiar el session
+		iconos: [variables.vistaAnterior(req.session.urlAnterior), variables.vistaInicio], // se usa actual porque no llegó a cambiar el session
 	};
-	console.log(9);
 
 	// Fin
 	return res.status(404).render("CMP-0Estructura", {informacion});
