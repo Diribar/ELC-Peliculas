ffbfeb047ac07fcbb41b6cb812fe5929bfa251bc
diribar <diegoiribarren2015@gmail.com>
2024-07-04 09:16:22 -0300

    Mejora continua

--- proyecto/publico/javascript/2.0-Familias/FM-PR-Recuperar.js
+++ proyecto/publico/javascript/2.0-Familias/FM-PR-Recuperar.js
@@ -7,6 +7,9 @@ window.addEventListener("load", async () => {
 		form: document.querySelector("#datosLargos #recuadroDL form"),
 		submit: document.querySelector("#sectorIconos button[type='submit']"),
 
+		// inputs
+		inputs: document.querySelectorAll("#historial input[type='radio']"),
+
 		// Comentario
 		comentario: document.querySelector("#comentario textarea"),
 		contador: document.querySelector("#comentario #contador"),
@@ -30,25 +33,29 @@ window.addEventListener("load", async () => {
 	};
 
 	// Event listener - cambios en el formulario
-	DOM.comentario.addEventListener("keypress", (e) => {
-		keyPressed(e);
-		return;
-	});
-	DOM.comentario.addEventListener("input", (e) => {
-		// Validaciones estándar
-		amplio.restringeCaracteres(e);
+	if (DOM.inputs) for (let input of DOM.inputs) input.addEventListener("change", () => DOM.submit.classList.remove("inactivo"));
 
-		// Actualiza el contador y el botón submit
-		FN.contador();
-		FN.botonSubmit();
+	if (DOM.comentario) {
+		DOM.comentario.addEventListener("keypress", (e) => {
+			keyPressed(e);
+			return;
+		});
+		DOM.comentario.addEventListener("input", (e) => {
+			// Validaciones estándar
+			amplio.restringeCaracteres(e);
 
-		// Fin
-		return;
-	});
+			// Actualiza el contador y el botón submit
+			FN.contador();
+			FN.botonSubmit();
+
+			// Fin
+			return;
+		});
+	}
 	DOM.form.addEventListener("submit", (e) => {
 		if (DOM.submit.className.includes("inactivo")) e.preventDefault();
 	});
 
 	// Si no hay inputs, focus en comentario
-	DOM.comentario.focus();
+	if (DOM.comentario) DOM.comentario.focus();
 });
