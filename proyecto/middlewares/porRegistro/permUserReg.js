683392309fc5c079151f7cdea60be82c278ca74d
diribar <diegoiribarren2015@gmail.com>
2024-07-12 14:54:24 -0300

    Mejora de procesos

--- proyecto/middlewares/porRegistro/permUserReg.js
+++ proyecto/middlewares/porRegistro/permUserReg.js
@@ -2,6 +2,14 @@
 
 module.exports = async (req, res, next) => {
 	// Variables
+	const rubro = req.originalUrl.startsWith("/revision/")
+		? "revision"
+		: req.originalUrl.startsWith("/producto/") || req.originalUrl.startsWith("/links/abm/")
+		? "producto"
+		: req.originalUrl.startsWith("/rclv/")
+		? "rclv"
+		: null;
+
 	let v = {
 		// Generales
 		entidad: req.query.entidad ? req.query.entidad : req.originalUrl.startsWith("/revision/usuarios") ? "usuarios" : "",
@@ -17,10 +25,10 @@ module.exports = async (req, res, next) => {
 
 		// Vistas
 		vistaAnterior: variables.vistaAnterior(req.session.urlSinCaptura),
-		vistaInactivar: variables.vistaInactivar(req),
 		vistaEntendido: variables.vistaEntendido(req.session.urlSinCaptura),
 		vistaTablero: variables.vistaTablero,
 	};
+	v.vistaInactivar = rubro ? variables.vistaInactivar[rubro](v.entidad, v.entID) : {};
 	v = {
 		...v,
 		entidadNombreMinuscula: comp.obtieneDesdeEntidad.entidadNombre(v.entidad).toLowerCase(),
