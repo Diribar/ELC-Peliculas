41b59afda73783efd23705458a7af5ce59a27f2c
diribar <diegoiribarren2015@gmail.com>
2024-07-12 14:37:33 -0300

    Mejora de procesos

--- proyecto/middlewares/porRegistro/prodYaEnBD.js
+++ proyecto/middlewares/porRegistro/prodYaEnBD.js
@@ -24,8 +24,9 @@ module.exports = async (req, res, next) => {
 		if (existe) {
 			// Variables
 			const linkAnterior = "/producto/agregar/desambiguar";
-			const linkDetalle = "/producto/detalle/?entidad=" + datos.entidad + "&id=" + existe.id;
 			const entidadNombre = comp.obtieneDesdeEntidad.entidadNombre(datos.entidad);
+			const vistaAnterior = variables.vistaAnterior(linkAnterior);
+			const vistaInactivar = variables.vistaInactivar.producto(datos.entidad, existe.id);
 
 			// Información para el cartel
 			informacion = {
@@ -33,10 +34,7 @@ module.exports = async (req, res, next) => {
 					"La " + entidadNombre + " ya está en nuestra BD.",
 					"Podés ver el detalle, con el ícono de abajo a la derecha",
 				],
-				iconos: [
-					{nombre: "fa-circle-left", link: linkAnterior, titulo: "Regresar a 'Desambiguar'"},
-					{nombre: "fa-circle-info", link: linkDetalle, titulo: "Ir a la vista Detalle"},
-				],
+				iconos: [vistaAnterior, vistaInactivar],
 			};
 
 			// Elimina los hallazgos anteriores
