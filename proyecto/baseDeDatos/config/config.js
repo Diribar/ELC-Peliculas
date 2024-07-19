c3cf6c43de227b5221e591999c00abb45a785575
diribar <diegoiribarren2015@gmail.com>
2024-07-19 13:07:29 -0300

    Mejora de procesos

--- proyecto/baseDeDatos/config/config.js
+++ proyecto/baseDeDatos/config/config.js
@@ -4,7 +4,7 @@ module.exports = {
 	development: {
 		username: "root",
 		password: "",
-		database: process.env.DB_NAME_DEV,
+		database: process.env.DB_NAME,
 		host: "127.0.0.1",
 		dialect: "mysql",
 		logging: false,
