6b5496e9d070b00b5c346abb31eefd4da1bcd2a7
diribar <diegoiribarren2015@gmail.com>
2024-07-23 17:03:58 -0300

    Solución de errores

--- proyecto/middlewares/porRegistro/linkIDvalido.js
+++ proyecto/middlewares/porRegistro/linkIDvalido.js
@@ -3,15 +3,15 @@
 module.exports = async (req, res, next) => {
 	// Variables
 	const {link_id} = req.query;
-	const vistaAnterior = variables.vistaAnterior(req.session.urlAnterior);
+	const urlSinParametros = variables.vistaEntendido(req.session.urlSinParametros);
 	let informacion;
 
 	// PROBLEMA 1: No existe el ID
-	if (!link_id) informacion = {mensajes: ["Falta el dato del 'ID'"], iconos: [vistaAnterior]};
+	if (!link_id) informacion = {mensajes: ["Falta el dato del 'ID'"], iconos: [urlSinParametros]};
 
 	// PROBLEMA 2: ID inválido
 	const registro = await baseDeDatos.obtienePorId("links", link_id);
-	if (!registro) informacion = {mensajes: ["No tenemos ese link"], iconos: [vistaAnterior]};
+	if (!registro) informacion = {mensajes: ["No tenemos ese link"], iconos: [urlSinParametros]};
 
 	// Conclusiones
 	if (informacion) return res.render("CMP-0Estructura", {informacion});
