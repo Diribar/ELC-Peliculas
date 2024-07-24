d30f78f74f8269b56960a136a070f1840f034c3e
diribar <diegoiribarren2015@gmail.com>
2024-07-24 12:47:45 -0300

    Mejora continua

--- proyecto/middlewares/porRegistro/linkAltaBaja.js
+++ proyecto/middlewares/porRegistro/linkAltaBaja.js
@@ -16,14 +16,14 @@ module.exports = async (req, res, next) => {
 	if (!respuesta && estables_ids.includes(link.statusRegistro_id)) respuesta = "En este status no se puede procesar";
 
 	// Valida que quede lugar en alguna semana
-	if (
-		!respuesta &&
-		IN == "SI" &&
-		comp.linksVencPorSem.condicEstandar(link) &&
-		((link.capitulo_id && !cantLinksVencPorSem.paraRevisar.capitulos) || // es un capítulo y no queda lugar
-			(!link.capitulo_id && !cantLinksVencPorSem.paraRevisar.pelisColes)) // no es un capítulo y no queda lugar
-	)
-		respuesta = "En esta semana ya no se puede revisar este link";
+	// if (
+	// 	!respuesta &&
+	// 	IN == "SI" &&
+	// 	comp.linksVencPorSem.condicEstandar(link) &&
+	// 	((link.capitulo_id && !cantLinksVencPorSem.paraRevisar.capitulos) || // es un capítulo y no queda lugar
+	// 		(!link.capitulo_id && !cantLinksVencPorSem.paraRevisar.pelisColes)) // no es un capítulo y no queda lugar
+	// )
+	// 	respuesta = "En esta semana ya no se puede revisar este link";
 
 	// Fin
 	if (respuesta) return res.json(respuesta);
