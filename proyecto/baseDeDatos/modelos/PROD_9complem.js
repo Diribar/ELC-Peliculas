42d08b85f2a09f9b15a5899dcbe02504c9123041
diribar <diegoiribarren2015@gmail.com>
2024-08-01 08:08:38 -0300

    Mejora de procesos

--- proyecto/baseDeDatos/modelos/PROD_9complem.js
+++ proyecto/baseDeDatos/modelos/PROD_9complem.js
@@ -1,6 +1,8 @@
 module.exports = (sequelize, dt) => {
-	const alias = "prod_9complem";
+	const alias = "prodComplem";
 	const columns = {
+		entidad: {type: dt.STRING(20)},
+		entidad_id: {type: dt.INTEGER},
 
 		linksTrailer: {type: dt.INTEGER},
 		linksGral: {type: dt.INTEGER},
@@ -18,58 +20,17 @@ module.exports = (sequelize, dt) => {
 		calificacion: {type: dt.INTEGER},
 		azar: {type: dt.INTEGER},
 
-		creadoPor_id: {type: dt.INTEGER},
-		creadoEn: {type: dt.DATE},
-		altaRevisadaPor_id: {type: dt.INTEGER},
-		altaRevisadaEn: {type: dt.DATE},
-		altaTermEn: {type: dt.DATE},
-		leadTimeCreacion: {type: dt.DECIMAL},
-
-		statusSugeridoPor_id: {type: dt.INTEGER},
-		statusSugeridoEn: {type: dt.DATE},
-
-		editadoPor_id: {type: dt.INTEGER},
-		editadoEn: {type: dt.DATE},
-		edicRevisadaPor_id: {type: dt.INTEGER},
-		edicRevisadaEn: {type: dt.DATE},
-		leadTimeEdicion: {type: dt.DECIMAL},
-
 		capturadoPor_id: {type: dt.INTEGER},
 		capturadoEn: {type: dt.DATE},
 		capturaActiva: {type: dt.BOOLEAN},
-
-		statusRegistro_id: {type: dt.INTEGER},
 	};
 	const config = {
-		tableName: "prod_1peliculas",
+		tableName: "prod_9complem",
 		timestamps: false,
 	};
 	const entidad = sequelize.define(alias, columns, config);
 	entidad.associate = (n) => {
-		entidad.belongsTo(n.idiomas, {as: "idioma_original", foreignKey: "idiomaOriginal_id"});
-		entidad.belongsTo(n.tiposActuacion, {as: "tipoActuacion", foreignKey: "tipoActuacion_id"});
-		entidad.belongsTo(n.publicos, {as: "publico", foreignKey: "publico_id"});
-
-		entidad.belongsTo(n.personajes, {as: "personaje", foreignKey: "personaje_id"});
-		entidad.belongsTo(n.hechos, {as: "hecho", foreignKey: "hecho_id"});
-		entidad.belongsTo(n.temas, {as: "tema", foreignKey: "tema_id"});
-		entidad.belongsTo(n.eventos, {as: "evento", foreignKey: "evento_id"});
-		entidad.belongsTo(n.epocasDelAno, {as: "epocaDelAno", foreignKey: "epocaDelAno_id"});
-		entidad.belongsTo(n.epocasOcurrencia, {as: "epocaOcurrencia", foreignKey: "epocaOcurrencia_id"});
-		entidad.belongsTo(n.epocasEstreno, {as: "epocaEstreno", foreignKey: "epocaEstreno_id"});
-
-		entidad.belongsTo(n.usuarios, {as: "creadoPor", foreignKey: "creadoPor_id"});
-		entidad.belongsTo(n.usuarios, {as: "altaRevisadaPor", foreignKey: "altaRevisadaPor_id"});
-		entidad.belongsTo(n.usuarios, {as: "statusSugeridoPor", foreignKey: "statusSugeridoPor_id"});
-		entidad.belongsTo(n.usuarios, {as: "editadoPor", foreignKey: "editadoPor_id"});
-		entidad.belongsTo(n.usuarios, {as: "edicRevisadaPor", foreignKey: "edicRevisadaPor_id"});
-
-		entidad.belongsTo(n.statusRegistros, {as: "statusRegistro", foreignKey: "statusRegistro_id"});
 		entidad.belongsTo(n.usuarios, {as: "capturadoPor", foreignKey: "capturadoPor_id"});
-
-		entidad.hasMany(n.prodsEdicion, {as: "ediciones", foreignKey: "pelicula_id"});
-		entidad.hasMany(n.links, {as: "links", foreignKey: "pelicula_id"});
-		entidad.hasMany(n.linksEdicion, {as: "links_edic", foreignKey: "pelicula_id"});
 	};
 	return entidad;
 };
