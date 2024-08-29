893b1e4d4d7da5383085722d7ef82f7f312e8c47
diribar <diegoiribarren2015@gmail.com>
2024-08-29 08:57:45 -0300

    Miscelaneas

--- proyecto/middlewares/varios/misDetalleProd.js
+++ proyecto/middlewares/varios/misDetalleProd.js
@@ -9,11 +9,10 @@ module.exports = async (req, res, next) => {
 	const {entidad, id: entidad_id} = req.query;
 	const condicion = {usuario_id, entidad, entidad_id};
 
-	// Elimina el registro que tenga el usuario para el producto
-	baseDeDatos.eliminaTodosPorCondicion("misConsultas", condicion).then(() =>
-		// Agrega un registro del usuario para el producto
-		baseDeDatos.agregaRegistro("misConsultas", condicion)
-	);
+	// Agrega el registro
+	baseDeDatos
+		.eliminaTodosPorCondicion("misConsultas", condicion) // Elimina el registro que tenga el usuario para el producto
+		.then(() => baseDeDatos.agregaRegistro("misConsultas", condicion)); // Agrega un registro del usuario para el producto
 
 	// Fin
 	return next();
