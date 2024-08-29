d111892b5fbb88cd3cbfe2e10b85270855e08b74
diribar <diegoiribarren2015@gmail.com>
2024-08-29 12:32:39 -0300

    Mejora de procesos

--- proyecto/rutas_y_contrs/2.0-Familias/FM-ControlVista.js
+++ proyecto/rutas_y_contrs/2.0-Familias/FM-ControlVista.js
@@ -4,48 +4,118 @@ const procesos = require("./FM-FN-Procesos");
 const validacs = require("./FM-FN-Validar");
 
 module.exports = {
-	motivosForm: async (req, res) => {
-		// Variables
-		const datos = await procesos.obtieneDatosForm(req);
+	form:{
+		motivos: async (req, res) => {
+			// Variables
+			const datos = await procesos.obtieneDatosForm(req);
 
-		// Obtiene datos para la vista
-		const ayudasTitulo = "Por favor decinos por qué sugerís " + datos.codigo + " este registro.";
-		const motivos = statusMotivos.filter((n) => n[datos.petitFamilias]);
-		const entidades = variables.entidades[datos.petitFamilias];
+			// Obtiene datos para la vista
+			const ayudasTitulo = "Por favor decinos por qué sugerís " + datos.codigo + " este registro.";
+			const motivos = statusMotivos.filter((n) => n[datos.petitFamilias]);
+			const entidades = variables.entidades[datos.petitFamilias];
 
-		// Render del formulario
-		return res.render("CMP-0Estructura", {...datos, ayudasTitulo, motivos, entidades});
+			// Render del formulario
+			return res.render("CMP-0Estructura", {...datos, ayudasTitulo, motivos, entidades});
+		},
+		historial: async (req, res) => {
+			// Variables
+			const {statusAlineado, prodRclv} = req.body;
+			const datos = await procesos.obtieneDatosForm(req);
+
+			// Obtiene el ayuda para el título
+			const ayudasTitulo =
+				datos.codigo == "historial"
+					? false
+					: datos.tema == "revisionEnts"
+					? ["Para tomar una decisión contraria a la del usuario, necesitamos tu comentario para darle feedback."]
+					: ["inactivar", "recuperar"].includes(datos.codigo)
+					? ["Por favor decinos por qué sugerís " + datos.codigo + " este registro."]
+					: datos.codigo == "eliminar"
+					? ["Este registro se eliminará en forma definitiva"]
+					: null;
+
+			// Obtiene datos para la vista
+			const historialStatus = await procesos.historialDeStatus.obtiene({entidad: datos.entidad, ...datos.registro});
+			const {usuario} = req.session;
+			const revisorPERL = usuario && usuario.rolUsuario.revisorPERL;
+			const anchorEncab = true;
+
+			// Render del formulario
+			return res.render("CMP-0Estructura", {
+				...{...datos, ayudasTitulo, revisorPERL},
+				...{historialStatus, statusAlineado, prodRclv, anchorEncab},
+			});
+		},
+		elimina: async (req, res) => {
+			// Variables
+			const {entidad, id, origen} = req.query;
+			const familia = comp.obtieneDesdeEntidad.familia(entidad);
+			const include = entidad == "colecciones" ? "capitulos" : "";
+			const original = await baseDeDatos.obtienePorId(entidad, id, include);
+			const campo_id = comp.obtieneDesdeEntidad.campo_id(entidad);
+			let espera = [];
+
+			// Elimina sus registros dependientes
+			espera.push(procesos.elimina.dependientes(entidad, id, original));
+
+			// Acciones si es un RCLV
+			if (familia == "rclv") {
+				// Borra el vínculo en las ediciones de producto y las elimina si quedan vacías
+				espera.push(procesos.elimina.vinculoEdicsProds({entidadRCLV: entidad, rclvID: id}));
+
+				// Borra el vínculo en los productos y les cambia el status si corresponde
+				espera.push(procesos.elimina.vinculoProds({entidadRCLV: entidad, rclvID: id}));
+
+				// Borra el vínculo en los fechasDelAno
+				if (entidad == "epocasDelAno")
+					await baseDeDatos.actualizaTodosPorCondicion("fechasDelAno", {[campo_id]: id}, {[campo_id]: 1});
+			}
+
+			// Elimina el registro
+			await Promise.all(espera);
+			await baseDeDatos.eliminaPorId(entidad, id);
+
+			// Elimina registros vinculados
+			for (let tabla of variables.tablasSinEntidadId) baseDeDatos.eliminaTodosPorCondicion(tabla, {entidad, entidad_id: id});
+
+			// Actualiza solapamiento y la variable 'fechasDelAno'
+			if (entidad == "epocasDelAno") comp.actualizaSolapam();
+
+			// Variables para la vista
+			const nombre = comp.nombresPosibles(original);
+			const articFinal = comp.obtieneDesdeEntidad.oa(entidad);
+			const articInicial = articFinal == "a" ? "La " : "El ";
+			const entidadNombre = comp.obtieneDesdeEntidad.entidadNombre(entidad).toLowerCase();
+			const capitulos = entidad == "colecciones" ? "y sus capítulos, " : "";
+			const plural1 = entidad == "colecciones" ? "ron" : "";
+			const plural2 = entidad == "colecciones" ? "s" : "";
+			const titulo = comp.letras.inicialMayus(entidadNombre) + " eliminad" + articFinal + plural2;
+			const vistaEntendido = variables.vistaEntendido(req.session.urlSinParametros);
+
+			// Cartel de registro eliminado
+			const informacion = {
+				mensajes: [
+					articInicial +
+						entidadNombre +
+						' "' +
+						nombre +
+						'" ' +
+						capitulos +
+						"fue" +
+						plural1 +
+						" eliminad" +
+						articFinal +
+						plural2 +
+						" de nuestra base de datos.",
+				],
+				iconos: [vistaEntendido],
+			};
+
+			// Fin
+			return res.render("CMP-0Estructura", {informacion, titulo});
+		},
 	},
-	historialForm: async (req, res) => {
-		// Variables
-		const {statusAlineado, prodRclv} = req.body;
-		const datos = await procesos.obtieneDatosForm(req);
-
-		// Obtiene el ayuda para el título
-		const ayudasTitulo =
-			datos.codigo == "historial"
-				? false
-				: datos.tema == "revisionEnts"
-				? ["Para tomar una decisión contraria a la del usuario, necesitamos tu comentario para darle feedback."]
-				: ["inactivar", "recuperar"].includes(datos.codigo)
-				? ["Por favor decinos por qué sugerís " + datos.codigo + " este registro."]
-				: datos.codigo == "eliminar"
-				? ["Este registro se eliminará en forma definitiva"]
-				: null;
-
-		// Obtiene datos para la vista
-		const historialStatus = await procesos.historialDeStatus.obtiene({entidad: datos.entidad, ...datos.registro});
-		const {usuario} = req.session;
-		const revisorPERL = usuario && usuario.rolUsuario.revisorPERL;
-		const anchorEncab = true;
-
-		// Render del formulario
-		return res.render("CMP-0Estructura", {
-			...{...datos, ayudasTitulo, revisorPERL},
-			...{historialStatus, statusAlineado, prodRclv, anchorEncab},
-		});
-	},
-	inacRecup_guardar: async (req, res) => {
+	inacRecupGuardar: async (req, res) => {
 		//  Variables
 		let datos = await procesos.obtieneDatosGuardar(req);
 		const {entidad, id, familia, motivo_id, codigo, userId, ahora, campo_id, original, statusFinal_id, comentario} = datos;
@@ -113,74 +183,6 @@ module.exports = {
 		// Fin
 		const destino = "/" + familia + "/detalle/?entidad=" + entidad + "&id=" + id;
 		return res.redirect(destino);
-	},
-	eliminaForm: async (req, res) => {
-		// Variables
-		const {entidad, id, origen} = req.query;
-		const familia = comp.obtieneDesdeEntidad.familia(entidad);
-		const include = entidad == "colecciones" ? "capitulos" : "";
-		const original = await baseDeDatos.obtienePorId(entidad, id, include);
-		const campo_id = comp.obtieneDesdeEntidad.campo_id(entidad);
-		let espera = [];
-
-		// Elimina sus registros dependientes
-		espera.push(procesos.elimina.dependientes(entidad, id, original));
-
-		// Acciones si es un RCLV
-		if (familia == "rclv") {
-			// Borra el vínculo en las ediciones de producto y las elimina si quedan vacías
-			espera.push(procesos.elimina.vinculoEdicsProds({entidadRCLV: entidad, rclvID: id}));
-
-			// Borra el vínculo en los productos y les cambia el status si corresponde
-			espera.push(procesos.elimina.vinculoProds({entidadRCLV: entidad, rclvID: id}));
-
-			// Borra el vínculo en los fechasDelAno
-			if (entidad == "epocasDelAno")
-				await baseDeDatos.actualizaTodosPorCondicion("fechasDelAno", {[campo_id]: id}, {[campo_id]: 1});
-		}
-
-		// Elimina el registro
-		await Promise.all(espera);
-		await baseDeDatos.eliminaPorId(entidad, id);
-
-		// Elimina registros vinculados
-		for (let tabla of variables.tablasSinEntidadId) baseDeDatos.eliminaTodosPorCondicion(tabla, {entidad, entidad_id: id});
-
-		// Actualiza solapamiento y la variable 'fechasDelAno'
-		if (entidad == "epocasDelAno") comp.actualizaSolapam();
-
-		// Variables para la vista
-		const nombre = comp.nombresPosibles(original);
-		const articFinal = comp.obtieneDesdeEntidad.oa(entidad);
-		const articInicial = articFinal == "a" ? "La " : "El ";
-		const entidadNombre = comp.obtieneDesdeEntidad.entidadNombre(entidad).toLowerCase();
-		const capitulos = entidad == "colecciones" ? "y sus capítulos, " : "";
-		const plural1 = entidad == "colecciones" ? "ron" : "";
-		const plural2 = entidad == "colecciones" ? "s" : "";
-		const titulo = comp.letras.inicialMayus(entidadNombre) + " eliminad" + articFinal + plural2;
-		const vistaEntendido = variables.vistaEntendido(req.session.urlSinParametros);
-
-		// Cartel de registro eliminado
-		const informacion = {
-			mensajes: [
-				articInicial +
-					entidadNombre +
-					' "' +
-					nombre +
-					'" ' +
-					capitulos +
-					"fue" +
-					plural1 +
-					" eliminad" +
-					articFinal +
-					plural2 +
-					" de nuestra base de datos.",
-			],
-			iconos: [vistaEntendido],
-		};
-
-		// Fin
-		return res.render("CMP-0Estructura", {informacion, titulo});
 	},
 	correcs: {
 		motivoForm: async (req, res) => {
--- proyecto/rutas_y_contrs/2.0-Familias/FM-Rutas.js
+++ proyecto/rutas_y_contrs/2.0-Familias/FM-Rutas.js
@@ -4,34 +4,37 @@ const router = express.Router();
 const vista = require("./FM-ControlVista");
 const API = require("./FM-ControlAPI");
 
-// Middlewares - Específicos de usuarios
-const usAltaTerm = require("../../middlewares/porUsuario/usAltaTerm");
-const usPenalizaciones = require("../../middlewares/porUsuario/usPenalizaciones");
-const usAptoInput = require("../../middlewares/porUsuario/usAptoInput");
-const usRolRevPERL = require("../../middlewares/porUsuario/usRolRevPERL");
+// Middlewares
+const m = {
+	// Middlewares - Específicos de usuarios
+	usAltaTerm: require("../../middlewares/porUsuario/usAltaTerm"),
+	usPenalizaciones: require("../../middlewares/porUsuario/usPenalizaciones"),
+	usAptoInput: require("../../middlewares/porUsuario/usAptoInput"),
+	usRolRevPERL: require("../../middlewares/porUsuario/usRolRevPERL"),
 
-// Middlewares - Específicos del registro
-const entValida = require("../../middlewares/porRegistro/entidadValida");
-const IDvalido = require("../../middlewares/porRegistro/IDvalido");
-const statusCorrecto = require("../../middlewares/porRegistro/statusCorrecto");
-const creadoPorUsuario = require("../../middlewares/porRegistro/creadoPorUsuario");
-const motivoNecesario = require("../../middlewares/porRegistro/motivoNecesario");
-const comentNecesario = require("../../middlewares/porRegistro/comentNecesario");
-const rutaCRUD_ID = require("../../middlewares/varios/rutaCRUD_ID");
-const statusCompara = require("../../middlewares/porRegistro/statusCompara");
+	// Middlewares - Específicos del registro
+	entValida: require("../../middlewares/porRegistro/entidadValida"),
+	IDvalido: require("../../middlewares/porRegistro/IDvalido"),
+	statusCorrecto: require("../../middlewares/porRegistro/statusCorrecto"),
+	creadoPorUsuario: require("../../middlewares/porRegistro/creadoPorUsuario"),
+	motivoNecesario: require("../../middlewares/porRegistro/motivoNecesario"),
+	comentNecesario: require("../../middlewares/porRegistro/comentNecesario"),
+	rutaCRUD_ID: require("../../middlewares/varios/rutaCRUD_ID"),
+	statusCompara: require("../../middlewares/porRegistro/statusCompara"),
 
-// Middlewares - Temas de captura
-const permUserReg = require("../../middlewares/porRegistro/permUserReg");
-const capturaActivar = require("../../middlewares/varios/capturaActivar");
-const capturaInactivar = require("../../middlewares/varios/capturaInactivar");
+	// Middlewares - Temas de captura
+	permUserReg: require("../../middlewares/porRegistro/permUserReg"),
+	capturaActivar: require("../../middlewares/varios/capturaActivar"),
+	capturaInactivar: require("../../middlewares/varios/capturaInactivar"),
+};
 
 // Middlewares - Consolidados
-const aptoUsuario = [usAltaTerm, usPenalizaciones, usAptoInput];
-const eliminadoPorCreador = [...aptoUsuario, entValida, IDvalido, statusCorrecto, creadoPorUsuario];
-const aptoDetalle = [entValida, IDvalido, rutaCRUD_ID];
-const aptoCRUD = [...aptoDetalle, statusCorrecto, statusCompara, ...aptoUsuario, permUserReg];
-const aptoEliminar = [...aptoCRUD, usRolRevPERL];
-const correcs = [entValida, IDvalido, statusCompara, aptoUsuario, permUserReg, usRolRevPERL];
+const aptoUsuario = [m.usAltaTerm, m.usPenalizaciones, m.usAptoInput];
+const eliminadoPorCreador = [...aptoUsuario, m.entValida, m.IDvalido, m.statusCorrecto, m.creadoPorUsuario];
+const aptoDetalle = [m.entValida, m.IDvalido, m.rutaCRUD_ID];
+const aptoCRUD = [...aptoDetalle, m.statusCorrecto, m.statusCompara, ...aptoUsuario, m.permUserReg];
+const aptoEliminar = [...aptoCRUD, m.usRolRevPERL];
+const correcs = [m.entValida, m.IDvalido, m.statusCompara, aptoUsuario, m.permUserReg, m.usRolRevPERL];
 
 // APIs
 router.get("/crud/api/obtiene-col-cap", API.obtieneColCap);
@@ -42,28 +45,28 @@ router.get("/crud/api/obtiene-info-del-be", API.obtieneInfo);
 router.get("/crud/api/obtiene-registro", API.obtieneRegistro);
 
 // Vistas form - Motivos
-router.get("/:familia/inactivar", aptoCRUD, capturaActivar, vista.motivosForm);
-router.get("/revision/:familia/rechazar", aptoCRUD, capturaActivar, vista.motivosForm);
+router.get("/:familia/inactivar", aptoCRUD, m.capturaActivar, vista.form.motivos);
+router.get("/revision/:familia/rechazar", aptoCRUD, m.capturaActivar, vista.form.motivos);
 
 // Vistas form - Historial
-router.get("/:familia/recuperar", aptoCRUD, capturaActivar, vista.historialForm);
-router.get("/revision/:familia/inactivar", aptoCRUD, capturaActivar, vista.historialForm);
-router.get("/revision/:familia/recuperar", aptoCRUD, capturaActivar, vista.historialForm);
-router.get("/:familia/historial", aptoDetalle, statusCompara, vista.historialForm);
+router.get("/:familia/recuperar", aptoCRUD, m.capturaActivar, vista.form.historial);
+router.get("/revision/:familia/inactivar", aptoCRUD, m.capturaActivar, vista.form.historial);
+router.get("/revision/:familia/recuperar", aptoCRUD, m.capturaActivar, vista.form.historial);
+router.get("/:familia/historial", aptoDetalle, m.statusCompara, vista.form.historial);
 
 // Vistas -  CRUD: Eliminado
-router.get("/:familia/eliminadoPorCreador", eliminadoPorCreador, vista.eliminaForm);
-router.get("/:familia/eliminar", aptoEliminar, vista.eliminaForm);
+router.get("/:familia/eliminadoPorCreador", eliminadoPorCreador, vista.form.elimina);
+router.get("/:familia/eliminar", aptoEliminar, vista.form.elimina);
 
 // Vistas post
-router.post("/:familia/inactivar", aptoCRUD, motivoNecesario, capturaInactivar, vista.inacRecup_guardar);
-router.post("/:familia/recuperar", aptoCRUD, comentNecesario, capturaInactivar, vista.inacRecup_guardar);
+router.post("/:familia/inactivar", aptoCRUD, m.motivoNecesario, m.capturaInactivar, vista.inacRecupGuardar);
+router.post("/:familia/recuperar", aptoCRUD, m.comentNecesario, m.capturaInactivar, vista.inacRecupGuardar);
 
 // Vistas - Correcciones
-router.get("/correccion/motivo", correcs, statusCorrecto, capturaActivar, vista.correcs.motivoForm);
-router.post("/correccion/motivo", correcs, statusCorrecto, motivoNecesario, capturaInactivar, vista.correcs.motivoGuardar);
-router.get("/correccion/status", correcs, capturaActivar, vista.correcs.statusForm);
-router.post("/correccion/status", correcs, capturaInactivar, vista.correcs.statusGuardar);
+router.get("/correccion/motivo", correcs, m.statusCorrecto, m.capturaActivar, vista.correcs.motivoForm);
+router.post("/correccion/motivo", correcs, m.statusCorrecto, m.motivoNecesario, m.capturaInactivar, vista.correcs.motivoGuardar);
+router.get("/correccion/status", correcs, m.capturaActivar, vista.correcs.statusForm);
+router.post("/correccion/status", correcs, m.capturaInactivar, vista.correcs.statusGuardar);
 
 // Vistas -  Historial
 
