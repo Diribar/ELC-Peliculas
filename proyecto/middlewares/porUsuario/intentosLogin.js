37019659ee620fc9fc2ef1ad907994e65b8c651b
diribar <diegoiribarren2015@gmail.com>
2024-08-30 13:35:30 -0300

    Mejora de procesos

--- proyecto/middlewares/porUsuario/intentosLogin.js
+++ proyecto/middlewares/porUsuario/intentosLogin.js
@@ -1,13 +1,15 @@
 "use strict";
 module.exports = (req, res, next) => {
 	// Reenvíos por cookies
-	if (req.cookies && req.cookies.intentosLogin >= intentosCookies) return res.redirect("/usuarios/login/suspendido");
+	if (req.cookies && req.cookies.intentosLogin && req.cookies.intentosLogin >= maxIntentosCookies)
+		return res.redirect("/usuarios/login/suspendido");
 
 	// Reenvíos x BD
 	let login, usuario;
 	if (req.session && req.session.login) login = req.session.login;
 	if (login && login.usuario) usuario = login.usuario;
-	if (usuario && usuario.intentosLogin >= intentosBD) return res.redirect("/usuarios/login/suspendido");
+	if (usuario && usuario.intentosLogin && usuario.intentosLogin >= maxIntentosBD)
+		return res.redirect("/usuarios/login/suspendido");
 
 	// Fin
 	return next();
