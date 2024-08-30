ce96f6dafd81d5cbfe2b811ce72cd84a60f6df5f
diribar <diegoiribarren2015@gmail.com>
2024-08-30 13:35:07 -0300

    Cambios de nombre

--- proyecto/middlewares/porUsuario/intentosAltaMail.js
+++ proyecto/middlewares/porUsuario/intentosAltaMail.js
@@ -4,7 +4,7 @@ const valida = require("../../rutas_y_contrs/1.1-Usuarios/US-FN-Validar");
 
 module.exports = async (req, res, next) => {
 	// Redirecciona por cookies
-	if (req.cookies && req.cookies.intentosAM >= intentosCookies) return res.redirect("/usuarios/alta-mail/suspendido");
+	if (req.cookies && req.cookies.intentosAM >= maxIntentosCookies) return res.redirect("/usuarios/alta-mail/suspendido");
 
 	// Fin
 	return next();
