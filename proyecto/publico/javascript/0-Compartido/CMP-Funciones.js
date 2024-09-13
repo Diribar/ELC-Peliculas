88b8f35cc7f4439a6509054f53dcd929c1de05e8
diribar <diegoiribarren2015@gmail.com>
2024-09-13 14:25:57 -0300

    Mejora de procesos

--- proyecto/publico/javascript/0-Compartido/CMP-Funciones.js
+++ proyecto/publico/javascript/0-Compartido/CMP-Funciones.js
@@ -33,7 +33,6 @@ let amplio = {
 				// Elimina - Limita el uso del teclado solamente a los caracteres que nos interesan
 				valor = valor
 					.replace(/[^a-záéíóúüñ ,.'&$:;…"°¿?¡!+/()\d\-]+$/gi, "")
-					.replace(/\t\n\r/g, " ") // previene el uso de 'return'
 					.replace(/[ ]/g, " ") // previene el uso de espacios 'raros'
 					.replace(/ +/g, " "); // previene repetición de espacios
 
