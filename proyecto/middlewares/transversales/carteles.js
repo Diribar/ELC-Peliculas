ffd7a49b8ef7a5842d11a2dd65527f989f784926
diribar <diegoiribarren2015@gmail.com>
2024-09-20 21:31:46 -0300

    Mejora de procesos

---
+++ proyecto/middlewares/transversales/carteles.js
@@ -0,0 +1,46 @@
+"use strict";
+
+const {obtienePorCondicion} = require("../../funciones/BaseDatos");
+
+module.exports = async (req, res, next) => {
+	// Si corresponde, interrumpe la función
+	if (req.originalUrl.includes("/api/")) return next();
+
+	// Variables
+	const {cliente} = req.session;
+
+	// Cartel de bienvenida
+
+	// Acciones si el cliente tiene una versión distinta de la actual
+	let informacion;
+	if (cliente && cliente.versionElc != versionELC) {
+		// Variables
+		const permisos = ["permInputs", "autTablEnts", "revisorPERL", "revisorLinks", "revisorEnts", "revisorUs"];
+		let novedades = novedadesELC.filter((n) => n.versionELC > cliente.versionElc && n.versionELC <= versionELC);
+		for (let i = novedades.length - 1; i >= 0; i--)
+			// Si la novedad especifica un permiso que el cliente no tiene, se la descarta
+			for (let permiso of permisos)
+				if (novedades[i][permiso] && !cliente.rolUsuario[permiso]) {
+					novedades.splice(i, 1);
+					break;
+				}
+
+		// Si hubieron novedades, genera la información
+		if (novedades.length)
+			informacion = {
+				mensajes: novedades.map((n) => n.comentario),
+				iconos: [variables.vistaEntendido(req.originalUrl)],
+				titulo: "Novedad" + (novedades.length > 1 ? "es" : "") + " del sitio",
+				check: true,
+				//ol: novedades.length > 2, // si son más de 2 novedades, las enumera
+			};
+
+		// Actualiza la versión en el usuario y la variable usuario
+		baseDeDatos.actualizaPorCondicion(tabla, {cliente_id}, {versionELC});
+		cliente.versionElc = versionELC;
+	}
+
+	// Fin
+	if (informacion) return res.render("CMP-0Estructura", {informacion});
+	return next();
+};
