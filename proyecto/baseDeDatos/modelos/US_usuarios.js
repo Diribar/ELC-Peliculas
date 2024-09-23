dcaaaf3f88019d88c6061700b63dc5eea8916065
diribar <diegoiribarren2015@gmail.com>
2024-09-23 17:35:52 -0300

    Miscelaneas

--- proyecto/baseDeDatos/modelos/US_usuarios.js
+++ proyecto/baseDeDatos/modelos/US_usuarios.js
@@ -5,7 +5,7 @@ module.exports = (sequelize, dt) => {
 		versionElc: {type: dt.STRING(4)},
 		fechaUltNaveg: {type: dt.DATE},
 		rolUsuario_id: {type: dt.INTEGER},
-		diasSinCartelBenefs: {type: dt.INTEGER}, // default '0'
+		diasSinCartelBenefs: {type: dt.INTEGER}, // default '0' (similar a 'diasSinLogin', pero vuelve a cero cuando se muestra el cartel)
 
 		email: {type: dt.STRING(100)},
 		contrasena: {type: dt.STRING(50)},
