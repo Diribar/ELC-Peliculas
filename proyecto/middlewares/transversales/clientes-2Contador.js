fef1a96ecc1ce286e46693535a65b91d32703dfa
diribar <diegoiribarren2015@gmail.com>
2024-09-23 18:43:52 -0300

    Mejora continua

--- proyecto/middlewares/transversales/clientes-2Contador.js
+++ proyecto/middlewares/transversales/clientes-2Contador.js
@@ -14,12 +14,12 @@ module.exports = async (req, res, next) => {
 	if (!req.session.clienteRecienCreado && fechaUltNaveg == hoy) return next();
 
 	// Actualiza 'fechaUltNaveg' en la tabla 'usuarios/visitas' y en la variable 'cliente'
-	baseDeDatos.actualizaTodosPorCondicion(tabla, {cliente_id}, {fechaUltNaveg: hoy});
-	cliente.fechaUltNaveg = hoy;
+	//baseDeDatos.actualizaTodosPorCondicion(tabla, {cliente_id}, {fechaUltNaveg: hoy});
+	//cliente.fechaUltNaveg = hoy;
 
 	// Contador de clientes
 	const usuario_id = usuario ? usuario.id : null;
-	contadorDeClientes(usuario_id, cliente_id);
+	contadorDeClientes(usuario_id, cliente);
 
 	// Actualiza cookies
 	if (usuario) res.cookie("email", usuario.email, {maxAge: unDia * 30});
@@ -37,7 +37,10 @@ module.exports = async (req, res, next) => {
 	return next();
 };
 
-let contadorDeClientes = async (usuario_id, cliente_id) => {
+let contadorDeClientes = async (usuario_id, cliente) => {
+	// Variables
+	const {cliente_id, diasNaveg, visitaCreadaEn} = cliente;
+
 	// Valida que no exista ya un registro del 'cliente_id' en esta fecha
 	const condicion = {fecha: hoy, cliente_id};
 	const existe = await baseDeDatos.obtienePorCondicion("clientesDelDia", condicion);
@@ -46,7 +49,7 @@ let contadorDeClientes = async (usuario_id, cliente_id) => {
 	if (existe) return;
 
 	// Agrega un registro en la tabla 'clientesDelDia'
-	let datos = {...condicion};
+	let datos = {...condicion, diasNaveg, visitaCreadaEn};
 	if (usuario_id) datos.usuario_id = usuario_id;
 	baseDeDatos.agregaRegistro("clientesDelDia", datos);
 
