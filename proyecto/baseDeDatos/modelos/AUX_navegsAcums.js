02d6db1a6c92c371171568bd2f18e6e7ad6b3153
diribar <diegoiribarren2015@gmail.com>
2024-09-24 00:28:06 -0300

    Cambios de nombre

--- proyecto/baseDeDatos/modelos/AUX_navegsAcums.js
+++ proyecto/baseDeDatos/modelos/AUX_navegsAcums.js
@@ -5,20 +5,13 @@ module.exports = (sequelize, dt) => {
 		diaSem: {type: dt.STRING(3)},
 		anoMes: {type: dt.STRING(3)},
 
-		// Cantidad de clientes
 		logins: {type: dt.INTEGER},
 		usSinLogin: {type: dt.INTEGER},
 		visitas: {type: dt.INTEGER},
 
-		// Calidad de clientes
-		altasDelDia: {type: dt.INTEGER},
-		unaSola: {type: dt.INTEGER},
-		menosDeCinco: {type: dt.INTEGER},
-		masDeTreinta: {type: dt.INTEGER},
-		otras: {type: dt.INTEGER},
 	};
 	const config = {
-		tableName: "aux_clientes_acums",
+		tableName: "aux_navegs_acums",
 		timestamps: false,
 	};
 	const entidad = sequelize.define(alias, columns, config);
--- proyecto/baseDeDatos/modelos/AUX_navegsDelDia.js
+++ proyecto/baseDeDatos/modelos/AUX_navegsDelDia.js
@@ -4,11 +4,9 @@ module.exports = (sequelize, dt) => {
 		fecha: {type: dt.STRING(10)},
 		usuario_id: {type: dt.INTEGER},
 		cliente_id: {type: dt.STRING(11)},
-		diasNaveg: {type: dt.INTEGER},
-		visitaCreadaEn: {type: dt.DATE},
 	};
 	const config = {
-		tableName: "aux_clientes_del_dia",
+		tableName: "aux_navegs_del_dia",
 		timestamps: false,
 	};
 	const entidad = sequelize.define(alias, columns, config);
