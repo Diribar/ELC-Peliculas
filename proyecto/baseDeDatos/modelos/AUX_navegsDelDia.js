e405be82d60f08c10f7a18ac49f812516b3673ee
diribar <diegoiribarren2015@gmail.com>
2024-09-24 09:17:05 -0300

    Mejora continua

--- proyecto/baseDeDatos/modelos/AUX_navegsDelDia.js
+++ proyecto/baseDeDatos/modelos/AUX_navegsDelDia.js
@@ -4,6 +4,8 @@ module.exports = (sequelize, dt) => {
 		fecha: {type: dt.STRING(10)},
 		usuario_id: {type: dt.INTEGER},
 		cliente_id: {type: dt.STRING(11)},
+		diasNaveg: {type: dt.INTEGER}, // para la estadística
+		visitaCreadaEn: {type: dt.STRING(10)}, // para la estadística
 	};
 	const config = {
 		tableName: "aux_navegs_del_dia",
