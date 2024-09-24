fa9df2f977aa1f771dc3d107973da7c2f31ebec9
diribar <diegoiribarren2015@gmail.com>
2024-09-24 00:55:13 -0300

    Cambios de nombre

--- proyecto/baseDeDatos/modelos/AUX_navegsDelDia.js
+++ proyecto/baseDeDatos/modelos/AUX_navegsDelDia.js
@@ -1,5 +1,5 @@
 module.exports = (sequelize, dt) => {
-	const alias = "clientesDelDia";
+	const alias = "navegsDelDia";
 	const columns = {
 		fecha: {type: dt.STRING(10)},
 		usuario_id: {type: dt.INTEGER},
