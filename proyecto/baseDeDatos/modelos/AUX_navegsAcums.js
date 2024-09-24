d2c40660fd6167ad0b8fb6d4cb5a801bb6dc16df
diribar <diegoiribarren2015@gmail.com>
2024-09-24 09:16:05 -0300

    Mejora continua

--- proyecto/baseDeDatos/modelos/AUX_navegsAcums.js
+++ proyecto/baseDeDatos/modelos/AUX_navegsAcums.js
@@ -5,10 +5,18 @@ module.exports = (sequelize, dt) => {
 		diaSem: {type: dt.STRING(3)},
 		anoMes: {type: dt.STRING(3)},
 
+		// Tipos de navegantes
 		logins: {type: dt.INTEGER},
 		usSinLogin: {type: dt.INTEGER},
 		visitas: {type: dt.INTEGER},
 
+		// Calidad de navegantes
+		altasDelDia: {type: dt.INTEGER},
+		transicion: {type: dt.INTEGER},
+		unoATres: {type: dt.INTEGER},
+		unoADiez: {type: dt.INTEGER},
+		masDeDiez: {type: dt.INTEGER},
+		masDeTreinta: {type: dt.INTEGER},
 	};
 	const config = {
 		tableName: "aux_navegs_acums",
