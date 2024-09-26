c8d8070ffb26d28265263b97e97b30a6196211a2
diribar <diegoiribarren2015@gmail.com>
2024-09-26 18:28:47 -0300

    Mejora continua

--- proyecto/middlewares/varios/capturaInactivar.js
+++ proyecto/middlewares/varios/capturaInactivar.js
@@ -4,9 +4,8 @@ module.exports = async (req, res, next) => {
 	if (!req.session.usuario) return next();
 
 	// Variables - Generales
+	const {entidad, id} = req.params;
 	const capturadoPor_id = req.session.usuario.id;
-	const entidad = req.query.entidad ? req.query.entidad : req.originalUrl.startsWith("/revision/usuarios") ? "usuarios" : "";
-	const id = req.query.id;
 
 	// Inactiva el registro
 	const condicion = {entidad, entidad_id: id, capturadoPor_id};
