1cac6484ef2d189cd3aab0e0298966e4558fafc5
diribar <diegoiribarren2015@gmail.com>
2024-09-25 23:10:51 -0300

    Mejora de procesos

--- proyecto/middlewares/transversales/clientes-0EsPersona.js
+++ proyecto/middlewares/transversales/clientes-0EsPersona.js
@@ -1,25 +1,19 @@
-// Obtiene 'usuario' y 'cliente'
 "use strict";
 
 module.exports = async (req, res, next) => {
 	// Verifica y avanza
-	if (
-		(req.session && (req.session.cookie || req.session.cliente || req.session.esPersona)) ||
-		(req.cookies && (req.cookies.cliente_id || req.cookies.email))
-	)
-		return next();
+	if ((req.session && (req.session.cliente || req.session.esPersona)) || (req.cookies && req.cookies.cliente_id)) return next();
 
 	// Prepara la información
 	req.session.esPersona = true;
 	const informacion = {
-		// mensajes: ["Por favor confirmanos que sos una persona", "Elegí el ícono del pulgar para arriba"],
-		// iconos: [
-		// 	{clase: iconos.xMark, titulo: "No confirmo"},
-		// 	{...variables.vistaEntendido(req.session.urlActual), autofocus: true},
-		// ],
-		// check: true,
-		titulo: "Sospecha de robot",
-		mensajes: ["Creemos que un robot activó esta petición"],
+		mensajes: ["Por favor confirmanos que sos una persona", "Elegí el ícono del pulgar para arriba"],
+		iconos: [
+			{clase: iconos.xMark, titulo: "No confirmo"},
+			{...variables.vistaEntendido(req.session.urlActual), autofocus: true},
+		],
+		check: true,
+		titulo: "Verificación",
 	};
 
 	// Fin
