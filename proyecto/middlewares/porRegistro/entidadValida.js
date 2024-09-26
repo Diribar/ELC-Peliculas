4e3dd3440d1658b175d1464b9282b3bf62d4a4d6
diribar <diegoiribarren2015@gmail.com>
2024-09-26 17:16:57 -0300

    Mejora de procesos

--- proyecto/middlewares/porRegistro/entidadValida.js
+++ proyecto/middlewares/porRegistro/entidadValida.js
@@ -2,26 +2,25 @@
 
 module.exports = (req, res, next) => {
 	// Variables
-	const entidad = req.query.entidad;
+	const {entidad, siglaFam} = req.params;
 	const vistaAnterior = variables.vistaAnterior(req.session.urlAnterior);
 	let informacion;
 
-	// Verifica los datos
+	// Verifica que existe la entidad
 	if (!entidad) informacion = {mensajes: ["Falta el dato de la 'entidad'"], iconos: [vistaAnterior]};
-	else {
-		// Entidad inexistente
-		const familia = comp.obtieneDesdeEntidad.familia(entidad);
-		const url = req.baseUrl + req.path;
-		const rutasStd = [familia, "/correccion/"];
-		const rutasPorFamilia = {
-			producto: [...rutasStd, "/links/"],
-			rclv: [...rutasStd],
+
+	// Verifica que se reconozca la 'siglaFam'
+	if (!informacion && siglaFam && !["p", "r"].includes(siglaFam))
+		informacion = {
+			mensajes: ["No tenemos esa dirección en nuestro sistema"],
+			iconos: [vistaAnterior, variables.vistaInicio], // se usa actual porque no llegó a cambiar el session
 		};
-		if (
-			!familia || // la entidad no pertenece a una familia
-			!rutasPorFamilia[familia].some((n) => url.includes(n)) // la familia no está presente en el url
-		)
-			informacion = {mensajes: ["La entidad ingresada es inválida."], iconos: [vistaAnterior]};
+
+	// Verifica la coherencia entre la siglaFam y la entidad
+	if (!informacion && siglaFam) {
+		// Obtiene la siglaFam a partir de la entidad, y las compara
+		const siglaFamEnt = comp.obtieneDesdeEntidad.siglaFam(entidad);
+		if (siglaFam != siglaFamEnt) informacion = {mensajes: ["La entidad ingresada es inválida."], iconos: [vistaAnterior]};
 	}
 
 	// Si corresponde, muestra el mensaje
