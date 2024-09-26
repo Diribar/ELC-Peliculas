6c22978b6c07c7bb068da316f3240061102379d9
diribar <diegoiribarren2015@gmail.com>
2024-09-26 06:56:03 -0300

    Mejora continua

---
+++ proyecto/middlewares/transversales/clientes-0Bienvenido.js
@@ -0,0 +1,26 @@
+"use strict";
+
+module.exports = async (req, res, next) => {
+	// Verifica y avanza
+	if ((req.session && (req.session.cliente || req.session.bienvenido)) || (req.cookies && req.cookies.cliente_id))
+		return next();
+
+	// Prepara la información
+	req.session.bienvenido = true;
+	const informacion = {
+		mensajes: [
+			"¡Bienvenido/a a nuestro sitio web de Recomendación de Películas!",
+			"Todas las películas con valores afines a la Fe Católica, en un sólo lugar.",
+			"Si no tenemos alguna, nos la podés agregar vos creándote un usuario.",
+			"Queremos ayudarte a resolver el típico problema de:<ul><li><em>No sé qué película ver</em></li><li><em>Quiero ver una película que me deje algo bueno</em></li></ul>",
+			"Acá te recomendamos películas y te derivamos a donde podés verla.",
+			"Usamos cookies para que tengas una mejor experiencia de usuario.",
+		],
+		iconos: [{...variables.vistaEntendido(req.session.urlActual), autofocus: true}],
+		titulo: "Te damos la Bienvenida",
+		check: true,
+	};
+
+	// Fin
+	return res.render("CMP-0Estructura", {informacion});
+};
