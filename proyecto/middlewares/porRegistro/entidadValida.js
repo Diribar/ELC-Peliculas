3c06bac3f58e9fbe775b2b81ab8c3afe513567c9
diribar <diegoiribarren2015@gmail.com>
2024-09-26 19:15:21 -0300

    Mejora continua

--- proyecto/middlewares/porRegistro/entidadValida.js
+++ proyecto/middlewares/porRegistro/entidadValida.js
@@ -11,27 +11,34 @@ module.exports = (req, res, next) => {
 	if (!entidad) informacion = {mensajes: ["Falta el dato de la 'entidad'"], iconos: [vistaAnterior]};
 
 	// Configura 'siglaFam'
-	if (!informacion) {
+	if (!informacion)
 		siglaFam =
 			req.params && req.params.siglaFam
-				? req.params.siglaFam //
-				:
-				?
+				? req.params.siglaFam
+				: ruta.slice(3, 1) != "/" // la convención es que el 4° elemento sea la siglaFam
+				? ruta.slice(3, 1)
 				: null;
+
+	// Verificaciones si existe la 'siglaFam'
+	if (!informacion && siglaFam) {
+		// Verifica que se reconozca la 'siglaFam'
+		if (!["p", "r"].includes(siglaFam))
+			informacion = {
+				mensajes: ["No tenemos esa dirección en nuestro sistema"],
+				iconos: [vistaAnterior, variables.vistaInicio], // se usa actual porque no llegó a cambiar el session
+			};
+		// Verifica la coherencia entre la siglaFam y la entidad
+		else {
+			// Obtiene la siglaFam a partir de la entidad, y las compara
+			const siglaFamEnt = comp.obtieneDesdeEntidad.siglaFam(entidad);
+			if (siglaFam != siglaFamEnt) informacion = {mensajes: ["La entidad ingresada es inválida."], iconos: [vistaAnterior]};
+		}
 	}
 
-	// Verifica que se reconozca la 'siglaFam'
-	if (!informacion && siglaFam && !["p", "r"].includes(siglaFam))
-		informacion = {
-			mensajes: ["No tenemos esa dirección en nuestro sistema"],
-			iconos: [vistaAnterior, variables.vistaInicio], // se usa actual porque no llegó a cambiar el session
-		};
-
-	// Verifica la coherencia entre la siglaFam y la entidad
-	if (!informacion && siglaFam) {
-		// Obtiene la siglaFam a partir de la entidad, y las compara
-		const siglaFamEnt = comp.obtieneDesdeEntidad.siglaFam(entidad);
-		if (siglaFam != siglaFamEnt) informacion = {mensajes: ["La entidad ingresada es inválida."], iconos: [vistaAnterior]};
+	if (!informacion && !siglaFam) {
+		const entidades = variables.entidades.prodsRclvs;
+		if (!entidades.includes(entidad))
+			informacion = {mensajes: ["La entidad ingresada es inválida."], iconos: [vistaAnterior]};
 	}
 
 	// Si corresponde, muestra el mensaje
