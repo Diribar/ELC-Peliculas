d44d6e4ae61832cab62debe33ddeec41268c00d5
diribar <diegoiribarren2015@gmail.com>
2024-09-27 10:13:14 -0300

    Mejora de procesos

--- proyecto/rutas_y_contrs/2.0-Familias/FM-ControlVista.js
+++ proyecto/rutas_y_contrs/2.0-Familias/FM-ControlVista.js
@@ -279,31 +279,4 @@ module.exports = {
 			return res.redirect("/" + familia + "/" + destino + cola);
 		},
 	},
-	redireccionar: (req, res) => {
-		// Variables
-		const {entidad, id} = req.query;
-		const familia = comp.obtieneDesdeEntidad.familia(entidad);
-		let {originalUrl} = req;
-
-		// Reemplaza la familia por la entidad
-		if (["/producto/", "/rclv/"].some((n) => originalUrl.includes(n)))
-			originalUrl = originalUrl.replace("/" + familia + "/", "/" + entidad + "/"); // /peliculas/
-		// Si no existía la familia, le agrega la entidad
-		else originalUrl = "/" + entidad + originalUrl; // /peliculas/cm - /personaje/cs
-
-		// Quita la entidad y el id del url
-		originalUrl = originalUrl.replace("entidad=" + entidad + "&", "");
-		originalUrl = originalUrl.replace("/?id=" + id, "/" + id + "/?");
-		originalUrl = originalUrl.replace("?&", "?");
-		if (originalUrl.endsWith("?")) originalUrl = originalUrl.slice(0, -1);
-		if (originalUrl.endsWith("/")) originalUrl = originalUrl.slice(0, -1);
-
-		// Reemplaza la ruta anterior por la actual
-		const rutasActualizadas = comp.rutasActualizadas(entidad);
-		const ruta = rutasActualizadas.find((n) => originalUrl.includes(n.ant));
-		if (ruta) originalUrl = originalUrl.replace(ruta.ant, ruta.act);
-
-		// Fin
-		return res.redirect(originalUrl);
-	},
 };
--- proyecto/rutas_y_contrs/2.0-Familias/FM-RutasAnt.js
+++ proyecto/rutas_y_contrs/2.0-Familias/FM-RutasAnt.js
@@ -1,23 +1,46 @@
 "use strict";
 // Variables
 const router = express.Router();
-const vista = require("./FM-ControlVista");
-
-// Middlewares - Específicos del registro
-const entValidaAnt = require("../../middlewares/porRegistro/entValidaAnt");
-const iDvalidoAnt = require("../../middlewares/porRegistro/iDvalidoAnt");
-const entId = [entValidaAnt, iDvalidoAnt];
 
 // Vistas
-router.get("/:familia/historial", entId, vista.redireccionar); // Historial
-router.get("/:familia/inactivar", entId, vista.redireccionar); // Inactivar
-router.get("/:familia/recuperar", entId, vista.redireccionar); // Recuperar
-router.get("/:familia/eliminadoPorCreador", entId, vista.redireccionar); // Eliminado por creador
-router.get("/:familia/eliminar", entId, vista.redireccionar); // Eliminado
+router.get("/:familia/historial", redireccionar); // Historial
+router.get("/:familia/inactivar", redireccionar); // Inactivar
+router.get("/:familia/recuperar", redireccionar); // Recuperar
+router.get("/:familia/eliminadoPorCreador", redireccionar); // Eliminado por creador
+router.get("/:familia/eliminar", redireccionar); // Eliminado
 
 // Vistas - Correcciones
-router.get("/correccion/motivo", entId, vista.redireccionar);
-router.get("/correccion/status", entId, vista.redireccionar);
+router.get("/correccion/motivo", redireccionar);
+router.get("/correccion/status", redireccionar);
 
 // Fin
 module.exports = router;
+
+// Funciones
+let redireccionar = (req, res) => {
+	// Variables
+	const {entidad, id} = req.query;
+	const familia = comp.obtieneDesdeEntidad.familia(entidad);
+	let {originalUrl} = req;
+
+	// Reemplaza la familia por la entidad
+	if (["/producto/", "/rclv/"].some((n) => originalUrl.includes(n)))
+		originalUrl = originalUrl.replace("/" + familia + "/", "/" + entidad + "/"); // /peliculas/
+	// Si no existía la familia, le agrega la entidad
+	else originalUrl = "/" + entidad + originalUrl; // /peliculas/cm - /personaje/cs
+
+	// Quita la entidad y el id del url
+	originalUrl = originalUrl.replace("entidad=" + entidad + "&", "");
+	originalUrl = originalUrl.replace("/?id=" + id, "/" + id + "/?");
+	originalUrl = originalUrl.replace("?&", "?");
+	if (originalUrl.endsWith("?")) originalUrl = originalUrl.slice(0, -1);
+	if (originalUrl.endsWith("/")) originalUrl = originalUrl.slice(0, -1);
+
+	// Reemplaza la ruta anterior por la actual
+	const rutasActualizadas = comp.rutasActualizadas(entidad);
+	const ruta = rutasActualizadas.find((n) => originalUrl.includes(n.ant));
+	if (ruta) originalUrl = originalUrl.replace(ruta.ant, ruta.act);
+
+	// Fin
+	return res.redirect(originalUrl);
+};
