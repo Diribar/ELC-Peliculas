fbae283e1d0e85037d6f09dfa15f93443d5d21dc
diribar <diegoiribarren2015@gmail.com>
2024-09-27 08:53:51 -0300

    Mejora de procesos

--- proyecto/rutas_y_contrs/9-Miscelaneas/MS-Rutas.js
+++ proyecto/rutas_y_contrs/9-Miscelaneas/MS-Rutas.js
@@ -1,14 +1,21 @@
 "use strict";
 // Variables
 const router = express.Router();
+const API = require("./MS-ControlAPI");
 const vista = require("./MS-ControlVista");
 
 // Middlewares - Varios
 const entidadRclv = require("../../middlewares/porRegistro/entidadRclv");
+const capturaInactivar = require("../../middlewares/varios/capturaInactivar");
+
+// APIs
+router.get("/api/horario-inicial/", API.horarioInicial);
+router.get("/api/busqueda-rapida/", API.busquedaRapida);
 
 // Redireciona
-router.get("/inicio", vista.redirecciona.redireccionaInicio);
 router.get("/", vista.redirecciona.inicio);
+router.get("/inicio", vista.redirecciona.redireccionaInicio);
+router.get("/inactivar-captura", capturaInactivar, vista.redirecciona);
 
 // Información para mostrar en el explorador
 router.get("/session", vista.listados.session);
