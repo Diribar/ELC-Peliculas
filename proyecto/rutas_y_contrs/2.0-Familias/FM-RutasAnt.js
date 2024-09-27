d34a584f484819d143fa7645e8c1d9333e8469c4
diribar <diegoiribarren2015@gmail.com>
2024-09-27 10:35:41 -0300

    Mejora de procesos

--- proyecto/rutas_y_contrs/2.0-Familias/FM-RutasAnt.js
+++ proyecto/rutas_y_contrs/2.0-Familias/FM-RutasAnt.js
@@ -2,45 +2,16 @@
 // Variables
 const router = express.Router();
 
-// Funciones
-let redirecciona = (req, res) => {
-	// Variables
-	const {entidad, id} = req.query;
-	const familia = comp.obtieneDesdeEntidad.familia(entidad);
-	let {originalUrl} = req;
-
-	// Reemplaza la familia por la entidad
-	if (["/producto/", "/rclv/"].some((n) => originalUrl.includes(n)))
-		originalUrl = originalUrl.replace("/" + familia + "/", "/" + entidad + "/"); // /peliculas/
-	// Si no existía la familia, le agrega la entidad
-	else originalUrl = "/" + entidad + originalUrl; // /peliculas/cm - /personaje/cs
-
-	// Quita la entidad y el id del url
-	originalUrl = originalUrl.replace("entidad=" + entidad + "&", "");
-	originalUrl = originalUrl.replace("/?id=" + id, "/" + id + "/?");
-	originalUrl = originalUrl.replace("?&", "?");
-	if (originalUrl.endsWith("?")) originalUrl = originalUrl.slice(0, -1);
-	if (originalUrl.endsWith("/")) originalUrl = originalUrl.slice(0, -1);
-
-	// Reemplaza la ruta anterior por la actual
-	const rutasActualizadas = comp.rutasActualizadas(entidad);
-	const ruta = rutasActualizadas.find((n) => originalUrl.includes(n.ant));
-	if (ruta) originalUrl = originalUrl.replace(ruta.ant, ruta.act);
-
-	// Fin
-	return res.redirect(originalUrl);
-};
-
 // Vistas
-router.get("/:familia/historial", redirecciona); // Historial
-router.get("/:familia/inactivar", redirecciona); // Inactivar
-router.get("/:familia/recuperar", redirecciona); // Recuperar
-router.get("/:familia/eliminadoPorCreador", redirecciona); // Eliminado por creador
-router.get("/:familia/eliminar", redirecciona); // Eliminado
+router.get("/:familia/historial", comp.redireccionaRutasAnts); // Historial
+router.get("/:familia/inactivar", comp.redireccionaRutasAnts); // Inactivar
+router.get("/:familia/recuperar", comp.redireccionaRutasAnts); // Recuperar
+router.get("/:familia/eliminadoPorCreador", comp.redireccionaRutasAnts); // Eliminado por creador
+router.get("/:familia/eliminar", comp.redireccionaRutasAnts); // Eliminado
 
 // Vistas - Correcciones
-router.get("/correccion/motivo", redirecciona);
-router.get("/correccion/status", redirecciona);
+router.get("/correccion/motivo", comp.redireccionaRutasAnts);
+router.get("/correccion/status", comp.redireccionaRutasAnts);
 
 // Fin
 module.exports = router;
--- proyecto/rutas_y_contrs/3-Rev-Entidades/RE-RutasAnt.js
+++ proyecto/rutas_y_contrs/3-Rev-Entidades/RE-RutasAnt.js
@@ -2,44 +2,16 @@
 // Variables
 const router = express.Router();
 
-// Funciones
-let redirecciona = (req, res) => {
-	// Variables
-	const {entidad, id} = req.query;
-	const familia = comp.obtieneDesdeEntidad.familia(entidad);
-	let {originalUrl} = req;
-
-	// Reemplaza la familia por la entidad
-	if (["/producto/", "/rclv/"].some((n) => originalUrl.includes(n)))
-		originalUrl = originalUrl.replace("/" + familia + "/", "/" + entidad + "/"); // /peliculas/
-	// Si no existía la familia, le agrega la entidad
-	else originalUrl = "/" + entidad + originalUrl; // /peliculas/cm - /personaje/cs
-
-	// Quita la entidad y el id del url
-	originalUrl = originalUrl.replace("entidad=" + entidad + "&", "");
-	originalUrl = originalUrl.replace("/?id=" + id, "/" + id + "/?");
-	originalUrl = originalUrl.replace("?&", "?");
-	if (originalUrl.endsWith("?")) originalUrl = originalUrl.slice(0, -1);
-	if (originalUrl.endsWith("/")) originalUrl = originalUrl.slice(0, -1);
-
-	// Reemplaza la ruta anterior por la actual
-	const rutasActualizadas = comp.rutasActualizadas(entidad);
-	const ruta = rutasActualizadas.find((n) => originalUrl.includes(n.ant));
-	if (ruta) originalUrl = originalUrl.replace(ruta.ant, ruta.act);
-
-	// Fin
-	return res.redirect(originalUrl);
-};
-
-router.get("/tablero-de-entidades", redirecciona);
-router.get("/tablero-de-mantenimiento", redirecciona);
-router.get("/:familia/alta", redirecciona); // Altas
-router.get("/:familia/rechazar", redirecciona); // Rechazar
-router.get("/:familia/edicion", redirecciona); // Edición
-router.get("/:familia/inactivar", redirecciona); // Revisar Inactivar
-router.get("/:familia/recuperar", redirecciona); // Revisar Recuperar
-router.get("/rclv/solapamiento", redirecciona); // Solapamiento
-router.get("/links", redirecciona); // Links
+// Vistas
+router.get("/tablero-de-entidades", comp.redireccionaRutasAnts);
+router.get("/tablero-de-mantenimiento", comp.redireccionaRutasAnts);
+router.get("/:familia/alta", comp.redireccionaRutasAnts); // Altas
+router.get("/:familia/rechazar", comp.redireccionaRutasAnts); // Rechazar
+router.get("/:familia/edicion", comp.redireccionaRutasAnts); // Edición
+router.get("/:familia/inactivar", comp.redireccionaRutasAnts); // Revisar Inactivar
+router.get("/:familia/recuperar", comp.redireccionaRutasAnts); // Revisar Recuperar
+router.get("/rclv/solapamiento", comp.redireccionaRutasAnts); // Solapamiento
+router.get("/links", comp.redireccionaRutasAnts); // Links
 
 // Fin
 module.exports = router;
