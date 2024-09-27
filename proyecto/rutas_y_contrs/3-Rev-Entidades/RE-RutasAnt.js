654dd4fcf16ae9f027715ea1a5c5eeb00d22964d
diribar <diegoiribarren2015@gmail.com>
2024-09-27 10:13:28 -0300

    Mejora continua

--- proyecto/rutas_y_contrs/3-Rev-Entidades/RE-RutasAnt.js
+++ proyecto/rutas_y_contrs/3-Rev-Entidades/RE-RutasAnt.js
@@ -1,62 +1,9 @@
 "use strict";
 // Variables
 const router = express.Router();
-const API = require("./RE-ControlAPI");
 const vista = require("./RE-ControlVista");
 const vistaRCLV = require("../2.2-RCLVs/RCLV-ControlVista");
 const vistaFM = require("../2.0-Familias/FM-ControlVista");
-
-// Middlewares particulares
-const m = {
-	// Específicos de usuarios
-	usAltaTerm: require("../../middlewares/porUsuario/usAltaTerm"),
-	usAptoInput: require("../../middlewares/porUsuario/usAptoInput"),
-	usPenalizaciones: require("../../middlewares/porUsuario/usPenalizaciones"),
-	usRolAutTablEnts: require("../../middlewares/porUsuario/usRolAutTablEnts"),
-	usRolRevPERL: require("../../middlewares/porUsuario/usRolRevPERL"),
-	usRolRevLinks: require("../../middlewares/porUsuario/usRolRevLinks"),
-
-	// Específicos del registro
-	entValida: require("../../middlewares/porRegistro/entidadValida"),
-	iDvalido: require("../../middlewares/porRegistro/iDvalido"),
-	linkAltaBaja: require("../../middlewares/porRegistro/linkAltaBaja"),
-	rutaCRUD_ID: require("../../middlewares/varios/rutaCRUD_ID"),
-	statusCorrecto: require("../../middlewares/porRegistro/statusCorrecto"),
-	edicionAPI: require("../../middlewares/porRegistro/edicionAPI"),
-	edicionVista: require("../../middlewares/porRegistro/edicionVista"),
-	prodSinRclvAprob: require("../../middlewares/porRegistro/prodSinRclvAprob"),
-	linksEnSemana: require("../../middlewares/porRegistro/linksEnSemana"),
-	motivoNecesario: require("../../middlewares/porRegistro/motivoNecesario"),
-	motivoOpcional: require("../../middlewares/porRegistro/motivoOpcional"),
-
-	// Temas de captura
-	permUserReg: require("../../middlewares/porRegistro/permUserReg"),
-	capturaActivar: require("../../middlewares/varios/capturaActivar"),
-	capturaInactivar: require("../../middlewares/varios/capturaInactivar"),
-
-	// Middlewares - Otros
-	multer: require("../../middlewares/varios/multer"),
-};
-
-// Middlewares - Consolidados
-const usuarioBase = [m.usAltaTerm, m.usPenalizaciones];
-const aptoUsuario = [...usuarioBase, m.usAptoInput];
-const aptoCRUD = [m.entValida, m.iDvalido, m.statusCorrecto, ...usuarioBase, m.permUserReg];
-const aptoEdicion = [...aptoCRUD, m.usRolRevPERL, m.edicionVista];
-
-// APIs - Tablero
-router.get("/api/actualiza-visibles", API.actualizaVisibles);
-
-// APIs - Producto y RCLV
-router.get("/api/edicion/motivo-generico", API.obtieneMotivoGenerico);
-router.get("/api/edicion/aprob-rech", m.edicionAPI, API.edicAprobRech);
-
-// APIs- Links
-router.get("/api/link/alta-baja", m.linkAltaBaja, API.links.altaBaja);
-//router.get("/api/link/eliminar", API.links.altaBaja);
-router.get("/api/link/edicion", m.edicionAPI, API.edicAprobRech);
-router.get("/api/link/siguiente-producto", API.links.sigProd);
-router.get("/api/link/obtiene-embeded-link", API.links.obtieneEmbededLink);
 
 // Vistas - Tablero de Control
 router.get("/tablero-de-entidades", usuarioBase, m.usRolAutTablEnts, vista.tableroEntidades);
