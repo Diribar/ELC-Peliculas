4f97d5816dd6a4ec165e44c363d5e44ab974eb0a
diribar <diegoiribarren2015@gmail.com>
2024-09-27 13:32:10 -0300

    Cambios de nombre

--- proyecto/rutas_y_contrs/2.0-Familias/FM-RutasAnt.js
+++ proyecto/rutas_y_contrs/2.0-Familias/FM-RutasAnt.js
@@ -3,15 +3,15 @@
 const router = express.Router();
 
 // Vistas
-router.get("/:familia/historial", comp.redireccionaRutasAnts); // Historial
-router.get("/:familia/inactivar", comp.redireccionaRutasAnts); // Inactivar
-router.get("/:familia/recuperar", comp.redireccionaRutasAnts); // Recuperar
-router.get("/:familia/eliminadoPorCreador", comp.redireccionaRutasAnts); // Eliminado por creador
-router.get("/:familia/eliminar", comp.redireccionaRutasAnts); // Eliminado
+router.get("/:familia/historial", comp.deRutasAntArutasAct); // Historial
+router.get("/:familia/inactivar", comp.deRutasAntArutasAct); // Inactivar
+router.get("/:familia/recuperar", comp.deRutasAntArutasAct); // Recuperar
+router.get("/:familia/eliminadoPorCreador", comp.deRutasAntArutasAct); // Eliminado por creador
+router.get("/:familia/eliminar", comp.deRutasAntArutasAct); // Eliminado
 
 // Vistas - Correcciones
-router.get("/correccion/motivo", comp.redireccionaRutasAnts);
-router.get("/correccion/status", comp.redireccionaRutasAnts);
+router.get("/correccion/motivo", comp.deRutasAntArutasAct);
+router.get("/correccion/status", comp.deRutasAntArutasAct);
 
 // Fin
 module.exports = router;
