946b6883525f4df2a88ba69ecc781a47eca1738b
diribar <diegoiribarren2015@gmail.com>
2024-09-27 15:12:34 -0300

    Mejora de procesos

--- proyecto/rutas_y_contrs/3-Rev-Entidades/RE-RutasAnt.js
+++ proyecto/rutas_y_contrs/3-Rev-Entidades/RE-RutasAnt.js
@@ -4,8 +4,8 @@ const router = express.Router();
 
 // Vistas
 router.get("/:familia/alta", comp.deRutasAntArutasAct); // Altas
-router.get("/:familia/rechazar", comp.deRutasAntArutasAct); // Rechazar
 router.get("/:familia/edicion", comp.deRutasAntArutasAct); // Edición
+router.get("/:familia/rechazar", comp.deRutasAntArutasAct); // Rechazar
 router.get("/:familia/inactivar", comp.deRutasAntArutasAct); // Inactivar
 router.get("/:familia/recuperar", comp.deRutasAntArutasAct); // Recuperar
 router.get("/rclv/solapamiento", comp.deRutasAntArutasAct); // Solapamiento
