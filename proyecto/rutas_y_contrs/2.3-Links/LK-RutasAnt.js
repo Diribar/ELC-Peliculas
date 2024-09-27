7d12527216d972d927a7a8b49bde031a992fabdc
diribar <diegoiribarren2015@gmail.com>
2024-09-27 09:59:48 -0300

    Mejora continua

--- proyecto/rutas_y_contrs/2.3-Links/LK-RutasAnt.js
+++ proyecto/rutas_y_contrs/2.3-Links/LK-RutasAnt.js
@@ -1,44 +1,17 @@
 "use strict";
 // Variables
 const router = express.Router();
-const API = require("./LK-ControlAPI");
 const vista = require("./LK-ControlVista");
 
-// Middlewares - Específicos de usuarios
-const usAltaTerm = require("../../middlewares/porUsuario/usAltaTerm");
-const usPenalizaciones = require("../../middlewares/porUsuario/usPenalizaciones");
-const usAptoInput = require("../../middlewares/porUsuario/usAptoInput");
-
 // Middlewares - Específicos del registro
-const entValida = require("../../middlewares/porRegistro/entidadValida");
-const iDvalido = require("../../middlewares/porRegistro/iDvalido");
 const linkIDvalido = require("../../middlewares/porRegistro/linkIDvalido");
-const statusCorrecto = require("../../middlewares/porRegistro/statusCorrecto");
-
-// Middlewares - Temas de captura
-const permUserReg = require("../../middlewares/porRegistro/permUserReg");
-const capturaActivar = require("../../middlewares/varios/capturaActivar");
-
-// Middlewares - Otros
-const rutaCRUD_ID = require("../../middlewares/varios/rutaCRUD_ID");
-
-// Middlewares - Consolidados
-const aptoUsuario = [usAltaTerm, usPenalizaciones, usAptoInput];
-const aptoABM = [...aptoUsuario, entValida, iDvalido, statusCorrecto, permUserReg, rutaCRUD_ID];
-
-// APIs - Links
-router.get("/api/valida", API.valida);
-router.get("/api/obtiene-provs-links", API.obtieneProvs);
-
-// APIs - ABM
-router.get("/api/guardar", API.guarda);
-router.get("/api/inactiva-o-elimina", API.inactivaElimina);
-router.get("/api/recuperar", API.recupera);
-router.get("/api/deshacer", API.deshace);
+const entValidaAnt = require("../../middlewares/porRegistro/entValidaAnt");
+const iDvalidoAnt = require("../../middlewares/porRegistro/iDvalidoAnt");
+const entId = [entValidaAnt, iDvalidoAnt];
 
 // Vistas
-router.get("/abpm", aptoABM, capturaActivar, vista.abm);
-router.get("/visualizacion",linkIDvalido, vista.visualizacion);
+router.get("/abm", entId, vista.abm);
+router.get("/visualizacion", linkIDvalido, vista.visualizacion);
 
 // Fin
 module.exports = router;
