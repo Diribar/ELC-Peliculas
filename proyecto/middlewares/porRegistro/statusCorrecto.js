41613e6684e275c674c7f3233627476aaf3c90ee
diribar <diegoiribarren2015@gmail.com>
2024-09-27 08:40:38 -0300

    Mejora de procesos

--- proyecto/middlewares/porRegistro/statusCorrecto.js
+++ proyecto/middlewares/porRegistro/statusCorrecto.js
@@ -3,11 +3,52 @@
 module.exports = async (req, res, next) => {
 	// Variables
 	const {entidad, id} = req.params;
-	const {baseUrl, ruta} = comp.partesDelUrl(req);
-	console.log(7,{baseUrl, ruta});
-
-	const statusEsperados_id = FN_statusEsperados_id(baseUrl, ruta);
+	const familia = comp.obtieneDesdeEntidad.familia(entidad);
+	const entidades = variables.entidades.todos;
+	let statusEsperados_id = [];
 	let informacion;
+
+	// Obtiene la 'baseUrl' y la 'ruta'
+	let {baseUrl, ruta} = comp.partesDelUrl(req);
+	baseUrl = baseUrl.slice(1);
+	ruta = ruta.slice(1);
+	console.log(7, {baseUrl, ruta});
+
+	// Obtiene el status esperado - CRUD
+	if (!statusEsperados_id && entidades.includes(baseUrl)) {
+		statusEsperados_id = ruta.startsWith("ed") // edición
+			? activos_ids
+			: ruta.startsWith("in") // inactivar
+			? aprobados_ids
+			: ["rc", "el"].some((n) => ruta.startsWith(n)) // recuperar y eliminar
+			? [inactivo_id]
+			: ruta.startsWith("ec") // eliminar por el creador
+			? [creado_id]
+			: ruta == "clp" // calificar producto
+			? activos_ids
+			: ruta == "abp" // abm links
+			? activos_ids
+			: ["cm", "cs"].some((n) => ruta.startsWith(n)) // correcciones
+			? [inactivo_id]
+			: [];
+	}
+
+	// Obtiene el status esperado - Revisión de Entidades
+	if (!statusEsperados_id && baseUrl == "revision") {
+		statusEsperados_id = ["ag", "ch"].some((n) => ruta.startsWith(n)) // revisar alta y rechazar
+			? [creado_id]
+			: ruta.startsWith("ed") // edición
+			? aprobados_ids
+			: ruta.startsWith("in") // inactivar
+			? [inactivar_id]
+			: ruta.startsWith("rc") // recuperar
+			? [recuperar_id]
+			: ruta == "lkp" // links
+			? activos_ids
+			: ruta == "rsr" // solapamiento
+			? activos_ids
+			: [];
+	}
 
 	// Obtiene el statusActual
 	const registro = await baseDeDatos.obtienePorId(entidad, id);
@@ -31,11 +72,11 @@ module.exports = async (req, res, next) => {
 		// Variables para el ícono
 		const origen = req.query.origen
 			? req.query.origen
-			: baseUrl == "/revision/usuarios"
-			? "TU"
 			: baseUrl == "/revision"
-			? "TE"
-			: baseUrl == "/rclv"
+			? familia
+				? "TE"
+				: "TU"
+			: familia == "rclv"
 			? "RDT"
 			: "PDT";
 		const link = "/inactivar-captura/?entidad=" + entidad + "&id=" + id + "&origen=" + origen;
@@ -51,39 +92,4 @@ module.exports = async (req, res, next) => {
 	// Conclusiones
 	if (informacion) return res.render("CMP-0Estructura", {informacion});
 	else return next();
-};
-
-// Funciones
-let FN_statusEsperados_id = (baseUrl, ruta) => {
-	return  ["e"]
-	ruta == "/edicion/"
-			? activos_ids
-			: ruta == "/inactivar/"
-			? aprobados_ids
-			: ruta == "/recuperar/" || ruta == "/eliminar/"
-			? [inactivo_id]
-			: ruta == "/eliminadoPorCreador/"
-			? [creado_id]
-			: ruta == "/calificar/"
-			? activos_ids
-			: []
-		: baseUrl == "/links" && ruta == "/abm/" // Preguntas para 'Links'
-		? activos_ids
-		: baseUrl == "/revision" // Preguntas para 'Revisión'
-		? ruta.includes("/alta/") || ruta.includes("/rechazar/") // para 'producto' y 'rclv'
-			? [creado_id]
-			: ruta.includes("/edicion/")
-			? aprobados_ids
-			: ruta.includes("/inactivar/")
-			? [inactivar_id]
-			: ruta.includes("/recuperar/")
-			? [recuperar_id]
-			: ruta.includes("/links/")
-			? activos_ids // aprobados_ids
-			: ruta.includes("/solapamiento/")
-			? activos_ids
-			: []
-		: baseUrl == "/correccion" && ruta == "/motivo/"
-		? [inactivo_id]
-		: [];
 };
