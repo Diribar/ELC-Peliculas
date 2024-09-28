461b76d761da5a9b6e222a30299d70259fc53907
diribar <diegoiribarren2015@gmail.com>
2024-09-27 23:07:46 -0300

    Solución de errores

--- proyecto/middlewares/porRegistro/entidadValida.js
+++ proyecto/middlewares/porRegistro/entidadValida.js
@@ -2,7 +2,7 @@
 
 module.exports = (req, res, next) => {
 	// Variables
-	const {entidad} = req.params;
+	const entidad = req.params.entidad ? req.params.entidad : req.baseUrl.slice(1);
 	const vistaAnterior = variables.vistaAnterior(req.session.urlAnterior);
 	const {baseUrl, ruta} = comp.partesDelUrl(req);
 	let informacion, siglaFam;
