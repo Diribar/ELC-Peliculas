9dd259a1162ed8f69e0c0b254d42fd80eff3ec97
diribar <diegoiribarren2015@gmail.com>
2024-09-27 23:08:44 -0300

    Mejora de procesos

--- proyecto/rutas_y_contrs/2.0-Familias/FM-RutasAnt.js
+++ proyecto/rutas_y_contrs/2.0-Familias/FM-RutasAnt.js
@@ -1,18 +1,18 @@
 "use strict";
 // Variables
 const router = express.Router();
+const vistaMS = require("../9-Miscelaneas/MS-ControlVista");
 
 // Vistas
-router.get("/:familia/historial", comp.deRutasAntArutasAct); // Historial
-router.get("/:familia/inactivar", comp.deRutasAntArutasAct); // Inactivar
-router.get("/:familia/recuperar", comp.deRutasAntArutasAct); // Recuperar
-router.get("/:familia/eliminadoPorCreador", comp.deRutasAntArutasAct); // Eliminado por creador
-router.get("/:familia/eliminar", comp.deRutasAntArutasAct); // Eliminado
+router.get("/:familia/historial", vistaMS.redirecciona.rutasAntiguas); // Historial
+router.get("/:familia/inactivar", vistaMS.redirecciona.rutasAntiguas); // Inactivar
+router.get("/:familia/recuperar", vistaMS.redirecciona.rutasAntiguas); // Recuperar
+router.get("/:familia/eliminadoPorCreador", vistaMS.redirecciona.rutasAntiguas); // Eliminado por creador
+router.get("/:familia/eliminar", vistaMS.redirecciona.rutasAntiguas); // Eliminado
 
 // Vistas - Correcciones
-router.get("/correccion/motivo", comp.deRutasAntArutasAct);
-router.get("/correccion/status", comp.deRutasAntArutasAct);
+router.get("/correccion/motivo", vistaMS.redirecciona.rutasAntiguas);
+router.get("/correccion/status", vistaMS.redirecciona.rutasAntiguas);
 
 // Fin
 module.exports = router;
-
