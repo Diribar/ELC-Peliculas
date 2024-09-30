bc60f5992a2c1de7f67ba3551aec5b2729f3f201
diribar <diegoiribarren2015@gmail.com>
2024-09-30 09:32:01 -0300

    Mejora de procesos

--- proyecto/app.js
+++ proyecto/app.js
@@ -1,5 +1,5 @@
 // Variable 'global'
-const constantes = require("./funciones/Constantes");
+const constantes = require("./funciones/Variables1.js");
 for (let metodo in constantes) global[metodo] = constantes[metodo];
 
 // Require 'path'
@@ -79,132 +79,16 @@ app.set("views", [
 
 // Procesos que requieren de 'async' y 'await'
 (async () => {
-	// Lectura de la base de datos
-	global.baseDeDatos = require("./funciones/BaseDatos");
-	let datos = {
-		// Variables de usuario
-		statusRegistrosUs: baseDeDatos.obtieneTodosConOrden("statusRegistrosUs", "orden"),
-		rolesUs: baseDeDatos.obtieneTodosConOrden("rolesUsuarios", "orden"),
-
-		// Variable de entidades
-		statusRegistros: baseDeDatos.obtieneTodosConOrden("statusRegistros", "orden"),
-		generos: baseDeDatos.obtieneTodosConOrden("generos", "orden"),
-		statusMotivos: baseDeDatos
-			.obtieneTodosConOrden("statusMotivos", "orden")
-			.then((n) => n.sort((a, b) => (a.grupo < b.grupo ? -1 : a.grupo > b.grupo ? 1 : 0))),
-		motivosEdics: baseDeDatos.obtieneTodosConOrden("motivosEdics", "orden"),
-
-		// Variables de productos
-		idiomas: baseDeDatos.obtieneTodosConOrden("idiomas", "nombre"),
-		paises: baseDeDatos.obtieneTodos("paises", "cantProds").then((n) => n.sort((a, b) => (a.nombre < b.nombre ? -1 : 1))),
-		publicos: baseDeDatos.obtieneTodosConOrden("publicos", "orden"),
-		tiposActuacion: baseDeDatos.obtieneTodosConOrden("tiposActuacion", "orden"),
-		epocasEstreno: baseDeDatos.obtieneTodosConOrden("epocasEstreno", "hasta", "DESC"),
-
-		// Calificación de productos
-		calCriterios: baseDeDatos.obtieneTodos("calCriterios"),
-		feValores: baseDeDatos.obtieneTodosConOrden("feValores", "orden"),
-		entretiene: baseDeDatos.obtieneTodosConOrden("entretiene", "orden"),
-		calidadTecnica: baseDeDatos.obtieneTodosConOrden("calidadTecnica", "orden"),
-
-		// Variables de RCLVs
-		epocasOcurrencia: baseDeDatos.obtieneTodosConOrden("epocasOcurrencia", "orden"),
-		rolesIglesia: baseDeDatos.obtieneTodosConOrden("rolesIglesia", "orden"),
-		canons: baseDeDatos.obtieneTodosConOrden("canons", "orden"),
-		hoyEstamos: baseDeDatos.obtieneTodosConOrden("hoyEstamos", "nombre"),
-
-		// Variables de links
-		linksProvs: baseDeDatos
-			.obtieneTodos("linksProvs", "cantLinks")
-			.then((n) => n.sort((a, b) => b.cantLinks.cantidad - a.cantLinks.cantidad)),
-		linksTipos: baseDeDatos.obtieneTodos("linksTipos"),
-
-		// Consultas
-		cnEntidades: baseDeDatos.obtieneTodos("cnEntidades"),
-		cnLayouts: baseDeDatos
-			.obtieneTodos("cnLayouts", "entidades")
-			.then((n) => n.filter((m) => m.activo))
-			.then((n) => n.sort((a, b) => a.orden - b.orden)),
-		pppOpcsArray: baseDeDatos.obtieneTodos("pppOpciones"),
-
-		// Menús
-		menuCapacitac: baseDeDatos.obtieneTodosConOrden("menuCapacitac", "orden").then((n) => n.filter((m) => m.actualizado)),
-		menuUsuario: baseDeDatos.obtieneTodosConOrden("menuUsuario", "orden").then((n) => n.filter((m) => m.actualizado)),
-
-		// Otros
-		meses: baseDeDatos.obtieneTodos("meses"),
-		fechasDelAno: baseDeDatos.obtieneTodos("fechasDelAno", "epocaDelAno"),
-		novedadesELC: baseDeDatos.obtieneTodosConOrden("novedadesELC", "fecha"),
-	};
-
-	// Procesa todas las lecturas
-	const valores = await Promise.all(Object.values(datos));
-	Object.keys(datos).forEach((campo, i) => (global[campo] = valores[i]));
-
 	// Variables que dependen de las lecturas de BD
-	// 1. Status de productos - Simples
-	global.creado_id = statusRegistros.find((n) => n.codigo == "creado").id;
-	global.creadoAprob_id = statusRegistros.find((n) => n.codigo == "creadoAprob").id;
-	global.aprobado_id = statusRegistros.find((n) => n.codigo == "aprobado").id;
-	global.inactivar_id = statusRegistros.find((n) => n.codigo == "inactivar").id;
-	global.recuperar_id = statusRegistros.find((n) => n.codigo == "recuperar").id;
-	global.inactivo_id = statusRegistros.find((n) => n.codigo == "inactivo").id;
-	// 1. Status de productos - Combinados
-	global.creados_ids = [creado_id, creadoAprob_id];
-	global.aprobados_ids = [creadoAprob_id, aprobado_id];
-	global.estables_ids = [aprobado_id, inactivo_id];
-	global.inacRecup_ids = [inactivar_id, recuperar_id];
-	global.activos_ids = [creado_id, creadoAprob_id, aprobado_id];
-	global.inactivos_ids = [inactivar_id, inactivo_id];
-
-	// 2. Tipos de actuación
-	global.anime_id = tiposActuacion.find((n) => n.codigo == "anime").id;
-	global.documental_id = tiposActuacion.find((n) => n.codigo == "documental").id;
-	global.actuada_id = tiposActuacion.find((n) => n.codigo == "actuada").id;
-
-	// 3.A. Roles de usuario
-	global.rolConsultas_id = rolesUs.find((n) => n.codigo == "consultas").id;
-	global.rolPermInputs_id = rolesUs.find((n) => n.codigo == "permInputs").id;
-	global.rolOmnipotente_id = rolesUs.find((n) => n.codigo == "omnipotente").id;
-	global.rolesRevPERL_ids = rolesUs.filter((n) => n.revisorPERL).map((n) => n.id);
-	global.rolesRevLinks_ids = rolesUs.filter((n) => n.revisorLinks).map((n) => n.id);
-
-	// 3.B. Status de usuario
-	global.mailPendValidar_id = statusRegistrosUs.find((n) => n.codigo == "mailPendValidar").id;
-	global.mailValidado_id = statusRegistrosUs.find((n) => n.codigo == "mailValidado").id;
-	global.editables_id = statusRegistrosUs.find((n) => n.codigo == "editables").id;
-	global.perennes_id = statusRegistrosUs.find((n) => n.codigo == "perennes").id;
-
-	// 4. Públicos
-	global.mayores_ids = publicos.filter((n) => n.mayores).map((n) => n.id);
-	global.familia_ids = publicos.find((n) => n.familia).id;
-	global.menores_ids = publicos.filter((n) => n.menores).map((n) => n.id);
-
-	// Preferencias por producto
-	global.pppOpcsObj = {};
-	for (let pppOcion of pppOpcsArray) global.pppOpcsObj[pppOcion.codigo] = pppOpcsArray.find((n) => n.codigo == pppOcion.codigo);
-
-	// Otros - Productos
-	global.atributosCalific = {feValores, entretiene, calidadTecnica};
-	global.pppOpcsSimples = pppOpcsArray.filter((n) => !n.combo);
-	global.hablaHispana = paises.filter((n) => n.idioma_id == "ES");
-	global.hablaNoHispana = paises.filter((n) => n.idioma_id != "ES");
-
-	// Links
-	global.linkPelicula_id = linksTipos.find((n) => n.pelicula).id;
-	global.linkTrailer_id = linksTipos.find((n) => n.trailer).id;
-	global.provsEmbeded = linksProvs.filter((n) => n.embededPoner);
-
-	// Otros
-	global.epocasVarias = epocasOcurrencia.find((n) => n.id == "var");
-	global.epocasSinVarias = epocasOcurrencia.filter((n) => n.id != "var");
-	global.mesesAbrev = meses.map((n) => n.abrev);
-	global.motivoInfoErronea = motivosEdics.find((n) => n.codigo == "infoErronea");
-	global.motivoVersionActual = motivosEdics.find((n) => n.codigo == "versionActual");
-	global.motivoDupl_id = statusMotivos.find((n) => n.codigo == "duplicado").id;
+	global.baseDeDatos = require("./funciones/BaseDatos");
+	const variables3 = require("./funciones/Variables3.js");
+	const lecturasDeBd = await variables3.lecturasDeBd();
+	for (let campo in lecturasDeBd) global[campo] = lecturasDeBd[campo];
+	const datosPartics = await variables3.datosPartics();
+	for (let campo in datosPartics) global[campo] = datosPartics[campo];
 
 	// Variables que requieren 'require'
-	global.variables = require("./funciones/Variables");
+	global.variables = require("./funciones/Variables2.js");
 	global.comp = require("./funciones/Compartidas"); // tiene que ir antes que las BD
 	const procesos = require("./funciones/Rutinas/RT-Procesos");
 	global.rutinasJSON = procesos.lecturaRutinasJSON();
