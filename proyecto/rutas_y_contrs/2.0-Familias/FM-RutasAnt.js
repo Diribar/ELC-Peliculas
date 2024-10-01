6916d25a9ea9db9a368ff401622ef14d6fad3c31
diribar <diegoiribarren2015@gmail.com>
2024-10-01 17:15:38 -0300

    Mejora continua

--- proyecto/rutas_y_contrs/2.0-Familias/FM-RutasAnt.js
+++ proyecto/rutas_y_contrs/2.0-Familias/FM-RutasAnt.js
@@ -3,16 +3,21 @@
 const router = express.Router();
 const vistaMS = require("../9-Miscelaneas/MS-ControlVista");
 
+// Middlewares - Específicos del registro
+const entValidaAnt = require("../../middlewares/porRegistro/entValidaAnt");
+const iDvalidoAnt = require("../../middlewares/porRegistro/iDvalidoAnt");
+const entID = [entValidaAnt, iDvalidoAnt];
+
 // Vistas
-router.get("/:familia/historial", vistaMS.redirecciona.rutasAntiguas); // Historial
-router.get("/:familia/inactivar", vistaMS.redirecciona.rutasAntiguas); // Inactivar
-router.get("/:familia/recuperar", vistaMS.redirecciona.rutasAntiguas); // Recuperar
-router.get("/:familia/eliminadoPorCreador", vistaMS.redirecciona.rutasAntiguas); // Eliminado por creador
-router.get("/:familia/eliminar", vistaMS.redirecciona.rutasAntiguas); // Eliminado
+router.get("/:familia/historial", entID, vistaMS.redirecciona.rutasAntiguas); // Historial
+router.get("/:familia/inactivar", entID, vistaMS.redirecciona.rutasAntiguas); // Inactivar
+router.get("/:familia/recuperar", entID, vistaMS.redirecciona.rutasAntiguas); // Recuperar
+router.get("/:familia/eliminadoPorCreador", entID, vistaMS.redirecciona.rutasAntiguas); // Eliminado por creador
+router.get("/:familia/eliminar", entID, vistaMS.redirecciona.rutasAntiguas); // Eliminado
 
 // Vistas - Correcciones
-router.get("/correccion/motivo", vistaMS.redirecciona.rutasAntiguas);
-router.get("/correccion/status", vistaMS.redirecciona.rutasAntiguas);
+router.get("/correccion/motivo", entID, vistaMS.redirecciona.rutasAntiguas);
+router.get("/correccion/status", entID, vistaMS.redirecciona.rutasAntiguas);
 
 // Fin
 module.exports = router;
