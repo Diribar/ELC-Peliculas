d3f1440583cccae4604a8b4d1604ce7dae351b96
diribar <diegoiribarren2015@gmail.com>
2024-10-02 04:07:37 -0300

    Mejora continua

--- proyecto/middlewares/porRegistro/prodAgregar.js
+++ proyecto/middlewares/porRegistro/prodAgregar.js
@@ -3,11 +3,9 @@
 const procesos = require("../../rutas_y_contrs/2.1-Prods-Agregar/PA-FN4-Procesos");
 
 module.exports = (req, res, next) => {
-	// Acciones comunes entre los pasos de 'producto agregar'
-
-	// Variables
-	const {ruta} = comp.partesDelUrl(req);
-	const codigoUrl = ruta.slice(1);
+	// Variables - Acciones comunes entre los pasos de 'producto agregar'
+	const entidad = req.baseUrl.slice(1);
+	const codigoUrl = req.url.slice(1);
 	const pasos = [
 		{url: "palabras-clave", codigo: "palabrasClave"},
 		{url: "desambiguar", codigo: "desambiguar"},
@@ -18,18 +16,16 @@ module.exports = (req, res, next) => {
 		{url: "confirma", codigo: "confirma"},
 	];
 
-	// Obtiene el código que corresponde al 'url'
+	// Obtiene el código que corresponde al 'url' y la session/cookie actual
 	const codigo = pasos.find((n) => codigoUrl.startsWith(n.url)).codigo;
-
-	// Averigua si está la session/cookie actual
 	const datos = req.session[codigo] ? req.session[codigo] : req.cookies[codigo];
-	const rutasSinSessionPrevio = ["palabrasClave", "IM"];
-	const redirigir = !datos && !rutasSinSessionPrevio.includes(codigo);
 
-	// Si no está, redirige a la url anterior
-	if (redirigir) {
+	// Si no está la session/cookie actual, redirige a la url anterior
+	if (!datos && codigo != "palabrasClave") {
 		const indice = pasos.findIndex((n) => n.url == codigoUrl);
+		// Si no es "datosDuros", redirige a la url anterior
 		if (codigo != "datosDuros") return res.redirect(pasos[indice - 1].url);
+		// Averigua cuál fue el paso anterior a "datosDuros", y redirige a la url anterior
 		else {
 			// Obtiene el origen
 			const origen =
@@ -41,16 +37,19 @@ module.exports = (req, res, next) => {
 			// Redirecciona
 			return res.redirect(origen);
 		}
+	} else if (entidad == "undefined") {
+		if (["palabrasClave", "desambiguar", "IM"].includes(codigo)) return res.redirect("/producto/" + codigoUrl);
+		else if (datos) return res.redirect("/" + datos.entidad + "/" + codigoUrl);
 	}
 
 	// Tareas si es 'GET',
 	if (req.method == "GET") {
 		// Elimina session y cookie posteriores
 		if (codigo != "confirma") procesos.borraSessionCookies(req, res, codigo);
+
 		// Extiende la vida 'util' de Datos Originales
-		if (codigo == "datosDuros" || codigo == "datosAdics" || codigo == "confirma") {
+		if (["datosDuros", "datosAdics", "confirma"].includes(codigo))
 			res.cookie("datosOriginales", req.cookies.datosOriginales, {maxAge: unDia});
-		}
 	}
 
 	// Fin
