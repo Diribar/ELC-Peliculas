9a5c5aa83bc2adcad44a91a20623b3b5f8e07e97
diribar <diegoiribarren2015@gmail.com>
2024-10-07 23:30:02 -0300

    Mejora de procesos

--- proyecto/rutas_y_contrs/5-Consultas/CN-Rutas.js
+++ proyecto/rutas_y_contrs/5-Consultas/CN-Rutas.js
@@ -8,23 +8,23 @@ const vista = require("./CN-ControlVista");
 const consultas = require("../../middlewares/varios/consultas");
 
 // API - Obtiene
-router.get("/api/obtiene-la-cabecera", API.obtiene.cabecera);
-router.get("/api/obtiene-las-preferencias", API.obtiene.preferencias);
-router.get("/api/obtiene-las-cabeceras-posibles-para-el-usuario", API.obtiene.cabecerasPosibles);
-router.get("/api/obtiene-variables", API.obtiene.variables);
+router.get("/api/cn-obtiene-la-cabecera", API.obtiene.cabecera);
+router.get("/api/cn-obtiene-las-preferencias", API.obtiene.preferencias);
+router.get("/api/cn-obtiene-las-cabeceras-posibles-para-el-usuario", API.obtiene.cabecerasPosibles);
+router.get("/api/cn-obtiene-variables", API.obtiene.variables);
 
 // API - Cambios en BD
-router.get("/api/actualiza-en-usuario-configCons_id", API.cambiosEnBD.actualizaEnUsuarioConfigCons_id);
-router.get("/api/crea-una-configuracion", API.cambiosEnBD.creaConfig);
-router.get("/api/guarda-una-configuracion", API.cambiosEnBD.guardaConfig);
-router.get("/api/elimina-configuracion-de-consulta", API.cambiosEnBD.eliminaConfig);
+router.get("/api/cn-actualiza-en-usuario-configCons_id", API.cambiosEnBD.actualizaEnUsuarioConfigCons_id);
+router.get("/api/cn-crea-una-configuracion", API.cambiosEnBD.creaConfig);
+router.get("/api/cn-guarda-una-configuracion", API.cambiosEnBD.guardaConfig);
+router.get("/api/cn-elimina-configuracion-de-consulta", API.cambiosEnBD.eliminaConfig);
 
 // API - Session y Cookie
-router.get("/api/guarda-la-configuracion-en-session-y-cookie", API.sessionCookie.guardaConfig);
-router.get("/api/elimina-la-configuracion-en-session-y-cookie", API.sessionCookie.eliminaConfig);
+router.get("/api/cn-guarda-la-configuracion-en-session-y-cookie", API.sessionCookie.guardaConfig);
+router.get("/api/cn-elimina-la-configuracion-en-session-y-cookie", API.sessionCookie.eliminaConfig);
 
 // API - Resultados
-router.get("/api/obtiene-los-resultados", API.resultados);
+router.get("/api/cn-obtiene-los-resultados", API.resultados);
 
 // Vistas
 router.get("/", consultas, vista.consultas);
