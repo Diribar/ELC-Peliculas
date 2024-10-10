fd8f4380b7d2f5b96ca6c0cfd55dda57573e754d
diribar <diegoiribarren2015@gmail.com>
2024-10-10 10:41:06 -0300

    Mejora de procesos

--- proyecto/rutas_y_contrs/6-Graficos/GR-Rutas.js
+++ proyecto/rutas_y_contrs/6-Graficos/GR-Rutas.js
@@ -10,22 +10,10 @@ const usRolAutTablEnts = require("../../middlewares/porUsuario/usRolAutTablEnts"
 const combinados = [usAltaTerm, usRolAutTablEnts];
 
 // API
-router.get("/api/gr-usuarios-clientes-acums", API.navegsAcums);
-router.get("/api/gr-peliculas-cfc-vpc", API.prodsCfcVpc);
-router.get("/api/gr-peliculas-publico", API.prodsPorPublico);
-router.get("/api/gr-peliculas-epoca-estreno", API.prodsPorEpocaEstr);
-router.get("/api/gr-rclvs-rangos-sin-efemerides", API.rclvsRangosSinEfems);
-router.get("/api/gr-vencimiento-de-links", API.linksVencim);
-router.get("/api/gr-links-por-proveedor", API.linksPorProv);
+for (let codigo in graficos) router.get("/api/gr-" + graficos[codigo].url, API[codigo]);
 
 // Vistas
-router.get("/usuarios-clientes-acums", combinados, vista.navegsAcums);
-router.get("/peliculas-cfc-vpc", combinados, vista.prodsCfcVpc);
-router.get("/peliculas-publico", combinados, vista.prodsPorPublico);
-router.get("/peliculas-epoca-estreno", combinados, vista.prodsPorEpocaEstr);
-router.get("/rclvs-rangos-sin-efemerides", combinados, vista.rclvsRangosSinEfems);
-router.get("/vencimiento-de-links", combinados, vista.linksVencim);
-router.get("/links-por-proveedor", combinados, vista.linksPorProv);
+for (let codigo in graficos) router.get("/api/" + graficos[codigo].url, vista[codigo]);
 
 // Fin
 module.exports = router;
