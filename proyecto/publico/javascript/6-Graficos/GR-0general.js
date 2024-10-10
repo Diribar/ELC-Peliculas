b62524aa7d149e8481227454db64d33d05407870
diribar <diegoiribarren2015@gmail.com>
2024-10-10 12:06:45 -0300

    Mejora de procesos

--- proyecto/publico/javascript/6-Graficos/GR-0general.js
+++ proyecto/publico/javascript/6-Graficos/GR-0general.js
@@ -12,55 +12,66 @@ const colores = {
 };
 
 // Funciones
-const FN_opciones = {
-	columnas: (alturaGrafico, anchoGrafico) => {
-		// Variables
-		const muestraEjeX = alturaGrafico > 200;
-		const muestraEjeY = anchoGrafico > 600;
-		const mostrarLeyenda = muestraEjeY && muestraEjeX;
-		const tamanoLetra = (min, max) => Math.min(Math.max(alturaGrafico / 20, min), max);
+google.charts.load("current", {packages: ["corechart"]});
+const FN_charts = {
+	formato: {
+		columnas: (alturaGrafico, anchoGrafico) => {
+			// Variables
+			const muestraEjeX = alturaGrafico > 200;
+			const muestraEjeY = anchoGrafico > 600;
+			const mostrarLeyenda = muestraEjeY && muestraEjeX;
+			const tamanoLetra = (min, max) => Math.min(Math.max(alturaGrafico / 20, min), max);
 
-		// Opciones
-		const opciones = {
-			// Temas generales
-			seriesType: "bars",
-			isStacked: true, // columnas apiladas
-			backgroundColor: "rgb(255,242,204)",
-			fontSize: 14,
+			// Opciones
+			const opciones = {
+				// Temas generales
+				seriesType: "bars",
+				isStacked: true, // columnas apiladas
+				backgroundColor: "rgb(255,242,204)",
+				fontSize: 14,
 
-			// Título
-			titleTextStyle: {color: "brown", fontSize: tamanoLetra(13, 18)},
+				// Título
+				titleTextStyle: {color: "brown", fontSize: tamanoLetra(13, 18)},
 
-			// Área y leyenda
-			chartArea: {
-				left: mostrarLeyenda ? "15%" : "5%",
-				right: "5%",
-				top: "15%",
-				bottom: mostrarLeyenda ? "20%" : "12%",
-			}, // reemplaza el ancho y alto
-			legend: {
-				position: mostrarLeyenda > 200 ? "bottom" : "none",
-				textStyle: {fontSize: 12},
-			},
+				// Área y leyenda
+				chartArea: {
+					left: mostrarLeyenda ? "15%" : "5%",
+					right: "5%",
+					top: "15%",
+					bottom: mostrarLeyenda ? "20%" : "12%",
+				}, // reemplaza el ancho y alto
+				legend: {
+					position: mostrarLeyenda > 200 ? "bottom" : "none",
+					textStyle: {fontSize: 12},
+				},
 
-			// Ejes
-			hAxis: {
-				maxAlternation: 1, // todos los valores en una misma fila
-				slantedText: false, // todos los valores en dirección horizontal
-				textStyle: {fontSize: 12},
-				textPosition: muestraEjeX ? "auto" : "none",
-			},
-			vAxis: {
-				viewWindow: {min: 0},
-				title: "Cantidad de personas",
-				titleTextStyle: {fontSize: muestraEjeY ? tamanoLetra(12, 18) : 1},
-				titleTextPosition: anchoGrafico < 600 ? "none" : "auto",
-				textStyle: {fontSize: tamanoLetra(10, 14)},
-			},
-		};
+				// Ejes
+				hAxis: {
+					maxAlternation: 1, // todos los valores en una misma fila
+					slantedText: false, // todos los valores en dirección horizontal
+					textStyle: {fontSize: 12},
+					textPosition: muestraEjeX ? "auto" : "none",
+				},
+				vAxis: {
+					viewWindow: {min: 0},
+					title: "Cantidad de personas",
+					titleTextStyle: {fontSize: muestraEjeY ? tamanoLetra(12, 18) : 1},
+					titleTextPosition: anchoGrafico < 600 ? "none" : "auto",
+					textStyle: {fontSize: tamanoLetra(10, 14)},
+				},
+			};
+
+			// Fin
+			return opciones;
+		},
+	},
+	google: (DOM, resultado) => {
+		// Operaciones
+		const grafico = new google.visualization.ColumnChart(DOM.grafico);
+		const data = new google.visualization.arrayToDataTable(resultado);
 
 		// Fin
-		return opciones;
+		return {grafico, data};
 	},
 };
 
