f1c13d2ec00b62bff619f4d87b80ab75c5cd3529
diribar <diegoiribarren2015@gmail.com>
2024-10-11 19:44:14 -0300

    Mejora continua

--- proyecto/publico/javascript/6-Graficos/GR-fidelidadClientes.js
+++ proyecto/publico/javascript/6-Graficos/GR-fidelidadClientes.js
@@ -2,7 +2,6 @@
 window.addEventListener("load", async () => {
 	// Obtiene datos del BE
 	const navegsAcums = await fetch(ruta).then((n) => n.json());
-	console.log(navegsAcums);
 
 	// Variables
 	const DOM = {grafico: document.querySelector("#zonaDeGraficos #cuadro #grafico")};
@@ -14,8 +13,6 @@ window.addEventListener("load", async () => {
 		"masDeDiez",
 		"masDeTreinta",
 	];
-
-	return;
 
 	// Establece los colores
 	const color = {
@@ -31,45 +28,27 @@ window.addEventListener("load", async () => {
 
 	// Genera la información
 	const resultado = [["Fecha", ...grupos.map((grupo) => [grupo, {role: "style"}]).flat()]];
-	for (let navegsPorDia of navegsAcums) {
+	for (let navegDelDia of navegsAcums) {
 		// Alimenta los datos del gráfico
-		const {fecha, logins, usSinLogin, visitas} = navegsPorDia;
+		const {fecha, altasDelDia, transicion, unoATres, unoADiez, masDeDiez, masDeTreinta} = navegDelDia;
 		resultado.push([
 			" " + fecha + " ",
-			...[logins, "stroke-color: " + coloresBorde[0]],
-			...[usSinLogin, "stroke-color: " + coloresBorde[1]],
-			...[visitas, "stroke-color: " + coloresBorde[2]],
+			...[altasDelDia, "stroke-color: " + coloresBorde[0]],
+			...[transicion, "stroke-color: " + coloresBorde[1]],
+			...[unoATres, "stroke-color: " + coloresBorde[2]],
+			...[unoADiez, "stroke-color: " + coloresBorde[0]],
+			...[masDeDiez, "stroke-color: " + coloresBorde[1]],
+			...[masDeTreinta, "stroke-color: " + coloresBorde[2]],
 		]);
 
-		// Agrega las cantidades
-		cantidades.logins += logins;
-		cantidades.usSinLogin += usSinLogin;
-		cantidades.visitas += visitas;
 	}
-
-	// Obtiene los promedios
-	for (let metodo of Object.keys(cantidades))
-		promedio[metodo] = Math.round((cantidades[metodo] / navegsAcums.length) * 10) / 10;
-	promedio.total = Object.values(promedio).reduce((acum, n) => acum + n);
-	resultado[0].push("Promedio");
-	for (let i = 1; i < resultado.length; i++) resultado[i].push(promedio.total);
-
-	// Titulo general
-	let leyendaTitulo = promedio.total.toLocaleString("pt");
-	grupos.forEach((grupo, i) => (leyendaTitulo += " | " + grupo + ": " + Object.values(promedio)[i].toLocaleString("pt")));
 
 	const dibujarGrafico = () => {
 		// Opciones
-		const {grafico, opciones} = FN_charts.opciones(DOM, "columnas");
-
-		// Título general
-		const anchoGrafico = DOM.grafico.offsetWidth;
-		const tituloGral = anchoGrafico > 600 ? leyendaTitulo : leyendaTitulo.split("|")[0];
+		const {grafico, opciones} = FN_charts.opciones(DOM, "area");
 
 		// Otras opciones particulares
-		opciones.title = "Prom.: " + tituloGral;
 		opciones.colors = coloresRelleno;
-		opciones.series = {3: {type: "line"}};
 		opciones.vAxis.title = "Cantidad de personas";
 
 		// Hace visible el gráfico
