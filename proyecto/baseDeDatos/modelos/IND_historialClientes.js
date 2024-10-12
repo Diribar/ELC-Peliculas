d71ddafb53c8d312df7bfc57a5c94ec429d6d0d1
diribar <diegoiribarren2015@gmail.com>
2024-10-12 00:39:08 -0300

    Mejora continua

--- proyecto/baseDeDatos/modelos/IND_historialClientes.js
+++ proyecto/baseDeDatos/modelos/IND_historialClientes.js
@@ -5,7 +5,8 @@ module.exports = (sequelize, dt) => {
 
 		// Fidelidad de navegantes
 		altasDelDia: {type: dt.INTEGER},
-		unoTres: {type: dt.INTEGER},
+		unoDos: {type: dt.INTEGER},
+		tres: {type: dt.INTEGER},
 		cuatroDiez: {type: dt.INTEGER},
 		diezTreinta: {type: dt.INTEGER},
 		masDeTreinta: {type: dt.INTEGER},
