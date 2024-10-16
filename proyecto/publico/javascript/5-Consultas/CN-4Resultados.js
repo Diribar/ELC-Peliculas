53e2fafb9075a18e58fb388c9f05bbd3538c3b48
diribar <diegoiribarren2015@gmail.com>
2024-10-16 11:33:54 -0300

    Mejora continua

--- proyecto/publico/javascript/5-Consultas/CN-4Resultados.js
+++ proyecto/publico/javascript/5-Consultas/CN-4Resultados.js
@@ -31,7 +31,7 @@ const FN_resultados = {
 		const datos = v.layoutBD.codigo.startsWith("fechaDelAno")
 			? {...prefs, dia: v.ahora.getDate(), mes: v.ahora.getMonth() + 1}
 			: prefs;
-		const APIs = [{ruta: "obtiene-los-resultados/?datos=" + JSON.stringify(datos), duracion: 200}];
+		const APIs = [{ruta: "obtiene-los-resultados/?datos=" + JSON.stringify(datos), duracion: 400}];
 		v.resultados = await barraProgreso(ruta, APIs);
 
 		// Acciones en consecuencia
