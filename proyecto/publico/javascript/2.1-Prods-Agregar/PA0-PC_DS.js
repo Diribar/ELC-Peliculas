f2fd2c50636fa62b8725ce1d5bdacaccea7517b5
diribar <diegoiribarren2015@gmail.com>
2024-10-16 20:37:58 -0300

    Mejora continua

--- proyecto/publico/javascript/2.1-Prods-Agregar/PA0-PC_DS.js
+++ proyecto/publico/javascript/2.1-Prods-Agregar/PA0-PC_DS.js
@@ -1,11 +1,16 @@
 "use strict";
 
 // Si corresponde, completa los datos de sesion
+const rutas = {
+	pre: "/producto/api/pa-",
+	buscaInfo: "busca-info-de-session-" + pathname.slice(-2),
+};
+
 const APIs_buscar = [
 	{ruta: "busca-los-productos", duracion: 2000},
 	{ruta: "reemplaza-las-peliculas-por-su-coleccion", duracion: 2000},
 	{ruta: "organiza-la-info", duracion: 1000},
 	{ruta: "agrega-hallazgos-de-IM-y-FA", duracion: 200},
 	{ruta: "obtiene-el-mensaje", duracion: 200},
-	{ruta: buscaInfo, duracion: 200},
+	{ruta: rutas.buscaInfo, duracion: 200},
 ];
